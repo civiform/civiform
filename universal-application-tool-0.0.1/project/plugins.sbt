9ed900f00b6933dd0de72e9a528fbc9d5b9eceba
Nathan McKinley Henderson <nmckinley@google.com>
2021-01-15 12:59:09 -0800

    Formatter and formatting. (#19)
    * Add local-run linter.
    
    * Initial formatting run.

--- universal-application-tool-0.0.1/project/plugins.sbt
+++ universal-application-tool-0.0.1/project/plugins.sbt
@@ -1,2 +1,3 @@
 // The Play plugin
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.7")
+addSbtPlugin("com.lightbend.sbt" % "sbt-java-formatter" % "0.6.0")
--- universal-application-tool-0.0.1/test/FunctionalTest.java
+++ universal-application-tool-0.0.1/test/FunctionalTest.java
@@ -8,20 +8,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * A functional test starts a Play application for every test.
  *
- * https://www.playframework.com/documentation/latest/JavaFunctionalTest
+ * <p>https://www.playframework.com/documentation/latest/JavaFunctionalTest
  */
 public class FunctionalTest extends WithApplication {
 
-    @Test
-    public void renderTemplate() {
-        // If you are calling out to Assets, then you must instantiate an application
-        // because it makes use of assets metadata that is configured from
-        // the application.
+  @Test
+  public void renderTemplate() {
+    // If you are calling out to Assets, then you must instantiate an application
+    // because it makes use of assets metadata that is configured from
+    // the application.
 
-        AssetsFinder assetsFinder = provideApplication().injector().instanceOf(AssetsFinder.class);
+    AssetsFinder assetsFinder = provideApplication().injector().instanceOf(AssetsFinder.class);
 
-        Content html = views.html.index.render("Your new application is ready.", assetsFinder);
-        assertThat("text/html").isEqualTo(html.contentType());
-        assertThat(html.body()).contains("Your new application is ready.");
-    }
+    Content html = views.html.index.render("Your new application is ready.", assetsFinder);
+    assertThat("text/html").isEqualTo(html.contentType());
+    assertThat(html.body()).contains("Your new application is ready.");
+  }
 }
