28484ce11a0a579605c140934362ddff7b60cc96
Nathan Henderson <nmckinley@google.com>
2021-01-15 10:35:15 -0800

    Initial formatting run.

--- universal-application-tool-0.0.1/app/filters/ExampleFilter.java
+++ universal-application-tool-0.0.1/app/filters/ExampleFilter.java
@@ -7,27 +7,22 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.concurrent.Executor;
 
-/**
- * This is a simple filter that adds a header to all requests.
- */
+/** This is a simple filter that adds a header to all requests. */
 @Singleton
 public class ExampleFilter extends EssentialFilter {
 
-    private final Executor exec;
+  private final Executor exec;
 
-    /**
-     * @param exec This class is needed to execute code asynchronously.
-     */
-    @Inject
-    public ExampleFilter(Executor exec) {
-        this.exec = exec;
-    }
+  /** @param exec This class is needed to execute code asynchronously. */
+  @Inject
+  public ExampleFilter(Executor exec) {
+    this.exec = exec;
+  }
 
-    @Override
-    public EssentialAction apply(EssentialAction next) {
-        return EssentialAction.of(request ->
-            next.apply(request).map(result ->
-                 result.withHeader("X-ExampleFilter", "foo"), exec)
-        );
-    }
+  @Override
+  public EssentialAction apply(EssentialAction next) {
+    return EssentialAction.of(
+        request ->
+            next.apply(request).map(result -> result.withHeader("X-ExampleFilter", "foo"), exec));
+  }
 }
--- universal-application-tool-0.0.1/test/UnitTest.java
+++ universal-application-tool-0.0.1/test/UnitTest.java
@@ -14,41 +14,42 @@ import static play.test.Helpers.contentAsString;
 /**
  * Unit testing does not require Play application start up.
  *
- * https://www.playframework.com/documentation/latest/JavaTest
+ * <p>https://www.playframework.com/documentation/latest/JavaTest
  */
 public class UnitTest {
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+  @Test
+  public void simpleCheck() {
+    int a = 1 + 1;
+    assertThat(a).isEqualTo(2);
+  }
+
+  // Unit test a controller
+  @Test
+  public void testCount() {
+    final CountController controller = new CountController(() -> 49);
+    Result result = controller.count();
+    assertThat(contentAsString(result)).isEqualTo("49");
+  }
+
+  // Unit test a controller with async return
+  @Test
+  public void testAsync() {
+    final ActorSystem actorSystem = ActorSystem.create("test");
+    try {
+      final ExecutionContextExecutor ec = actorSystem.dispatcher();
+      final AsyncController controller = new AsyncController(actorSystem, ec);
+      final CompletionStage<Result> future = controller.message();
+
+      // Block until the result is completed
+      await()
+          .untilAsserted(
+              () ->
+                  assertThat(future.toCompletableFuture())
+                      .isCompletedWithValueMatching(
+                          result -> contentAsString(result).equals("Hi!")));
+    } finally {
+      actorSystem.terminate();
     }
-
-    // Unit test a controller
-    @Test
-    public void testCount() {
-        final CountController controller = new CountController(() -> 49);
-        Result result = controller.count();
-        assertThat(contentAsString(result)).isEqualTo("49");
-    }
-
-    // Unit test a controller with async return
-    @Test
-    public void testAsync() {
-        final ActorSystem actorSystem = ActorSystem.create("test");
-        try {
-            final ExecutionContextExecutor ec = actorSystem.dispatcher();
-            final AsyncController controller = new AsyncController(actorSystem, ec);
-            final CompletionStage<Result> future = controller.message();
-
-            // Block until the result is completed
-            await().untilAsserted(() ->
-                    assertThat(future.toCompletableFuture())
-                        .isCompletedWithValueMatching(result -> contentAsString(result).equals("Hi!"))
-            );
-        } finally {
-            actorSystem.terminate();
-        }
-    }
-
+  }
 }
