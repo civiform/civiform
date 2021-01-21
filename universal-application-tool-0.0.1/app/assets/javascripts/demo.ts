491a39856da31f5c2d2ff94de70dfbfe7561a290
Natalie Sidhom <nsidhom@google.com>
2021-01-20 17:24:11 -0800

    Add console.log to TS.

--- universal-application-tool-0.0.1/app/assets/javascripts/demo.ts
+++ universal-application-tool-0.0.1/app/assets/javascripts/demo.ts
@@ -1,11 +1,1 @@
-class VirtualPoint {
-  x: number;
-  y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-const newVPoint = new VirtualPoint(13, 56);
+console.log('hello world');
