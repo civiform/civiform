99066dac294cf8736fb931bf15d402fd5fe0c156
Nathan Henderson <nmckinley@google.com>
2021-01-26 16:28:06 -0800

    Add comments on index and check.

--- universal-application-tool-0.0.1/conf/evolutions/default/3.sql
+++ universal-application-tool-0.0.1/conf/evolutions/default/3.sql
@@ -10,10 +10,14 @@ create table if not exists applicants (
 create table if not exists questions (
   id bigserial primary key,
   object jsonb not null,
+  -- ensure that all entries include a 'target' key.
   check (object ? 'target')
 );
 
 create unique index if not exists question_target on questions(
+  -- Create an index on 'target' - ensure that no two entries have
+  -- the same 'target' value.  This will allow us to look up what
+  -- question can help us gather a piece of necessary information.
   (object ->>'target')
 );
 
