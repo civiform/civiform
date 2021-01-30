6572dda1db711b01d96b2c8cd163a5e08fcc5ca2
Nathan Henderson <nmckinley@google.com>
2021-01-29 18:40:11 -0800

    extract postgres container config to superclass.

--- universal-application-tool-0.0.1/test/ApplicantRepositoryTest.java
+++ universal-application-tool-0.0.1/test/ApplicantRepositoryTest.java
@@ -1,30 +1,11 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static play.test.Helpers.fakeApplication;
 
 import com.google.common.collect.ImmutableMap;
 import models.Applicant;
 import org.junit.Test;
-import play.Application;
-import play.test.WithApplication;
 import repository.ApplicantRepository;
 
-public class ApplicantRepositoryTest extends WithApplication {
-  protected Application provideApplication() {
-    return fakeApplication(
-        ImmutableMap.of(
-            "db.default.driver",
-            "org.testcontainers.jdbc.ContainerDatabaseDriver",
-            "db.default.url",
-            /* This is a magic string.  The components of it are
-             * jdbc: the standard java database connection uri scheme
-             * tc: Testcontainers - the tool that starts a new container per test.
-             * postgresql: which container to start
-             * 9.6.8: which version of postgres to start
-             * ///: hostless URI scheme - anything here would be ignored
-             * databasename: the name of the db to connect to - any string is okay.
-             */
-            "jdbc:tc:postgresql:9.6.8:///databasename"));
-  }
+public class ApplicantRepositoryTest extends WithPostgresContainer {
 
   @Test
   public void createApplicant() {
---
+++ universal-application-tool-0.0.1/test/WithPostgresContainer.java
@@ -0,0 +1,24 @@
+import static play.test.Helpers.fakeApplication;
+
+import com.google.common.collect.ImmutableMap;
+import play.Application;
+import play.test.WithApplication;
+
+public class WithPostgresContainer extends WithApplication {
+  protected Application provideApplication() {
+    return fakeApplication(
+        ImmutableMap.of(
+            "db.default.driver",
+            "org.testcontainers.jdbc.ContainerDatabaseDriver",
+            "db.default.url",
+            /* This is a magic string.  The components of it are
+             * jdbc: the standard java database connection uri scheme
+             * tc: Testcontainers - the tool that starts a new container per test.
+             * postgresql: which container to start
+             * 9.6.8: which version of postgres to start
+             * ///: hostless URI scheme - anything here would be ignored
+             * databasename: the name of the db to connect to - any string is okay.
+             */
+            "jdbc:tc:postgresql:9.6.8:///databasename"));
+  }
+}
