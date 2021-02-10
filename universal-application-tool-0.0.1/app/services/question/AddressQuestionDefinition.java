d4b161b7db164c439d3f99d8866c8801686b0373
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-02-09 21:04:03 -0800

    Test

--- universal-application-tool-0.0.1/app/services/question/AddressQuestionDefinition.java
+++ universal-application-tool-0.0.1/app/services/question/AddressQuestionDefinition.java
@@ -1,20 +1,31 @@
 package services.question;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.Locale;
 
 public class AddressQuestionDefinition extends QuestionDefinition {
-    @Override 
-    public QuestionType questionType() {
-      return QuestionType.ADDRESS;
-    }
-     
-    @Override
-    public ImmutableMap<String, Class> getScalars() {
-        return ImmutableMap.of(
-            "street", String.class,
-            "unit", String.class,
-            "city", String.class,
-            "state", String.class,
-            "zip", String.class);
-    }
+
+  public AddressQuestionDefinition(
+      String id,
+      String version,
+      String name,
+      String path,
+      String description,
+      ImmutableMap<Locale, String> questionText,
+      ImmutableMap<Locale, String> questionHelpText,
+      ImmutableSet<String> tags) {
+    super(id, version, name, path, description, questionText, questionHelpText, tags);
+  }
+
+  @Override
+  public QuestionType getQuestionType() {
+    return QuestionType.ADDRESS;
+  }
+
+  @Override
+  public ImmutableMap<String, Class> getScalars() {
+    return ImmutableMap.of(
+        "street", String.class, "city", String.class, "state", String.class, "zip", String.class);
+  }
 }
--- universal-application-tool-0.0.1/app/services/question/NameQuestionDefinition.java
+++ universal-application-tool-0.0.1/app/services/question/NameQuestionDefinition.java
@@ -1,20 +1,30 @@
 package services.question;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.Locale;
 
 public class NameQuestionDefinition extends QuestionDefinition {
-    @Override 
-    public QuestionType questionType() {
-      return QuestionType.NAME;
-    }
-  
-    @Override
-    public  ImmutableMap<String, Class> getScalars() {
-        return ImmutableMap.of(
-            "title", String.class,
-            "first", String.class,
-            "middle", String.class,
-            "last", String.class,
-            "suffix", String.class);
-    }
+
+  public NameQuestionDefinition(
+      String id,
+      String version,
+      String name,
+      String path,
+      String description,
+      ImmutableMap<Locale, String> questionText,
+      ImmutableMap<Locale, String> questionHelpText,
+      ImmutableSet<String> tags) {
+    super(id, version, name, path, description, questionText, questionHelpText, tags);
+  }
+
+  @Override
+  public QuestionType getQuestionType() {
+    return QuestionType.NAME;
+  }
+
+  @Override
+  public ImmutableMap<String, Class> getScalars() {
+    return ImmutableMap.of("first", String.class, "middle", String.class, "last", String.class);
+  }
 }
