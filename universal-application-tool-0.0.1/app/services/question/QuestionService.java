1d7a6cd2943476f80c269e6b2b60c0f4015e170b
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-02-10 13:12:56 -0800

    Format Java

--- universal-application-tool-0.0.1/app/services/question/QuestionService.java
+++ universal-application-tool-0.0.1/app/services/question/QuestionService.java
@@ -11,18 +11,19 @@ public abstract class QuestionService {
   public abstract Optional<QuestionDefinition> getQuestionDefinition(String pathString);
 
   /**
-   * Returns all of the scalar properties for a given path. If the path is invalid this returns Optional.empty().
+   * Returns all of the scalar properties for a given path. If the path is invalid this returns
+   * Optional.empty().
    */
   public abstract Optional<ImmutableMap<String, ScalarType>> getPathScalars(String pathString);
 
-  /**
-   * Gets the type of the node if it exist. Otherwise returns Optional.empty().
-   */
+  /** Gets the type of the node if it exist. Otherwise returns Optional.empty(). */
   public abstract Optional<PathType> getPathType(String pathString);
 
   /** Returns all question definitions for this version. */
   public abstract QuestionDefinition[] getAllQuestions();
 
-  /** Returns a map of full path to ScalarType for all scalars referenced in the question service. */
+  /**
+   * Returns a map of full path to ScalarType for all scalars referenced in the question service.
+   */
   public abstract ImmutableMap<String, ScalarType> getFullyQualifiedScalars();
 }
