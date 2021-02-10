aa3b1203387ebfe0375fbe151268659b182ce3c4
Caroline Danzi <cdanzi@google.com>
2021-02-09 18:34:45 -0800

    Format Java

--- universal-application-tool-0.0.1/test/services/question/QuestionDefinitionTest.java
+++ universal-application-tool-0.0.1/test/services/question/QuestionDefinitionTest.java
@@ -7,49 +7,48 @@ import com.google.common.collect.ImmutableSet;
 import java.util.Locale;
 import org.junit.Test;
 
-
 public class QuestionDefinitionTest {
 
-    @Test
-    public void newQuestionHasCorrectFields() {
-        QuestionDefinition question =
-                new QuestionDefinition(
-                        "id",
-                        "version",
-                        "name",
-                        "my.path",
-                        "description",
-                        ImmutableMap.of(Locale.ENGLISH, "question?"),
-                        ImmutableMap.of(Locale.ENGLISH, "help text"),
-                        ImmutableSet.of("general"));
+  @Test
+  public void newQuestionHasCorrectFields() {
+    QuestionDefinition question =
+        new QuestionDefinition(
+            "id",
+            "version",
+            "name",
+            "my.path",
+            "description",
+            ImmutableMap.of(Locale.ENGLISH, "question?"),
+            ImmutableMap.of(Locale.ENGLISH, "help text"),
+            ImmutableSet.of("general"));
 
-        assertThat(question.getId()).isEqualTo("id");
-        assertThat(question.getVersion()).isEqualTo("version");
-        assertThat(question.getName()).isEqualTo("name");
-        assertThat(question.getPath()).isEqualTo("my.path");
-        assertThat(question.getDescription()).isEqualTo("description");
-        assertThat(question.getQuestionText())
-                .containsAllEntriesOf(ImmutableMap.of(Locale.ENGLISH, "question?"));
-        assertThat(question.getQuestionHelpText())
-                .containsAllEntriesOf(ImmutableMap.of(Locale.ENGLISH, "help text"));
-        assertThat(question.getTags()).containsExactly("general");
-    }
+    assertThat(question.getId()).isEqualTo("id");
+    assertThat(question.getVersion()).isEqualTo("version");
+    assertThat(question.getName()).isEqualTo("name");
+    assertThat(question.getPath()).isEqualTo("my.path");
+    assertThat(question.getDescription()).isEqualTo("description");
+    assertThat(question.getQuestionText())
+        .containsAllEntriesOf(ImmutableMap.of(Locale.ENGLISH, "question?"));
+    assertThat(question.getQuestionHelpText())
+        .containsAllEntriesOf(ImmutableMap.of(Locale.ENGLISH, "help text"));
+    assertThat(question.getTags()).containsExactly("general");
+  }
 
-    @Test
-    public void newQuestionHasTypeText() {
-        QuestionDefinition question =
-                new QuestionDefinition(
-                        "", "", "", "", "", ImmutableMap.of(), ImmutableMap.of(), ImmutableSet.of());
+  @Test
+  public void newQuestionHasTypeText() {
+    QuestionDefinition question =
+        new QuestionDefinition(
+            "", "", "", "", "", ImmutableMap.of(), ImmutableMap.of(), ImmutableSet.of());
 
-        assertThat(question.getQuestionType()).isEqualTo(QuestionType.TEXT);
-    }
+    assertThat(question.getQuestionType()).isEqualTo(QuestionType.TEXT);
+  }
 
-    @Test
-    public void newQuestionHasTextScalar() {
-        QuestionDefinition question =
-                new QuestionDefinition(
-                        "", "", "", "", "", ImmutableMap.of(), ImmutableMap.of(), ImmutableSet.of());
+  @Test
+  public void newQuestionHasTextScalar() {
+    QuestionDefinition question =
+        new QuestionDefinition(
+            "", "", "", "", "", ImmutableMap.of(), ImmutableMap.of(), ImmutableSet.of());
 
-        assertThat(question.getScalars()).containsExactly("text");
-    }
+    assertThat(question.getScalars()).containsExactly("text");
+  }
 }
