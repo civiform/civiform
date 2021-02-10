a0d90dad7147ebd2e0de982aa6c00a8b61748c6e
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-02-10 13:08:03 -0800

    Create question service stub
    ### Description
    Stubbed out the QuestionService class for other features to interface with.
    
    ### Checklist
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary
    
    ### Issue(s)
    [Fixes] #<issue_number>

---
+++ universal-application-tool-0.0.1/app/services/question/PathType.java
@@ -0,0 +1,7 @@
+package services.question;
+
+enum PathType {
+  // INTERMEDIATE,
+  QUESTION,
+  SCALAR
+}
---
+++ universal-application-tool-0.0.1/app/services/question/QuestionService.java
@@ -0,0 +1,28 @@
+package services.question;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Optional;
+
+public abstract class QuestionService {
+  /** Checks whether a specific path is valid. */
+  public abstract boolean isValid(String pathString);
+
+  /** Gets the question definition for a given path. */
+  public abstract Optional<QuestionDefinition> getQuestionDefinition(String pathString);
+
+  /**
+   * Returns all of the scalar properties for a given path. If the path is invalid this returns Optional.empty().
+   */
+  public abstract Optional<ImmutableMap<String, ScalarType>> getPathScalars(String pathString);
+
+  /**
+   * Gets the type of the node if it exist. Otherwise returns Optional.empty().
+   */
+  public abstract Optional<PathType> getPathType(String pathString);
+
+  /** Returns all question definitions for this version. */
+  public abstract QuestionDefinition[] getAllQuestions();
+
+  /** Returns a map of full path to ScalarType for all scalars referenced in the question service. */
+  public abstract ImmutableMap<String, ScalarType> getFullyQualifiedScalars();
+}
