a09672208e6f2dab7ccfdb6da8395e9104fa4f1a
Kendrick Tang <kct@google.com>
2021-02-10 16:34:13 -0800

    Add simple comments to program AutoValue classes, and remove Predicate.variables.

--- universal-application-tool-0.0.1/app/services/program/BlockDefinition.java
+++ universal-application-tool-0.0.1/app/services/program/BlockDefinition.java
@@ -3,20 +3,25 @@ package services.program;
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import java.util.Optional;
+import services.question.QuestionDefinition;
 
 /** Defines a single program block, which contains a list of questions and data about the block. */
 @AutoValue
 public abstract class BlockDefinition {
-
+  /** Name of a Block. */
   public abstract String name();
 
+  /** Description of a Block. */
   public abstract String description();
 
+  /** A {@link Predicate} that determines whether this is hidden or shown. */
   public abstract Optional<Predicate> hidePredicate();
 
+  /** A {@link Predicate} that determines whether this is optional or required. */
   public abstract Optional<Predicate> optionalPredicate();
 
-  public abstract ImmutableList<String> questionDefinitions();
+  /** The list of {@link QuestionDefinition}s that make up this block. */
+  public abstract ImmutableList<QuestionDefinition> questionDefinitions();
 
   public abstract Builder toBuilder();
 
@@ -36,11 +41,11 @@ public abstract class BlockDefinition {
 
     public abstract Builder setQuestionDefinitions(ImmutableList<String> questionDefinitions);
 
-    public abstract ImmutableList.Builder<String> questionDefinitionsBuilder();
+    public abstract ImmutableList.Builder<QuestionDefinition> questionDefinitionsBuilder();
 
     public abstract BlockDefinition build();
 
-    public Builder addQuestion(String question) {
+    public Builder addQuestion(QuestionDefinition question) {
       questionDefinitionsBuilder().add(question);
       return this;
     }
