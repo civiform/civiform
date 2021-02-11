65acdfa7d0e3998fc34bf42298fb8903ce9af1ca
Caroline Danzi <cdanzi@google.com>
2021-02-10 17:06:19 -0800

    Add BlockDefinition operations to the program interface and update comments

--- universal-application-tool-0.0.1/app/services/program/ProgramService.java
+++ universal-application-tool-0.0.1/app/services/program/ProgramService.java
@@ -1,16 +1,88 @@
 package services.program;
 
 import com.google.common.collect.ImmutableList;
+import java.util.Optional;
 
-/** Operations you can perform on ProgramDefinitions. */
+/** Operations you can perform on {@link ProgramDefinition}s. */
 public interface ProgramService {
 
-    /** List all programs. */
-    ImmutableList<ProgramDefinition> listProgramDefinitions();
+  /**
+   * List all programs.
+   *
+   * @return a list of {@link ProgramDefinition}s
+   */
+  ImmutableList<ProgramDefinition> listProgramDefinitions();
 
-    /** Get the definition for a given program. */
-    ProgramDefinition getProgramDefinition(long id);
+  /**
+   * Get the definition for a given program.
+   *
+   * @param id the ID of the program to retrieve
+   * @return an {@link Optional} of the {@link ProgramDefinition} for the given ID if it exists, or
+   *     {@code Optional.empty()} if not
+   */
+  Optional<ProgramDefinition> getProgramDefinition(long id);
 
-    /** Create a new program. */
-    ProgramDefinition createProgram(String name, String description);
+  /**
+   * Create a new program.
+   *
+   * @param name a name for this program
+   * @param description the description of what the program provides
+   * @return the {@link ProgramDefinition} that was created
+   */
+  ProgramDefinition createProgram(String name, String description);
+
+  /**
+   * Adds a {@link BlockDefinition} to the given program.
+   *
+   * @param programId the ID of the program to update
+   * @param blockName a name for the block to add
+   * @param blockDescription a description of what the questions in the block address
+   * @return the updated {@link ProgramDefinition}
+   */
+  ProgramDefinition addBlockToProgram(long programId, String blockName, String blockDescription);
+
+  /**
+   * Insert a new {@link BlockDefinition} at a specific index for a given program.
+   *
+   * @param programId the ID of the program to update
+   * @param blockIndex the index in the block list for the new block
+   * @param blockName a name for the block to add
+   * @param blockDescription a description of what the questions in the block address
+   * @return the updated {@link ProgramDefinition}
+   */
+  ProgramDefinition addBlockToProgram(
+      long programId, int blockIndex, String blockName, String blockDescription);
+
+  /**
+   * Update a {@link BlockDefinition} with a set of questions.
+   *
+   * @param programId the ID of the program to update
+   * @param blockIndex the index of the block to update
+   * @param questionIds an {@link ImmutableList} of questions for the block
+   * @return the updated {@link ProgramDefinition}
+   */
+  ProgramDefinition setBlockQuestions(
+      long programId, int blockIndex, ImmutableList<String> questionIds);
+
+  /**
+   * Set the hide {@link Predicate} for a block. This predicate describes under what conditions the
+   * block should be hidden from an applicant filling out the program form.
+   *
+   * @param programId the ID of the program to update
+   * @param blockIndex the index of the block to update
+   * @param predicate the {@link Predicate} for hiding the block
+   * @return the updated {@link ProgramDefinition}
+   */
+  ProgramDefinition setBlockHidePredicate(long programId, int blockIndex, Predicate predicate);
+
+  /**
+   * Set the optional {@link Predicate} for a block. This predicate describes under what conditions
+   * the block should be optional when filling out the program form.
+   *
+   * @param programId the ID of the program to update
+   * @param blockIndex the index of the block to update
+   * @param predicate the {@link Predicate} for making the block optional
+   * @return the updated {@link ProgramDefinition}
+   */
+  ProgramDefinition setBlockOptionalPredicate(long programId, int blockIndex, Predicate predicate);
 }
