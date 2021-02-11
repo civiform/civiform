4c533e5aa306d6a68d5756b78a9ad30ce8a0fe5e
Caroline Danzi <cdanzi@google.com>
2021-02-11 14:13:29 -0800

    Fix tests and formatting

--- universal-application-tool-0.0.1/app/Module.java
+++ universal-application-tool-0.0.1/app/Module.java
@@ -1,5 +1,7 @@
 import com.google.inject.AbstractModule;
 
+import java.time.Clock;
+
 /**
  * This class is a Guice module that tells Guice how to bind several different types. This Guice
  * module is created when the Play application starts.
@@ -11,5 +13,8 @@ import com.google.inject.AbstractModule;
 public class Module extends AbstractModule {
 
   @Override
-  public void configure() {}
+  public void configure() {
+    // Use the system clock as the default implementation of Clock
+    bind(Clock.class).toInstance(Clock.systemDefaultZone());
+  }
 }
