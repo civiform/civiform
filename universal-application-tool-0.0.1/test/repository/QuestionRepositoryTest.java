6b291ffcd98d528cfdabfd20bdf503f93ee13672
Natalie Sidhom <nsidhom@google.com>
2021-02-11 09:58:17 -0800

    Update question repo test to also rely on generated IDs.

--- universal-application-tool-0.0.1/test/repository/QuestionRepositoryTest.java
+++ universal-application-tool-0.0.1/test/repository/QuestionRepositoryTest.java
@@ -10,10 +10,8 @@ public class QuestionRepositoryTest extends WithPostgresContainer {
 
   @Test
   public void createQuestion() {
-    // arrange
     final QuestionRepository repo = app.injector().instanceOf(QuestionRepository.class);
     Question question = new Question();
-    question.id = 1L;
     question.setObject(
         ImmutableMap.of(
             "nestedObject",
@@ -22,11 +20,12 @@ public class QuestionRepositoryTest extends WithPostgresContainer {
             "value",
             "target",
             "key.key"));
-    // act
+
     repo.insertQuestion(question).toCompletableFuture().join();
-    // assert
-    Question q = repo.lookupQuestion(1L).toCompletableFuture().join().get();
-    assertThat(q.id).isEqualTo(1L);
+
+    long id = question.id;
+    Question q = repo.lookupQuestion(id).toCompletableFuture().join().get();
+    assertThat(q.id).isEqualTo(id);
     assertThat(q.getObject()).containsAllEntriesOf(question.getObject());
   }
 }
