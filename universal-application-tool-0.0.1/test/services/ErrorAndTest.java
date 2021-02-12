b8b21e40f75962287ad5fa06d1faf08e98bdd38e
Natalie Sidhom <nsidhom@google.com>
2021-02-12 12:14:01 -0800

    Run fmt

--- universal-application-tool-0.0.1/test/services/ErrorAndTest.java
+++ universal-application-tool-0.0.1/test/services/ErrorAndTest.java
@@ -1,15 +1,15 @@
 package services;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableSet;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ErrorAndTest {
   @Test
   public void canBeCreatedWithResultAndErrors() {
     ErrorAnd<String, ImmutableSet<String>> errorAndResult =
-            new ErrorAnd<String, ImmutableSet<String>>("result", ImmutableSet.of("hello", "world"));
+        new ErrorAnd<String, ImmutableSet<String>>("result", ImmutableSet.of("error 1", "error 2"));
 
     assertThat(errorAndResult.hasResult()).isTrue();
     assertThat(errorAndResult.getResult()).isEqualTo("result");
@@ -20,17 +20,17 @@ public class ErrorAndTest {
   @Test
   public void canBeCreatedWithOnlyErrors() {
     ErrorAnd<String, ImmutableSet<String>> errorAndResult =
-            new ErrorAnd<String, ImmutableSet<String>>(ImmutableSet.of("hello", "world"));
+        new ErrorAnd<String, ImmutableSet<String>>(ImmutableSet.of("error 1", "error 2"));
 
     assertThat(errorAndResult.hasResult()).isFalse();
+    // TODO: expect errorAndResult.getResult() to throw
     assertThat(errorAndResult.isError()).isTrue();
     assertThat(errorAndResult.getErrors()).containsAll(ImmutableSet.of("error 1", "error 2"));
   }
 
   @Test
   public void canBeCreatedWithOnlyResult() {
-    ErrorAnd<String, ImmutableSet<String>> errorAndResult =
-            new ErrorAnd<>("result");
+    ErrorAnd<String, ImmutableSet<String>> errorAndResult = new ErrorAnd<>("result");
 
     assertThat(errorAndResult.hasResult()).isTrue();
     assertThat(errorAndResult.getResult()).isEqualTo("result");
