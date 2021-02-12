5f03e62bf8de05e939dae7fbd3820b782350a125
Bion Johnson <bionj@google.com>
2021-02-11 18:19:21 -0800

    Add comments for application service interface

---
+++ universal-application-tool-0.0.1/app/services/applicant/ReadOnlyApplicantProgramService.java
@@ -0,0 +1,20 @@
+package services.applicant;
+
+/**
+ * Provides synchronous, read-only behavior relevant to an applicant
+ * for a specific program.
+ */
+interface ReadOnlyApplicantProgramService {
+
+  /** Get the program's current Blocks for the applicant. */
+  ImmutableList<Block> getCurrentBlockList();
+
+  /** Get the block that comes after the block with the given ID if there is one. */
+  Optional<Block> getBlockAfter(long blockId);
+
+  /** Get the block that comes after the given block if there is one. */
+  Optional<Block> getBlockAfter(Block block);
+
+  /** Get the program block with the lowest index that has missing answer data if there is one. */
+  Optional<Block> getFirstIncompleteBlock();
+}
