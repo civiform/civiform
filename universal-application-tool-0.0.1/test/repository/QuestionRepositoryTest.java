75d81c9b686c0665477bf9da0fe321faa3916f8e
Caroline Danzi <cdanzi@google.com>
2021-02-19 16:12:03 -0800

    Added extra tests for QuestionRepository

--- universal-application-tool-0.0.1/test/repository/QuestionRepositoryTest.java
+++ universal-application-tool-0.0.1/test/repository/QuestionRepositoryTest.java
@@ -5,15 +5,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableMap;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
 import models.Question;
+import org.junit.Before;
 import org.junit.Test;
 import services.question.QuestionDefinition;
 
 public class QuestionRepositoryTest extends WithPostgresContainer {
 
+  private QuestionRepository repo;
+
+  @Before
+  public void setupQuestionRepository() {
+    repo = app.injector().instanceOf(QuestionRepository.class);
+  }
+
   @Test
-  public void createQuestion() {
-    QuestionRepository repo = app.injector().instanceOf(QuestionRepository.class);
+  public void listQuestions_empty() {
+    assertThat(repo.listQuestions().toCompletableFuture().join()).isEmpty();
+  }
+
+  @Test
+  public void listQuestions() {
+    Question one = insertQuestion(1L);
+    Question two = insertQuestion(2L);
+
+    Set<Question> list = repo.listQuestions().toCompletableFuture().join();
+
+    assertThat(list).containsExactly(one, two);
+  }
+
+  @Test
+  public void lookupQuestion_returnsEmptyOptionalWhenQuestionNotFound() {
+    Optional<Question> found = repo.lookupQuestion(1L).toCompletableFuture().join();
+
+    assertThat(found).isEmpty();
+  }
+
+  @Test
+  public void lookupQuestion_findsCorrectQuestion() {
+    insertQuestion(1L);
+    Question existing = insertQuestion(20L);
+
+    Optional<Question> found = repo.lookupQuestion(existing.id).toCompletableFuture().join();
+
+    assertThat(found).hasValue(existing);
+  }
+
+  @Test
+  public void insertQuestion() {
     QuestionDefinition questionDefinition =
         new QuestionDefinition(
             165L,
@@ -30,5 +70,31 @@ public class QuestionRepositoryTest extends WithPostgresContainer {
     long id = question.id;
     Question q = repo.lookupQuestion(id).toCompletableFuture().join().get();
     assertThat(q.id).isEqualTo(id);
+  }
+
+  @Test
+  public void insertQuestionSync() {
+    QuestionDefinition questionDefinition =
+        new QuestionDefinition(
+            1L,
+            2L,
+            "question",
+            "applicant.name",
+            "applicant's name",
+            ImmutableMap.of(Locale.US, "What is your name?"),
+            Optional.empty());
+    Question question = new Question(questionDefinition);
+
+    repo.insertQuestionSync(question);
+
+    assertThat(repo.lookupQuestion(question.id).toCompletableFuture().join()).hasValue(question);
+  }
+
+  private Question insertQuestion(long id) {
+    QuestionDefinition definition =
+        new QuestionDefinition(id, 1L, "", "", "", ImmutableMap.of(), Optional.empty());
+    Question question = new Question(definition);
+    question.save();
+    return question;
   }
 }
