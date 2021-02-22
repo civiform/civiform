f5032889dcb31218c0b8360eb8a44a891fe5ea80
Caroline Danzi <cdanzi@google.com>
2021-02-22 08:03:30 -0800

    Add more *Repository tests (#200)
    * Add more robust ProgramRepository tests
    
    * Added extra tests for QuestionRepository
    
    * Add more ApplicantRepository tests

--- universal-application-tool-0.0.1/test/repository/ProgramRepositoryTest.java
+++ universal-application-tool-0.0.1/test/repository/ProgramRepositoryTest.java
@@ -2,20 +2,80 @@ package repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
 import models.Program;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProgramRepositoryTest extends WithPostgresContainer {
 
-  @Test
-  public void createProgram() {
-    final ProgramRepository repo = app.injector().instanceOf(ProgramRepository.class);
+  private ProgramRepository repo;
 
+  @Before
+  public void setupProgramRepository() {
+    repo = app.injector().instanceOf(ProgramRepository.class);
+  }
+
+  @Test
+  public void listPrograms_empty() {
+    List<Program> allPrograms = repo.listPrograms().toCompletableFuture().join();
+
+    assertThat(allPrograms).isEmpty();
+  }
+
+  @Test
+  public void listPrograms() {
+    Program one = saveProgram("one");
+    Program two = saveProgram("two");
+
+    List<Program> allPrograms = repo.listPrograms().toCompletableFuture().join();
+
+    assertThat(allPrograms).containsExactly(one, two);
+  }
+
+  @Test
+  public void lookupProgram_returnsEmptyOptionalWhenProgramNotFound() {
+    Optional<Program> found = repo.lookupProgram(1L).toCompletableFuture().join();
+
+    assertThat(found).isEmpty();
+  }
+
+  @Test
+  public void lookupProgram_findsCorrectProgram() {
+    saveProgram("one");
+    Program two = saveProgram("two");
+
+    Optional<Program> found = repo.lookupProgram(two.id).toCompletableFuture().join();
+
+    assertThat(found).hasValue(two);
+  }
+
+  @Test
+  public void insertProgramSync() {
     Program program = new Program("ProgramRepository", "desc");
 
     Program withId = repo.insertProgramSync(program);
 
     Program found = repo.lookupProgram(withId.id).toCompletableFuture().join().get();
     assertThat(found.getProgramDefinition().name()).isEqualTo("ProgramRepository");
+  }
+
+  @Test
+  public void updateProgramSync() {
+    Program existing = saveProgram("old name");
+    Program updates =
+        new Program(existing.getProgramDefinition().toBuilder().setName("new name").build());
+
+    Program updated = repo.updateProgramSync(updates);
+
+    assertThat(updated.getProgramDefinition().id()).isEqualTo(existing.id);
+    assertThat(updated.getProgramDefinition().name()).isEqualTo("new name");
+  }
+
+  private static Program saveProgram(String name) {
+    Program program = new Program(name, "description");
+    program.save();
+    return program;
   }
 }
