dd3b304f554f2d1d5c02d795a5eb88874a397e20
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-02-23 12:15:36 -0800

    Java format

--- universal-application-tool-0.0.1/app/views/admin/questions/QuestionRow.java
+++ universal-application-tool-0.0.1/app/views/admin/questions/QuestionRow.java
@@ -1,4 +1,3 @@
-
 package views.admin.questions;
 
 import static j2html.TagCreator.a;
@@ -11,58 +10,58 @@ import services.question.QuestionDefinition;
 import services.question.TranslationNotFoundException;
 
 enum QuestionRow {
-    ACTIONS("Actions"),
-    DESCRIPTION("Description"),
-    ID("Id"),
-    NAME("Name"),
-    PATH("Path"),
-    QUESTION_HELP_TEXT("Question Help Text"),
-    QUESTION_TEXT("Question Text"),
-    QUESTION_TYPE("Question Type"),
-    VERSION("Version");
-    
-    private String headerText;
+  ACTIONS("Actions"),
+  DESCRIPTION("Description"),
+  ID("Id"),
+  NAME("Name"),
+  PATH("Path"),
+  QUESTION_HELP_TEXT("Question Help Text"),
+  QUESTION_TEXT("Question Text"),
+  QUESTION_TYPE("Question Type"),
+  VERSION("Version");
 
-    QuestionRow(String headerText) {
-      this.headerText = headerText;
-    }
+  private String headerText;
 
-    public String getHeaderText() {
-        return headerText;
-    }
+  QuestionRow(String headerText) {
+    this.headerText = headerText;
+  }
 
-    public Tag getCellValue(QuestionDefinition definition) {
-      switch(this) {
-        case PATH:
-          return td(definition.getPath());
-        case ID:
-          return td("" + definition.getId());
-        case VERSION:
-          return td("" + definition.getVersion());
-        case NAME:
-          return td(definition.getName());
-        case DESCRIPTION:
-          return td(definition.getDescription());
-        case QUESTION_TEXT:
-          try {
-            return td(definition.getQuestionText(Locale.ENGLISH));
-          } catch (TranslationNotFoundException e) {
-            return td("");
-          }
-        case QUESTION_HELP_TEXT:
-          try {
-            return td(definition.getQuestionHelpText(Locale.ENGLISH));
-          } catch (TranslationNotFoundException e) {
-            return td("");
-          }
-        case QUESTION_TYPE:
-          return td(definition.getQuestionType().toString());
-        case ACTIONS:
-          return td().with(span("view"))
+  public String getHeaderText() {
+    return headerText;
+  }
+
+  public Tag getCellValue(QuestionDefinition definition) {
+    switch (this) {
+      case PATH:
+        return td(definition.getPath());
+      case ID:
+        return td("" + definition.getId());
+      case VERSION:
+        return td("" + definition.getVersion());
+      case NAME:
+        return td(definition.getName());
+      case DESCRIPTION:
+        return td(definition.getDescription());
+      case QUESTION_TEXT:
+        try {
+          return td(definition.getQuestionText(Locale.ENGLISH));
+        } catch (TranslationNotFoundException e) {
+          return td("");
+        }
+      case QUESTION_HELP_TEXT:
+        try {
+          return td(definition.getQuestionHelpText(Locale.ENGLISH));
+        } catch (TranslationNotFoundException e) {
+          return td("");
+        }
+      case QUESTION_TYPE:
+        return td(definition.getQuestionType().toString());
+      case ACTIONS:
+        return td().with(span("view"))
             .with(span(" | "))
             .with(a("edit").withHref("/admin/questions/edit/" + definition.getPath()));
-        default:
-          return td("");
-      }
+      default:
+        return td("");
     }
   }
+}
