aa12f99e65d85c7da5607229db2a3cf508bcfbb5
Caroline Danzi <cdanzi@google.com>
2021-02-23 16:34:21 -0800

    Add a ProgramIndexView for applicants

---
+++ universal-application-tool-0.0.1/app/controllers/applicant/ApplicantProgramsController.java
@@ -0,0 +1,38 @@
+package controllers.applicant;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import javax.inject.Inject;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.program.ProgramService;
+import views.applicant.ProgramIndexView;
+
+/** Controller for handling methods for an applicant applying to programs. */
+public class ApplicantProgramsController extends Controller {
+
+  private final ProgramService programService;
+  private final ProgramIndexView programIndexView;
+
+  @Inject
+  public ApplicantProgramsController(
+      ProgramService programService, ProgramIndexView programIndexView) {
+    this.programService = checkNotNull(programService);
+    this.programIndexView = checkNotNull(programIndexView);
+  }
+
+  public CompletionStage<Result> index(long applicantId) {
+    return programService
+        .listProgramDefinitionsAsync()
+        .thenApply(programs -> ok(programIndexView.render(applicantId, programs)));
+  }
+
+  // TODO(https://github.com/seattle-uat/universal-application-tool/issues/224): Go to the next
+  // block for the chosen program
+  public CompletionStage<Result> edit(long applicantId, long programId) {
+    return CompletableFuture.completedFuture(
+        ok("Applicant " + applicantId + " chose program " + programId));
+  }
+}
---
+++ universal-application-tool-0.0.1/app/views/applicant/ProgramIndexView.java
@@ -0,0 +1,54 @@
+package views.applicant;
+
+import static j2html.TagCreator.body;
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.each;
+import static j2html.TagCreator.h1;
+import static j2html.TagCreator.h2;
+import static j2html.TagCreator.p;
+
+import com.google.common.collect.ImmutableList;
+import j2html.tags.Tag;
+import javax.inject.Inject;
+import play.twirl.api.Content;
+import services.program.ProgramDefinition;
+import views.BaseHtmlView;
+
+/** Returns a list of programs that an applicant can browse, with buttons for applying. */
+public class ProgramIndexView extends BaseHtmlView {
+
+  private final ApplicantLayout layout;
+
+  @Inject
+  public ProgramIndexView(ApplicantLayout layout) {
+    this.layout = layout;
+  }
+
+  /**
+   * For each program in the list, render the program information along with an "Apply" button that
+   * redirects the user to that program's application.
+   *
+   * @param programs an {@link ImmutableList} of {@link ProgramDefinition}s with the most recent
+   *     published versions
+   * @return HTML content for rendering the list of available programs
+   */
+  public Content render(long applicantId, ImmutableList<ProgramDefinition> programs) {
+    return layout.render(
+        body()
+            .with(h1("Programs"))
+            .with(each(programs, program -> shortProgram(applicantId, program))));
+  }
+
+  private Tag shortProgram(long applicantId, ProgramDefinition program) {
+    return div()
+        .with(h2(program.name()))
+        .with(p(program.description()))
+        .with(
+            redirectButton(
+                "apply",
+                "Apply",
+                controllers.applicant.routes.ApplicantProgramsController.edit(
+                        applicantId, program.id())
+                    .url()));
+  }
+}
