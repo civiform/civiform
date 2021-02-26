8dae3af1c31f2496995786bc21c6fff856f37577
Natalie Sidhom <nsidhom@google.com>
2021-02-26 14:50:26 -0800

    Pretty print the current list of programs on the dev database seed page

--- universal-application-tool-0.0.1/app/controllers/dev/DatabaseSeedController.java
+++ universal-application-tool-0.0.1/app/controllers/dev/DatabaseSeedController.java
@@ -1,24 +1,155 @@
 package controllers.dev;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
+import io.ebean.Ebean;
+import io.ebean.EbeanServer;
+import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
+import models.Program;
+import models.Question;
+import play.db.ebean.EbeanConfig;
+import play.inject.ApplicationLifecycle;
 import play.mvc.Controller;
+import play.mvc.Http.Request;
 import play.mvc.Result;
+import services.program.ProgramDefinition;
+import services.program.ProgramQuestionDefinition;
+import services.program.ProgramService;
+import services.question.AddressQuestionDefinition;
+import services.question.NameQuestionDefinition;
+import services.question.QuestionDefinition;
+import services.question.QuestionService;
+import services.question.TextQuestionDefinition;
 import views.dev.DatabaseSeedView;
 
 /** Controller for seeding the database with test content to develop against. */
 public class DatabaseSeedController extends Controller {
   private final DatabaseSeedView view;
+  private final ApplicationLifecycle appLifecycle;
+  private final EbeanConfig ebeanConfig;
+  private final QuestionService questionService;
+  private final ProgramService programService;
 
   @Inject
-  public DatabaseSeedController(DatabaseSeedView view) {
+  public DatabaseSeedController(
+      DatabaseSeedView view,
+      ApplicationLifecycle appLifecycle,
+      EbeanConfig ebeanConfig,
+      QuestionService questionService,
+      ProgramService programService) {
     this.view = view;
+    this.appLifecycle = appLifecycle;
+    this.ebeanConfig = ebeanConfig;
+    this.questionService = questionService;
+    this.programService = programService;
+
+    this.appLifecycle.addStopHook(
+        () -> {
+          truncateTables();
+          return CompletableFuture.completedFuture(null);
+        });
   }
 
-  public Result ask() {
-    return ok(view.render());
+  public Result index(Request request) {
+    // Display state of the database in roughly formatted string
+    // Offer two buttons:
+    //   One to generate base mock content
+    //   One to clear the databases
+    ImmutableList<ProgramDefinition> programDefinitions = programService.listProgramDefinitions();
+    return ok(view.render(request, programDefinitions, request.flash().get("success")));
   }
 
   public Result seed() {
-    return ok("Seeds planted");
+    // Need to make sure to only insert the base mock content if it's not already there - otherwise
+    // we will run into issues with duplicate paths. Do this by checking for the program rather
+    // than truncating tables.
+    truncateTables();
+    insertProgramWithBlocks("Mock program");
+    return redirect(routes.DatabaseSeedController.index().url())
+        .flashing("success", "The database has been seeded");
+  }
+
+  public Result clear() {
+    // Remove all content from database.
+    truncateTables();
+    return redirect(routes.DatabaseSeedController.index().url())
+        .flashing("success", "The database has been cleared");
+  }
+
+  public QuestionDefinition insertNameQuestionDefinition() {
+    return questionService
+        .create(
+            new NameQuestionDefinition(
+                1L,
+                "name",
+                "applicant.name",
+                "description",
+                ImmutableMap.of(Locale.ENGLISH, "What is your name?"),
+                ImmutableMap.of(Locale.ENGLISH, "help text")))
+        .get();
+  }
+
+  public QuestionDefinition insertTextQuestionDefinition(String name) {
+    return questionService
+        .create(
+            new TextQuestionDefinition(
+                1L,
+                name,
+                "applicant." + name,
+                "description",
+                ImmutableMap.of(Locale.ENGLISH, "What is your favorite color?"),
+                ImmutableMap.of(Locale.ENGLISH, "help text")))
+        .get();
+  }
+
+  public QuestionDefinition insertAddressQuestionDefinition() {
+    return questionService
+        .create(
+            new AddressQuestionDefinition(
+                1L,
+                "address",
+                "applicant.address",
+                "description",
+                ImmutableMap.of(Locale.ENGLISH, "What is your address?"),
+                ImmutableMap.of(Locale.ENGLISH, "help text")))
+        .get();
+  }
+
+  public ProgramDefinition insertProgramWithBlocks(String name) {
+    try {
+      ProgramDefinition programDefinition = programService.createProgramDefinition(name, "desc");
+
+      programDefinition =
+          programService.addBlockToProgram(
+              programDefinition.id(), "Block 1", "name and favorite color");
+      programDefinition =
+          programService.setBlockQuestions(
+              programDefinition.id(),
+              programDefinition.blockDefinitions().get(0).id(),
+              ImmutableList.of(
+                  ProgramQuestionDefinition.create(insertNameQuestionDefinition()),
+                  ProgramQuestionDefinition.create(insertTextQuestionDefinition("color"))));
+
+      programDefinition =
+              programService.addBlockToProgram(
+                      programDefinition.id(), "Block 2", "address");
+      programDefinition =
+              programService.setBlockQuestions(
+                      programDefinition.id(),
+                      programDefinition.blockDefinitions().get(1).id(),
+                      ImmutableList.of(
+                              ProgramQuestionDefinition.create(insertAddressQuestionDefinition())));
+
+      return programDefinition;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void truncateTables() {
+    EbeanServer server = Ebean.getServer(ebeanConfig.defaultServer());
+    server.truncate(Program.class, Question.class);
   }
 }
--- universal-application-tool-0.0.1/app/views/dev/DatabaseSeedView.java
+++ universal-application-tool-0.0.1/app/views/dev/DatabaseSeedView.java
@@ -1,30 +1,70 @@
 package views.dev;
 
-import static j2html.TagCreator.a;
 import static j2html.TagCreator.body;
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.form;
 import static j2html.TagCreator.h1;
+import static j2html.TagCreator.h2;
 import static j2html.TagCreator.head;
+import static j2html.TagCreator.pre;
 import static j2html.TagCreator.title;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
 import controllers.dev.routes;
+import java.util.Optional;
 import javax.inject.Inject;
+import play.mvc.Http.Request;
 import play.twirl.api.Content;
+import services.program.ProgramDefinition;
 import views.BaseHtmlLayout;
 import views.BaseHtmlView;
 
 public class DatabaseSeedView extends BaseHtmlView {
   private final BaseHtmlLayout layout;
+  private final ObjectMapper objectMapper;
 
   @Inject
-  public DatabaseSeedView(BaseHtmlLayout layout) {
+  public DatabaseSeedView(BaseHtmlLayout layout, ObjectMapper objectMapper) {
     this.layout = layout;
+    this.objectMapper = objectMapper;
   }
 
-  public Content render() {
+  public Content render(
+      Request request,
+      ImmutableList<ProgramDefinition> programDefinitions,
+      Optional<String> maybeFlash) {
+
+    String flash = "";
+    if (maybeFlash.isPresent()) flash = maybeFlash.get();
+
+    String prettyPrograms;
+    try {
+      prettyPrograms =
+              objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(programDefinitions);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+
     return layout.htmlContent(
         head(title("Seed database")),
         body()
-            .with(h1("Seed the database with mock content by clicking below"))
-            .with(a("Seed the database").withHref(routes.DatabaseSeedController.seed().url())));
+            .with(div(flash))
+            .with(h1("Dev mode database manager"))
+            .with(div().with(h2("Current Programs:")).with(pre(prettyPrograms)))
+            .with(
+                form()
+                    .with(makeCsrfTokenInputTag(request))
+                    .with(submitButton("Generate mock program"))
+                    .withMethod("post")
+                    .withAction(routes.DatabaseSeedController.seed().url()))
+            .with(
+                form()
+                    .with(makeCsrfTokenInputTag(request))
+                    .with(submitButton("Clear all programs"))
+                    .withMethod("post")
+                    .withAction(routes.DatabaseSeedController.clear().url()))
+            .with(layout.tailwindStyles()));
   }
 }
