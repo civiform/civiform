1e40cf7878af5e8f1e576d36eb2dc68f6cd3959d
Kendrick Tang <kct@google.com>
2021-02-26 11:49:24 -0800

    Add block list ContainerTag to BlockEdit view.

--- universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockEditView.java
+++ universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockEditView.java
@@ -2,14 +2,18 @@ package views.admin.programs;
 
 import static j2html.TagCreator.a;
 import static j2html.TagCreator.div;
+import static j2html.TagCreator.li;
+import static j2html.TagCreator.ul;
 
 import com.google.inject.Inject;
+import j2html.tags.ContainerTag;
 import play.mvc.Http.Request;
 import play.twirl.api.Content;
 import services.program.BlockDefinition;
 import services.program.ProgramDefinition;
 import views.BaseHtmlView;
 import views.admin.AdminLayout;
+import views.Styles;
 
 public class ProgramBlockEditView extends BaseHtmlView {
 
@@ -22,10 +26,24 @@ public class ProgramBlockEditView extends BaseHtmlView {
 
   public Content render(Request request, ProgramDefinition program, BlockDefinition block) {
     return layout.render(
-        div(
+        div().with(
             a().withText("Add block")
                 .withHref(
                     controllers.admin.routes.AdminProgramBlocksController.create(program.id())
-                        .url())));
+                        .url()))
+    .with(blockOrderPanel(program, block)));
+  }
+
+  public ContainerTag blockOrderPanel(ProgramDefinition program, BlockDefinition focusedBlock) {
+    ContainerTag list = ul();
+    for (BlockDefinition block : program.blockDefinitions()) {
+      ContainerTag link = a().withText(block.name()).withHref(controllers.admin.routes.AdminProgramBlocksController.edit(program.id(), block.id()).url());
+      if (block.equals(focusedBlock)) {
+        link.withClass(Styles.FONT_BOLD);
+      }
+
+      list.with(li().with(link));
+    }
+    return list;
   }
 }
