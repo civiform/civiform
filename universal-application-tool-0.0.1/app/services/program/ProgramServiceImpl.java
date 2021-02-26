190fa9fe21358816b5e872a96d71b4018296ddd1
Kendrick Tang <kct@google.com>
2021-02-25 17:00:51 -0800

    Add `addBlockToProgram(long)` to `ProgramService` for adding an empty block to a program.

--- universal-application-tool-0.0.1/app/services/program/ProgramServiceImpl.java
+++ universal-application-tool-0.0.1/app/services/program/ProgramServiceImpl.java
@@ -103,6 +103,12 @@ public class ProgramServiceImpl implements ProgramService {
 
   @Override
   @Transactional
+  public ProgramDefinition addBlockToProgram(long programId) throws ProgramNotFoundException {
+    return addBlockToProgram(programId, "", "", ImmutableList.of());
+  }
+
+  @Override
+  @Transactional
   public ProgramDefinition addBlockToProgram(
       long programId, String blockName, String blockDescription) throws ProgramNotFoundException {
     return addBlockToProgram(programId, blockName, blockDescription, ImmutableList.of());
