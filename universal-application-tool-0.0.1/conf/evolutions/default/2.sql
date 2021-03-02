32cda419c64f71bbca7cab4c5541e8f334275b4f
Bion Johnson <bionj@google.com>
2021-03-02 11:35:21 -0800

    Remove demo "person" code

--- universal-application-tool-0.0.1/conf/evolutions/default/2.sql
+++ universal-application-tool-0.0.1/conf/evolutions/default/2.sql
@@ -1,13 +1,6 @@
-# --- Sample dataset
+# --- There used to be sample data in the first couple evolutions. It's not needed
+# --- anymore so now they're empty.
 
 # --- !Ups
 
-insert into person (name) values ('Alice');
-insert into person (name) values ('Bob');
-insert into person (name) values ('Charles');
-insert into person (name) values ('Diana');
-insert into person (name) values ('Eliza');
-
 # --- !Downs
-
-delete from person;
