c9e6c06aed7d2268035641acd59fe70dad83f285
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-03-02 16:44:13 -0800

    Java formatting...
    Someday I'll remember to do this before pushing

--- universal-application-tool-0.0.1/test/views/StyleUtilsTest.java
+++ universal-application-tool-0.0.1/test/views/StyleUtilsTest.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 
 public class StyleUtilsTest {
 
-    @Test
-    public void applyUtilityClass_singleReturnsExpected() {
-        String expected = "hover:bg-blue-100";
-        String result = StyleUtils.applyUtilityClass(StyleUtils.HOVER, Styles.BG_BLUE_100);
+  @Test
+  public void applyUtilityClass_singleReturnsExpected() {
+    String expected = "hover:bg-blue-100";
+    String result = StyleUtils.applyUtilityClass(StyleUtils.HOVER, Styles.BG_BLUE_100);
 
-        assertEquals(expected, result);
-    }
+    assertEquals(expected, result);
+  }
 
-    @Test
-    public void applyUtility_multiReturnsExpected() {
-        String expected = "focus:outline-none focus:ring-2";
-        String result = StyleUtils.applyUtilityClass(
+  @Test
+  public void applyUtility_multiReturnsExpected() {
+    String expected = "focus:outline-none focus:ring-2";
+    String result =
+        StyleUtils.applyUtilityClass(
             StyleUtils.FOCUS, ImmutableList.of(Styles.OUTLINE_NONE, Styles.RING_2));
 
-        assertEquals(expected, result);
-    }
-    
+    assertEquals(expected, result);
+  }
 }
