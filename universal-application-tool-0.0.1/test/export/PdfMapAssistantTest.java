d5143c1b809b395d26e33bd52dd884be09027551
Nathan Henderson <nmckinley@google.com>
2021-03-03 16:28:52 -0800

    formatting

--- universal-application-tool-0.0.1/test/export/PdfMapAssistantTest.java
+++ universal-application-tool-0.0.1/test/export/PdfMapAssistantTest.java
@@ -7,8 +7,6 @@ import com.google.common.collect.ImmutableMap;
 import java.io.File;
 import java.io.IOException;
 import models.Question;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.cos.COSObject;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDListBox;
@@ -19,34 +17,41 @@ import services.question.QuestionType;
 import services.question.UnsupportedQuestionTypeException;
 
 public class PdfMapAssistantTest {
-    public Question makeFakeQuestionWithName(String name) throws UnsupportedQuestionTypeException {
-        QuestionDefinition questionDefinition = new QuestionDefinitionBuilder().setName(name).
-                setDescription("fake question").setQuestionText(ImmutableMap.of()).setQuestionHelpText(ImmutableMap.of())
-                .setQuestionType(QuestionType.TEXT).setPath("$.applicant.fake.path").
-                build();
-        return new Question(questionDefinition);
-    }
+  public Question makeFakeQuestionWithName(String name) throws UnsupportedQuestionTypeException {
+    QuestionDefinition questionDefinition =
+        new QuestionDefinitionBuilder()
+            .setName(name)
+            .setDescription("fake question")
+            .setQuestionText(ImmutableMap.of())
+            .setQuestionHelpText(ImmutableMap.of())
+            .setQuestionType(QuestionType.TEXT)
+            .setPath("$.applicant.fake.path")
+            .build();
+    return new Question(questionDefinition);
+  }
 
-    @Test
-    public void testMapping() throws IOException, UnsupportedQuestionTypeException {
-        // Check that the form is as expected.
-        File basePdf = new File("test/export/base.pdf");
-        assertThat(basePdf.canRead()).isTrue();
-        PDDocument doc = PDDocument.load(basePdf);
-        PDField formfield = doc.getDocumentCatalog().getAcroForm().getField("formfield");
-        assertThat(formfield).isNotNull();
-        assertThat(formfield.getValueAsString()).isEmpty();
+  @Test
+  public void testMapping() throws IOException, UnsupportedQuestionTypeException {
+    // Check that the form is as expected.
+    File basePdf = new File("test/export/base.pdf");
+    assertThat(basePdf.canRead()).isTrue();
+    PDDocument doc = PDDocument.load(basePdf);
+    PDField formfield = doc.getDocumentCatalog().getAcroForm().getField("formfield");
+    assertThat(formfield).isNotNull();
+    assertThat(formfield.getValueAsString()).isEmpty();
 
-        PDDocument newDoc = PdfMapAssistant.convertToMapPdf(doc, ImmutableList.of(
-                makeFakeQuestionWithName("foo"), makeFakeQuestionWithName("bar")
-        ));
-        doc.close();
+    PDDocument newDoc =
+        PdfMapAssistant.convertToMapPdf(
+            doc,
+            ImmutableList.of(makeFakeQuestionWithName("foo"), makeFakeQuestionWithName("bar")));
+    doc.close();
 
-        PDField newField = newDoc.getDocumentCatalog().getAcroForm().getField("formfield");
-        assertThat(newField).isNotNull();
-        assertThat(newField).isInstanceOf(PDListBox.class);
-        assertThat(((PDListBox)newField).getOptions()).hasSameElementsAs(ImmutableList.of("foo", "bar"));
-        assertThat(newField.isRequired()).isTrue();
-        newDoc.close();
-    }
+    PDField newField = newDoc.getDocumentCatalog().getAcroForm().getField("formfield");
+    assertThat(newField).isNotNull();
+    assertThat(newField).isInstanceOf(PDListBox.class);
+    assertThat(((PDListBox) newField).getOptions())
+        .hasSameElementsAs(ImmutableList.of("foo", "bar"));
+    assertThat(newField.isRequired()).isTrue();
+    newDoc.close();
+  }
 }
