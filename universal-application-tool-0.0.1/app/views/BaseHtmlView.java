6d121e60a5d1f71049640b1ede586835de0174d3
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-03-09 10:27:46 -0800

    Update BaseHtmlView.java

--- universal-application-tool-0.0.1/app/views/BaseHtmlView.java
+++ universal-application-tool-0.0.1/app/views/BaseHtmlView.java
@@ -1,11 +1,16 @@
 package views;
 
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.each;
 import static j2html.TagCreator.h1;
 import static j2html.TagCreator.input;
 import static j2html.TagCreator.label;
+import static j2html.TagCreator.span;
 import static j2html.TagCreator.text;
 
+import com.google.common.collect.ImmutableSet;
 import j2html.TagCreator;
+import j2html.tags.ContainerTag;
 import j2html.tags.Tag;
 import play.mvc.Http;
 import services.applicant.ValidationErrorMessage;
@@ -25,6 +30,14 @@ public abstract class BaseHtmlView {
   
   protected ContainerTag fieldErrors(ImmutableSet<ValidationErrorMessage> errors) {
     return div(each(errors, error -> span(error.message())));
+  }
+  
+  protected Tag checkboxInputWithLabel(
+      String labelText, String inputId, String inputName, String inputValue) {
+    return label()
+        .with(
+            input().withType("checkbox").withName(inputName).withValue(inputValue).withId(inputId),
+            text(labelText));
   }
 
   protected Tag button(String textContents) {
