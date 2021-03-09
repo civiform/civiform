94b8f19dfb039b0554df10318b13f4b1f2af0d9c
Natalie Sidhom <nsidhom@google.com>
2021-03-08 16:39:17 -0800

    WIP: Implement ApplicantProgramBlocksController#update

--- universal-application-tool-0.0.1/app/controllers/applicant/ApplicantProgramBlocksController.java
+++ universal-application-tool-0.0.1/app/controllers/applicant/ApplicantProgramBlocksController.java
@@ -2,32 +2,41 @@ package controllers.applicant;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.errorprone.annotations.DoNotCall;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 import javax.inject.Inject;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Http.Request;
 import play.mvc.Result;
 import services.applicant.ApplicantService;
 import services.applicant.Block;
+import services.applicant.ReadOnlyApplicantProgramService;
 import views.applicant.ApplicantProgramBlockEditView;
 
 public final class ApplicantProgramBlocksController extends Controller {
+  private static final ImmutableSet<String> STRIPPED_FORM_FIELDS = ImmutableSet.of("csrfToken");
 
   private final ApplicantService applicantService;
   private final HttpExecutionContext httpExecutionContext;
   private final ApplicantProgramBlockEditView editView;
+  private final FormFactory formFactory;
 
   @Inject
   public ApplicantProgramBlocksController(
       ApplicantService applicantService,
       HttpExecutionContext httpExecutionContext,
-      ApplicantProgramBlockEditView editView) {
+      ApplicantProgramBlockEditView editView,
+      FormFactory formFactory) {
     this.applicantService = checkNotNull(applicantService);
     this.httpExecutionContext = checkNotNull(httpExecutionContext);
     this.editView = checkNotNull(editView);
+    this.formFactory = checkNotNull(formFactory);
   }
 
   public CompletionStage<Result> edit(
@@ -47,9 +56,50 @@ public final class ApplicantProgramBlocksController extends Controller {
             httpExecutionContext.current());
   }
 
-  @DoNotCall
   public CompletionStage<Result> update(
       Request request, long applicantId, long programId, long blockId) {
-    throw new UnsupportedOperationException("Updates aren't implemented yet");
+    DynamicForm form = formFactory.form().bindFromRequest(request);
+    ImmutableMap<String, String> formData = cleanForm(form.rawData());
+
+    return applicantService
+        .stageAndUpdateIfValid(applicantId, programId, blockId, formData)
+        .thenApplyAsync(
+            (errorAndROApplicantProgramService) -> {
+              if (errorAndROApplicantProgramService.isError()) {
+                return badRequest("The server cannot process this request.");
+              }
+
+              ReadOnlyApplicantProgramService roApplicantProgramService =
+                  errorAndROApplicantProgramService.getResult();
+
+              Optional<Block> updatedBlockMaybe = roApplicantProgramService.getBlock(blockId);
+              if (updatedBlockMaybe.isEmpty()) {
+                return badRequest("The server cannot process this request.");
+              }
+              Block updatedBlock = updatedBlockMaybe.get();
+
+              if (!updatedBlock.hasErrors()) {
+                // Validation errors: re-render this block with errors and previously entered data.
+                return ok(editView.render(request, applicantId, programId, updatedBlock));
+              }
+
+              // Successfully saved applicant data
+
+              Optional<Block> nextBlockMaybe = roApplicantProgramService.getFirstIncompleteBlock();
+              if (nextBlockMaybe.isEmpty()) {
+                // TODO: Redirect to answer review page.
+                return redirect(routes.ApplicantProgramsController.index(applicantId));
+              }
+
+              return redirect(
+                  routes.ApplicantProgramBlocksController.edit(
+                      applicantId, programId, nextBlockMaybe.get().getId()));
+            });
+  }
+
+  private ImmutableMap<String, String> cleanForm(Map<String, String> formData) {
+    return formData.entrySet().stream()
+        .filter(entry -> !STRIPPED_FORM_FIELDS.contains(entry.getKey()))
+        .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 }
--- universal-application-tool-0.0.1/app/services/applicant/ApplicantServiceImpl.java
+++ universal-application-tool-0.0.1/app/services/applicant/ApplicantServiceImpl.java
@@ -2,6 +2,7 @@ package services.applicant;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -11,6 +12,7 @@ import models.Applicant;
 import play.libs.concurrent.HttpExecutionContext;
 import repository.ApplicantRepository;
 import services.ErrorAnd;
+import services.Path;
 import services.program.BlockDefinition;
 import services.program.PathNotInBlockException;
 import services.program.ProgramBlockNotFoundException;
@@ -75,6 +77,18 @@ public class ApplicantServiceImpl implements ApplicantService {
               return CompletableFuture.completedFuture(ErrorAnd.of(roApplicantProgramService));
             },
             httpExecutionContext.current());
+  }
+
+  @Override
+  public CompletionStage<ErrorAnd<ReadOnlyApplicantProgramService, Exception>>
+      stageAndUpdateIfValid(
+          long applicantId, long programId, long blockId, ImmutableMap<String, String> updateMap) {
+    ImmutableSet<Update> updates =
+        updateMap.entrySet().stream()
+            .map(entry -> Update.create(Path.create(entry.getKey()), entry.getValue()))
+            .collect(ImmutableSet.toImmutableSet());
+
+    return stageAndUpdateIfValid(applicantId, programId, blockId, updates);
   }
 
   @Override
