5a7502c6860add1bbb71d69b8f3ae9236631d7de
Kendrick Tang <kct@google.com>
2021-03-10 20:19:14 -0800

    Add question caching so that only one of each is made for each test. The cache must be cleared between tests.

--- universal-application-tool-0.0.1/test/support/Questions.java
+++ universal-application-tool-0.0.1/test/support/Questions.java
@@ -1,54 +1,76 @@
 package support;
 
 import com.google.common.collect.ImmutableMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import models.Question;
 import services.Path;
 import services.question.AddressQuestionDefinition;
+import services.question.NameQuestionDefinition;
 import services.question.QuestionDefinition;
 import services.question.TextQuestionDefinition;
 
 public class Questions {
   private static final long VERSION = 1L;
 
+  private static final Map<String, Question> questionCache = new ConcurrentHashMap<>();
+
+  public static void reset() {
+    questionCache.clear();
+  }
+
   public static Question applicantName() {
-    QuestionDefinition definition =
-        new TextQuestionDefinition(
-            VERSION,
-            "applicant name",
-            Path.create("applicant.name"),
-            "name of applicant",
-            ImmutableMap.of(),
-            ImmutableMap.of());
-    Question question = new Question(definition);
-    question.save();
-    return question;
+    return questionCache.computeIfAbsent(
+        "applicant name",
+        ignore -> {
+          QuestionDefinition definition =
+              new NameQuestionDefinition(
+                  VERSION,
+                  "applicant name",
+                  Path.create("applicant.name"),
+                  "name of applicant",
+                  ImmutableMap.of(Locale.ENGLISH, "what is your name?"),
+                  ImmutableMap.of(Locale.US, "help text"));
+          Question question = new Question(definition);
+          question.save();
+          return question;
+        });
   }
 
   public static Question applicantAddress() {
-    QuestionDefinition definition =
-        new AddressQuestionDefinition(
-            VERSION,
-            "applicant address",
-            Path.create("applicant.address"),
-            "address of applicant",
-            ImmutableMap.of(),
-            ImmutableMap.of());
-    Question question = new Question(definition);
-    question.save();
-    return question;
+    return questionCache.computeIfAbsent(
+        "applicant address",
+        ignore -> {
+          QuestionDefinition definition =
+              new AddressQuestionDefinition(
+                  VERSION,
+                  "applicant address",
+                  Path.create("applicant.address"),
+                  "address of applicant",
+                  ImmutableMap.of(Locale.US, "what is your address?"),
+                  ImmutableMap.of(Locale.US, "help text"));
+          Question question = new Question(definition);
+          question.save();
+          return question;
+        });
   }
 
   public static Question applicantFavoriteColor() {
-    QuestionDefinition definition =
-        new TextQuestionDefinition(
-            VERSION,
-            "applicant favorite color",
-            Path.create("applicant.color"),
-            "favorite color of applicant",
-            ImmutableMap.of(),
-            ImmutableMap.of());
-    Question question = new Question(definition);
-    question.save();
-    return question;
+    return questionCache.computeIfAbsent(
+        "applicant favorite color",
+        ignore -> {
+          QuestionDefinition definition =
+              new TextQuestionDefinition(
+                  VERSION,
+                  "applicant favorite color",
+                  Path.create("applicant.color"),
+                  "favorite color of applicant",
+                  ImmutableMap.of(Locale.US, "what is your favorite color?"),
+                  ImmutableMap.of(Locale.US, "help text"));
+          Question question = new Question(definition);
+          question.save();
+          return question;
+        });
   }
 }
