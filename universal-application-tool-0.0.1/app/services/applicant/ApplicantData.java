0de3191914082cc182cc21c7480ce73fdad73fe3
Nathan Henderson <nmckinley@google.com>
2021-03-12 10:52:30 -0800

    Add todo.

--- universal-application-tool-0.0.1/app/services/applicant/ApplicantData.java
+++ universal-application-tool-0.0.1/app/services/applicant/ApplicantData.java
@@ -171,6 +171,7 @@ public class ApplicantData {
           pathsRemoved.addAll(mergeFrom(path, (Map) entry.getValue()));
         } else if (entry.getValue() instanceof List) {
           // Add items from lists.
+          // TODO(github.com/seattle-uat/civiform/issues/405): improve merge for repeated fields.
           for (Object item : (List) entry.getValue()) {
             this.jsonData.add(path.path(), item);
           }
