c9a29d3c9de7f963b6ebc5a07eb8e276a9650966
Nathan McKinley Henderson <nmckinley@google.com>
2021-03-16 18:12:28 -0700

    Store applications in the database as a snapshot, display the ID of the snapshot (#513)
    * Store applications in the database as a snapshot, display the ID of the snapshot.
    
    * Add better test for getApplications.
    
    * formatting

---
+++ universal-application-tool-0.0.1/app/models/Application.java
@@ -0,0 +1,44 @@
+package models;
+
+import io.ebean.annotation.DbJson;
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import play.data.validation.Constraints;
+import services.Path;
+import services.applicant.ApplicantData;
+
+@Entity
+@Table(name = "applications")
+public class Application extends BaseModel {
+  @ManyToOne private Applicant applicant;
+
+  @ManyToOne private Program program;
+
+  // used by generated code
+  @SuppressWarnings("UnusedVariable")
+  @Constraints.Required
+  @DbJson
+  private String object;
+
+  public Application(Applicant applicant, Program program, Instant submitTime) {
+    this.applicant = applicant;
+    ApplicantData data = applicant.getApplicantData();
+    data.putString(Path.create("metadata.submitted_time"), submitTime.toString());
+    this.object = data.asJsonString();
+    this.program = program;
+  }
+
+  public Applicant getApplicant() {
+    return this.applicant;
+  }
+
+  public Program getProgram() {
+    return this.program;
+  }
+
+  public ApplicantData getApplicantData() {
+    return new ApplicantData(this.object);
+  }
+}
