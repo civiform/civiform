6850e383d269ff062e060880ff24b435fd3225d3
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-03-17 22:00:28 -0700

    Add visual branding in the admin UI (#514)
    Add simple branding
    
    Placeholder for now.

--- universal-application-tool-0.0.1/app/views/admin/AdminLayout.java
+++ universal-application-tool-0.0.1/app/views/admin/AdminLayout.java
@@ -2,9 +2,12 @@ package views.admin;
 
 import static j2html.TagCreator.a;
 import static j2html.TagCreator.body;
+import static j2html.TagCreator.div;
 import static j2html.TagCreator.head;
 import static j2html.TagCreator.main;
 import static j2html.TagCreator.nav;
+import static j2html.TagCreator.span;
+import static j2html.TagCreator.text;
 
 import j2html.tags.ContainerTag;
 import j2html.tags.DomContent;
@@ -24,17 +27,44 @@ public class AdminLayout extends BaseHtmlLayout {
     super(viewUtils);
   }
 
-  public Content renderMain(ContainerTag mainContent, String... mainStyles) {
+  private ContainerTag renderNavBar() {
     String questionLink = controllers.admin.routes.QuestionController.index().url();
     String programLink = controllers.admin.routes.AdminProgramController.index().url();
     String logoutLink = org.pac4j.play.routes.LogoutController.logout().url();
+
+    ContainerTag headerIcon =
+        div(span("C"), span("F").withClasses(Styles.FONT_THIN))
+            .withClasses(
+                Styles.ABSOLUTE,
+                Styles.BG_CONTAIN,
+                Styles.BG_GRAY_700,
+                Styles.H_7,
+                Styles.LEFT_5,
+                Styles.M_1,
+                Styles.OPACITY_75,
+                Styles.TEXT_CENTER,
+                Styles.TEXT_LG,
+                Styles.TEXT_WHITE,
+                Styles.TOP_2,
+                Styles.W_7,
+                Styles.ROUNDED);
+    ContainerTag headerTitle =
+        div()
+            .withClasses(
+                Styles.FONT_NORMAL, Styles.INLINE, Styles.PL_10, Styles.PY_0, Styles.TEXT_XL)
+            .with(span("Civi"), span("Form").withClasses(Styles.FONT_THIN));
+
     ContainerTag adminHeader =
         nav()
+            .with(headerIcon, headerTitle)
             .with(headerLink("Questions", questionLink))
             .with(headerLink("Programs", programLink))
             .with(headerLink("Logout", logoutLink, Styles.FLOAT_RIGHT))
             .withClasses(BaseStyles.NAV_STYLES);
+    return adminHeader;
+  }
 
+  public Content renderMain(ContainerTag mainContent, String... mainStyles) {
     mainContent.withClasses(
         Styles.BG_WHITE,
         Styles.BORDER,
@@ -46,7 +76,7 @@ public class AdminLayout extends BaseHtmlLayout {
     return htmlContent(
         head(tailwindStyles()),
         body()
-            .with(adminHeader)
+            .with(renderNavBar())
             .with(mainContent)
             .withClasses(
                 BaseStyles.BODY_GRADIENT_STYLE,
@@ -63,6 +93,12 @@ public class AdminLayout extends BaseHtmlLayout {
   }
 
   public Tag headerLink(String text, String href, String... styles) {
-    return a(text).withHref(href).withClasses(Styles.PX_3, StyleUtils.joinStyles(styles));
+    return a(text)
+        .withHref(href)
+        .withClasses(
+            Styles.PX_3,
+            Styles.OPACITY_75,
+            StyleUtils.hover(Styles.OPACITY_100),
+            StyleUtils.joinStyles(styles));
   }
 }
