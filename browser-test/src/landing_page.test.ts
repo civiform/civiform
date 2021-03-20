b3ddd98368d90bb41d153e42499c81a891e4d1f5
bion <bionjohnson@gmail.com>
2021-03-19 18:20:06 -0700

    WIP app server getting no response from pg on 2345

--- browser-test/src/landing_page.test.ts
+++ browser-test/src/landing_page.test.ts
@@ -1,5 +1,9 @@
 import { chromium } from 'playwright'
 
-it('should work', () => {
-  console.log('HONK')
+it('should work', async () => {
+  let browser = await chromium.launch()
+  let page = await browser.newPage()
+  await page.goto('https://localhost:9999')
+
+  console.log(await page.title())
 })
--- browser-test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-docker run -it --rm --ipc=host mcr.microsoft.com/playwright:bionic \
-    $PWD/src
-    /bin/bash
