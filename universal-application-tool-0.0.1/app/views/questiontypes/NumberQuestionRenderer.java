516e40a89ec779e6f1e93a7ffff48c48c7305f5b
Caroline Danzi <cdanzi@google.com>
2021-03-23 09:09:53 -0700

    Update NumberQuestionRenderer to use new styles

--- universal-application-tool-0.0.1/app/views/questiontypes/NumberQuestionRenderer.java
+++ universal-application-tool-0.0.1/app/views/questiontypes/NumberQuestionRenderer.java
@@ -1,13 +1,14 @@
 package views.questiontypes;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static j2html.TagCreator.div;
 import static j2html.TagCreator.input;
-import static j2html.TagCreator.label;
-import static j2html.TagCreator.text;
 
 import j2html.tags.Tag;
 import services.applicant.ApplicantQuestion;
 import views.BaseHtmlView;
+import views.style.ReferenceClasses;
+import views.style.Styles;
 
 public class NumberQuestionRenderer extends BaseHtmlView implements ApplicantQuestionRenderer {
 
@@ -21,14 +22,26 @@ public class NumberQuestionRenderer extends BaseHtmlView implements ApplicantQue
   public Tag render() {
     ApplicantQuestion.NumberQuestion numberQuestion = question.getNumberQuestion();
 
-    return label(
-        text(question.getQuestionText()),
-        input()
-            .withType("number")
-            .withCondValue(
-                numberQuestion.hasValue(),
-                numberQuestion.getNumberValue().map(String::valueOf).orElse(""))
-            .withName(numberQuestion.getNumberPath().path()),
-        fieldErrors(numberQuestion.getQuestionErrors()));
+    return div()
+        .withId(question.getPath().path())
+        .withClasses(Styles.MX_AUTO, Styles.W_MAX)
+        .with(
+            div()
+                .withClasses(ReferenceClasses.APPLICANT_QUESTION_TEXT)
+                .withText(question.getQuestionText()),
+            div()
+                .withClasses(
+                    ReferenceClasses.APPLICANT_QUESTION_HELP_TEXT,
+                    Styles.TEXT_BASE,
+                    Styles.FONT_THIN,
+                    Styles.MB_2)
+                .withText(question.getQuestionHelpText()),
+            input()
+                .withType("number")
+                .withCondValue(
+                    numberQuestion.hasValue(),
+                    numberQuestion.getNumberValue().map(String::valueOf).orElse(""))
+                .withName(numberQuestion.getNumberPath().path()),
+            fieldErrors(numberQuestion.getQuestionErrors()));
   }
 }
