1d06c9d08272fe7187cfb3b5ab9072f031cacae1
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-03-23 10:18:04 -0700

    Use floating labels for address inputs.
    Styling update #1 for applicant questions.

--- universal-application-tool-0.0.1/app/views/questiontypes/AddressQuestionRenderer.java
+++ universal-application-tool-0.0.1/app/views/questiontypes/AddressQuestionRenderer.java
@@ -2,18 +2,21 @@ package views.questiontypes;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static j2html.TagCreator.div;
-import static j2html.TagCreator.input;
-import static j2html.TagCreator.label;
 
 import j2html.tags.Tag;
 import services.applicant.ApplicantQuestion;
 import views.BaseHtmlView;
+import views.components.FieldWithLabel;
 import views.style.ReferenceClasses;
 import views.style.Styles;
+import views.style.StyleUtils;
 
 public class AddressQuestionRenderer extends BaseHtmlView implements ApplicantQuestionRenderer {
 
   private final ApplicantQuestion question;
+
+  private final static String SECONDARY_ADDRESS_STYLES = 
+    StyleUtils.joinStyles(Styles.MY_0, Styles.P_2, Styles.PB_1);
 
   public AddressQuestionRenderer(ApplicantQuestion question) {
     this.question = checkNotNull(question);
@@ -25,7 +28,7 @@ public class AddressQuestionRenderer extends BaseHtmlView implements ApplicantQu
 
     return div()
         .withId(question.getPath().path())
-        .withClasses(Styles.MX_AUTO, Styles.W_MAX)
+        .withClasses(Styles.MX_AUTO, Styles.PX_16)
         .with(
             div()
                 .withClasses(ReferenceClasses.APPLICANT_QUESTION_TEXT)
@@ -37,38 +40,39 @@ public class AddressQuestionRenderer extends BaseHtmlView implements ApplicantQu
                     Styles.FONT_THIN,
                     Styles.MB_2)
                 .withText(question.getQuestionHelpText()),
-            label(
-                input()
-                    .withType("text")
-                    .withCondValue(
-                        addressQuestion.hasStreetValue(),
-                        addressQuestion.getStreetValue().orElse(""))
-                    .withName(addressQuestion.getStreetPath().path())
-                    .withPlaceholder("Street address"),
-                fieldErrors(addressQuestion.getStreetErrors())),
-            label(
-                input()
-                    .withType("text")
-                    .withCondValue(
-                        addressQuestion.hasCityValue(), addressQuestion.getCityValue().orElse(""))
-                    .withName(addressQuestion.getCityPath().path())
-                    .withPlaceholder("City"),
-                fieldErrors(addressQuestion.getCityErrors())),
-            label(
-                input()
-                    .withType("text")
-                    .withCondValue(
-                        addressQuestion.hasStateValue(), addressQuestion.getStateValue().orElse(""))
-                    .withName(addressQuestion.getStatePath().path())
-                    .withPlaceholder("State"),
-                fieldErrors(addressQuestion.getStateErrors())),
-            label(
-                input()
-                    .withType("text")
-                    .withCondValue(
-                        addressQuestion.hasZipValue(), addressQuestion.getZipValue().orElse(""))
-                    .withName(addressQuestion.getZipPath().path())
-                    .withPlaceholder("Zip code"),
-                fieldErrors(addressQuestion.getZipErrors())));
+            div()
+                .withClasses("rounded bg-opacity-50 bg-gray-100 pt-2 pb-4")
+                .with(
+                    /** First line of address entry: Street */
+                    FieldWithLabel.input()
+                        .setFieldName(addressQuestion.getStreetPath().path())
+                        .setLabelText("Street address")
+                        .setFloatLabel(true)
+                        .setValue(addressQuestion.getStreetValue().orElse(""))
+                        .getContainer().withClasses("my-2 p-2 pb-0"),
+                    /** Second line of address entry: City, State, Zip */
+                    div()
+                        .withClasses(Styles.FLEX, Styles.FLEX_ROW)
+                        .with(
+                            FieldWithLabel.input()
+                                .setFieldName(addressQuestion.getCityPath().path())
+                                .setLabelText("City")
+                                .setFloatLabel(true)
+                                .setValue(addressQuestion.getCityValue().orElse(""))
+                                .getContainer().withClasses(SECONDARY_ADDRESS_STYLES, Styles.W_1_2),
+                            FieldWithLabel.input()
+                                .setFieldName(addressQuestion.getStatePath().path())
+                                .setLabelText("State")
+                                .setFloatLabel(true)
+                                .setValue(addressQuestion.getStateValue().orElse(""))
+                                .getContainer().withClasses(SECONDARY_ADDRESS_STYLES, Styles.W_1_3),
+                            FieldWithLabel.input()
+                                .setFieldName(addressQuestion.getZipPath().path())
+                                .setLabelText("Zip")
+                                .setFloatLabel(true)
+                                .setValue(addressQuestion.getZipValue().orElse(""))
+                                .getContainer().withClasses(SECONDARY_ADDRESS_STYLES, Styles.W_1_6)
+                                )));
+    // TODO: Need to add field errors back.
   }
 }
