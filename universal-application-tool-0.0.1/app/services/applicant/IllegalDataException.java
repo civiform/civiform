794d61331a047c1ce47592fc698b78b1657417cc
Caroline Danzi <cdanzi@google.com>
2021-03-25 14:12:20 -0700

    Remove unused exception

--- universal-application-tool-0.0.1/app/services/applicant/IllegalDataException.java
+++
@@ -1,9 +0,0 @@
-package services.applicant;
-
-import services.Path;
-
-public class IllegalDataException extends Exception {
-  public IllegalDataException(Path path) {
-    super("Tried to write bad data at path: " + path);
-  }
-}
