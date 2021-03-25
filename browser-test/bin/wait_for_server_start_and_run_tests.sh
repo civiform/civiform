ead626084360d25a25d40257933ea2420ed10e12
Bion <bion@users.noreply.github.com>
2021-03-25 14:50:21 -0700

    Make browser testing nice and approachable (#608)
    * WIP improve browser tests
    
    * Allow passing a --debug flag for turning on debug logs in playwright
    
    * Fix debug bash script
    
    * allow base url to be set by an environment variable
    
    * Truncate tables before running tests locally
    
    * No need to fill path
    
    * Address a few PR comments
    
    * fix docker run command for local run
    
    * Use common source for accepted localhosts
    
    Co-authored-by: Natalie Sidhom <nsidhom@google.com>
    Co-authored-by: Hung-Ming Hsu <hmhsu@google.com>

--- browser-test/bin/wait_for_server_start_and_run_tests.sh
+++ browser-test/bin/wait_for_server_start_and_run_tests.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 start_time=$(date +%s)
 deadline=$(($start_time + 200))
+server_url=http://civiform:9000
 
 echo polling to check server start
 
-until $(curl --output /dev/null --silent --head --fail --max-time 2 http://civiform:9000); do
+until $(curl --output /dev/null --silent --head --fail --max-time 2 $server_url); do
     if (( $(date +%s) > $deadline )); then
         echo "deadline exceeded waiting for server start"
         exit 1
@@ -16,6 +17,17 @@ done
 
 echo detected server start
 
-./bin/truncate_tables.sh
+debug=0
+for arg do
+    shift
+    # if debug flag, set the var and leave it out of the forwarded args list
+    [ "$arg" = "--debug" ] && debug=1 && continue
+    set -- "$@" "$arg"
+done
 
-yarn test $@
+if (( $debug == 1 )); then
+    DEBUG=pw:api BASE_URL=$server_url yarn test $@
+else
+    BASE_URL=$server_url yarn test $@
+fi
+
