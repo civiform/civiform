ab00f7ba727681352ff74f41827345d2e6b5e468
Hung-Ming Hsu <hmhsu@google.com>
2021-03-29 16:11:07 -0700

    fix failing test

--- universal-application-tool-0.0.1/test/views/BaseHtmlLayoutTest.java
+++ universal-application-tool-0.0.1/test/views/BaseHtmlLayoutTest.java
@@ -22,6 +22,8 @@ public class BaseHtmlLayoutTest extends WithPostgresContainer {
     Content result = layout.htmlContent(div("hello"));
 
     assertThat(result).isInstanceOf(BaseHtmlLayout.HtmlResponseContent.class);
-    assertThat(result.body()).isEqualTo("<!DOCTYPE html><html><div>hello</div></html>");
+    assertThat(result.body()).startsWith("<!DOCTYPE html><html>");
+    assertThat(result.body()).contains("<div>hello</div>");
+    assertThat(result.body()).endsWith("</html>");
   }
 }
