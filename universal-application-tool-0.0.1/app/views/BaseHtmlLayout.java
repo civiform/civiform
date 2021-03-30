ca4149c4c5c983ab0fa81617b32fa0f4642a3f03
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-03-30 10:58:58 -0700

    Create dismissible warning for demo site.

--- universal-application-tool-0.0.1/app/views/BaseHtmlLayout.java
+++ universal-application-tool-0.0.1/app/views/BaseHtmlLayout.java
@@ -2,12 +2,18 @@ package views;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static j2html.TagCreator.document;
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.span;
+import static j2html.TagCreator.text;
 
 import j2html.tags.ContainerTag;
 import j2html.tags.DomContent;
 import j2html.tags.Tag;
 import javax.inject.Inject;
 import play.twirl.api.Content;
+import views.components.Icons;
+import views.style.StyleUtils;
+import views.style.Styles;
 
 /**
  * Base class for all layout classes.
@@ -17,6 +23,8 @@ import play.twirl.api.Content;
  */
 public class BaseHtmlLayout extends BaseHtmlView {
   private static final String TAILWIND_COMPILED_FILENAME = "tailwind";
+
+  private final String BANNER_TEXT = "Do not enter actual or personal data in this demo site";
 
   protected final ViewUtils viewUtils;
 
@@ -41,6 +49,21 @@ public class BaseHtmlLayout extends BaseHtmlView {
    */
   public Tag tailwindStyles() {
     return viewUtils.makeLocalCssTag(TAILWIND_COMPILED_FILENAME);
+  }
+
+  public Tag warningMessage() {
+    ContainerTag wrappedWarningSvg = div().withClasses(Styles.FLEX_NONE, Styles.PR_2).with(
+      Icons.svg(Icons.WARNING_SVG_PATH,20)
+        .attr("fill-rule", "evenodd")
+        .withClasses(Styles.INLINE_BLOCK, Styles.H_6, Styles.W_6)
+    );
+    ContainerTag messageSpan = span(BANNER_TEXT);
+    ContainerTag dismissButton = div("x").withId("warning-message-dismiss").withClasses(Styles.FONT_BOLD, Styles.PL_6, Styles.OPACITY_40, Styles.CURSOR_POINTER, StyleUtils.hover(Styles.OPACITY_100));
+
+    return div(wrappedWarningSvg, messageSpan, dismissButton).withId("warning-message")
+      .withClasses(
+        Styles.ABSOLUTE, Styles.FLEX, Styles.FLEX_ROW, Styles.BG_RED_400, Styles.BORDER_RED_500, Styles.BG_OPACITY_90, Styles.MAX_W_MD, Styles.PX_2, Styles.PY_2,
+        Styles.TEXT_GRAY_700, Styles.TOP_2, Styles.ROUNDED_SM, Styles.SHADOW_LG, Styles.TRANSFORM, Styles._TRANSLATE_X_1_2, Styles.LEFT_1_2, Styles.HIDDEN);
   }
 
   protected static class HtmlResponseContent implements Content {
