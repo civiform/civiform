c33573aabbd311ec9b91855d48ce4b07080ffbf3
Caroline Danzi <cdanzi@google.com>
2021-03-31 12:23:29 -0700

    Remove multiple from select element

--- universal-application-tool-0.0.1/app/views/questiontypes/DropdownQuestionRenderer.java
+++ universal-application-tool-0.0.1/app/views/questiontypes/DropdownQuestionRenderer.java
@@ -5,7 +5,6 @@ import static j2html.TagCreator.each;
 import static j2html.TagCreator.select;
 
 import j2html.TagCreator;
-import j2html.attributes.Attr;
 import j2html.tags.Tag;
 import services.applicant.ApplicantQuestion;
 import views.BaseHtmlView;
@@ -39,7 +38,6 @@ public class DropdownQuestionRenderer extends BaseHtmlView implements ApplicantQ
                     Styles.MB_2)
                 .withText(question.getQuestionHelpText()),
             select()
-                .attr(Attr.MULTIPLE)
                 .with(each(multiOptionQuestion.getOptions(), TagCreator::option))
                 .withName(multiOptionQuestion.getSelectionPath().toString()));
   }
