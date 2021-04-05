eee1826e513d0621ef06110ad708d95c03f2befb
Nathan Henderson <nmckinley@google.com>
2021-04-05 14:12:59 -0700

    Browser test.

--- browser-test/src/support/index.ts
+++ browser-test/src/support/index.ts
@@ -1,12 +1,13 @@
 import { Browser, chromium, Page } from 'playwright'
 export { AdminQuestions } from './admin_questions'
 export { AdminPrograms } from './admin_programs'
+export { ApplicantQuestions } from './applicant_questions'
 
 const { BASE_URL = 'http://civiform:9000' } = process.env
 
 export const startSession = async () => {
   const browser = await chromium.launch()
-  const page = await browser.newPage()
+  const page = await browser.newPage({acceptDownloads: true})
 
   await page.goto(BASE_URL)
 
@@ -19,10 +20,13 @@ export const endSession = async (browser: Browser) => {
 
 export const logout = async (page: Page) => {
   // TODO: add logout button to applicant page and use that
-  page.goto('/logout')
+  await page.goto(BASE_URL + '/logout')
 }
 
 export const loginAsAdmin = async (page: Page) => {
-  await page.screenshot({ path: 'tmp/screenshot.png', fullPage: true });
   await page.click('#admin')
+}
+
+export const loginAsGuest = async (page: Page) => {
+  await page.click('#guest')
 }
