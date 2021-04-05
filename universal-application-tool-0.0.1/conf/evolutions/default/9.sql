6da3cf91fab709f9c036d46796943c7067417b1f
Hung-Ming Hsu <hmhsu@google.com>
2021-04-05 14:20:25 -0700

    set all legacy entities' lifecycle_stage to 'active'

--- universal-application-tool-0.0.1/conf/evolutions/default/9.sql
+++ universal-application-tool-0.0.1/conf/evolutions/default/9.sql
@@ -9,6 +9,10 @@ alter table applications add column lifecycle_stage varchar;
 alter table applications add column submit_time timestamp;
 alter table programs add column version bigint;
 
+update table questions set lifecycle_stage = 'active' where lifecycle_stage = null;
+update table programs set lifecycle_stage = 'active' where lifecycle_stage = null;
+update table applications set lifecycle_stage = 'active' where lifecycle_stage = null;
+
 # --- !Downs
 alter table questions drop constraint if exists nameversion cascade;
 alter table questions drop column if exists lifecycle_stage;
