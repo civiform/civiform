0a949cc1dbbe611df1b24445ccffb5f8292ddcd1
Nathan Henderson <nmckinley@google.com>
2021-04-05 16:10:32 -0700

    Run browser tests sequentially

--- browser-test/src/support/index.ts
+++ browser-test/src/support/index.ts
@@ -1,11 +1,15 @@
 import { Browser, chromium, Page } from 'playwright'
+import { exec } from 'child_process'
+const path = require('path')
 export { AdminQuestions } from './admin_questions'
 export { AdminPrograms } from './admin_programs'
 export { ApplicantQuestions } from './applicant_questions'
 
+
 const { BASE_URL = 'http://civiform:9000' } = process.env
 
 export const startSession = async () => {
+  await truncateTables()
   const browser = await chromium.launch()
   const page = await browser.newPage({acceptDownloads: true})
 
@@ -29,4 +33,15 @@ export const loginAsAdmin = async (page: Page) => {
 
 export const loginAsGuest = async (page: Page) => {
   await page.click('#guest')
+}
+
+export const truncateTables = async () => {
+  var p = exec(path.join(path.dirname(path.dirname(__dirname)), 'bin/truncate_tables.sh'),
+ (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    throw error;
+  }
+  console.log('successfully truncated tables.');
+});
 }
