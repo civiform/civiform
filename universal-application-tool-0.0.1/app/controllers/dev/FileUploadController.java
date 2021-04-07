a415fd4820678ef64080d03ca6dbefb4050f23be
Hung-Ming Hsu <hmhsu@google.com>
2021-04-07 15:53:20 -0700

    show uploaded files

--- universal-application-tool-0.0.1/app/controllers/dev/FileUploadController.java
+++ universal-application-tool-0.0.1/app/controllers/dev/FileUploadController.java
@@ -2,35 +2,44 @@ package controllers.dev;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import java.io.FileInputStream;
 import java.nio.file.Files;
+import java.util.Comparator;
+import java.util.Set;
+import models.StoredFile;
 import play.Environment;
 import play.libs.Files.TemporaryFile;
 import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.Request;
 import play.mvc.Result;
-import repository.AmazonS3Client;
+import repository.StoredFileRepository;
 import views.dev.FileUploadView;
 
 /** Controller for interacting with S3 directly in dev mode. */
 public class FileUploadController extends Controller {
   private final Environment environment;
   private final FileUploadView view;
-  private final AmazonS3Client s3client;
+  private final StoredFileRepository storedFileRepository;
 
   @Inject
   public FileUploadController(
-      FileUploadView view, AmazonS3Client s3client, Environment environment) {
+      FileUploadView view, StoredFileRepository storedFileRepository, Environment environment) {
     this.view = checkNotNull(view);
-    this.s3client = checkNotNull(s3client);
+    this.storedFileRepository = checkNotNull(storedFileRepository);
     this.environment = checkNotNull(environment);
   }
 
   public Result index(Request request) {
     if (environment.isDev()) {
-      return ok(view.render(request));
+      Set<StoredFile> files = storedFileRepository.list().toCompletableFuture().join();
+      ImmutableList<StoredFile> fileList =
+          files.stream()
+              .sorted(Comparator.comparing(StoredFile::getName))
+              .collect(ImmutableList.toImmutableList());
+      return ok(view.render(request, fileList));
     } else {
       return notFound();
     }
@@ -45,7 +54,7 @@ public class FileUploadController extends Controller {
         long fileSize = data.getFileSize();
         String contentType = data.getContentType();
         TemporaryFile file = data.getRef();
-        uploadToS3(file);
+        uploadToS3(fileName, file);
         return ok(
             String.format(
                 "File uploaded: name: %s, size: %d, type: %s.", fileName, fileSize, contentType));
@@ -57,11 +66,14 @@ public class FileUploadController extends Controller {
     }
   }
 
-  private void uploadToS3(TemporaryFile file) {
+  private void uploadToS3(String name, TemporaryFile file) {
+    StoredFile storedFile = new StoredFile();
+    storedFile.setName(name);
     try (FileInputStream fileInputStream = new FileInputStream(file.path().toFile())) {
       byte[] data = new byte[(int) Files.size(file.path())];
       fileInputStream.read(data);
-      s3client.putObject("my_file", data);
+      storedFile.setContent(data);
+      storedFileRepository.insert(storedFile);
     } catch (java.io.IOException e) {
       throw new RuntimeException(e);
     }
