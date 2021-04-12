fae76bfeee2d08560e360be3e4aa13331cb92da5
Sami Abdul-Wahid <syntapy@pm.me>
2021-04-12 08:41:26 -0700

    Add draft test for local s3 connection

---
+++ universal-application-tool-0.0.1/test/repository/AmazonS3ClientNull.java
@@ -0,0 +1,7 @@
+public class AmazonS3ClientNull extends AmazonS3Client {
+    @Override
+    private void connectS3() {
+      return;
+
+    }
+}
---
+++ universal-application-tool-0.0.1/test/repository/AmazonS3ClientTest.java
@@ -0,0 +1,13 @@
+package repository;
+
+import static org.mockito.Mockito.mock;
+
+public class AmazonS3ClientTest extends WithPostgresContainer {
+
+    @Test
+    public void testNullClient() {
+      AmazonS3Client s3ClientMock = mock(AmazonS3ClientNull.class);
+    }
+}
+
+
