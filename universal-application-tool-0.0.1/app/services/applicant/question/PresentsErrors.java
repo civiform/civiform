b5a327e2e862b647e95eb3c3468c1202c62af323
Natalie Sidhom <nsidhom@google.com>
2021-04-13 15:08:01 -0600

    Add better docstring to isAnswered

--- universal-application-tool-0.0.1/app/services/applicant/question/PresentsErrors.java
+++ universal-application-tool-0.0.1/app/services/applicant/question/PresentsErrors.java
@@ -12,6 +12,7 @@ public interface PresentsErrors {
 
   /**
    * Returns true if the question has been answered by the applicant, even if that answer was blank.
+   * In general, if a question is not answered, it cannot have errors associated with it.
    */
   boolean isAnswered();
 }
