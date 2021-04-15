82ba0aa4d2530c527b22a2a678ff684816342799
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-04-15 12:21:04 -0700

    Add browser tests.

--- browser-test/src/admin_program_creation.test.ts
+++ browser-test/src/admin_program_creation.test.ts
@@ -11,6 +11,12 @@ describe('create dropdown question with options', () => {
     await page.click('#create-question-button');
     await page.click('#create-dropdown-question');
 
+    // Verify question preview has default text.
+    expect(await page.innerText('.cf-applicant-question-text'))
+        .toContain("Sample question text");
+    expect(await page.innerText('.cf-applicant-question-help-text'))
+        .toContain("Sample question help text");
+    
     // Fill in basic info
     const questionName = 'favorite ice cream';
     await page.fill('text="Name"', questionName);
@@ -36,6 +42,12 @@ describe('create dropdown question with options', () => {
     // Assert there are only two options now
     questionSettingsDiv = await page.innerHTML('#question-settings');
     expect(questionSettingsDiv.match(/<input/g)).toHaveLength(2);
+
+    // Verify question preview text has changed based on user input.
+    expect(await page.innerText('.cf-applicant-question-text'))
+        .toContain("questionText");
+    expect(await page.innerText('.cf-applicant-question-help-text'))
+        .toContain("helpText");
 
     // Submit the form, then edit that question again
     await page.click('text=Create');
