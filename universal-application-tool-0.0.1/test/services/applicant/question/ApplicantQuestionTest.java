cc13b73dd2d45d52c76a296e54ad52e3a85fd846
Natalie Sidhom <nsidhom@google.com>
2021-04-16 11:12:58 -0600

    Use QuestionDefinitionBuilder.sample in tests

--- universal-application-tool-0.0.1/test/services/applicant/question/ApplicantQuestionTest.java
+++ universal-application-tool-0.0.1/test/services/applicant/question/ApplicantQuestionTest.java
@@ -2,114 +2,21 @@ package services.applicant.question;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.testing.EqualsTester;
-import java.util.EnumSet;
-import java.util.Locale;
-import java.util.Optional;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import models.LifecycleStage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import services.Path;
 import services.applicant.ApplicantData;
 import services.question.exceptions.UnsupportedQuestionTypeException;
 import services.question.types.AddressQuestionDefinition;
-import services.question.types.CheckboxQuestionDefinition;
-import services.question.types.DropdownQuestionDefinition;
-import services.question.types.NameQuestionDefinition;
-import services.question.types.NumberQuestionDefinition;
 import services.question.types.QuestionDefinitionBuilder;
 import services.question.types.QuestionType;
-import services.question.types.RadioButtonQuestionDefinition;
 import services.question.types.TextQuestionDefinition;
 
 @RunWith(JUnitParamsRunner.class)
 public class ApplicantQuestionTest {
-
-  private static final AddressQuestionDefinition addressQuestionDefinition =
-          new AddressQuestionDefinition(
-                  1L,
-                  "question name",
-                  Path.create("applicant.my.path.name"),
-                  Optional.empty(),
-                  "description",
-                  LifecycleStage.ACTIVE,
-                  ImmutableMap.of(Locale.US, "question?"),
-                  ImmutableMap.of(Locale.US, "help text"));
-  private static final CheckboxQuestionDefinition checkboxQuestionDefinition =
-      new CheckboxQuestionDefinition(
-          1L,
-          "question name",
-          Path.create("applicant.my.path.name"),
-          Optional.empty(),
-          "description",
-          LifecycleStage.ACTIVE,
-          ImmutableMap.of(Locale.US, "question?"),
-          ImmutableMap.of(Locale.US, "help text"),
-          ImmutableListMultimap.of(Locale.US, "option 1", Locale.US, "option 2"));
-  private static final DropdownQuestionDefinition dropdownQuestionDefinition =
-      new DropdownQuestionDefinition(
-          1L,
-          "question name",
-          Path.create("applicant.my.path.name"),
-          Optional.empty(),
-          "description",
-          LifecycleStage.ACTIVE,
-          ImmutableMap.of(Locale.US, "question?"),
-          ImmutableMap.of(Locale.US, "help text"),
-          ImmutableListMultimap.of(
-              Locale.US,
-              "option 1",
-              Locale.US,
-              "option 2",
-              Locale.FRANCE,
-              "un",
-              Locale.FRANCE,
-              "deux"));
-  private static final NameQuestionDefinition nameQuestionDefinition =
-      new NameQuestionDefinition(
-          1L,
-          "question name",
-          Path.create("applicant.my.path.name"),
-          Optional.empty(),
-          "description",
-          LifecycleStage.ACTIVE,
-          ImmutableMap.of(Locale.US, "question?"),
-          ImmutableMap.of(Locale.US, "help text"));
-  private static final NumberQuestionDefinition numberQuestionDefinition =
-      new NumberQuestionDefinition(
-          1L,
-          "question name",
-          Path.create("applicant.my.path.name"),
-          Optional.empty(),
-          "description",
-          LifecycleStage.ACTIVE,
-          ImmutableMap.of(Locale.US, "question?"),
-          ImmutableMap.of(Locale.US, "help text"));
-  private static final RadioButtonQuestionDefinition radioButtonQuestionDefinition =
-      new RadioButtonQuestionDefinition(
-          1L,
-          "question name",
-          Path.create("applicant.my.path.name"),
-          Optional.empty(),
-          "description",
-          LifecycleStage.ACTIVE,
-          ImmutableMap.of(Locale.US, "question?"),
-          ImmutableMap.of(Locale.US, "help text"),
-          ImmutableListMultimap.of(Locale.US, "option 1", Locale.US, "option 2"));
-  private static final TextQuestionDefinition textQuestionDefinition =
-          new TextQuestionDefinition(
-                  1L,
-                  "question name",
-                  Path.create("applicant.my.path.name"),
-                  Optional.empty(),
-                  "description",
-                  LifecycleStage.ACTIVE,
-                  ImmutableMap.of(Locale.US, "question?"),
-                  ImmutableMap.of(Locale.US, "help text"));
 
   @Test
   @Parameters(source = QuestionType.class)
@@ -122,42 +29,54 @@ public class ApplicantQuestionTest {
   }
 
   @Test
-  public void getsExpectedQuestionType() {
+  public void getsExpectedQuestionType() throws UnsupportedQuestionTypeException {
     ApplicantQuestion addressApplicantQuestion =
-        new ApplicantQuestion(addressQuestionDefinition, new ApplicantData());
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.ADDRESS).build(), new ApplicantData());
     assertThat(addressApplicantQuestion.createAddressQuestion())
         .isInstanceOf(AddressQuestion.class);
 
+    ApplicantQuestion checkboxApplicantQuestion =
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.CHECKBOX).build(), new ApplicantData());
+    assertThat(checkboxApplicantQuestion.createMultiSelectQuestion())
+        .isInstanceOf(MultiSelectQuestion.class);
+
     ApplicantQuestion nameApplicantQuestion =
-        new ApplicantQuestion(nameQuestionDefinition, new ApplicantData());
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.NAME).build(), new ApplicantData());
     assertThat(nameApplicantQuestion.createNameQuestion()).isInstanceOf(NameQuestion.class);
 
     ApplicantQuestion numberApplicantQuestion =
-        new ApplicantQuestion(numberQuestionDefinition, new ApplicantData());
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.NUMBER).build(), new ApplicantData());
     assertThat(numberApplicantQuestion.createNumberQuestion()).isInstanceOf(NumberQuestion.class);
 
+    ApplicantQuestion radioApplicantQuestion =
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.RADIO_BUTTON).build(),
+            new ApplicantData());
+    assertThat(radioApplicantQuestion.createSingleSelectQuestion())
+        .isInstanceOf(SingleSelectQuestion.class);
+
     ApplicantQuestion singleSelectApplicantQuestion =
-        new ApplicantQuestion(dropdownQuestionDefinition, new ApplicantData());
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.DROPDOWN).build(), new ApplicantData());
     assertThat(singleSelectApplicantQuestion.createSingleSelectQuestion())
         .isInstanceOf(SingleSelectQuestion.class);
 
     ApplicantQuestion textApplicantQuestion =
-        new ApplicantQuestion(textQuestionDefinition, new ApplicantData());
+        new ApplicantQuestion(
+            QuestionDefinitionBuilder.sample(QuestionType.TEXT).build(), new ApplicantData());
     assertThat(textApplicantQuestion.createTextQuestion()).isInstanceOf(TextQuestion.class);
-
-    ApplicantQuestion radioApplicantQuestion =
-        new ApplicantQuestion(radioButtonQuestionDefinition, new ApplicantData());
-    assertThat(radioApplicantQuestion.createSingleSelectQuestion())
-        .isInstanceOf(SingleSelectQuestion.class);
-
-    ApplicantQuestion checkboxApplicantQuestion =
-        new ApplicantQuestion(checkboxQuestionDefinition, new ApplicantData());
-    assertThat(checkboxApplicantQuestion.createMultiSelectQuestion())
-        .isInstanceOf(MultiSelectQuestion.class);
   }
 
   @Test
-  public void equals() {
+  public void equals() throws UnsupportedQuestionTypeException {
+    AddressQuestionDefinition addressQuestionDefinition =
+        (AddressQuestionDefinition) QuestionDefinitionBuilder.sample(QuestionType.ADDRESS).build();
+    TextQuestionDefinition textQuestionDefinition =
+        (TextQuestionDefinition) QuestionDefinitionBuilder.sample(QuestionType.TEXT).build();
     ApplicantData dataWithAnswers = new ApplicantData();
     dataWithAnswers.putString(Path.create("applicant.color"), "blue");
 
@@ -165,15 +84,6 @@ public class ApplicantQuestionTest {
         .addEqualityGroup(
             new ApplicantQuestion(addressQuestionDefinition, new ApplicantData()),
             new ApplicantQuestion(addressQuestionDefinition, new ApplicantData()))
-        .addEqualityGroup(
-            new ApplicantQuestion(dropdownQuestionDefinition, new ApplicantData()),
-            new ApplicantQuestion(dropdownQuestionDefinition, new ApplicantData()))
-        .addEqualityGroup(
-            new ApplicantQuestion(nameQuestionDefinition, new ApplicantData()),
-            new ApplicantQuestion(nameQuestionDefinition, new ApplicantData()))
-        .addEqualityGroup(
-            new ApplicantQuestion(numberQuestionDefinition, new ApplicantData()),
-            new ApplicantQuestion(numberQuestionDefinition, new ApplicantData()))
         .addEqualityGroup(
             new ApplicantQuestion(textQuestionDefinition, new ApplicantData()),
             new ApplicantQuestion(textQuestionDefinition, new ApplicantData()))
