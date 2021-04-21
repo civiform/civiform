cbb8794a6d3d6f7045fb4e69d42cc70308135bdd
Natalie Sidhom <nsidhom@google.com>
2021-04-20 18:43:19 -0700

    Add back accidentally deleted line

--- universal-application-tool-0.0.1/test/support/TestQuestionBank.java
+++ universal-application-tool-0.0.1/test/support/TestQuestionBank.java
@@ -83,6 +83,8 @@ public class TestQuestionBank {
 
   // Repeated name
   public Question applicantHouseholdMemberName() {
+    // Make sure the next call will have the question ready
+    applicantHouseholdMembers();
     return questionCache.computeIfAbsent(
         QuestionEnum.APPLICANT_HOUSEHOLD_MEMBER_NAME, this::applicantHouseholdMemberName);
   }
