e84b9ad49b113672f79dd268df740dc35e32ec82
Caroline Danzi <cdanzi@google.com>
2021-04-29 12:07:00 -0700

    Fix styling for translation views

--- universal-application-tool-0.0.1/app/views/style/ReferenceClasses.java
+++ universal-application-tool-0.0.1/app/views/style/ReferenceClasses.java
@@ -1,10 +1,11 @@
 package views.style;
 
-/** Non-styled clases that point to different components. */
+/** Non-styled classes that point to different components. */
 public final class ReferenceClasses {
   public static final String ADD_QUESTION_BUTTON = "cf-add-question-button";
   public static final String ADMIN_APPLICATION_CARD = "cf-admin-application-card";
   public static final String ADMIN_APPLICATION_BLOCK_CARD = "cf-admin-application-block-card";
+  public static final String ADMIN_LANGUAGE_LINK = "cf-admin-language-link";
   public static final String ADMIN_PROGRAM_CARD = "cf-admin-program-card";
   public static final String ADMIN_QUESTION_TABLE_ROW = "cf-admin-question-table-row";
   public static final String APPLICANT_QUESTION_HELP_TEXT = "cf-applicant-question-help-text";
