b18e47a23853eb86b080e5b103558651675a73e4
Gerardo Capiel <gcapiel@alum.mit.edu>
2021-05-03 13:39:54 -0700

    Update TrustedIntermediaryGroupListView.java

--- universal-application-tool-0.0.1/app/views/admin/ti/TrustedIntermediaryGroupListView.java
+++ universal-application-tool-0.0.1/app/views/admin/ti/TrustedIntermediaryGroupListView.java
@@ -40,10 +40,10 @@ public class TrustedIntermediaryGroupListView extends BaseHtmlView {
   public Content render(List<TrustedIntermediaryGroup> tis, Http.Request request) {
     ContainerTag body =
         body(
-            renderHeader("Trusted Intermediary Groups"),
-            renderTiGroupCards(tis),
-            renderHeader("Create New Group").withClass(Styles.MT_8),
-            renderAddNewButton(request));
+            renderHeader("Create New Trusted Intermediary").withClass(Styles.MT_8),
+            renderAddNewButton(request),
+            renderHeader("Existing Trusted Intermediaries"),
+            renderTiGroupCards(tis));
     if (request.flash().get("error").isPresent()) {
       LoggerFactory.getLogger(TrustedIntermediaryGroupListView.class)
           .info(request.flash().get("error").get());
