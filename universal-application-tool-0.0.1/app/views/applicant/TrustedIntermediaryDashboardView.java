e0792810df0333ee64d9341c53ff0c2df760365b
Gerardo Capiel <gcapiel@alum.mit.edu>
2021-05-06 23:20:07 -0700

    Text changes to TI dashboard (#1046)
    * Update TrustedIntermediaryDashboardView.java
    
    * fix compile bug

--- universal-application-tool-0.0.1/app/views/applicant/TrustedIntermediaryDashboardView.java
+++ universal-application-tool-0.0.1/app/views/applicant/TrustedIntermediaryDashboardView.java
@@ -54,14 +54,14 @@ public class TrustedIntermediaryDashboardView extends BaseHtmlView {
                     renderHeader(tiGroup.getName()),
                     h2(tiGroup.getDescription()).withClasses(Styles.ML_2),
                     hr(),
-                    renderHeader("Group Members"),
-                    renderTIMembersTable(tiGroup).withClasses(Styles.ML_2),
+                    renderHeader("Add Client"),
+                    renderAddNewForm(tiGroup, request),
                     hr().withClasses(Styles.MT_6),
-                    renderHeader("Managed Applicants"),
+                    renderHeader("Clients"),
                     renderTIApplicantsTable(tiGroup),
                     hr().withClasses(Styles.MT_6),
-                    renderHeader("Create New Applicant"),
-                    renderAddNewForm(tiGroup, request)))
+                    renderHeader("Trusted Intermediary Members"),
+                    renderTIMembersTable(tiGroup).withClasses(Styles.ML_2)))
             .withClasses(Styles.PX_2, Styles.MAX_W_SCREEN_XL, Styles.MX_AUTO);
     if (request.flash().get("error").isPresent()) {
       LoggerFactory.getLogger(TrustedIntermediaryGroupListView.class)
