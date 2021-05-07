78922fc38f4d7ad4c9ceb0a5476dca979a464ce9
Caroline Danzi <cdanzi@google.com>
2021-05-07 15:24:28 -0700

    Add foreign key constraint

--- universal-application-tool-0.0.1/conf/evolutions/default/21.sql
+++ universal-application-tool-0.0.1/conf/evolutions/default/21.sql
@@ -4,6 +4,8 @@
 create table programs_accounts (
   programs_id bigint not null,
   accounts_id bigint not null,
+  constraint fk_program foreign key (programs_id) references programs(id),
+  constraint fk_account foreign key (accounts_id) references accounts(id),
   primary key (programs_id, accounts_id)
 );
 
