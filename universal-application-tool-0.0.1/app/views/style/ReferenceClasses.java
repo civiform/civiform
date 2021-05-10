4fbf9f6ac210a7845d85a4b4f97ef7315fd67a3b
Natalie Sidhom <nsidhom@google.com>
2021-05-10 15:54:18 -0700

    Rearrange reference classes and add some for checkboxes

--- universal-application-tool-0.0.1/app/views/style/ReferenceClasses.java
+++ universal-application-tool-0.0.1/app/views/style/ReferenceClasses.java
@@ -2,7 +2,10 @@ package views.style;
 
 /** Non-styled classes that point to different components. */
 public final class ReferenceClasses {
-  public static final String ADD_QUESTION_BUTTON = "cf-add-question-button";
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  // Admin reference classes
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
   public static final String ADMIN_APPLICATION_BLOCK_CARD = "cf-admin-application-block-card";
   public static final String ADMIN_APPLICATION_CARD = "cf-admin-application-card";
   public static final String ADMIN_LANGUAGE_LINK = "cf-admin-language-link";
@@ -10,20 +13,36 @@ public final class ReferenceClasses {
   public static final String ADMIN_QUESTION_TABLE_ROW = "cf-admin-question-table-row";
   public static final String ADMIN_TI_GROUP_ROW = "cf-ti-row";
   public static final String ADMIN_VERSION_CARD = "cf-admin-version-card";
+  public static final String QUESTION_CONFIG = "cf-question-config";
+
+  public static final String ADD_QUESTION_BUTTON = "cf-add-question-button";
+  public static final String REMOVE_QUESTION_BUTTON = "cf-remove-question-button";
+  public static final String DOWNLOAD_ALL_BUTTON = "cf-download-all-button";
+  public static final String DOWNLOAD_BUTTON = "cf-download-button";
+  public static final String VIEW_BUTTON = "cf-view-application-button";
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  // Applicant reference classes
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
   public static final String APPLICANT_QUESTION_HELP_TEXT = "cf-applicant-question-help-text";
   public static final String APPLICANT_QUESTION_TEXT = "cf-applicant-question-text";
   public static final String APPLICANT_SUMMARY_ROW = "cf-applicant-summary-row";
   public static final String APPLICATION_CARD = "cf-application-card";
+
   public static final String APPLY_BUTTON = "cf-apply-button";
-  public static final String DOWNLOAD_ALL_BUTTON = "cf-download-all-button";
-  public static final String DOWNLOAD_BUTTON = "cf-download-button";
+
   public static final String ENUMERATOR_FIELD = "cf-enumerator-field";
-  public static final String FLOATED_LABEL = "cf-floated";
-  public static final String QUESTION_CONFIG = "cf-question-config";
+  public static final String APPLICANT_INPUT_LABEL = "cf-applicant-input-label";
+  public static final String CHECKBOX_INPUT = "cf-checkbox-input";
+  public static final String CHECKBOX_OPTION = "cf-checkbox-input";
   public static final String RADIO_DEFAULT = "cf-radio-default";
   public static final String RADIO_INPUT = "cf-radio-input";
   public static final String RADIO_OPTION = "cf-radio-option";
-  public static final String REMOVE_QUESTION_BUTTON = "cf-remove-question-button";
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  // Common reference classes
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
   public static final String TOAST_MESSAGE = "cf-toast-data";
-  public static final String VIEW_BUTTON = "cf-view-application-button";
 }
