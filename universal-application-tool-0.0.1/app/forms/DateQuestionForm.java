e19e7efd4b15039740e7b3a96fe56aec257ddfe4
Indira Alase <indira.alase@seattle.gov>
2021-05-11 16:28:22 -0700

    add these new files to branch

---
+++ universal-application-tool-0.0.1/app/forms/DateQuestionForm.java
@@ -0,0 +1,34 @@
+package forms;
+
+import services.Path;
+import services.question.types.DateQuestionDefinition;
+import services.question.types.QuestionDefinitionBuilder;
+import services.question.types.QuestionType;
+
+public class DateQuestionForm extends QuestionForm {
+
+  public DateQuestionForm() {
+    super();
+  }
+
+  public DateQuestionForm(DateQuestionDefinition qd) {
+    super(qd);
+  }
+
+  @Override
+  public QuestionType getQuestionType() {
+    return QuestionType.DATE;
+  }
+
+  @Override
+  public QuestionDefinitionBuilder getBuilder(Path path) {
+    //        DateQuestionDefinition.DateValidationPredicates.Builder
+    // dateValidationPredicatesBuilder =
+    //                DateQuestionDefinition.DateValidationPredicates.create();
+    //
+    //        return super.getBuilder(path)
+    //                .setValidationPredicates(dateValidationPredicatesBuilder.build());
+
+    return null;
+  }
+}
---
+++ universal-application-tool-0.0.1/app/views/questiontypes/DateQuestionRenderer.java
@@ -0,0 +1,53 @@
+package views.questiontypes;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static j2html.TagCreator.div;
+
+import j2html.tags.Tag;
+import java.util.OptionalLong;
+import services.applicant.question.ApplicantQuestion;
+import services.applicant.question.DateQuestion;
+import views.BaseHtmlView;
+import views.components.FieldWithLabel;
+import views.style.ReferenceClasses;
+import views.style.Styles;
+
+public class DateQuestionRenderer extends BaseHtmlView implements ApplicantQuestionRenderer {
+  private final ApplicantQuestion question;
+
+  public DateQuestionRenderer(ApplicantQuestion question) {
+    this.question = checkNotNull(question);
+  }
+
+  @Override
+  public Tag render(ApplicantQuestionRendererParams params) {
+    DateQuestion dateQuestion = question.createDateQuestion();
+
+    FieldWithLabel dateField =
+        FieldWithLabel.number()
+            .setFieldName(dateQuestion.getDatePath().toString())
+            .setFloatLabel(true);
+    if (dateQuestion.getDateValue().isPresent()) {
+      // TODO: [Refactor] Oof! Converting Optional<Long> to OptionalLong.
+      OptionalLong value = OptionalLong.of(dateQuestion.getDateValue().orElse(0L));
+      dateField.setValue(value);
+    }
+
+    return div()
+        .withId(question.getContextualizedPath().toString())
+        .withClasses(Styles.MX_AUTO, Styles.PX_16)
+        .with(
+            div()
+                .withClasses(ReferenceClasses.APPLICANT_QUESTION_TEXT)
+                .withText(question.getQuestionText()),
+            div()
+                .withClasses(
+                    ReferenceClasses.APPLICANT_QUESTION_HELP_TEXT,
+                    Styles.TEXT_BASE,
+                    Styles.FONT_THIN,
+                    Styles.MB_2)
+                .withText(question.getQuestionHelpText()),
+            dateField.getContainer(),
+            fieldErrors(params.messages(), dateQuestion.getQuestionErrors()));
+  }
+}
