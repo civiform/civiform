a9da3c02b0c3c98a38e8c4f8d281136481e83937
Caroline Danzi <cdanzi@google.com>
2021-05-11 18:13:13 -0700

    Allow admins to configure the entity name for enumerator questions (#1098)
    * Allow admin to configure enumerator entity type
    
    Co-authored-by: Kendrick <kentangsrice@gmail.com>

--- universal-application-tool-0.0.1/app/forms/EnumeratorQuestionForm.java
+++ universal-application-tool-0.0.1/app/forms/EnumeratorQuestionForm.java
@@ -1,19 +1,38 @@
 package forms;
 
+import services.LocalizedStrings;
 import services.question.types.EnumeratorQuestionDefinition;
+import services.question.types.QuestionDefinitionBuilder;
 import services.question.types.QuestionType;
 
 public class EnumeratorQuestionForm extends QuestionForm {
+  private String entityType;
+
   public EnumeratorQuestionForm() {
     super();
+    this.entityType = "";
   }
 
   public EnumeratorQuestionForm(EnumeratorQuestionDefinition qd) {
     super(qd);
+    this.entityType = qd.getEntityType().isEmpty() ? "" : qd.getEntityType().getDefault();
   }
 
   @Override
   public QuestionType getQuestionType() {
     return QuestionType.ENUMERATOR;
+  }
+
+  public String getEntityType() {
+    return entityType;
+  }
+
+  public void setEntityType(String entityType) {
+    this.entityType = entityType;
+  }
+
+  @Override
+  public QuestionDefinitionBuilder getBuilder() {
+    return super.getBuilder().setEntityType(LocalizedStrings.withDefaultValue(this.entityType));
   }
 }
