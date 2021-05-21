15a17b4dc77b61af1737ada94253edee1de1d3e4
Nathan McKinley Henderson <nmckinley@google.com>
2021-05-20 17:44:12 -0700

    Maybe we should try caching this key and fetching it only once per server (#1233)
    * Maybe we should try caching this key and fetching it only once per server.
    
    * make class static
    
    * make errorprone happy

--- universal-application-tool-0.0.1/app/auth/IdcsProfileAdapter.java
+++ universal-application-tool-0.0.1/app/auth/IdcsProfileAdapter.java
@@ -1,8 +1,14 @@
 package auth;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.Resource;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -112,23 +118,66 @@ public class IdcsProfileAdapter extends UatProfileAdapter {
       return;
     }
 
-    // Note that there would normally be a significant thread-safety issue here - but
-    // we actually don't need to match up signing key tokens with actual tokens, because
-    // any valid token is sufficient.
-    this.configuration.setResourceRetriever(
-        new DefaultResourceRetriever(
-            this.configuration.getConnectTimeout(), this.configuration.getReadTimeout()) {
-          @Override
-          public Map<String, List<String>> getHeaders() {
-            Map<String, List<String>> headers = super.getHeaders();
-            if (headers == null) {
-              headers = new HashMap<>();
-            }
-            String authHeader = ((OidcCredentials) cred).getAccessToken().toAuthorizationHeader();
-            LOG.debug("Auth header in the resource retriever: {}", authHeader);
-            headers.put("Authorization", List.of(authHeader));
-            return headers;
-          }
-        });
+    if (this.configuration.getResourceRetriever() instanceof CachedResourceRetriever) {
+      LOG.debug("Already have jwk cached.");
+      return;
+    }
+
+    try {
+      URI jwkSetUri = this.configuration.getProviderMetadata().getJWKSetURI();
+      ImmutableMap<URI, Resource> jwkCache =
+          ImmutableMap.of(
+              jwkSetUri,
+              new CredentialedResourceRetriever(configuration, cred)
+                  .retrieveResource(jwkSetUri.toURL()));
+      this.configuration.setResourceRetriever(new CachedResourceRetriever(configuration, jwkCache));
+    } catch (IOException | NullPointerException e) {
+      LOG.error("Failed to fetch JWK", e);
+    }
+  }
+
+  private static class CredentialedResourceRetriever extends DefaultResourceRetriever {
+    private final Credentials cred;
+
+    public CredentialedResourceRetriever(OidcConfiguration configuration, Credentials cred) {
+      super(configuration.getConnectTimeout(), configuration.getReadTimeout());
+      this.cred = cred;
+    }
+
+    @Override
+    public Map<String, List<String>> getHeaders() {
+      Map<String, List<String>> headers = super.getHeaders();
+      if (headers == null) {
+        headers = new HashMap<>();
+      }
+      String authHeader = ((OidcCredentials) cred).getAccessToken().toAuthorizationHeader();
+      LOG.debug("Auth header in the resource retriever: {}", authHeader);
+      headers.put("Authorization", List.of(authHeader));
+      return headers;
+    }
+  }
+
+  private static class CachedResourceRetriever extends DefaultResourceRetriever {
+    private final ImmutableMap<URI, Resource> resources;
+
+    public CachedResourceRetriever(
+        OidcConfiguration configuration, ImmutableMap<URI, Resource> resources) {
+      super(configuration.getConnectTimeout(), configuration.getReadTimeout());
+      this.resources = resources;
+    }
+
+    @Override
+    public Resource retrieveResource(URL url) throws IOException {
+      LOG.debug("Attempting to fetch {}", url.toString());
+      try {
+        if (resources.containsKey(url.toURI())) {
+          LOG.debug("Cached: {}", url.toString());
+          return resources.get(url.toURI());
+        }
+      } catch (URISyntaxException e) {
+        LOG.debug("failed to convert to URI", e);
+      }
+      return super.retrieveResource(url);
+    }
   }
 }
