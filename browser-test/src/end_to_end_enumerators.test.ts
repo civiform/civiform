d987cb63538e8c1b21a7d5dc741031cedf54a495
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-05-26 15:23:39 -0700

    Update end_to_end_enumerators.test.ts

--- browser-test/src/end_to_end_enumerators.test.ts
+++ browser-test/src/end_to_end_enumerators.test.ts
@@ -109,7 +109,6 @@ describe('End to end enumerator test', () => {
     // Verify that the error message is visible.
     expect(await page.innerText('.cf-enumerator-error:visible')).toEqual('Please enter a unique value for each line.');
 
-
     // Remove one of the 'Banker' entries and add 'Painter'.
     // the value attribute of the inputs isn't set, so we're clicking the second one.
     await page.click(':nth-match(:text("Remove Entity"), 2)');    
