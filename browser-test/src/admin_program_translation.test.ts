ca5d358317a1293b3f314ff6a895d50bb085782b
kendrickt <kentangsrice@gmail.com>
2021-05-26 13:12:06 -0700

    Bugfix/1261 empty helptext (#1265)
    * Only set question help text if it there is a real value.
    
    * Delete all help text translations if the question edit form has empty string.
    
    * Add browser test for question help text translation deletion
    
    * Update browser-test/src/admin_program_translation.test.ts
    
    Co-authored-by: Hung-Ming Hsu <hmhsu@users.noreply.github.com>
    
    Co-authored-by: Hung-Ming Hsu <hmhsu@users.noreply.github.com>

--- browser-test/src/admin_program_translation.test.ts
+++ browser-test/src/admin_program_translation.test.ts
@@ -164,6 +164,38 @@ describe('Admin can manage translations', () => {
     await endSession(browser);
   });
 
+  it('deleting help text in question edit view deletes all help text translations', async () => {
+    const { browser, page } = await startSession();
+
+    await loginAsAdmin(page);
+    const adminQuestions = new AdminQuestions(page);
+
+    // Add a new question with help text
+    const questionName = 'translate-help-text-deletion';
+    await adminQuestions.addNumberQuestion(questionName);
+
+    // Add a translation for a non-English language.
+    await adminQuestions.goToQuestionTranslationPage(questionName);
+    const adminTranslations = new AdminTranslations(page);
+    await adminTranslations.selectLanguage('Spanish');
+    await adminTranslations.editQuestionTranslations('something different', 'help text different');
+
+    // Edit the question and delete the help text.
+    await adminQuestions.changeQuestionHelpText(questionName, '');
+
+    // Edit the question and add help text back
+    await adminQuestions.changeQuestionHelpText(questionName, 'a new help text');
+
+
+    // View the question translations and check that the Spanish translations for question help text are gone.
+    await adminQuestions.goToQuestionTranslationPage(questionName);
+    await adminTranslations.selectLanguage('Spanish');
+    expect(await page.getAttribute('#localize-question-text', 'value')).toContain('something different');
+    expect(await page.getAttribute('#localize-question-help-text', 'value')).toEqual('');
+
+    await endSession(browser);
+  });
+
   it('Applicant sees toast message warning translation is not complete', async () => {
     const { browser, page } = await startSession();
 
