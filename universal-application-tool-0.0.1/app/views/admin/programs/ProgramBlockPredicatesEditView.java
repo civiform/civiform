a29be5326540d55d90b15e65850285cae1153104
Natalie Sidhom <nsidhom@google.com>
2021-06-08 09:47:20 -0700

    Improve the look and layout of the question predicate list

--- universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockPredicatesEditView.java
+++ universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockPredicatesEditView.java
@@ -5,8 +5,10 @@ import static j2html.TagCreator.div;
 import static j2html.TagCreator.each;
 import static j2html.TagCreator.form;
 import static j2html.TagCreator.h1;
+import static j2html.TagCreator.wbr;
 
 import com.google.common.collect.ImmutableList;
+import j2html.tags.ContainerTag;
 import j2html.tags.Tag;
 import javax.inject.Inject;
 import play.mvc.Http;
@@ -17,6 +19,9 @@ import services.question.types.QuestionDefinition;
 import views.BaseHtmlView;
 import views.HtmlBundle;
 import views.admin.AdminLayout;
+import views.components.Icons;
+import views.style.StyleUtils;
+import views.style.Styles;
 
 public class ProgramBlockPredicatesEditView extends BaseHtmlView {
   private final AdminLayout layout;
@@ -31,7 +36,7 @@ public class ProgramBlockPredicatesEditView extends BaseHtmlView {
       ProgramDefinition programDefinition,
       BlockDefinition blockDefinition,
       ImmutableList<QuestionDefinition> potentialPredicateQuestions) {
-    String title = "Edit block predicates";
+    String title = String.format("%s: Add a condition to show or hide this block", blockDefinition.name());
     Tag csrfTag = makeCsrfTokenInputTag(request);
     HtmlBundle htmlBundle =
         layout
@@ -40,14 +45,42 @@ public class ProgramBlockPredicatesEditView extends BaseHtmlView {
             .addMainContent(
                 form().with(csrfTag),
                 layout.renderProgramInfo(programDefinition),
-                h1().withText(
-                        String.format("Add show/hide predicates for %s", blockDefinition.name())),
-                div()
-                    .with(
-                        each(
-                            potentialPredicateQuestions,
-                            question -> div().withText(question.getName()))));
+                h1().withText(title).withClasses(Styles.MY_4, Styles.FONT_BOLD, Styles.TEXT_XL),
+                renderQuestionDefinitions(potentialPredicateQuestions));
 
     return layout.renderCentered(htmlBundle);
+  }
+
+  private ContainerTag renderQuestionDefinitions(
+      ImmutableList<QuestionDefinition> questionDefinitions) {
+    return div()
+        .withClasses(Styles.FLEX, Styles.FLEX_COL, Styles.GAP_2)
+        .with(
+            each(
+                questionDefinitions,
+                questionDefinition -> renderQuestionDefinition(questionDefinition)));
+  }
+
+  private ContainerTag renderQuestionDefinition(QuestionDefinition questionDefinition) {
+    return div()
+        .withClasses(
+            Styles.FLEX,
+            Styles.FLEX_ROW,
+            Styles.GAP_4,
+            Styles.PX_4,
+            Styles.PY_2,
+            Styles.BORDER,
+            Styles.BORDER_GRAY_200,
+            StyleUtils.hover(Styles.TEXT_GRAY_800, Styles.BG_GRAY_100))
+        .with(
+            Icons.questionTypeSvg(questionDefinition.getQuestionType(), 24)
+                .withClasses(Styles.FLEX_SHRINK_0, Styles.H_12, Styles.W_6))
+        .with(
+            div()
+                .withClasses()
+                .with(
+                    div(questionDefinition.getName()),
+                    div(questionDefinition.getDescription())
+                        .withClasses(Styles.MT_1, Styles.TEXT_SM)));
   }
 }
