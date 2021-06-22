84edc6ec80e07a95f0279580a1b3737bcda705c1
AWBaumann <77127160+AWBaumann@users.noreply.github.com>
2021-06-22 10:44:27 -0700

    Update StaticContentQuestionTest.java

--- universal-application-tool-0.0.1/test/services/applicant/question/StaticContentQuestionTest.java
+++ universal-application-tool-0.0.1/test/services/applicant/question/StaticContentQuestionTest.java
@@ -2,29 +2,38 @@ package services.applicant.question;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.ImmutableList;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.OptionalLong;
 import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import models.Applicant;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import play.i18n.Lang;
+import play.i18n.Messages;
+import play.i18n.MessagesApi;
 import repository.WithPostgresContainer;
 import services.LocalizedStrings;
 import services.applicant.ApplicantData;
 import services.question.types.StaticContentQuestionDefinition;
+import support.QuestionAnswerer;
 
 @RunWith(JUnitParamsRunner.class)
 public class StaticContentQuestionTest extends WithPostgresContainer {
 
   private static final StaticContentQuestionDefinition questionDefinition =
-      new StaticContentQuestionDefinition(
-          "name",
-          Optional.empty(),
-          "description",
-          LocalizedStrings.of(Locale.US, "Some text. Not an actual question."),
-          LocalizedStrings.empty());
+    new StaticContentQuestionDefinition(
+      OptionalLong.of(1),
+      "name",
+      Optional.empty(),
+      "description",
+      LocalizedStrings.of(Locale.US, "Some text. Not an actual question."),
+      LocalizedStrings.empty());
 
+  
   private Applicant applicant;
   private ApplicantData applicantData;
 
@@ -44,4 +53,5 @@ public class StaticContentQuestionTest extends WithPostgresContainer {
     assertThat(question.hasQuestionErrors()).isFalse();
     assertThat(question.hasTypeSpecificErrors()).isFalse();
   }
+
 }
