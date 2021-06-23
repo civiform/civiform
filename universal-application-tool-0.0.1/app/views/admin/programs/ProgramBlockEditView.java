028fcbbccdd9d4629a01d3c9ccd55da5b643ca60
Kendrick <kentangsrice@gmail.com>
2021-06-22 22:52:16 -0700

    wip

--- universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockEditView.java
+++ universal-application-tool-0.0.1/app/views/admin/programs/ProgramBlockEditView.java
@@ -10,6 +10,7 @@ import static j2html.TagCreator.text;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
+import com.typesafe.config.Config;
 import controllers.admin.routes;
 import forms.BlockForm;
 import j2html.TagCreator;
@@ -41,6 +42,7 @@ import views.style.Styles;
 public class ProgramBlockEditView extends BaseHtmlView {
 
   private final AdminLayout layout;
+  private final Config config;
 
   public static final String ENUMERATOR_ID_FORM_FIELD = "enumeratorId";
   private static final String CREATE_BLOCK_FORM_ID = "block-create-form";
@@ -48,8 +50,9 @@ public class ProgramBlockEditView extends BaseHtmlView {
   private static final String DELETE_BLOCK_FORM_ID = "block-delete-form";
 
   @Inject
-  public ProgramBlockEditView(AdminLayout layout) {
+  public ProgramBlockEditView(AdminLayout layout, Config config) {
     this.layout = checkNotNull(layout);
+    this.config = checkNotNull(config);
   }
 
   public Content render(
@@ -374,49 +377,55 @@ public class ProgramBlockEditView extends BaseHtmlView {
             .with(p(questionDefinition.getName()))
             .with(p(questionDefinition.getDescription()).withClasses(Styles.MT_1, Styles.TEXT_SM));
 
-    ContainerTag optionalButton =
-        TagCreator.button()
-            .withClasses(
-                Styles.FLEX,
-                Styles.GAP_2,
-                Styles.ITEMS_CENTER,
-                isOptional ? Styles.TEXT_BLACK : Styles.TEXT_GRAY_400,
-                Styles.FONT_MEDIUM,
-                Styles.BG_TRANSPARENT,
-                Styles.ROUNDED_FULL,
-                StyleUtils.hover(Styles.BG_GRAY_400, Styles.TEXT_GRAY_300))
-            .withType("submit")
-            .with(p("optional").withClasses("hover-group:text-white"))
-            .with(
-                div()
-                    .withClasses(Styles.RELATIVE)
-                    .with(
-                        div()
-                            .withClasses(
-                                isOptional ? Styles.BG_BLUE_600 : Styles.BG_GRAY_600,
-                                Styles.W_14,
-                                Styles.H_8,
-                                Styles.ROUNDED_FULL))
-                    .with(
-                        div()
-                            .withClasses(
-                                Styles.ABSOLUTE,
-                                Styles.BG_WHITE,
-                                isOptional ? Styles.RIGHT_1 : Styles.LEFT_1,
-                                Styles.TOP_1,
-                                Styles.W_6,
-                                Styles.H_6,
-                                Styles.ROUNDED_FULL)));
-    String toggleOptionalAction =
-        controllers.admin.routes.AdminProgramBlockQuestionsController.setOptional(
-                programDefinitionId, blockDefinitionId, questionDefinition.getId())
-            .url();
-    ContainerTag optionalToggle =
-        form(csrfTag)
-            .withMethod(HttpVerbs.POST)
-            .withAction(toggleOptionalAction)
-            .with(input().isHidden().withName("optional").withValue(isOptional ? "false" : "true"))
-            .with(optionalButton);
+    Tag optionalToggle;
+    if (config.hasPath("cf.optional_questions")) {
+      ContainerTag optionalButton =
+          TagCreator.button()
+              .withClasses(
+                  Styles.FLEX,
+                  Styles.GAP_2,
+                  Styles.ITEMS_CENTER,
+                  isOptional ? Styles.TEXT_BLACK : Styles.TEXT_GRAY_400,
+                  Styles.FONT_MEDIUM,
+                  Styles.BG_TRANSPARENT,
+                  Styles.ROUNDED_FULL,
+                  StyleUtils.hover(Styles.BG_GRAY_400, Styles.TEXT_GRAY_300))
+              .withType("submit")
+              .with(p("optional").withClasses("hover-group:text-white"))
+              .with(
+                  div()
+                      .withClasses(Styles.RELATIVE)
+                      .with(
+                          div()
+                              .withClasses(
+                                  isOptional ? Styles.BG_BLUE_600 : Styles.BG_GRAY_600,
+                                  Styles.W_14,
+                                  Styles.H_8,
+                                  Styles.ROUNDED_FULL))
+                      .with(
+                          div()
+                              .withClasses(
+                                  Styles.ABSOLUTE,
+                                  Styles.BG_WHITE,
+                                  isOptional ? Styles.RIGHT_1 : Styles.LEFT_1,
+                                  Styles.TOP_1,
+                                  Styles.W_6,
+                                  Styles.H_6,
+                                  Styles.ROUNDED_FULL)));
+      String toggleOptionalAction =
+          controllers.admin.routes.AdminProgramBlockQuestionsController.setOptional(
+                  programDefinitionId, blockDefinitionId, questionDefinition.getId())
+              .url();
+      optionalToggle =
+          form(csrfTag)
+              .withMethod(HttpVerbs.POST)
+              .withAction(toggleOptionalAction)
+              .with(
+                  input().isHidden().withName("optional").withValue(isOptional ? "false" : "true"))
+              .with(optionalButton);
+    } else {
+      optionalToggle = div();
+    }
 
     Tag removeButton =
         TagCreator.button(text("DELETE"))
