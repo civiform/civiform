068a6cb43f8e7f85dd5862f17e3dbdde9f9f6b1a
Caroline Danzi <cdanzi@google.com>
2021-06-24 09:39:58 -0700

    Add setup for every type test

--- browser-test/src/admin_predicates.test.ts
+++ browser-test/src/admin_predicates.test.ts
@@ -22,5 +22,63 @@ describe('create and edit predicates', () => {
     const adminPredicates = new AdminPredicates(page);
     await adminPredicates.addPredicate('predicate-q', 'hidden if', 'text', 'is equal to', 'hide me');
     await adminPredicates.expectVisibilityConditionEquals('Block 2 is hidden if predicate-q\'s text is equal to "hide me"');
-  })
+  });
+
+  it('every right hand type evaluates correctly', async () => {
+  	const { browser, page } = await startSession();
+
+  	await loginAsAdmin(page);
+  	const adminQuestions = new AdminQuestions();
+  	const adminPrograms = new AdminPrograms();
+
+  	// DATE, STRING, LONG, LIST_OF_STRINGS, LIST_OF_LONGS
+    await adminQuestions.addNameQuestion('string');
+    await adminQuestions.addTextQuestion('list of strings');
+    await adminQuestions.addNumberQuestion('long');
+    await adminQuestions.addNumberQuestion('list of longs');
+    await adminQuestions.addDateQuestion('date');
+    await adminQuestions.addCheckboxQuestion('both sides are lists', ['a', 'b', 'c']);
+    await adminQuestions.addTextQuestion('depends on previous');
+
+    const programName = 'test all predicate types';
+    await adminPrograms.addProgram(programName);
+    await adminPrograms.editProgramBlock(programName, 'string', ['string']);
+    await adminPrograms.addProgramBlock(programName, 'list of strings', ['list of strings']);
+    await adminPrograms.addProgramBlock(programName, 'long', ['long']);
+    await adminPrograms.addProgramBlock(programName, 'list of longs', ['list of longs']);
+    await adminPrograms.addProgramBlock(programName, 'date', ['date']);
+    await adminPrograms.addProgramBlock(programName, 'two lists', ['both sides are lists']);
+    await adminPrograms.addProgramBlock(programName, 'last', ['depends on previous']);
+
+    // Simple string predicate
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 2');
+    const adminPredicates = new AdminPredicates(page);
+    await adminPredicates.addPredicate('string', 'shown if', 'first name', 'is not equal to', 'hidden');
+
+    // Single string one of a list of strings
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 3');
+    const adminPredicates = new AdminPredicates(page);
+    await adminPredicates.addPredicate('list of strings', 'shown if', 'text', 'is one of', 'blue,green');
+
+    // Simple long predicate
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 4');
+    const adminPredicates = new AdminPredicates(page);
+    await adminPredicates.addPredicate('long', 'shown if', 'number', 'is equal to', '42');
+
+    // Long is one of a list of longs
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 5');
+    const adminPredicates = new AdminPredicates(page);
+    await adminPredicates.addPredicate('list of longs', 'shown if', 'number', 'is not one of', '100,200');
+
+    // Date predicate
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 6');
+    const adminPredicates = new AdminPredicates(page);
+    await adminPredicates.addPredicate('date', 'shown if', 'date', 'is earlier than', '2021-04-31');
+
+    // Lists of strings on both sides
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 7');
+    const adminPredicates = new AdminPredicates(page);
+    await adminPredicates.addPredicate('both sides are lists', 'shown if', 'selections', 'contains any of', 'a,c');
+
+  });
 })
--- browser-test/src/support/admin_predicates.ts
+++ browser-test/src/support/admin_predicates.ts
@@ -12,6 +12,8 @@ export class AdminPredicates {
     await this.page.selectOption('.cf-predicate-action select', { label: action });
     await this.page.selectOption('.cf-scalar-select select', { label: scalar });
     await this.page.selectOption('.cf-operator-select select', { label: operator });
+
+    // TODO: determine whether the value is an input or select
     await this.page.fill('.cf-predicate-value-input input', value);
     await this.page.click('text=Submit');
   }
