3f89d01b8b31d6d93f0066bdede67283782f6cb8
Kendrick <kentangsrice@gmail.com>
2021-06-25 12:26:37 -0700

    Add comment to Block and BlockDefinition classes

--- universal-application-tool-0.0.1/app/services/applicant/Block.java
+++ universal-application-tool-0.0.1/app/services/applicant/Block.java
@@ -16,7 +16,12 @@ import services.program.BlockDefinition;
 import services.program.predicate.PredicateDefinition;
 import services.question.types.ScalarType;
 
-/** Represents a block in the context of a specific user's application. */
+/**
+ * Represents a block in the context of a specific user's application.
+ *
+ * <p>"Block" is synonymous to "screen", which is what we show in the admin UI. At some point, it
+ * would be nice to rename the classes and methods to reflect the more desired name "screen".
+ */
 public final class Block {
 
   /**
