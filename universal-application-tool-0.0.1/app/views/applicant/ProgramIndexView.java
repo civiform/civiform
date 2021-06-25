cb90411ba88a0daa8cb5052338c46ecb7889e975
Nathan Henderson <nmckinley@google.com>
2021-06-25 14:45:12 -0700

    screen readers should read full name of program.

--- universal-application-tool-0.0.1/app/views/applicant/ProgramIndexView.java
+++ universal-application-tool-0.0.1/app/views/applicant/ProgramIndexView.java
@@ -229,7 +229,12 @@ public class ProgramIndexView extends BaseHtmlView {
             .setStyles(Styles.BLOCK, Styles.TEXT_XS, Styles.UNDERLINE)
             .setText(messages.at(MessageKey.LINK_PROGRAM_DETAILS.getKeyName()))
             .setHref(infoUrl)
-            .asAnchorText();
+            .asAnchorText()
+            .attr(
+                "aria-label",
+                messages.at(
+                    MessageKey.LINK_PROGRAM_DETAILS_SR.getKeyName(),
+                    program.localizedName().getOrDefault(preferredLocale)));
     programData.with(infoLink);
 
     // Add external link if it is set.
@@ -250,6 +255,11 @@ public class ProgramIndexView extends BaseHtmlView {
             .url();
     ContainerTag applyButton =
         a().attr(HREF, applyUrl)
+            .attr(
+                "aria-label",
+                messages.at(
+                    MessageKey.BUTTON_APPLY_SR.getKeyName(),
+                    program.localizedName().getOrDefault(preferredLocale)))
             .withText(
                 isDraft
                     ? messages.at(MessageKey.BUTTON_CONTINUE.getKeyName())
