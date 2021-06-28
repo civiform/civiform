53332f46ad940966f77c1404c05688f934d67648
Caroline Danzi <cdanzi@google.com>
2021-06-28 10:12:26 -0700

    Fix browser test to use screen instead of block and log in as program admin

--- browser-test/src/admin_predicates.test.ts
+++ browser-test/src/admin_predicates.test.ts
@@ -1,4 +1,4 @@
-import { AdminPredicates, AdminPrograms, AdminQuestions, ApplicantQuestions, endSession, loginAsAdmin, loginAsTestUser, logout, selectApplicantLanguage, startSession, userDisplayName } from './support'
+import { AdminPredicates, AdminPrograms, AdminQuestions, ApplicantQuestions, endSession, loginAsAdmin, loginAsProgramAdmin, loginAsTestUser, logout, selectApplicantLanguage, startSession, userDisplayName } from './support'
 
 describe('create and edit predicates', () => {
   it('add a hide predicate', async () => {
@@ -8,7 +8,7 @@ describe('create and edit predicates', () => {
     const adminQuestions = new AdminQuestions(page);
     const adminPrograms = new AdminPrograms(page);
 
-    // Add a program with two blocks
+    // Add a program with two screens
     await adminQuestions.addTextQuestion('hide-predicate-q');
     await adminQuestions.addTextQuestion('hide-other-q', 'desc', 'conditional question');
 
@@ -33,19 +33,19 @@ describe('create and edit predicates', () => {
     const applicant = new ApplicantQuestions(page);
     await applicant.applyProgram(programName);
 
-    // Initially fill out the first block so that the next block will be shown
+    // Initially fill out the first screen so that the next screen will be shown
     await applicant.answerTextQuestion('show me');
     await applicant.clickNext();
 
-    // Fill out the second block
+    // Fill out the second screen
     await applicant.answerTextQuestion('will be hidden and not submitted');
     await applicant.clickNext();
 
-    // We should be on the review page, with an answer to Block 2's question
+    // We should be on the review page, with an answer to Screen 2's question
     expect(await page.innerText('#application-summary')).toContain('conditional question');
 
-    // Return to the first block and answer it so that the second block is hidden
-    page.click('text=Edit'); // first block edit
+    // Return to the first screen and answer it so that the second screen is hidden
+    page.click('text=Edit'); // first screen edit
     await applicant.answerTextQuestion('hide me');
     await applicant.clickNext();
 
@@ -55,10 +55,10 @@ describe('create and edit predicates', () => {
 
     // Visit the program admin page and assert the hidden question does not show
     await logout(page);
-    await loginAsAdmin(page);
+    await loginAsProgramAdmin(page);
     await adminPrograms.viewApplications(programName);
     await adminPrograms.viewApplicationForApplicant(userDisplayName());
-    expect(await page.innerText('#application-view')).not.toContain('Block 2');
+    expect(await page.innerText('#application-view')).not.toContain('Screen 2');
 
     await endSession(browser);
   });
@@ -70,7 +70,7 @@ describe('create and edit predicates', () => {
     const adminQuestions = new AdminQuestions(page);
     const adminPrograms = new AdminPrograms(page);
 
-    // Add a program with two blocks
+    // Add a program with two screens
     await adminQuestions.addTextQuestion('show-predicate-q');
     await adminQuestions.addTextQuestion('show-other-q', 'desc', 'conditional question');
 
@@ -79,7 +79,7 @@ describe('create and edit predicates', () => {
     await adminPrograms.editProgramBlock(programName, 'first screen', ['show-predicate-q']);
     await adminPrograms.addProgramBlock(programName, 'screen with predicate', ['show-other-q']);
 
-    // Edit predicate for second block
+    // Edit predicate for second screen
     await adminPrograms.goToEditBlockPredicatePage(programName, 'Screen 2');
     const adminPredicates = new AdminPredicates(page);
     await adminPredicates.addPredicate('show-predicate-q', 'shown if', 'text', 'is equal to', 'show me');
@@ -95,21 +95,21 @@ describe('create and edit predicates', () => {
     const applicant = new ApplicantQuestions(page);
     await applicant.applyProgram(programName);
 
-    // Initially fill out the first block so that the next block will be hidden
-    await applicant.answerTextQuestion('hide next block');
+    // Initially fill out the first screen so that the next screen will be hidden
+    await applicant.answerTextQuestion('hide next screen');
     await applicant.clickNext();
 
-    // We should be on the review page, with no Block 2 questions shown. We should
+    // We should be on the review page, with no Screen 2 questions shown. We should
     // be able to submit the application
     expect(await page.innerText('#application-summary')).not.toContain('conditional question');
     expect((await page.innerText('.cf-submit-button')).toLowerCase()).toContain('submit');
 
-    // Return to the first block and answer it so that the second block is shown
-    page.click('text=Edit'); // first block edit
+    // Return to the first screen and answer it so that the second screen is shown
+    page.click('text=Edit'); // first screen edit
     await applicant.answerTextQuestion('show me');
     await applicant.clickNext();
 
-    // The second block should now appear, and we must fill it out
+    // The second screen should now appear, and we must fill it out
     await applicant.answerTextQuestion('hello world!');
     await applicant.clickNext();
 
@@ -119,10 +119,10 @@ describe('create and edit predicates', () => {
 
     // Visit the program admin page and assert the hidden question does not show
     await logout(page);
-    await loginAsAdmin(page);
+    await loginAsProgramAdmin(page);
     await adminPrograms.viewApplications(programName);
     await adminPrograms.viewApplicationForApplicant(userDisplayName());
-    expect(await page.innerText('#application-view')).toContain('Block 2');
+    expect(await page.innerText('#application-view')).toContain('Screen 2');
 
     await endSession(browser);
   })
