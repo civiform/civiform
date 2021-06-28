990b56515609b175dbfb40915838b986c541044c
Caroline Danzi <cdanzi@google.com>
2021-06-28 11:19:32 -0700

    Update test to reflect block->screen and checkbox

--- browser-test/src/admin_predicates.test.ts
+++ browser-test/src/admin_predicates.test.ts
@@ -49,37 +49,37 @@ describe('create and edit predicates', () => {
     await adminPrograms.addProgramBlock(programName, 'last', ['depends on previous']);
 
     // Simple string predicate
-    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 2');
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Screen 2');
     const adminPredicates = new AdminPredicates(page);
     await adminPredicates.addPredicate('single-string', 'shown if', 'first name', 'is not equal to', 'hidden');
 
-    // Single string one of a list of strings
-    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 3');
+    // Single string one of a list of strings (as an input)
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Screen 3');
     await adminPredicates.addPredicate('list of strings', 'shown if', 'text', 'is one of', 'blue, green');
 
     // Simple long predicate
-    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 4');
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Screen 4');
     await adminPredicates.addPredicate('single-long', 'shown if', 'number', 'is equal to', '42');
 
     // Date predicate
-    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 5');
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Screen 5');
     await adminPredicates.addPredicate('date', 'shown if', 'date', 'is earlier than', '2021-01-01');
 
-    // Lists of strings on both sides
-    await adminPrograms.goToEditBlockPredicatePage(programName, 'Block 6');
-    await adminPredicates.addPredicate('both sides are lists', 'shown if', 'selections', 'contains any of', 'a, c');
+    // Lists of strings on both sides (multi-option question checkbox)
+    await adminPrograms.goToEditBlockPredicatePage(programName, 'Screen 6');
+    await adminPredicates.addPredicate('both sides are lists', 'shown if', 'selections', 'contains any of', 'a,c');
 
     await adminPrograms.publishProgram(programName);
 
     // Switch to applicant view - if they answer each question according to the predicate,
-    // the next block will be shown.
+    // the next screen will be shown.
     await logout(page);
     await loginAsTestUser(page);
     await selectApplicantLanguage(page, 'English');
     const applicant = new ApplicantQuestions(page);
     await applicant.applyProgram(programName);
 
-    await applicant.answerNameQuestion('show', 'next', 'block');
+    await applicant.answerNameQuestion('show', 'next', 'screen');
     await applicant.clickNext();
     await applicant.answerTextQuestion('blue');
     await applicant.clickNext();
@@ -93,4 +93,5 @@ describe('create and edit predicates', () => {
     // We should now be on the summary page
     await applicant.submitFromReviewPage(programName);
     await endSession(browser);
+  });
 })
