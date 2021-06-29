a64a5593c093851dc5f42add76f32fb7e3de10e9
Kendrick <kentangsrice@gmail.com>
2021-06-29 16:05:06 -0700

    wip

--- universal-application-tool-0.0.1/app/assets/javascripts/modal.ts
+++ universal-application-tool-0.0.1/app/assets/javascripts/modal.ts
@@ -10,16 +10,20 @@ class ModalController {
     const modals = Array.from(document.querySelectorAll('.cf-modal'));
     modals.forEach(modal => {
       const modalButton = document.querySelector(`#${modal.id}-button`);
-      modalButton.addEventListener("click", function() {
-        modalContainer.classList.toggle('hidden');
-        modal.classList.toggle('hidden');
-      });
+      if (modalButton) {
+        modalButton.addEventListener("click", function() {
+          modalContainer.classList.toggle('hidden');
+          modal.classList.toggle('hidden');
+        });
+      }
 
       const modalClose = document.querySelector(`#${modal.id}-close`);
-      modalClose.addEventListener("click", function() {
-        modalContainer.classList.toggle('hidden');
-        modal.classList.toggle('hidden');
-      });
+      if (modalClose) {
+        modalClose.addEventListener("click", function() {
+          modalContainer.classList.toggle('hidden');
+          modal.classList.toggle('hidden');
+        });
+      }
     });
   }
 
