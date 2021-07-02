1f758916907f2c94bb62216b227a0c6266669ac9
Hung-Ming Hsu <hmhsu@users.noreply.github.com>
2021-07-02 12:14:21 -0700

    Update universal-application-tool-0.0.1/app/services/question/exceptions/InvalidUpdateException.java
    Co-authored-by: Caroline Danzi <cdanzi@google.com>

--- universal-application-tool-0.0.1/app/services/question/exceptions/InvalidUpdateException.java
+++ universal-application-tool-0.0.1/app/services/question/exceptions/InvalidUpdateException.java
@@ -1,6 +1,6 @@
 package services.question.exceptions;
 
-/** InvalidUpdateException is thown when update to a question configuration cannot be processed. */
+/** InvalidUpdateException is thrown when update to a question configuration cannot be processed. */
 public class InvalidUpdateException extends Exception {
   public InvalidUpdateException(String reason) {
     super("Question update failed: " + reason);
