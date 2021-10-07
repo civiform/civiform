937b58cbbae072b06f2aec6c2f4d3436ae5c9d80
Bion Johnson <bionj@google.com>
2021-10-07 09:35:23 -0700

    Add test assertions for logged in user

--- browser-test/src/program_deep_link.test.ts
+++ browser-test/src/program_deep_link.test.ts
@@ -1,10 +1,11 @@
-import { gotoEndpoint, startSession, loginAsAdmin, AdminQuestions, AdminPrograms, endSession, logout, selectApplicantLanguage, loginAsGuest, waitForPageJsLoad } from './support'
+import { gotoEndpoint, startSession, loginAsAdmin, AdminQuestions, AdminPrograms, endSession, logout, selectApplicantLanguage, loginAsGuest, loginAsTestUser, waitForPageJsLoad } from './support'
 
 describe('navigating to a deep link', () => {
   it('as a guest user', async () => {
     const { browser, page } = await startSession()
     page.setDefaultTimeout(5000);
 
+    // Arrange
     await loginAsAdmin(page);
     const adminQuestions = new AdminQuestions(page);
     const adminPrograms = new AdminPrograms(page);
@@ -27,10 +28,24 @@ describe('navigating to a deep link', () => {
 
     await logout(page);
 
+    // Exercise guest path
+    // Act
     await gotoEndpoint(page, '/programs/test-deep-link');
     await loginAsGuest(page);
     await selectApplicantLanguage(page, 'English');
 
+    // Assert
+    expect(await page.innerText('.cf-applicant-question-text')).toEqual(questionText);
+
+    await logout(page);
+
+    // Exercise test user path
+    // Act
+    await gotoEndpoint(page, '/programs/test-deep-link');
+    await loginAsTestUser(page);
+    await selectApplicantLanguage(page, 'English');
+
+    // Assert
     expect(await page.innerText('.cf-applicant-question-text')).toEqual(questionText);
 
     await endSession(browser);
