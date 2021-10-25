11ac5b9fbde107609d42a81a790991166388f0f9
shanemc <90928876+shanemc-goog@users.noreply.github.com>
2021-10-25 13:05:43 -0400

    fix comment formatting

--- browser-test/src/application_optional_questions.test.ts
+++ browser-test/src/application_optional_questions.test.ts
@@ -43,8 +43,7 @@ describe('optional application flow', () => {
 
     // Skip first block without uploading a file
     await applicantQuestions.clickSkip();
-    // Skip blocks 2-12 without an
-    // swering any questions
+    // Skip blocks 2-12 without answering any questions
     for (let i = 2; i <= 12; i++) {
       await applicantQuestions.clickNext();
     }
