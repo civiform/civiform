beafe380e758f776c68127313fa5e46c72a1029d
Bion Johnson <bionj@google.com>
2021-11-29 07:52:16 -0800

    Ok everything starts now

--- cloud/azure/main.tf
+++ cloud/azure/main.tf
@@ -39,6 +39,15 @@ resource "azurerm_subnet" "server_subnet" {
   virtual_network_name = azurerm_virtual_network.civiform_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Sql"]
+
+  delegation {
+    name = "acidelegationservice"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 
 resource "azurerm_postgresql_virtual_network_rule" "civiform" {
@@ -66,60 +75,60 @@ locals {
   redirect_configuration_name    = "${azurerm_virtual_network.civiform_vnet.name}-rdrcfg"
 }
 
-resource "azurerm_application_gateway" "network" {
-  name                = "civiform-appgateway"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+# resource "azurerm_application_gateway" "network" {
+#   name                = "civiform-appgateway"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
 
-  sku {
-    name     = "Standard_Small"
-    tier     = "Standard"
-    capacity = 2
-  }
+#   sku {
+#     name     = "Standard_Small"
+#     tier     = "Standard"
+#     capacity = 2
+#   }
 
-  gateway_ip_configuration {
-    name      = "civiform-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.server_subnet.id
-  }
+#   gateway_ip_configuration {
+#     name      = "civiform-gateway-ip-configuration"
+#     subnet_id = azurerm_subnet.server_subnet.id
+#   }
 
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
+#   frontend_port {
+#     name = local.frontend_port_name
+#     port = 80
+#   }
 
-  frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.server_container_group_ip.id
-  }
+#   frontend_ip_configuration {
+#     name                 = local.frontend_ip_configuration_name
+#     public_ip_address_id = azurerm_public_ip.server_container_group_ip.id
+#   }
 
-  backend_address_pool {
-    name = local.backend_address_pool_name
-  }
+#   backend_address_pool {
+#     name = local.backend_address_pool_name
+#   }
 
-  backend_http_settings {
-    name                  = local.http_setting_name
-    cookie_based_affinity = "Disabled"
-    path                  = "/path1/"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
+#   backend_http_settings {
+#     name                  = local.http_setting_name
+#     cookie_based_affinity = "Disabled"
+#     path                  = "/path1/"
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 60
+#   }
 
-  http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
-  }
+#   http_listener {
+#     name                           = local.listener_name
+#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
+#     frontend_port_name             = local.frontend_port_name
+#     protocol                       = "Http"
+#   }
 
-  request_routing_rule {
-    name                       = local.request_routing_rule_name
-    rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
-  }
-}
+#   request_routing_rule {
+#     name                       = local.request_routing_rule_name
+#     rule_type                  = "Basic"
+#     http_listener_name         = local.listener_name
+#     backend_address_pool_name  = local.backend_address_pool_name
+#     backend_http_settings_name = local.http_setting_name
+#   }
+# }
 
 resource "azurerm_network_profile" "civiform_network_profile" {
   name                = "civiform-network-profile"
@@ -130,8 +139,8 @@ resource "azurerm_network_profile" "civiform_network_profile" {
     name = "civiform-server-network-interface"
 
     ip_configuration {
-      name      = "civiform-application-gateway-network-interface-ip-configuration"
-      subnet_id = azurerm_subnet.application_gateway_subnet.id
+      name      = "civiform-network-interface-ip-configuration"
+      subnet_id = azurerm_subnet.server_subnet.id
     }
   }
 }
