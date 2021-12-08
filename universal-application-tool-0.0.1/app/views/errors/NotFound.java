3212fba4ac6b369dc9b946d83bf9aa30a66bb8b4
Sami Abdul-Wahid <syntapy@pm.me>
2021-12-07 17:07:57 -0800

    Center main content, match text with design

--- universal-application-tool-0.0.1/app/views/errors/NotFound.java
+++ universal-application-tool-0.0.1/app/views/errors/NotFound.java
@@ -2,7 +2,9 @@ package views.errors;
 
 import static j2html.TagCreator.h1;
 import static j2html.TagCreator.div;
+import static j2html.TagCreator.span;
 import static j2html.TagCreator.p;
+import static j2html.TagCreator.a;
 import static j2html.TagCreator.text;
 
 import com.google.inject.Inject;
@@ -14,6 +16,7 @@ import services.MessageKey;
 import play.i18n.Messages;
 import j2html.tags.ContainerTag;
 import play.mvc.Http;
+import views.style.ApplicantStyles;
 import views.BaseHtmlView;
 import views.BaseHtmlLayout;
 import views.HtmlBundle;
@@ -39,13 +42,37 @@ public class NotFound extends BaseHtmlView {
   }
 
   private ContainerTag mainContent(Messages messages) {
+      String img_author_url = "https://unsplash.com/@lazycreekimages";
+      String img_url = "https://unsplash.com/photos/0W4XLGITrHg";
       return div(
                 div(
-                  h1(messages.at(MessageKey.ERROR_NOT_FOUND_TITLE.getKeyName())),
-                  p(messages.at(MessageKey.ERROR_NOT_FOUND_DESCRIPTION.getKeyName())),
-                  layout.viewUtils.makeLocalImageTag("404")
-                )
-              ).withClasses(Styles.MAX_W_SCREEN_SM, Styles.W_FULL, Styles.MX_AUTO);
+                  h1(messages.at(MessageKey.ERROR_NOT_FOUND_TITLE.getKeyName()))
+                    .withClasses(ApplicantStyles.H1_PROGRAM_APPLICATION, Styles.TEXT_CENTER),
+                  p(messages.at(MessageKey.ERROR_NOT_FOUND_DESCRIPTION_A.getKeyName())),
+                  span(
+                      span(messages.at(MessageKey.ERROR_NOT_FOUND_DESCRIPTION_B.getKeyName())),
+                      span(" "),
+                      a(messages.at(MessageKey.ERROR_NOT_FOUND_DESCRIPTION_C.getKeyName()))
+                        .withHref("/"),
+                      span(".")
+                    ),
+                  div(
+                    layout.viewUtils.makeLocalImageTag("404")
+                      .withClasses(Styles.M_AUTO),
+                    span(
+                        span(messages.at(MessageKey.ERROR_NOT_FOUND_IMG_CAPTION_A.getKeyName())),
+                        span(" "),
+                        a(messages.at(MessageKey.ERROR_NOT_FOUND_IMG_CAPTION_B.getKeyName()))
+                          .withHref(img_author_url),
+                        span(" "),
+                        span(messages.at(MessageKey.ERROR_NOT_FOUND_IMG_CAPTION_C.getKeyName())),
+                        span(" "),
+                        a(messages.at(MessageKey.ERROR_NOT_FOUND_IMG_CAPTION_D.getKeyName()))
+                          .withHref(img_url)
+                      )
+                    )
+                  )
+                ).withClasses(Styles.TEXT_CENTER, Styles.MAX_W_SCREEN_SM, Styles.W_FULL, Styles.MX_AUTO);
   }
 
   public Content render(
