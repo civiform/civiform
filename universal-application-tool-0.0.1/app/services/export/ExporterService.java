09d149e4771d56c78abf633f1c30df7d023fcc80
Shane McDaniel <shanemc@google.com>
2021-12-13 14:29:06 -0800

    Add issue to TODO

--- universal-application-tool-0.0.1/app/services/export/ExporterService.java
+++ universal-application-tool-0.0.1/app/services/export/ExporterService.java
@@ -150,9 +150,10 @@ public class ExporterService {
     try {
       OutputStream inMemoryBytes = new ByteArrayOutputStream();
       Writer writer = new OutputStreamWriter(inMemoryBytes, StandardCharsets.UTF_8);
-      // Cache Program data which doesn't change, so we only look it up once rather than on every exported row.
-      // TODO: Lookup all relevant programs in one request to reduce cost of N lookups.
-      // TODO: Consider Play's JavaCache over this caching.
+      // Cache Program data which doesn't change, so we only look it up once rather than on every
+      // exported row.
+      // TODO(#1750): Lookup all relevant programs in one request to reduce cost of N lookups.
+      // TODO(#1750): Consider Play's JavaCache over this caching.
       HashMap<Long, ProgramDefinition> programDefinitions = new HashMap<>();
       for (Application application : applications) {
         Long programId = application.getProgram().id;
