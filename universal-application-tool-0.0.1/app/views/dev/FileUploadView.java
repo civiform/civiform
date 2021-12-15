327d17f23e59f794d71cfcb2303b836b0fe3530e
Shubha Rajan <shubhadayini@google.com>
2021-12-14 21:29:10 -0800

    formatting fix

--- universal-application-tool-0.0.1/app/views/dev/FileUploadView.java
+++ universal-application-tool-0.0.1/app/views/dev/FileUploadView.java
@@ -33,7 +33,9 @@ import views.HtmlBundle;
 import views.ViewUtils;
 import views.style.Styles;
 
-/** Renders a page for a developer to test uploading files. */
+/**
+ * Renders a page for a developer to test uploading files.
+ */
 public class FileUploadView extends BaseHtmlView {
 
   private static final String AZURE_STORAGE_BLOB_CDN =
