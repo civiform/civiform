b7525663608c3292bfc813f913c566d046e9d604
Sami Abdul-Wahid <syntapy@pm.me>
2021-12-18 19:20:53 -0800

    Add basic error handler

---
+++ universal-application-tool-0.0.1/app/controllers/ErrorHandler.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import com.typesafe.config.Config;
+
+import play.*;
+import play.api.OptionalSourceMapper;
+import play.api.UsefulException;
+import play.api.routing.Router;
+import play.http.DefaultHttpErrorHandler;
+import play.mvc.Http.*;
+import play.mvc.*;
+
+import javax.inject.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+@Singleton
+public class ErrorHandler extends DefaultHttpErrorHandler {
+
+  @Inject
+  public ErrorHandler(
+      Config config,
+      Environment environment,
+      OptionalSourceMapper sourceMapper,
+      Provider<Router> routes) {
+    super(config, environment, sourceMapper, routes);
+  }
+
+  protected CompletionStage<Result> onNotFound(Http.RequestHeader request, String message) {
+    return CompletableFuture.completedFuture(Results.notFound("Page is not found"));
+  }
+}
