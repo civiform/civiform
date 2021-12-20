4d6cbddf4e5c91ea2d082173bbf561fce90f1432
Sami Abdul-Wahid <syntapy@pm.me>
2021-12-19 18:11:28 -0800

    Remove unused ErrorController + commented out ErrorHandler

--- universal-application-tool-0.0.1/app/controllers/ErrorHandler.java
+++ universal-application-tool-0.0.1/app/controllers/ErrorHandler.java
@@ -1,37 +1,3 @@
-/*package controllers;
-
-import com.typesafe.config.Config;
-
-import play.*;
-import play.api.OptionalSourceMapper;
-import play.api.UsefulException;
-import play.api.routing.Router;
-import play.http.DefaultHttpErrorHandler;
-import play.mvc.Http.*;
-import play.mvc.*;
-import play.mvc.Results;
-
-import javax.inject.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-
-@Singleton
-public class ErrorHandler extends DefaultHttpErrorHandler {
-
-  @Inject
-  public ErrorHandler(
-      Config config,
-      Environment environment,
-      OptionalSourceMapper sourceMapper,
-      Provider<Router> routes) {
-    super(config, environment, sourceMapper, routes);
-  }
-
-  protected CompletionStage<Result> onNotFound(Http.RequestHeader request, String message) {
-    return Results.redirect(routes.ErrorController.onNotFound().url());
-  }
-}*/
-
 package controllers;
 
 import static com.google.common.base.Preconditions.checkNotNull;
