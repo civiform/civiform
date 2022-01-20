a9be4228630cb17c055b400f20f828751ffb62b6
Kunwar Toor <kunwarsingh1092@gmail.com>
2022-01-20 15:25:06 -0800

    fix nit

--- cloud/gcp/variables.tf
+++ cloud/gcp/variables.tf
@@ -1,6 +1,6 @@
 variable "terraform_service_account" {
   type        = string
-  description = "Service Account to use for running terroform on GCP"
+  description = "Service Account to use for running terraform on GCP"
 }
 
 variable "region" {
