ca4e5fbfc62a6a65e6913cfa7d62306519d3813d
Bion Johnson <bionj@google.com>
2022-01-25 15:43:03 -0800

    Wait for page js load

--- browser-test/src/dev_file_upload.test.ts
+++ browser-test/src/dev_file_upload.test.ts
@@ -19,6 +19,7 @@ describe('the dev file upload page', () => {
     })
 
     await page.click('button:visible')
+    await waitForPageJsLoad(page)
 
     expect(await page.textContent('h1')).toContain('Dev file upload')
     expect(await page.content()).toContain('dev/file.txt')
