2831bbf0e3b72f0216b7ce4229ae7ce5cada8c66
Susan Goldblatt <sgoldblatt@google.com>
2022-01-24 16:20:02 -0800

    fixing for the formatting

--- universal-application-tool-0.0.1/app/controllers/dev/FileUploadController.java
+++ universal-application-tool-0.0.1/app/controllers/dev/FileUploadController.java
@@ -50,7 +50,7 @@ public class FileUploadController extends DevController {
   }
 
   public Result index(Request request) {
-    if (!isDevEnvironment() || !isStaging() ) {
+    if (!isDevEnvironment() || !isStaging()) {
       return notFound();
     }
 
