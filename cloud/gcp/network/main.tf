f01d7e2f93ebcdbcd37874c16bb5c499b56ff41e
Kunwar Toor <kunwarsingh1092@gmail.com>
2022-01-26 14:07:08 -0800

    removed subnets as they might not be required

--- cloud/gcp/network/main.tf
+++ cloud/gcp/network/main.tf
@@ -4,33 +4,3 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "private_subnet_nat_1" {
-  name          = "private-subnet-nat-1"
-  ip_cidr_range = "10.192.20.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc_network.id
-  private_ip_google_access = true
-}
-
-resource "google_compute_subnetwork" "private_subnet_nat_2" {
-  name          = "private-subnet-nat-2"
-  ip_cidr_range = "10.192.21.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc_network.id
-  private_ip_google_access = true
-}
-
-resource "google_compute_subnetwork" "public_subnet_vm_1" {
-  name          = "public-subnet-vm-1"
-  ip_cidr_range = "10.192.10.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc_network.id
-}
-
-resource "google_compute_subnetwork" "public_subnet_vm_2" {
-  name          = "public-subnet-vm-2"
-  ip_cidr_range = "10.192.11.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc_network.id
-}
-
