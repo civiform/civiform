dd79ffee00ce04e1eeb80aafd82a855fd04a1b4c
Bion Johnson <bion@users.noreply.github.com>
2022-01-27 14:01:07 -0800

    Refactor shell scripts (#1844)
    * Refactor shell scripts
    
    * Address PR comments
    
    * Few more pieces
    
    * Another couple things!
    
    * Couple more brackets
    
    Co-authored-by: bion <bion@feta.Home>

--- browser-test/bin/wait_for_server_start_and_run_tests.sh
+++ browser-test/bin/wait_for_server_start_and_run_tests.sh
@@ -2,20 +2,20 @@
 
 set -euo pipefail
 
-start_time=$(date +%s)
-deadline=$(($start_time + 200))
-server_url=http://civiform:9000
+START_TIME=$(date +%s)
+DEADLINE=$(($START_TIME + 200))
+SERVER_URL="http://civiform:9000"
 
-echo polling to check server start
+echo "Polling to check server start"
 
-until $(curl --output /dev/null --silent --head --fail --max-time 2 $server_url); do
-    if (( $(date +%s) > $deadline )); then
-        echo "deadline exceeded waiting for server start"
-        exit 1
-    fi
+until $(curl --output /dev/null --silent --head --fail --max-time 2 "${SERVER_URL}"); do
+  if (( $(date +%s) > "${DEADLINE}" )); then
+    echo "Deadline exceeded waiting for server start"
+    exit 1
+  fi
 done
 
-echo detected server start
+echo "Detected server start"
 
 debug=0
 for arg do
@@ -26,7 +26,7 @@ for arg do
 done
 
 if (( $debug == 1 )); then
-    DEBUG=pw:api BASE_URL=$server_url yarn test $@
+    DEBUG="pw:api" BASE_URL="${SERVER_URL}" yarn test $@
 else
-    BASE_URL=$server_url yarn test $@
+    BASE_URL="${SERVER_URL}" yarn test $@
 fi
