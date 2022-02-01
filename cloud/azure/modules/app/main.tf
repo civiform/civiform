d117ef15d18a3dd8a5693330afeed4e5f44e92ae
Susan G <6268375+goldblatt@users.noreply.github.com>
2022-02-01 11:04:36 -0800

    Adding the Storage Account Contributor role to the terraform config (#1856)
    * Adding the Storage Account Contributor role to the terraform config-- needed for the CORS setting

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -275,3 +275,11 @@ resource "azurerm_role_assignment" "storage_blob_data_contributor" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_app_service.civiform_app.identity.0.principal_id
 }
+
+# Grant the app the role of storage account contributor, as the app needs 
+# to set its own CORs
+resource "azurerm_role_assignment" "storage_account_contributor" {
+  scope                = azurerm_storage_account.files_storage_account.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_app_service.civiform_app.identity.0.principal_id
+}
