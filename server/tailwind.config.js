6f647f8a9d706bb3601f40bdcdef75fb33455220
Sami Abdul-Wahid <syntapy@pm.me>
2022-02-07 10:59:56 -0800

    Refactor tailwind.config transfrom obj

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     enabled: true,
     content: [
       './app/assets/javascripts/*.ts',
+      //'./app/views/style/Styles.java',
       './app/views/style/Styles.java',
       './app/views/style/BaseStyles.java',
     ],
@@ -13,24 +14,25 @@ module.exports = {
     },
     transform: {
       java: (content) => {
-        var output = []
+        var output = [];
+        const prefixes = {
+          'even': 'EVEN',
+          'focus': 'FOCUS',
+          'focus-within': 'FOCUS_WITHIN',
+          'hover': 'HOVER',
+          'disabled': 'DISABLED',
+          'sm': 'RESPONSIVE_SM',
+          'md': 'RESPONSIVE_MD',
+          'lg': 'RESPONSIVE_LG',
+          'xl': 'RESPONSIVE_XL',
+          '2xl': 'RESPONSIVE_2XL'
+        };
         for (const match of content.matchAll(/"([\w-/:]+)"/g)) {
           output.push(match[1])
           // We don't know which, if any, of these prefixes are in use for any class in particular.
           // We therefore have to use every combination of them.
-          for (const prefix of [
-            'even',
-            'focus',
-            'focus-within',
-            'hover',
-            'disabled',
-            'sm',
-            'md',
-            'lg',
-            'xl',
-            '2xl',
-          ]) {
-            output.push(prefix + ':' + match[1])
+          for (const [key, value] of Object.entries(prefixes)) {
+            output.push(key + ':' + match[1])
           }
         }
         return output
