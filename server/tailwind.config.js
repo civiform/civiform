27a63d0e6479f0465fd5b9ba949e066bbd64a2c1
Sami Abdul-Wahid <syntapy@pm.me>
2022-02-08 11:48:09 -0800

    First stab reduces to 18k but styles are broken

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,11 +1,69 @@
+const fs = require('fs');
+
+class StylesJavaReader {
+  constructor(file_contents) {
+    this.file_contents = file_contents;
+    this.regexp = /public +static +final +String +([0-9A-Z_]+) += +"(.*)"/g;
+  }
+
+  getMatches(matches) {
+    for (const match in this.file_contents.matchAll(this.regexp)) {
+      matches[match[0]] = match[1];
+    }
+  }
+}
+
+class StylesTsReader {
+  constructor(file_contents) {
+    this.file_contents = file_contents;
+    this.dir = './app/assets/javascripts/';
+    //this.regexp = /classList\.{add|remove|toggle}\('(.*)'\)/g;
+    this.regexp_a = /"([\w-/:]+)"/g;
+    this.regexp_b = /'([\w-/:]+)'/g;
+  }
+
+  getMatches(matches) {
+    for (const match in this.file_contents.matchAll(this.regexp_a)) {
+      matches[match[0]] = match[1];
+    }
+    for (const match in this.file_contents.matchAll(this.regexp_b)) {
+      matches[match[0]] = match[1];
+    }
+  }
+}
+
+function getStyles() {
+  const matches = {};
+  try {
+
+    let data = fs.readFileSync('./app/views/style/Styles.java', 'utf8');
+    let stylesReader = new StylesJavaReader(data);
+    
+    stylesReader.getMatches(matches);
+
+    const assetsFolder = './app/assets/javascripts/';
+    let files = fs.readdirSync(assetsFolder);
+
+    for (const f of files) {
+      let data = fs.readFileSync(assetsFolder + f, 'utf8');
+      let tsReader = new StylesTsReader(data);
+      tsReader.getMatches(matches);
+    }
+  }
+
+  catch (error) {
+    throw 'error reading Styles.java for tailwindcss processing: ' + error.message;
+  }
+
+  return matches;
+}
+
 module.exports = {
   purge: {
     enabled: true,
     content: [
       './app/assets/javascripts/*.ts',
-      //'./app/views/style/Styles.java',
-      './app/views/style/Styles.java',
-      './app/views/style/BaseStyles.java',
+      './app/views/**/*.java',
     ],
     extract: {
       java: (content) => {
@@ -27,12 +85,17 @@ module.exports = {
           'xl': 'RESPONSIVE_XL',
           '2xl': 'RESPONSIVE_2XL'
         };
-        for (const match of content.matchAll(/"([\w-/:]+)"/g)) {
-          output.push(match[1])
-          // We don't know which, if any, of these prefixes are in use for any class in particular.
-          // We therefore have to use every combination of them.
-          for (const [key, value] of Object.entries(prefixes)) {
-            output.push(key + ':' + match[1])
+
+        const styleDict = getStyles();
+        for (const matches of content.matchAll(/Styles\.([A-Z_]+)/g)) {
+          for (const m of matches) {
+            let s = styleDict[m];
+            output.push(s)
+            // We don't know which, if any, of these prefixes are in use for any class in particular.
+            // We therefore have to use every combination of them.
+            for (const [key, value] of Object.entries(prefixes)) {
+              output.push(key + ':' + s)
+            }
           }
         }
         return output
@@ -61,3 +124,4 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/line-clamp')],
 }
+
