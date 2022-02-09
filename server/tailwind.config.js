8cdabe9555d7bac48a33b736d1aa7f64cabc414b
Sami Abdul-Wahid <syntapy@pm.me>
2022-02-09 15:03:58 -0800

    Add html tags manually to fix most final issues

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,4 +1,18 @@
 const fs = require('fs');
+const prefixes = {
+  'even': 'EVEN',
+  'focus': 'FOCUS',
+  'focus-within': 'FOCUS_WITHIN',
+  'hover': 'HOVER',
+  'disabled': 'DISABLED',
+  'sm': 'RESPONSIVE_SM',
+  'md': 'RESPONSIVE_MD',
+  'lg': 'RESPONSIVE_LG',
+  'xl': 'RESPONSIVE_XL',
+  '2xl': 'RESPONSIVE_2XL'
+};
+
+const htmlTags = fs.readFileSync('./tags.txt', 'utf8').split('\n');
 
 class StylesJavaReader {
   constructor(file_contents) {
@@ -31,14 +45,15 @@ class StylesJavaReader {
 
 function getStyles() {
   const matches = {};
+  let folder = './app/views/style/'
   try {
-    let specialFiles = ['Styles.java', 'BaseStyles.java', 'ReferenceClasses.java'];
-    let folder = './app/views/style/'
+    let specialFiles = ['Styles.java', 'BaseStyles.java'];
     for (file of specialFiles) {
       let data = fs.readFileSync(folder+file, 'utf8').split('\n');
       let stylesReader = new StylesJavaReader(data);
       stylesReader.getMatches(matches);
     }
+
   }
   catch (error) {
     throw 'error reading Styles.java for tailwindcss processing: ' + error.message;
@@ -49,6 +64,28 @@ function getStyles() {
 
 var PROCESSED_TS = false;
 const styleDict = getStyles();
+
+function original(content, output) {
+  for (const match of content.matchAll(/"([\w-/:]+)"/g)) {
+    output.push(match[1])
+    // We don't know which, if any, of these prefixes are in use for any class in particular.
+    // We therefore have to use every combination of them.
+    for (const prefix of [
+      'even',
+      'focus',
+      'focus-within',
+      'hover',
+      'disabled',
+      'sm',
+      'md',
+      'lg',
+      'xl',
+      '2xl',
+    ]) {
+      output.push(prefix + ':' + match[1])
+    }
+  }
+}
 
 module.exports = {
   purge: {
@@ -64,18 +101,6 @@ module.exports = {
     transform: {
       java: (content) => {
         var output = [];
-        const prefixes = {
-          'even': 'EVEN',
-          'focus': 'FOCUS',
-          'focus-within': 'FOCUS_WITHIN',
-          'hover': 'HOVER',
-          'disabled': 'DISABLED',
-          'sm': 'RESPONSIVE_SM',
-          'md': 'RESPONSIVE_MD',
-          'lg': 'RESPONSIVE_LG',
-          'xl': 'RESPONSIVE_XL',
-          '2xl': 'RESPONSIVE_2XL'
-        };
 
         var output = []
         let matchIter = content.match(/(?<=Styles\.)([0-9A-Z_]+)/g);
@@ -83,7 +108,6 @@ module.exports = {
         if (matchIter) {
           for (const m of matchIter) {
             let s = styleDict[m];
-            //console.log(m);
             
             if (s === undefined) {
               console.log(m);
@@ -109,26 +133,6 @@ module.exports = {
           }
         }
 
-        /*for (const match of content.matchAll(/"([\w-/:]+)"/g)) {
-          output.push(match[1])
-          // We don't know which, if any, of these prefixes are in use for any class in particular.
-          // We therefore have to use every combination of them.
-          for (const prefix of [
-            'even',
-            'focus',
-            'focus-within',
-            'hover',
-            'disabled',
-            'sm',
-            'md',
-            'lg',
-            'xl',
-            '2xl',
-          ]) {
-            output.push(prefix + ':' + match[1])
-          }
-        }*/
-
         const assetsFolder = './app/assets/javascripts/';
         let files = fs.readdirSync(assetsFolder);
 
@@ -139,6 +143,13 @@ module.exports = {
             for (m of matches) {
               output.push(m);
             }
+          }
+
+          output.push("button");
+
+          // Had to manually push all the html tags for some reason
+          for (const t of htmlTags) {
+            output.push(t);
           }
 
           PROCESSED_TS = true;
