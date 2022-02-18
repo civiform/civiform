c1453011f4eb55fe041b20e001652271cb0eae52
bion <bionjohnson@gmail.com>
2022-02-17 17:54:47 -0800

    Test it out, fix it, add logging

--- cloud/azure/bin/lib/azure_log.sh
+++ cloud/azure/bin/lib/azure_log.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
+readonly AZURE_LOG_STORAGE_ACCOUNT_NAME="civiformdeploymentlog"
+readonly AZURE_LOG_CONTAINER_NAME="civiformdeploymentlogstoragecontainer"
 readonly AZURE_LOG_FILE_NAME="civiform_deployment_log.txt"
-readonly AZURE_STORAGE_CONTAINER_NAME="civiform_deployment_log_storage_container"
-readonly AZURE_LOG_STORAGE_ACCOUNT_NAME="civiform_deployment_log_storage"
 
 #######################################
 # Download the deploy log to a temporary file.
@@ -17,6 +17,7 @@ function azure_log::fetch_log_file() {
   export LOG_TEMPFILE="$(mktemp)"
 
   az storage blob download \
+    --auth-mode login \
     --account-name "${AZURE_LOG_STORAGE_ACCOUNT_NAME}" \
     --container-name "${AZURE_LOG_CONTAINER_NAME}" \
     --name "${AZURE_LOG_FILE_NAME}" \
@@ -32,13 +33,32 @@ function azure_log::fetch_log_file() {
 #   LOG_TEMPFILE
 #######################################
 function azure_log::upload_log_file() {
+  echo "Uploading deployment log file..."
   az storage blob upload \
+    --auth-mode login \
     --account-name "${AZURE_LOG_STORAGE_ACCOUNT_NAME}" \
     --container-name "${AZURE_LOG_CONTAINER_NAME}" \
     --name "${AZURE_LOG_FILE_NAME}" \
     --file "${LOG_TEMPFILE}"
+  echo "Done uploading deployment log file."
 
   rm "${LOG_TEMPFILE}"
+}
+
+#######################################
+# Returns a successful status code if the deployment logfile already exists.
+# Globals read:
+#   AZURE_LOG_STORAGE_ACCOUNT_NAME
+#   AZURE_LOG_CONTAINER_NAME
+#   AZURE_LOG_FILE_NAME
+#######################################
+function azure_log::log_file_exists() {
+  az storage blob exists \
+    --auth-mode login \
+    --account-name "${AZURE_LOG_STORAGE_ACCOUNT_NAME}" \
+    --container-name "${AZURE_LOG_CONTAINER_NAME}" \
+    --name "${AZURE_LOG_FILE_NAME}" \
+    | grep -q true
 }
 
 #######################################
@@ -52,26 +72,46 @@ function azure_log::upload_log_file() {
 #   AZURE_SUBSCRIPTION
 #######################################
 function azure_log::initialize_log_file() {
+  echo "Start azure_log::initialize_log_file"
+
+  echo "Creating deploy log storage account..."
   az storage account create \
     --name "${AZURE_LOG_STORAGE_ACCOUNT_NAME}" \
     --resource-group "${AZURE_RESOURCE_GROUP}" \
     --location "${AZURE_LOCATION}" \
     --sku Standard_ZRS \
     --encryption-services blob
+  echo "Done creating deploy log storage account."
 
+  echo "Creating deploy log storage container..."
+  az storage container create \
+    --auth-mode login \
+    --account-name "${AZURE_LOG_STORAGE_ACCOUNT_NAME}" \
+    --name "${AZURE_LOG_CONTAINER_NAME}"
+  echo "Done creating deploy log storage container."
+
+  echo "Granting current user access deploy log storage account..."
   az ad signed-in-user show --query objectId -o tsv \
     | az role assignment create \
       --role "Storage Blob Data Contributor" \
       --assignee @- \
       --scope "/subscriptions/${AZURE_SUBSCRIPTION}/resourceGroups/${AZURE_RESOURCE_GROUP}/providers/Microsoft.Storage/storageAccounts/${AZURE_LOG_STORAGE_ACCOUNT_NAME}"
+  echo "Done granting current user access deploy log storage account."
 
-  az storage container create \
-    --account-name "${AZURE_LOG_STORAGE_ACCOUNT_NAME}" \
-    --name "${AZURE_STORAGE_CONTAINER_NAME}"
+  # If the logfile already exists, we fetch it and append the initialized
+  # event into the log. Otherwise we create a new file and upload it.
+  if azure_log::log_file_exists; then
+    echo "Existing logfile found, updating..."
+    azure_log::fetch_log_file
+  else
+    echo "No existing logfile found, creating a new one..."
+    export LOG_TEMPFILE="$(mktemp)"
+  fi
 
-  export LOG_TEMPFILE="$(mktemp)"
   log::initialized "$(azure_log::get_current_user_id)"
   azure_log::upload_log_file
+
+  echo "Done azure_log::initialize_log_file"
 }
 
 #######################################
@@ -79,5 +119,5 @@ function azure_log::initialize_log_file() {
 # by underscores for suitability as a token in the deploy log.
 #######################################
 function azure_log::get_current_user_id() {
-  az ad signed-in-user show | sed -E 's/ +/_/g'
+  az ad signed-in-user show --query mail | sed -E 's/ +/_/g'
 }
