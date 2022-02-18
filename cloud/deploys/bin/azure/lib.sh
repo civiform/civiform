c36c25874a1cb3a46ff89f1819265b390a8d187b
Shubha Rajan <shubhadayini@google.com>
2022-02-17 16:30:20 -0800

    Create setup script for Azure Key Vault (#1909)
    * create bash script and functions for setting up Azure Key Vault
    
    * fix some errors and make script executable
    
    * limit characters in password generation
    
    * update README for key vault setup
    
    * Update cloud/deploys/bin/azure/README.md
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault-setup
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault-setup
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * Update cloud/deploys/bin/azure/key-vault.sh
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>
    
    * address review comments
    
    * address review comments
    
    * address review comments
    
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>

---
+++ cloud/deploys/bin/azure/lib.sh
@@ -0,0 +1,65 @@
+#! /usr/bin/env bash
+
+# CHARSET is a regex pattern that matches the acceptable characters to 
+# use when generating a secret value
+readonly CHARSET='A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~'
+
+#######################################
+# Create resource group
+# Arguments:
+#   1: The resource group name 
+#   2: The location of the resource group
+#######################################
+function azure::create_resource_group() {
+  if [ $(az group exists --name "${1}") = false ]; then
+    az group create --name "${1}" --location "${2}"
+  fi
+}
+
+#######################################
+# Create key vault
+# Arguments:
+#   1: The resource group name for the key vault
+#   2: The region (e.g. EastUS) to create the key vault in
+#   3: The name of the key vault 
+#######################################
+function azure::create_vault() {
+  az keyvault create \
+    --name "${3}" \
+    --resource-group "${1}"\
+    --location "${2}" \
+    --enable-rbac-authorization
+}
+
+#######################################
+# Add a secret to the key vault
+# Arguments:
+#   1: The name of the key vault
+#   2: The name of the secret (used to identify it e.g. "postgres-password")
+#   3: The value of the secret
+#######################################
+function azure::add_secret() {
+  az keyvault secret set \
+    --vault-name "${1}" \
+    --name "${2}" \
+    --value "${3}"
+}
+
+#######################################
+# For each argument after the first, generates a secret value and adds it to the
+# key vault, using the argument as the secret name.
+# Arguments:
+#   1: The name of the key vault
+#   2..n: Names of the secrets to be created (e.g. "postgres-password")
+#######################################
+function azure::add_generated_secrets() {
+  local VAULT_NAME="${1}"
+  shift;
+  for key in "$@";
+  do
+    echo "Generating secret: ${key}"
+    local SECRET_VALUE="$(head /dev/urandom | tr -dc "${charset}" | cut -c -40)"
+    echo "Setting secret: ${key}"
+    azure::add_secret "${VAULT_NAME}" "${key}" "${SECRET_VALUE}"
+  done
+}
