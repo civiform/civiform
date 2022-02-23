ab016c4eddb8b41b003a2ff12dfc2fcf6c36d8ed
Susan Goldblatt <sgoldblatt@google.com>
2022-02-23 14:28:51 -0800

    readme + sourcedazurelib change

--- cloud/azure/bin/lib.sh
+++ cloud/azure/bin/lib.sh
@@ -2,10 +2,10 @@
 
 readonly LIB_DIR="${BASH_SOURCE%/*}/lib"
 
-if [[ "${SOURCED_LIB}" != "true" ]]; then
+if [[ "${SOURCED_AZURE_LIB}" != "true" ]]; then
   source "${LIB_DIR}/azure_log.sh"
   source "${LIB_DIR}/bastion.sh"
   source "${LIB_DIR}/out.sh"
 
-  SOURCED_LIB="true"
+  SOURCED_AZURE_LIB="true"
 fi
