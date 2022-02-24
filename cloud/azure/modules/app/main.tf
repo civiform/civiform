4b441b148cf3a331ed80bb281ea4d446861cd62d
Susan Goldblatt <sgoldblatt@google.com>
2022-02-24 12:21:47 -0800

    awskeys start

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -47,6 +47,11 @@ data "azurerm_key_vault_secret" "postgres_password" {
   key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
 }
 
+data "azurerm_key_vault_secret" "aws_secret_access_token" {
+  name         = local.aws_secret_access_token
+  key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
+}
+
 data "azurerm_key_vault_secret" "app_secret_key" {
   name         = local.app_secret_key_keyvault_id
   key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
