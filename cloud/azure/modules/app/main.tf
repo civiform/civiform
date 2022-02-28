605734ac5f47f0f0f11980f7d97eb44ba852262a
Susan Goldblatt <sgoldblatt@google.com>
2022-02-28 09:39:37 -0800

    removing some more unrelated code

--- cloud/azure/modules/app/main.tf
+++ cloud/azure/modules/app/main.tf
@@ -156,15 +156,6 @@ resource "azurerm_app_service" "civiform_app" {
   identity {
     type = "SystemAssigned"
   }
-
-  logs {
-    http_logs {
-      file_system {
-        retention_in_days = 1
-        retention_in_mb   = 35
-      }
-    }
-  }
 }
 
 resource "azurerm_app_service_virtual_network_swift_connection" "appservice_vnet_connection" {
