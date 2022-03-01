3a5848b4187128bf3a78b57d3cadee3a0334f9c2
Susan Goldblatt <sgoldblatt@google.com>
2022-03-01 13:11:21 -0800

    updating the key vault script to also add the right access

--- cloud/deploys/bin/aws/lib.sh
+++ cloud/deploys/bin/aws/lib.sh
@@ -1,14 +1,12 @@
 #! /usr/bin/env bash
 
-ACCES_KEY_ID_PATTERN="\"AccessKeyId\"\: \"(\S+)\""
-SECRET_ACCESS_KEY_PATTERN="\"SecretAccessKey\"\: \"(\S+)\""
-
 #######################################
 # Creates the access key via aws
 # Arguments:
+#   1. username of the aws user to create access key under
 #######################################
 function aws::create_access_key() {
-  aws iam create-access-key
+  aws iam create-access-key --user-name "${1}"
 }
 
 #######################################
@@ -17,8 +15,9 @@ function aws::create_access_key() {
 #   1. the result json of the create_access_key call
 #######################################
 function aws::parse_access_key_id() {
+  ACCES_KEY_ID_PATTERN="\"AccessKeyId\"\: \"([^\"]+)\""
   if [[ "${1}" =~ $ACCES_KEY_ID_PATTERN ]]; then
-    ${BASH_REMATCH[1]}
+    echo "${BASH_REMATCH[1]}"
   else
     echo "no access key id found"
   fi
@@ -30,8 +29,9 @@ function aws::parse_access_key_id() {
 #   1. the result json of the create_access_key call
 #######################################
 function aws::parse_access_secret_key() {
+  SECRET_ACCESS_KEY_PATTERN="\"SecretAccessKey\"\: \"([^\"]+)\""
   if [[ "${1}" =~ $SECRET_ACCESS_KEY_PATTERN ]]; then
-    ${BASH_REMATCH[1]}
+    echo "${BASH_REMATCH[1]}"
   else
     echo "no secret access key found"
   fi
