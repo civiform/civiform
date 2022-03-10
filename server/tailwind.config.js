aab44025353d8e2265dc9c28decc7925fd00394b
Sami Abdul-Wahid <syntapy@pm.me>
2022-03-10 10:00:19 -0800

    WIP refactor

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,78 +1,49 @@
 const fs = require('fs');
-const PREFIXES = {
-  'even':'even',
-  'focus':'focus',
-  'focusWithin':'focus-within',
-  'hover':'hover',
-  'disabled':'disabled',
-  'resonsiveSmall':'sm',
-  'responsiveMedium':'md',
-  'responsiveLarge':'lg',
-  'responsiveXLarge':'xl',
-  'responsive2XLarge':'2xl'
-};
 
-// Used in the main tailwind's method of the transform obj to ensure we only
-// read certain files (e.g. typescript) only once, since those have their contents read directly
+// For stylesDict from Styles.java and ReferenceClasses.java
+const RGX_KEY = /(?<= +public +static +final +String +)([0-9A-Z_]+)/g;
+const RGX_VAL = /(?<= +public +static +final +String +[0-9A-Z_]+ += +")([a-z0-9-/]+)/g;
+
+//const STYLE_RGX = /(?<=(Styles|ReferenceClasses)\.[0-9A-Z_]+)(/g
+const STYLE_RGX = /(?<=(Styles|ReferenceClasses)\.)([0-9A-Z_]+)/g
+
+const STR_LITERAL = /["'][\.a-z0-9/:-]+["']/g
+
+const PREFIXES = [ 'even', 'focus', 'focus-within', 'hover', 'disabled', 'sm', 'md', 'lg', 'xl', '2xl', ]
+
 let processedTs = false;
 
-// Used to get a dictionary mapping for all possible base styles (no prefixes) in
-// java files refered to in `function getStyles()`
-class StylesJavaReader {
+// Used to read Styles.java and ReferenceClasses.java to get dictionary mapping 
+// for all possible base styles (no prefixes)
+function addStyleDictMatches(matches, file_contents) {
+  let count = 1
+  for (const line of file_contents) {
+    let match_key = line.match(RGX_KEY);
+    let match_val = line.match(RGX_VAL);
 
-  constructor(file_contents) {
-    this.file_contents = file_contents;
-  }
-
-  // Styles.java variable as a key, e.g. Styles.BG_BLUE_200
-  static get rgxKey() {
-    return /(?<= +public +static +final +String +)([0-9A-Z_]+)/g;
-  }
-
-  // Tailwind string value refered to by variable, e.g. 'bg-blue-200'
-  static get rgxVal() {
-    return /(?<= +public +static +final +String +[0-9A-Z_]+ += +")([a-z0-9-/]+)/g;
-  }
-
-  getMatches(matches) {
-    let count = 1
-    for (const line of this.file_contents) {
-      let match_key = line.match(this.constructor.rgxKey);
-      let match_val = line.match(this.constructor.rgxVal);
-
-      // Both 'variable' and tailwind str are probably on same line
-      // Even though java probably doesn't require it
-      if (Array.isArray(match_key) && Array.isArray(match_val)) {
-        if (match_key.length === 1 && match_val.length === 1) {
-          matches[match_key[0]] = match_val[0];
-        } else {
-          throw "strange line in 'Styles.java' at line " + count.toString();
-        }
+    // Both 'variable' and tailwind str are probably on same line
+    // Even though java probably doesn't require it
+    if (Array.isArray(match_key) && Array.isArray(match_val)) {
+      if (match_key.length === 1 && match_val.length === 1) {
+        matches[match_key[0]] = match_val[0];
+      } else {
+        throw "strange line in 'Styles.java' at line " + count.toString();
       }
-
-      count++;
     }
+
+    count++;
   }
 }
 
-class StylesPrefixReader {
-  constructor(file_contents, stylesDict) {
-    this.file_contents = file_contents;
-    this.stylesDict = stylesDict;
-  }
-}
-
-function getStyles() {
+function getStylesDict() {
   const matches = {};
   let folder = './app/views/style/'
   try {
     let specialFiles = ['Styles.java', 'ReferenceClasses.java'];
     for (file of specialFiles) {
       let data = fs.readFileSync(folder+file, 'utf8').split('\n');
-      let stylesReader = new StylesJavaReader(data);
-      stylesReader.getMatches(matches);
+      addStyleDictMatches(matches, data);
     }
-
   }
   catch (error) {
     throw 'error reading Styles.java for tailwindcss processing: ' + error.message;
@@ -81,27 +52,50 @@ function getStyles() {
   return matches;
 }
 
-const styleDict = getStyles();
+const styleDict = getStylesDict();
 
-function original(content, output) {
-  for (const match of content.matchAll(/"([\w-/:]+)"/g)) {
-    output.push(match[1])
-    // We don't know which, if any, of these prefixes are in use for any class in particular.
-    // We therefore have to use every combination of them.
-    for (const prefix of [
-      'even',
-      'focus',
-      'focus-within',
-      'hover',
-      'disabled',
-      'sm',
-      'md',
-      'lg',
-      'xl',
-      '2xl',
-    ]) {
-      output.push(prefix + ':' + match[1])
+function getStyles(output, code) {
+
+  let matchIter = code.match(STYLE_RGX)
+
+  if (matchIter) {
+    for (const tailwindClassId of matchIter) {
+      let tailwindClass = styleDict[tailwindClassId]
+      
+      if (tailwindClass !== undefined) {
+        output.push(tailwindClass)
+        // We don't know which, if any, of these prefixes are in use for any class in particular.
+        // We therefore have to use every combination of them.
+        for (const prefix of PREFIXES) {
+          output.push(prefix + ':' + tailwindClass)
+        }
+      }
     }
+  }
+}
+
+function processTypescript(output) {
+  const assetsFolder = './app/assets/javascripts/';
+  let files = fs.readdirSync(assetsFolder);
+
+  // Just get every string literal
+  for (const f of files) {
+    let data = fs.readFileSync(assetsFolder + f, 'utf8').split('\n');
+    for (const line of data) {
+      matches = line.matchAll(STR_LITERAL);
+      for (m of matches) {
+        let mr = m[0].replace(/['"]+/g, '');
+        output.push(mr);
+      }
+    }
+  }
+}
+
+function processHtmlTags(output) {
+  // Its faster and easier to just manually push all html tags instead of parsing them in code
+  const htmlTags = fs.readFileSync('./htmltags.txt', 'utf8').split('\n');
+  for (const t of htmlTags) {
+    output.push(t);
   }
 }
 
@@ -118,57 +112,13 @@ module.exports = {
     },
     transform: {
       java: (content) => {
-        var output = [];
+        const output = [];
 
-        let matchIter = content.match(/(?<=Styles\.)([0-9A-Z_]+)/g);
-
-        if (matchIter) {
-          for (const tailwindClassId of matchIter) {
-            let tailwindClass = styleDict[tailwindClassId];
-            
-            if (tailwindClass !== undefined) {
-              output.push(tailwindClass)
-              // We don't know which, if any, of these prefixes are in use for any class in particular.
-              // We therefore have to use every combination of them.
-              for (const prefix of [
-                        'even',
-                        'focus',
-                        'focus-within',
-                        'hover',
-                        'disabled',
-                        'sm',
-                        'md',
-                        'lg',
-                        'xl',
-                        '2xl',
-                      ]) {
-                output.push(prefix + ':' + tailwindClass)
-              }
-            }
-          }
-        }
-
-        const assetsFolder = './app/assets/javascripts/';
-        let files = fs.readdirSync(assetsFolder);
+        getStyles(output, content)
 
         if (processedTs === false) {
-          for (const f of files) {
-            let data = fs.readFileSync(assetsFolder + f, 'utf8').split('\n');
-            for (const line of data) {
-              matches = line.matchAll(/["'][\.a-z0-9/:-]+["']/g);
-              for (m of matches) {
-                let mr = m[0].replace(/['"]+/g, '');
-                output.push(mr);
-              }
-            }
-          }
-
-          // Had to manually push all the html tags for some reason
-          const htmlTags = fs.readFileSync('./tags.txt', 'utf8').split('\n');
-          for (const t of htmlTags) {
-            output.push(t);
-          }
-
+          processTypescript(output)
+          processHtmlTags(output)
           processedTs = true;
         }
 
@@ -199,4 +149,3 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/line-clamp')],
 }
-
