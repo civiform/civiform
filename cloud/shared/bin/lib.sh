58f43696a9c566da95475e7b4d6eba9b3264f416
Zara Louis <zaralouis@google.com>
2022-03-28 11:33:26 -0700

    Attempt to fix deploy script

--- cloud/shared/bin/lib.sh
+++ cloud/shared/bin/lib.sh
@@ -6,9 +6,8 @@ set -o pipefail
 export TF_VAR_FILENAME="setup.auto.tfvars"
 export BACKEND_VARS_FILENAME="backend_vars"
 
-readonly CLOUD_LIB_DIR="${BASH_SOURCE%/*}/lib"
-
 if [[ "${SOURCED_CLOUD_LIB}" != "true" ]]; then
+  readonly CLOUD_LIB_DIR="${BASH_SOURCE%/*}/lib"
   source "bin/lib/out.sh"
   source "${CLOUD_LIB_DIR}/health.sh"
   source "${CLOUD_LIB_DIR}/log.sh"
