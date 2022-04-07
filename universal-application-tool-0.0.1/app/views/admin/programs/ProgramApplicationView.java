9b8874260810aef459fd01032c161f14d7930d3e
swatkat1 <swathi.murthi@seattle.com>
2022-04-07 10:23:44 -0700

    changing the Qtest to grey per Elise's feedback

--- universal-application-tool-0.0.1/app/views/admin/programs/ProgramApplicationView.java
+++ universal-application-tool-0.0.1/app/views/admin/programs/ProgramApplicationView.java
@@ -118,8 +118,6 @@ public final class ProgramApplicationView extends BaseHtmlView {
   private Tag renderAnswer(long programId, AnswerData answerData) {
     LocalDate date =
         Instant.ofEpochMilli(answerData.timestamp()).atZone(ZoneId.systemDefault()).toLocalDate();
-    String questionIdentifier =
-        String.format("Question ID: %d", answerData.questionDefinition().getId());
     Tag answerContent;
     if (answerData.fileKey().isPresent()) {
       String encodedFileKey = URLEncoder.encode(answerData.fileKey().get(), StandardCharsets.UTF_8);
@@ -135,11 +133,8 @@ public final class ProgramApplicationView extends BaseHtmlView {
             div()
                 .withClasses(Styles.MB_8)
                 .with(
-                    div(questionIdentifier)
-                        .withClasses(Styles.HIDDEN))
-                .with(
                     div(answerData.questionDefinition().getName())
-                        .withClasses(Styles.TEXT_GRAY_900, Styles.TEXT_BASE, Styles.LINE_CLAMP_3)))
+                        .withClasses(Styles.TEXT_GRAY_400, Styles.TEXT_BASE, Styles.LINE_CLAMP_3)))
         .with(p().withClasses(Styles.W_8))
         .with(
             answerContent.withClasses(Styles.TEXT_GRAY_700, Styles.TEXT_BASE, Styles.LINE_CLAMP_3))
