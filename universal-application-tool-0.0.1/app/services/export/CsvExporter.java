f78986be818c209829c0a6904e14883ef60446d2
bion <bionj@google.com>
2022-04-13 17:25:38 -0700

    Reduce N+1 queries for CSV export

--- universal-application-tool-0.0.1/app/services/export/CsvExporter.java
+++ universal-application-tool-0.0.1/app/services/export/CsvExporter.java
@@ -10,11 +10,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import models.Application;
-import models.Program;
 import models.TrustedIntermediaryGroup;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import repository.ProgramRepository;
 import services.Path;
 import services.applicant.ReadOnlyApplicantProgramService;
 import services.program.Column;
@@ -33,21 +31,17 @@ public class CsvExporter {
   private boolean wroteHeaders;
   private ImmutableList<Column> columns;
   private Optional<String> secret;
-  private Optional<ProgramRepository> programRepository;
 
   public CsvExporter(List<Column> columns) {
     this.wroteHeaders = false;
     this.columns = ImmutableList.copyOf(columns);
     this.secret = Optional.empty();
-    this.programRepository = Optional.empty();
   }
 
   /** Provide a secret if you will need to use OPAQUE_ID type columns. */
-  public CsvExporter(
-      ImmutableList<Column> columns, String secret, ProgramRepository programRepository) {
+  public CsvExporter(ImmutableList<Column> columns, String secret) {
     this(columns);
     this.secret = Optional.of(secret);
-    this.programRepository = Optional.of(programRepository);
   }
 
   private void writeHeadersOnFirstExport(CSVPrinter printer) throws IOException {
@@ -74,7 +68,7 @@ public class CsvExporter {
       throws IOException {
     CSVPrinter printer = new CSVPrinter(writer, DEFAULT_CSV_FORMAT);
 
-    this.writeHeadersOnFirstExport(printer);
+    writeHeadersOnFirstExport(printer);
 
     ImmutableMap<Path, String> answerMap =
         roApplicantService.getSummaryData().stream()
@@ -117,27 +111,7 @@ public class CsvExporter {
           printer.print(application.getSubmitterEmail().orElse("Applicant"));
           break;
         case PROGRAM:
-          Program program = application.getProgram();
-          if (programRepository.isEmpty()) {
-            throw new RuntimeException(
-                "No program repository provided, but program details requested.");
-          }
-          // This is a strange workaround for a bug in ebean.  For some reason, the program that
-          // is returned from the application crashes ebean's server when we attempt to access
-          // anything
-          // other than the id.  This is hard to debug since ebean doesn't write code, it writes
-          // bytecode,
-          // directly.  This workaround costs 1 extremely cheap query per application - bad, but
-          // probably not problematic until the size of the database gets huge.
-          printer.print(
-              programRepository
-                  .get()
-                  .lookupProgram(program.id)
-                  .toCompletableFuture()
-                  .join()
-                  .get()
-                  .getProgramDefinition()
-                  .adminName());
+          printer.print(application.getProgram().getProgramDefinition().adminName());
           break;
         case TI_ORGANIZATION:
           printer.print(
