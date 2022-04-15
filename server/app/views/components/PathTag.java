71f95c5969dd244f0652e3cd7a2b182547d1cb5c
Sami Abdul-Wahid <syntapy@pm.me>
2022-04-14 17:54:19 -0700

    add SVgTag and PathTag classes

---
+++ server/app/views/components/PathTag.java
@@ -0,0 +1,9 @@
+package views.components;
+
+import j2html.tags.ContainerTag;
+
+public final class PathTag extends ContainerTag<PathTag> {
+    public PathTag() {
+        super("svg");
+    }
+}
---
+++ server/app/views/components/SvgTag.java
@@ -0,0 +1,9 @@
+package views.components;
+
+import j2html.tags.ContainerTag;
+
+public final class SvgTag extends ContainerTag<SvgTag> {
+    public SvgTag() {
+        super("svg");
+    }
+}
