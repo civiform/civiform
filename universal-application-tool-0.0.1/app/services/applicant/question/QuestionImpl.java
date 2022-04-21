79d5bb0937b066cc0c0f9e3054d567069735fbe7
Brett Clouser <clouser@google.com>
2022-04-21 13:54:58 -0700

    Adding javadocs for several methods.

--- universal-application-tool-0.0.1/app/services/applicant/question/QuestionImpl.java
+++ universal-application-tool-0.0.1/app/services/applicant/question/QuestionImpl.java
@@ -4,11 +4,15 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableSet;
 import services.question.types.QuestionType;
 
+/**
+ * Abstract implementation for the {@link Question} interface. Subclasses are expected to implement
+ * the majority of the interface methods. Common logic is pulled up to this class for code sharing.
+ */
 abstract class QuestionImpl implements Question {
   protected final ApplicantQuestion applicantQuestion;
 
   public QuestionImpl(ApplicantQuestion applicantQuestion) {
-    this.applicantQuestion = applicantQuestion;
+    this.applicantQuestion = Preconditinos.checkNotNull(applicantQuestion);
     if (!validQuestionTypes().contains(applicantQuestion.getType())) {
       throw new RuntimeException(
           String.format(
@@ -19,8 +23,17 @@ abstract class QuestionImpl implements Question {
     }
   }
 
+  /**
+   * The set of acceptable question types for the {@link ApplicantQuestion} provided in the
+   * constructor. This is used for validation purposes.
+   */
   protected abstract ImmutableSet<QuestionType> validQuestionTypes();
 
+  /**
+   * A question is considered answered if the applicant data has been set for any of the paths
+   * associated with the question. If a question type contains no paths, then it will be considered
+   * as not answered.
+   */
   @Override
   public boolean isAnswered() {
     return getAllPaths().stream().anyMatch(p -> applicantQuestion.getApplicantData().hasPath(p));
