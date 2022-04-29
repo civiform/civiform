52a7e509160d699c8218154fd914597e64aba699
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-04-29 13:15:59 -0400

    fmt

--- server/test/controllers/admin/AdminProgramBlockQuestionsControllerTest.java
+++ server/test/controllers/admin/AdminProgramBlockQuestionsControllerTest.java
@@ -1,26 +1,21 @@
 package controllers.admin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static play.mvc.Http.Status.SEE_OTHER;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.stubMessagesApi;
+
 import com.google.common.collect.ImmutableMap;
 import java.util.Locale;
 import models.LifecycleStage;
 import models.Program;
 import org.junit.Before;
-import static play.test.Helpers.stubMessagesApi;
-import static play.test.Helpers.contentAsString;
-
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static play.api.test.CSRFTokenHelper.addCSRFToken;
-import static play.mvc.Http.Status.SEE_OTHER;
-import static play.test.Helpers.fakeRequest;
-
 import org.junit.Test;
 import play.mvc.Http.Request;
 import play.mvc.Result;
 import repository.ResetPostgres;
-//import repository.VersionRepository;
 import services.LocalizedStrings;
-//import services.question.QuestionService;
 import services.question.exceptions.UnsupportedQuestionTypeException;
 import services.question.types.QuestionDefinition;
 import services.question.types.QuestionDefinitionBuilder;
@@ -29,14 +24,10 @@ import support.ProgramBuilder;
 public class AdminProgramBlockQuestionsControllerTest extends ResetPostgres {
 
   private AdminProgramBlockQuestionsController controller;
-  //private QuestionService questionService;
-  //private VersionRepository versionRepository;
 
   @Before
   public void setUp() {
     controller = instanceOf(AdminProgramBlockQuestionsController.class);
-    //questionService = instanceOf(QuestionService.class);
-    //versionRepository = instanceOf(VersionRepository.class);
   }
 
   @Test
@@ -56,17 +47,16 @@ public class AdminProgramBlockQuestionsControllerTest extends ResetPostgres {
 
     // Execute.
     Request request =
-            fakeRequest(routes.AdminProgramBlockQuestionsController.create(program.id,1))
+        fakeRequest(routes.AdminProgramBlockQuestionsController.create(program.id, 1))
             .langCookie(Locale.forLanguageTag("es-US"), stubMessagesApi())
             .bodyForm(ImmutableMap.of("question-", activeId.toString()))
             .build();
-    Result result = controller.create(request, program.id,1);
+    Result result = controller.create(request, program.id, 1);
 
     // Verify.
     assertThat(result.status()).withFailMessage(contentAsString(result)).isEqualTo(SEE_OTHER);
     program.refresh();
     assertThat(program.getProgramDefinition().hasQuestion(toUpdate)).isTrue();
     assertThat(program.getProgramDefinition().hasQuestion(nameQuestion)).isFalse();
-
   }
 }
