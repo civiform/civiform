535d9c032cb2e3836eb629967d19a0cf196a3ca4
bion <bionjohnson@gmail.com>
2022-05-02 18:11:40 -0700

    Fix applicant's name on admin applications page

--- server/app/views/ApplicantUtils.java
+++ server/app/views/ApplicantUtils.java
@@ -1,10 +1,31 @@
 package views;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
 import java.util.Optional;
+import play.i18n.Lang;
 import play.i18n.Messages;
+import play.i18n.MessagesApi;
 import services.MessageKey;
 
 public final class ApplicantUtils {
+
+  private final MessagesApi messagesApi;
+
+  @Inject
+  public ApplicantUtils(MessagesApi messagesApi) {
+    this.messagesApi = checkNotNull(messagesApi);
+  }
+
+  /** Get the applicant's name or the GUEST message with en-US lang. */
+  public String getApplicantNameEnUs(Optional<String> maybeName) {
+    Messages messages = messagesApi.preferred(ImmutableList.of(Lang.forCode("en-US")));
+    return ApplicantUtils.getApplicantName(maybeName, messages);
+  }
+
+  /** Get the applicant's name or the GUEST message in the provided messages. */
   public static String getApplicantName(Optional<String> maybeName, Messages messages) {
     return maybeName.orElse(messages.at(MessageKey.GUEST.getKeyName()));
   }
