301c13cfc0416906e95363b8723566c270f39525
Leyla Azizova <azizoval@google.com>
2022-05-19 16:29:50 -0700

    remove records, don't seem to need it

--- cloud/aws/main.tf
+++ cloud/aws/main.tf
@@ -55,42 +55,9 @@ resource "aws_apprunner_service" "civiform_dev_leyla" {
   }                          
 }
 
-data "aws_route53_zone" "civiform" {
-  name = "staging-aws.civiform.dev"
-  private_zone = false
-}
-
 resource "aws_apprunner_custom_domain_association" "civiform_domain" {
   domain_name = "staging-aws.civiform.dev"
   service_arn = aws_apprunner_service.civiform_dev_leyla.arn
-}
-
-resource "aws_route53_record" "civiform_domain_record" {
-  name = "staging-aws.civiform.dev"
-  zone_id = data.aws_route53_zone.civiform.zone_id
-  type = "CNAME"
-  records = [aws_apprunner_custom_domain_association.civiform_domain.dns_target]
-  ttl = 60
-}
-
-# resource "aws_route53_record" "civiform_domain_validation" {
-#   for_each = {
-#     for dvo in aws_apprunner_custom_domain_association.civiform_domain.certificate_validation_records : dvo.name => {
-#       name = dvo.name
-#       type = dvo.type
-#       record = dvo.value
-#     }
-#   }
-  
-#   name = each.value.name
-#   zone_id = data.aws_route53_zone.civiform.zone_id
-#   type = each.value.type
-#   records = [each.value.record]
-#   ttl = 60
-# }
-
-output "custom_domain_records" {                            
-  value = aws_apprunner_custom_domain_association.civiform_domain.certificate_validation_records                      
 }
 
 data "aws_availability_zones" "available" {}
