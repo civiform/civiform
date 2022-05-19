021ea96866d4900b238534ad56f0fb54ddbba234
Sami Abdul-Wahid <syntapy@pm.me>
2022-05-18 20:29:15 -0700

    add missing methods in SelectWithLabel

--- server/app/views/components/SelectWithLabel.java
+++ server/app/views/components/SelectWithLabel.java
@@ -7,6 +7,7 @@ import com.google.common.collect.ImmutableMap;
 import j2html.TagCreator;
 import j2html.attributes.Attr;
 import j2html.tags.specialized.DivTag;
+import j2html.tags.specialized.LabelTag;
 import j2html.tags.specialized.OptionTag;
 import j2html.tags.specialized.SelectTag;
 
@@ -17,6 +18,7 @@ public class SelectWithLabel extends FieldWithLabel {
   private ImmutableList<OptionTag> customOptions = ImmutableList.of();
 
   public SelectWithLabel() {
+    super();
     this.setFieldType("text");
   }
 
@@ -90,6 +92,11 @@ public class SelectWithLabel extends FieldWithLabel {
   public DivTag getContainer() {
     SelectTag fieldTag = TagCreator.select();
     OptionTag placeholder = option(placeholderText).withValue("").attr(Attr.HIDDEN);
+    genRandIdIfEmpty();
+    applyAttributesFromList(fieldTag);
+
+    fieldTag.attr("value", this.fieldValue);
+
     if (this.fieldValue.isEmpty()) {
       placeholder.attr(Attr.SELECTED);
     }
@@ -109,6 +116,10 @@ public class SelectWithLabel extends FieldWithLabel {
           });
     }
 
-    return super.getContainer();
+    boolean hasFieldErrors = getHasFieldErrors();
+    generalApplyAttrsClasesToTag(fieldTag, hasFieldErrors);
+    LabelTag labelTag = genLabelTag();
+
+    return wrapInDivTag(fieldTag, labelTag);
   }
 }
