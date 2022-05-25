3cbd1b48c8c7f976d4d1e38c55d542521ba60414
Leyla Azizova <azizoval@google.com>
2022-05-25 14:56:30 -0700

    add comment

--- cloud/aws/autoscaling.tf
+++ cloud/aws/autoscaling.tf
@@ -1,4 +1,4 @@
-
+# TODO: determine reasonable max concurrency for a civiform server
 resource "aws_apprunner_auto_scaling_configuration_version" "auto-scaling-config" {
   auto_scaling_configuration_name = "civiform-config"
   max_concurrency                 = 100
