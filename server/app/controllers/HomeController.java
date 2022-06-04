e74c2999ad4fd7ecd6a56259bb103819d9af31d8
Sami Abdul-Wahid <syntapy@pm.me>
2022-06-03 17:52:50 -0700

    Revert "add flat earth exception to test stack traces"
    This reverts commit 3fc762ab824da8bc552909374c8dfa60cc6e9f5c.

--- server/app/controllers/HomeController.java
+++ server/app/controllers/HomeController.java
@@ -78,7 +78,7 @@ public class HomeController extends Controller {
   }
 
   public Result loginForm(Http.Request request, Optional<String> message)
-      throws TechnicalException, Exception {
+      throws TechnicalException {
     return ok(loginForm.render(request, messagesApi.preferred(request), message));
   }
 
