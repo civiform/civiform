67cc040854491a7823bce8d87867f275e82b41ef
Brett Clouser <clouser@google.com>
2022-06-06 12:49:32 -0700

    PR feedback.

--- localstack/localstack-docker-entrypoint.sh
+++ localstack/localstack-docker-entrypoint.sh
@@ -3,4 +3,6 @@
 # process to be the original entrypoint for localstack.
 nginx
 
+# Call the original localstack entrypoint:
+# https://github.com/localstack/localstack/blob/6a892aa43e41ff3df25631cbe8bbe0d8c75d9992/Dockerfile#L265
 exec docker-entrypoint.sh "$@"
