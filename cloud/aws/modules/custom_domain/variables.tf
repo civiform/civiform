1ba3a288593fb189b2e217a080b67e2b31ceb11a
azizova-leyla <azizovalster@gmail.com>
2022-06-06 12:37:03 -0700

    fmt

--- cloud/aws/modules/custom_domain/variables.tf
+++ cloud/aws/modules/custom_domain/variables.tf
@@ -1,13 +1,13 @@
 variable "custom_route_zone" {
-    type = string
-    description = "Route zone for Civiform (e.g. civiform.dev)"
-  
+  type        = string
+  description = "Route zone for Civiform (e.g. civiform.dev)"
+
 }
 variable "custom_subdomain" {
-    type = string
-    description = "Custom domain for Civiform (e.g. staging-aws)."
+  type        = string
+  description = "Custom domain for Civiform (e.g. staging-aws)."
 }
 variable "apprunner_arn" {
-    type = string
-    description = "ARN for apprunner instance"
+  type        = string
+  description = "ARN for apprunner instance"
 }
