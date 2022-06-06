73d88bac51c206b343f2d61bab28f995b89b4a87
azizova-leyla <azizovalster@gmail.com>
2022-06-06 16:35:52 -0700

    pull more values into variables

--- cloud/aws/templates/aws_oidc/autoscaling.tf
+++ cloud/aws/templates/aws_oidc/autoscaling.tf
@@ -1,7 +1,7 @@
 # TODO: determine reasonable max concurrency for a civiform server
 resource "aws_apprunner_auto_scaling_configuration_version" "auto_scaling_config" {
   auto_scaling_configuration_name = "civiform_config"
-  max_concurrency                 = 100
-  max_size                        = 10
-  min_size                        = 1
+  max_concurrency                 = var.auto_scaling_config["max_concurrency"]
+  max_size                        = var.auto_scaling_config["max_size"]
+  min_size                        = var.auto_scaling_config["min_size"]
 }
