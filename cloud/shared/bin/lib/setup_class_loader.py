11b0567c7e06eace241dd2372f7c4390322c7237
Kiana McNellis <kianam@google.com>
2022-06-10 19:07:25 -0500

    Run yapf

--- cloud/shared/bin/lib/setup_class_loader.py
+++ cloud/shared/bin/lib/setup_class_loader.py
@@ -1,11 +1,12 @@
 import importlib.util
 import os
-
 """
 load_class takes in a template dir and uses that to load the 
 setup module which knows how to setup the template
 """
-def load_class(template_dir): 
+
+
+def load_class(template_dir):
     spec = importlib.util.spec_from_file_location(
         "setup", f"{os.getcwd()}/{template_dir}/bin/setup.py")
     loaded_module = importlib.util.module_from_spec(spec)
