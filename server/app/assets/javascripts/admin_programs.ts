d1c81ec09d2de0755d486b07af4bcec8c36e2da1
Brett Clouser <clouser@google.com>
2022-06-14 16:25:37 -0700

    Convert "View" to "Share link" in the new program index UI (#2704)
    * Switching view -> copy link and copying the link to the clipboard.
    
    * PR feedback.
    
    * Change to share link and add hover text.
    
    * Removing hover text.

--- server/app/assets/javascripts/admin_programs.ts
+++ server/app/assets/javascripts/admin_programs.ts
@@ -5,6 +5,7 @@ class AdminPrograms {
     '.cf-admin-program-card-list-placeholder'
   private static NAME_ATTRIBUTE = 'data-name'
   private static LAST_UPDATED_MILLIS = 'data-last-updated-millis'
+  private static PROGRAM_LINK_ATTRIBUTE = 'data-copyable-program-link'
 
   constructor() {
     const cardsParent = document.querySelector(
@@ -26,13 +27,27 @@ class AdminPrograms {
         cardsPlaceholder.classList.add('hidden')
       }
     }
+
+    const withCopyableProgramLink = Array.from(
+      cardsParent.querySelectorAll(`[${AdminPrograms.PROGRAM_LINK_ATTRIBUTE}]`)
+    )
+    withCopyableProgramLink.forEach((el) => {
+      const programLink = el.getAttribute(AdminPrograms.PROGRAM_LINK_ATTRIBUTE)
+      if (!programLink) {
+        console.warn(
+          `Empty ${AdminPrograms.PROGRAM_LINK_ATTRIBUTE} for element`
+        )
+        return
+      }
+      el.addEventListener('click', () => {
+        AdminPrograms.copyProgramLinkToClipboard(programLink)
+      })
+    })
   }
 
   sortCards(cardsParent: HTMLElement) {
     const cards = Array.from(
-      cardsParent.querySelectorAll(
-        AdminPrograms.PROGRAM_CARDS_SELECTOR
-      ) as any as Array<HTMLElement>
+      cardsParent.querySelectorAll(AdminPrograms.PROGRAM_CARDS_SELECTOR)
     )
     cards.sort((first, second) => {
       const firstComparator = this.comparatorObject(first)
@@ -52,13 +67,54 @@ class AdminPrograms {
     })
   }
 
-  comparatorObject(el: HTMLElement) {
+  comparatorObject(el: Element) {
     const lastUpdatedMillisString =
       el.getAttribute(AdminPrograms.LAST_UPDATED_MILLIS) || ''
     const lastUpdatedMillis = Number(lastUpdatedMillisString)
     return {
       name: el.getAttribute(AdminPrograms.NAME_ATTRIBUTE) || '',
       lastUpdatedMillis,
+    }
+  }
+
+  /**
+   * Attempts to copy the given content to the clipboard.
+   * @param content
+   * @returns bool indicating whether the content was copied to the clipboard
+   */
+  static async tryCopyToClipboard(content: string): Promise<boolean> {
+    if (!window.navigator['clipboard']) {
+      return false
+    }
+    try {
+      await window.navigator['clipboard'].writeText(content)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  static async copyProgramLinkToClipboard(programLink: string) {
+    const succeeded = await AdminPrograms.tryCopyToClipboard(programLink)
+    const toastController = window['toastController'] as any
+    if (succeeded) {
+      toastController.showToastMessage({
+        id: `program-link-${Math.random()}`,
+        content: 'Program link copied to clipboard',
+        duration: 3000,
+        type: 'success',
+        canDismiss: true,
+        canIgnore: false,
+      })
+    } else {
+      toastController.showToastMessage({
+        id: `program-link-${Math.random()}`,
+        content: `Could not copy program link to clipboard: ${programLink}`,
+        duration: -1,
+        type: 'warning',
+        canDismiss: true,
+        canIgnore: false,
+      })
     }
   }
 }
