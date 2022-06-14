921b0c27252b39b200bec5f9b1529e4b53070a60
Susan Goldblatt <sgoldblatt@google.com>
2022-06-14 12:02:50 -0700

    fixing formatting

--- cloud/aws/templates/aws_oidc/bin/setup.py
+++ cloud/aws/templates/aws_oidc/bin/setup.py
@@ -11,35 +11,32 @@ class Setup(SetupTemplate):
         self._run_tf_to_setup()
         print(" - Setting up shared state file")
         self._setup_shared_state_file()
-    
+
     def _run_tf_to_setup(self):
         template_dir = os.path.join(self.config.get_template_dir(), 'setup')
         print(" - Run terraform init")
-        subprocess.check_call([
-            "terraform",
-            f"-chdir={template_dir}",
-            "init",
-            "-input=false",
-            "-upgrade",
-        ])
-        
+        subprocess.check_call(
+            [
+                "terraform",
+                f"-chdir={template_dir}",
+                "init",
+                "-input=false",
+                "-upgrade",
+            ])
+
         tf_apply_args = [
-            "terraform",
-            f"-chdir={template_dir}",
-            "apply",
-            "-input=false"
+            "terraform", f"-chdir={template_dir}", "apply", "-input=false"
         ]
-        
+
         if not self.config.is_dev():
             tf_apply_args.append("-auto-approve")
 
         print(" - Run terraform apply")
         subprocess.check_call(tf_apply_args)
 
-    
     def _setup_shared_state_file(self):
-        backend_file_location = os.path.join(self.config.get_template_dir(), 
-                                             self.config.backend_vars_filename)
+        backend_file_location = os.path.join(
+            self.config.get_template_dir(), self.config.backend_vars_filename)
         with open(backend_file_location, 'w') as f:
             f.write(f'bucket         = "civiform-tfstate-bucket"\n')
             f.write(f'key            = "tfstate/terraform.tfstate"\n')
