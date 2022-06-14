252b43fb019694bd2e8b9f3faf37c512f8333d35
Kiana McNellis <kianam@google.com>
2022-06-14 17:25:57 -0500

    format

--- server/app/filters/DisableCachingFilter.java
+++ server/app/filters/DisableCachingFilter.java
@@ -2,10 +2,7 @@ package filters;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
-import java.util.Optional;
 import java.util.concurrent.Executor;
 import javax.inject.Inject;
 import play.mvc.EssentialAction;
@@ -41,14 +38,15 @@ public class DisableCachingFilter extends EssentialFilter {
                       final Integer status = result.status();
                       final String path = request.uri().toLowerCase();
 
-                      if (!environment.isDev() && // Must revalidate status asset caches in dev mode
-                          ASSET_PATH_PREFIXES.stream().anyMatch(path::startsWith) &&
-                          OK_STATUS_CODES.contains(status)){
-                          // In prod/staging, static assets are fingerprinted,
-                          // so we can cache for a longer time.
-                          // Cache for 2 weeks.
-                          return result.withHeader(
-                              "Cache-Control", "public, max-age=1209600, immutable");
+                      if (environment.isDev()
+                          && // Must revalidate status asset caches in dev mode
+                          ASSET_PATH_PREFIXES.stream().anyMatch(path::startsWith)
+                          && OK_STATUS_CODES.contains(status)) {
+                        // In prod/staging, static assets are fingerprinted,
+                        // so we can cache for a longer time.
+                        // Cache for 2 weeks.
+                        return result.withHeader(
+                            "Cache-Control", "public, max-age=1209600, immutable");
                       }
                       // Don't cache anything else.
                       return result.withHeader(
