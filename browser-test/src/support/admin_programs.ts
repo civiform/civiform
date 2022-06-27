1d745b9eeba615acc1fc568b83add4333e1dc0bd
Brett Clouser <clouser@google.com>
2022-06-27 15:21:50 -0700

    Undoing attempted browser test fix and updates for the CSV changes.

--- browser-test/src/support/admin_programs.ts
+++ browser-test/src/support/admin_programs.ts
@@ -480,14 +480,10 @@ export class AdminPrograms {
   }
 
   async getDemographicsCsv() {
-    await clickAndWaitForModal(this.page, 'download-demographics-csv-modal')
     const [downloadEvent] = await Promise.all([
       this.page.waitForEvent('download'),
-      this.page.click(
-        '#download-demographics-csv-modal button:has-text("Download Exported Data (CSV)")',
-      ),
+      this.page.click('text="Download Exported Data (CSV)"'),
     ])
-    await this.page.click('#download-demographics-csv-modal-close')
     const path = await downloadEvent.path()
     if (path === null) {
       throw new Error('download failed')
