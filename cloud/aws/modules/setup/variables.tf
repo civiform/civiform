dd19fc5eaef0e5f4e31dfaa63e16b771b99cc1cd
Kiana McNellis <kianam@google.com>
2022-06-30 14:32:38 -0500

    Pipe civiform mode to terraform

--- cloud/aws/modules/setup/variables.tf
+++ cloud/aws/modules/setup/variables.tf
@@ -3,9 +3,13 @@ variable "app_prefix" {
   description = "A prefix to add to values so we can have multiple deploys in the same aws account"
 }
 
+variable "civiform_mode" {
+  type        = string
+  description = "The civiform environment mode (test/dev/staging/prod)"
+}
+
 variable "aws_region" {
   type        = string
   description = "Region where the AWS servers will live"
   default     = "us-east-1"
 }
-
--- cloud/aws/templates/aws_oidc/vpc.tf
+++ cloud/aws/templates/aws_oidc/vpc.tf
@@ -41,13 +41,6 @@ resource "aws_security_group" "rds" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 }
 
 
