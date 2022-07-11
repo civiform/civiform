3c618390cb0e7660f443af1655162a575a090b1f
Brett Clouser <clouser@google.com>
2022-07-11 10:37:27 -0700

    Add filtering for program admin CSV / JSON exports (#2826)
    * Removing placeholder text in search labels, clarifying search text, and removing unnecessarily shared code. Why remove the helper from the base class? It's inflexible if we want to add additional logic to the search form.
    
    * Rendering application submitted filter in the program admin view. Not wired up to logic yet.
    
    * Wiring date filtering to the returned applications view.
    
    * Moving download buttons to same row as 'filter' as secondary action.
    
    * bin/fmt
    
    * Adding download buttons to modal. Currently, they always set query params based on the current filters. Additionally, backend handlers are not yet set up.
    
    * Updating CSV / JSON export server-side handlers to process search / time query params and filter the results.
    
    * Adding radio select to modal to ignore filters or use them.
    
    * Adding FILTER_ALT icon for the filter button.
    
    * Reverting text until we change the actual underlying filter logic.
    
    * Making tests compile.
    
    * Updating placeholder text.
    
    * Fix other search text.
    
    * Style updates based on UX feedback.
    
    * Margin fixes.
    
    * Changing question name that is seeded. Partial application of PR #2842.
    
    * Browser test support and assertions for applying program CSV / JSON download with filters applied.
    
    * Moving helper back to the controller it's used in rather than generalizing it prematurely.
    
    * Removing CSRF tag since the original implementation didn't have it.
    
    * Fixing tests.
    
    * PR feedback.

--- server/app/views/admin/programs/ProgramApplicationListView.java
+++ server/app/views/admin/programs/ProgramApplicationListView.java
@@ -4,13 +4,21 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static j2html.TagCreator.br;
 import static j2html.TagCreator.div;
 import static j2html.TagCreator.each;
+import static j2html.TagCreator.fieldset;
+import static j2html.TagCreator.form;
 import static j2html.TagCreator.h1;
 import static j2html.TagCreator.iframe;
+import static j2html.TagCreator.input;
+import static j2html.TagCreator.legend;
 import static j2html.TagCreator.p;
 import static j2html.TagCreator.span;
 
+import com.google.auto.value.AutoValue;
 import com.google.inject.Inject;
 import controllers.admin.routes;
+import j2html.TagCreator;
+import j2html.attributes.Attr;
+import j2html.tags.ContainerTag;
 import j2html.tags.Tag;
 import java.util.Optional;
 import models.Application;
@@ -28,12 +36,22 @@ import views.HtmlBundle;
 import views.admin.AdminLayout;
 import views.admin.AdminLayout.NavPage;
 import views.admin.AdminLayoutFactory;
+import views.components.FieldWithLabel;
+import views.components.Icons;
 import views.components.LinkElement;
+import views.components.Modal;
+import views.style.AdminStyles;
 import views.style.ReferenceClasses;
+import views.style.StyleUtils;
 import views.style.Styles;
 
 /** Renders a page for viewing applications to a program. */
 public final class ProgramApplicationListView extends BaseHtmlView {
+  private static final String FROM_DATE_PARAM = "fromDate";
+  private static final String UNTIL_DATE_PARAM = "untilDate";
+  private static final String SEARCH_PARAM = "search";
+  private static final String IGNORE_FILTERS_PARAM = "ignoreFilters";
+
   private final AdminLayout layout;
   private final ApplicantUtils applicantUtils;
   private final DateConverter dateConverter;
@@ -54,10 +72,11 @@ public final class ProgramApplicationListView extends BaseHtmlView {
       ProgramDefinition program,
       PageNumberBasedPaginationSpec paginationSpec,
       PaginationResult<Application> paginatedApplications,
-      Optional<String> search) {
-    Tag contentDiv =
+      RenderFilterParams filterParams) {
+
+    Modal downloadModal = renderDownloadApplicationsModal(program, filterParams);
+    Tag applicationListDiv =
         div()
-            .withClasses(Styles.PX_20)
             .with(
                 h1(program.adminName()).withClasses(Styles.MY_4),
                 renderPaginationDiv(
@@ -65,26 +84,26 @@ public final class ProgramApplicationListView extends BaseHtmlView {
                         paginatedApplications.getNumPages(),
                         pageNumber ->
                             routes.AdminApplicationController.index(
-                                program.id(), search, Optional.of(pageNumber)))
+                                program.id(),
+                                filterParams.search(),
+                                Optional.of(pageNumber),
+                                filterParams.fromDate(),
+                                filterParams.untilDate()))
                     .withClasses(Styles.MB_2),
                 br(),
-                renderSearchForm(
-                        request,
-                        search,
-                        routes.AdminApplicationController.index(
-                            program.id(), Optional.empty(), Optional.empty()),
-                        Optional.of(Styles.W_FULL),
-                        Optional.of("Search by name, email, or application ID"))
-                    .withClasses(Styles.MT_6),
-                each(paginatedApplications.getPageContents(), this::renderApplicationListItem),
-                br(),
-                renderCsvDownloadButton(program.id()),
-                renderJsonDownloadButton(program.id()))
-            .withClasses(Styles.MB_16, Styles.MR_2);
+                renderSearchForm(program, downloadModal.getButton(), filterParams),
+                each(paginatedApplications.getPageContents(), this::renderApplicationListItem))
+            .withClasses(
+                Styles.MT_6,
+                StyleUtils.responsiveLarge(Styles.MT_12),
+                Styles.MB_16,
+                Styles.ML_6,
+                Styles.MR_2);
 
     Tag applicationShowDiv =
         div()
-            .withClasses(Styles.W_FULL, Styles.H_FULL)
+            .withClasses(
+                Styles.MT_6, StyleUtils.responsiveLarge(Styles.MT_12), Styles.W_FULL, Styles.H_FULL)
             .with(
                 iframe()
                     .withId("application-display-frame")
@@ -95,31 +114,135 @@ public final class ProgramApplicationListView extends BaseHtmlView {
             .getBundle()
             .setTitle(program.adminName() + " - Applications")
             .addFooterScripts(layout.viewUtils.makeLocalJsTag("admin_applications"))
+            .addModals(downloadModal)
             .addMainStyles(Styles.FLEX)
-            .addMainContent(contentDiv, applicationShowDiv);
+            .addMainContent(applicationListDiv, applicationShowDiv);
 
     return layout.renderCentered(htmlBundle);
   }
 
-  private Tag renderCsvDownloadButton(long programId) {
-    String link = controllers.admin.routes.AdminApplicationController.downloadAll(programId).url();
-    return new LinkElement()
-        .setId("download-all-button")
-        .setHref(link)
-        .setText("Download all versions (CSV)")
-        .setStyles(ReferenceClasses.DOWNLOAD_ALL_BUTTON)
-        .asButton();
+  private Tag renderSearchForm(
+      ProgramDefinition program, Tag downloadButton, RenderFilterParams filterParams) {
+    return form()
+        .withClasses(Styles.MT_6)
+        .withMethod("GET")
+        .withAction(
+            routes.AdminApplicationController.index(
+                    program.id(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty())
+                .url())
+        .with(
+            fieldset()
+                .withClasses(Styles.PT_1)
+                .with(
+                    legend("Application submitted").withClasses(Styles.ML_1, Styles.TEXT_GRAY_600),
+                    div()
+                        .withClasses(Styles.FLEX, Styles.SPACE_X_3)
+                        .with(
+                            FieldWithLabel.date()
+                                .setFieldName(FROM_DATE_PARAM)
+                                .setValue(filterParams.fromDate().orElse(""))
+                                .setLabelText("From:")
+                                .getContainer()
+                                .withClasses(Styles.FLEX),
+                            FieldWithLabel.date()
+                                .setFieldName(UNTIL_DATE_PARAM)
+                                .setValue(filterParams.untilDate().orElse(""))
+                                .setLabelText("Until:")
+                                .getContainer()
+                                .withClasses(Styles.FLEX))),
+            FieldWithLabel.input()
+                .setFieldName(SEARCH_PARAM)
+                .setValue(filterParams.search().orElse(""))
+                .setLabelText("Search by name, email, or application ID")
+                .getContainer()
+                .withClasses(Styles.W_FULL, Styles.MT_4),
+            div()
+                .withClasses(Styles.MT_6, Styles.MB_8, Styles.FLEX, Styles.SPACE_X_2)
+                .with(
+                    div().withClass(Styles.FLEX_GROW),
+                    downloadButton,
+                    makeSvgTextButton("Filter", Icons.FILTER_ALT)
+                        .withClass(AdminStyles.PRIMARY_BUTTON_STYLES)
+                        .withType("submit")));
   }
 
-  private Tag renderJsonDownloadButton(long programId) {
-    String link =
-        controllers.admin.routes.AdminApplicationController.downloadAllJson(programId).url();
-    return new LinkElement()
-        .setId("download-all-json-button")
-        .setHref(link)
-        .setText("Download all versions (JSON)")
-        .setStyles(ReferenceClasses.DOWNLOAD_ALL_BUTTON)
-        .asButton();
+  private Modal renderDownloadApplicationsModal(
+      ProgramDefinition program, RenderFilterParams filterParams) {
+    String modalId = "download-program-applications-modal";
+    ContainerTag modalContent =
+        div()
+            .withClasses(Styles.PX_8)
+            .with(
+                form()
+                    .withMethod("GET")
+                    .with(
+                        FieldWithLabel.radio()
+                            .setFieldName(IGNORE_FILTERS_PARAM)
+                            .setLabelText("Current results")
+                            .setChecked(true)
+                            .getContainer(),
+                        FieldWithLabel.radio()
+                            .setFieldName(IGNORE_FILTERS_PARAM)
+                            .setLabelText("All data")
+                            .setValue("1")
+                            .setChecked(false)
+                            .getContainer(),
+                        input()
+                            .withName(FROM_DATE_PARAM)
+                            .withValue(filterParams.fromDate().orElse(""))
+                            .withType("hidden"),
+                        input()
+                            .withName(UNTIL_DATE_PARAM)
+                            .withValue(filterParams.untilDate().orElse(""))
+                            .withType("hidden"),
+                        input()
+                            .withName(SEARCH_PARAM)
+                            .withValue(filterParams.search().orElse(""))
+                            .withType("hidden"),
+                        div()
+                            .withClasses(Styles.FLEX, Styles.MT_6, Styles.SPACE_X_2)
+                            .with(
+                                TagCreator.button("Download CSV")
+                                    .withClasses(
+                                        ReferenceClasses.DOWNLOAD_ALL_BUTTON,
+                                        AdminStyles.PRIMARY_BUTTON_STYLES)
+                                    .attr(
+                                        Attr.FORMACTION,
+                                        controllers.admin.routes.AdminApplicationController
+                                            .downloadAll(
+                                                program.id(),
+                                                Optional.empty(),
+                                                Optional.empty(),
+                                                Optional.empty(),
+                                                Optional.empty())
+                                            .url())
+                                    .withType("submit"),
+                                TagCreator.button("Download JSON")
+                                    .withClasses(
+                                        ReferenceClasses.DOWNLOAD_ALL_BUTTON,
+                                        AdminStyles.PRIMARY_BUTTON_STYLES)
+                                    .attr(
+                                        Attr.FORMACTION,
+                                        controllers.admin.routes.AdminApplicationController
+                                            .downloadAllJson(
+                                                program.id(),
+                                                Optional.empty(),
+                                                Optional.empty(),
+                                                Optional.empty(),
+                                                Optional.empty())
+                                            .url())
+                                    .withType("submit"))));
+    return Modal.builder(modalId, modalContent)
+        .setModalTitle("Download application data")
+        .setTriggerButtonContent(
+            makeSvgTextButton("Download", Icons.DOWNLOAD)
+                .withClass(AdminStyles.SECONDARY_BUTTON_STYLES)
+                .withType("button"))
+        .build();
   }
 
   private Tag renderApplicationListItem(Application application) {
@@ -152,7 +275,7 @@ public final class ProgramApplicationListView extends BaseHtmlView {
 
     return div(cardContent)
         .withClasses(
-            ReferenceClasses.ADMIN_APPLICATION_CARD, Styles.W_FULL, Styles.SHADOW_LG, Styles.MB_4);
+            ReferenceClasses.ADMIN_APPLICATION_CARD, Styles.W_FULL, Styles.SHADOW_LG, Styles.MT_4);
   }
 
   private Tag renderSubmitTime(Application application) {
@@ -176,5 +299,29 @@ public final class ProgramApplicationListView extends BaseHtmlView {
         .setText(text)
         .setStyles(Styles.MR_2, ReferenceClasses.VIEW_BUTTON)
         .asAnchorText();
+  }
+
+  @AutoValue
+  public abstract static class RenderFilterParams {
+    public abstract Optional<String> search();
+
+    public abstract Optional<String> fromDate();
+
+    public abstract Optional<String> untilDate();
+
+    public static Builder builder() {
+      return new AutoValue_ProgramApplicationListView_RenderFilterParams.Builder();
+    }
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+      public abstract Builder setSearch(Optional<String> search);
+
+      public abstract Builder setFromDate(Optional<String> fromDate);
+
+      public abstract Builder setUntilDate(Optional<String> untilDate);
+
+      public abstract RenderFilterParams build();
+    }
   }
 }
