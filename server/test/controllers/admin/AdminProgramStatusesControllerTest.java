156a9e3c52ba38ca8852e3f496f6de1b03216e0a
Brett Clouser <clouser@google.com>
2022-07-19 12:47:01 -0700

    Add test for flag disablement.

--- server/test/controllers/admin/AdminProgramStatusesControllerTest.java
+++ server/test/controllers/admin/AdminProgramStatusesControllerTest.java
@@ -3,6 +3,7 @@ package controllers.admin;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static play.api.test.CSRFTokenHelper.addCSRFToken;
+import static play.mvc.Http.Status.NOT_FOUND;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.fakeRequest;
@@ -10,10 +11,13 @@ import static play.test.Helpers.fakeRequest;
 import models.Program;
 import org.junit.Before;
 import org.junit.Test;
+import play.data.FormFactory;
 import play.mvc.Result;
 import repository.ResetPostgres;
 import services.program.ProgramNotFoundException;
+import services.program.ProgramService;
 import support.ProgramBuilder;
+import views.admin.programs.ProgramStatusesView;
 
 public class AdminProgramStatusesControllerTest extends ResetPostgres {
 
@@ -22,6 +26,24 @@ public class AdminProgramStatusesControllerTest extends ResetPostgres {
   @Before
   public void setup() {
     controller = instanceOf(AdminProgramStatusesController.class);
+  }
+
+  @Test
+  public void index_flagDisabled() throws ProgramNotFoundException {
+    Program program = ProgramBuilder.newDraftProgram("test name", "test description").build();
+
+    // Initialize the controller explicitly to override status tracking enablement.
+    controller =
+        new AdminProgramStatusesController(
+            instanceOf(ProgramService.class),
+            instanceOf(ProgramStatusesView.class),
+            instanceOf(RequestChecker.class),
+            instanceOf(FormFactory.class),
+            /* statusTrackingEnabled= */ false);
+
+    Result result = controller.index(addCSRFToken(fakeRequest()).build(), program.id);
+
+    assertThat(result.status()).isEqualTo(NOT_FOUND);
   }
 
   @Test
