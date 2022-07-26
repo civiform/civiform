9d711946e972650c9bfc3897cc9e51ffcc27a8ae
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-07-26 13:58:08 -0700

    First browser tests for view

---
+++ browser-test/src/admin_application_statuses.test.ts
@@ -0,0 +1,72 @@
+import {
+  startSession,
+  seedCanonicalQuestions,
+  dropTables,
+  logout,
+  loginAsTestUser,
+  loginAsGuest,
+  loginAsProgramAdmin,
+  loginAsAdmin,
+  selectApplicantLanguage,
+  ApplicantQuestions,
+  AdminQuestions,
+  AdminPrograms,
+  endSession,
+  isLocalDevEnvironment, getUserAdminName,
+} from './support'
+import {Page} from "playwright";
+
+describe('view program statuses', () => {
+  let pageObject: Page
+  const programName = 'test program with statuses'
+  beforeAll(async () => {
+    await dropTables(page)
+    await seedCanonicalQuestions(page)
+    const {page} = await startSession()
+    pageObject = page
+  })
+
+  describe('with program statuses', () => {
+    let userName: String
+
+    beforeAll(async () => {
+    // Timeout for clicks and element fills. If your selector fails to locate
+    // the HTML element, the test hangs. If you find the tests time out, you
+    // want to verify that your selectors are working as expected first.
+    // Because all tests are run concurrently, it could be that your selector
+    // selects a different entity from another test.
+    pageObject.setDefaultTimeout(4000)
+
+    await loginAsAdmin(pageObject)
+    //const adminQuestions = new AdminQuestions(pageObject)
+    const adminPrograms = new AdminPrograms(pageObject)
+    const applicantQuestions = new ApplicantQuestions(pageObject)
+
+    await adminPrograms.addAndPublishProgramWithQuestions( ['Name'], programName )
+
+    await logout(pageObject)
+    await loginAsTestUser(pageObject)
+    await selectApplicantLanguage(pageObject, 'English')
+      userName = await getUserAdminName(pageObject)
+
+    await applicantQuestions.applyProgram(programName)
+
+    // Applicant fills out first application block.
+    await applicantQuestions.answerNameQuestion('sarah', 'smith')
+    await applicantQuestions.clickNext()
+
+    // Applicant submits answers from review pageObject.
+    await applicantQuestions.submitFromReviewPage(programName)
+
+    await logout(pageObject)
+    })
+
+    it('Shows status options', async () => {
+      await loginAsProgramAdmin(pageObject)
+      const adminPrograms = new AdminPrograms(pageObject)
+
+      await adminPrograms.viewApplications(programName)
+
+  })
+  })
+})
