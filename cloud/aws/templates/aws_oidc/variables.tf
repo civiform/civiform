f2088efb6410a3d531e27a45a49ee79464ed0610
Mikita Belahlazau <nbeloglazov@google.com>
2022-07-27 18:06:30 -0700

    Remove OIDC variables.

--- cloud/aws/templates/aws_oidc/variables.tf
+++ cloud/aws/templates/aws_oidc/variables.tf
@@ -230,24 +230,6 @@ variable "civiform_applicant_idp" {
   default     = ""
 }
 
-variable "applicant_oidc_client_id" {
-  type        = string
-  description = "Client ID"
-  default     = ""
-}
-
-variable "applicant_oidc_client_secret" {
-  type        = string
-  description = "Client Secret"
-  default     = ""
-}
-
-variable "applicant_oidc_discovery_uri" {
-  type        = string
-  description = "Discovery URI"
-  default     = ""
-}
-
 variable "custom_hostname" {
   type        = string
   description = "The custom hostname this app is deployed on"
