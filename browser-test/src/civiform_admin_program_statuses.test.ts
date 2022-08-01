ab3e23303543ec3a8bb520089801da6e3490be45
Brett Clouser <clouser@google.com>
2022-08-01 09:34:41 -0700

    Edit and delete statuses tests.

--- browser-test/src/civiform_admin_program_statuses.test.ts
+++ browser-test/src/civiform_admin_program_statuses.test.ts
@@ -15,16 +15,18 @@ import {Page} from 'playwright'
 
 describe('modify program statuses', () => {
   let pageObject: Page
+  let adminPrograms: AdminPrograms
+  let adminProgramStatuses: AdminProgramStatuses
 
   beforeAll(async () => {
     const {page} = await startSession()
     pageObject = page
+    adminPrograms = new AdminPrograms(pageObject)
+    adminProgramStatuses = new AdminProgramStatuses(pageObject)
   })
 
   it('new program has no statuses', async () => {
     await loginAsAdmin(pageObject)
-    const adminPrograms = new AdminPrograms(pageObject)
-    const adminProgramStatuses = new AdminProgramStatuses(pageObject)
 
     // Add a draft program, no questions are needed.
     const programName = 'test program without statuses'
@@ -34,20 +36,15 @@ describe('modify program statuses', () => {
   })
 
   describe('new status creation', () => {
-    let adminPrograms: AdminPrograms
-    let adminProgramStatuses: AdminProgramStatuses
     const programName = 'test program create statuses'
 
     beforeAll(async () => {
-      adminPrograms = new AdminPrograms(pageObject)
-      adminProgramStatuses = new AdminProgramStatuses(pageObject)
-
       await adminPrograms.addProgram(programName)
       await adminPrograms.gotoDraftProgramManageStatusesPage(programName)
     })
 
     it('creates a new status with no email', async () => {
-      await adminProgramStatuses.addStatus('Status with no email')
+      await adminProgramStatuses.createStatus('Status with no email')
       await adminPrograms.expectProgramManageStatusesPage(programName)
       await adminProgramStatuses.expectStatusExists(
         'Status with no email',
@@ -56,13 +53,13 @@ describe('modify program statuses', () => {
     })
 
     it('creates a new status with email', async () => {
-      await adminProgramStatuses.addStatus('Status with email', 'An email')
+      await adminProgramStatuses.createStatus('Status with email', 'An email')
       await adminPrograms.expectProgramManageStatusesPage(programName)
       await adminProgramStatuses.expectStatusExists('Status with email', true)
     })
 
     it('fails to create status with an empty name', async () => {
-      await adminProgramStatuses.addStatus('')
+      await adminProgramStatuses.createStatus('')
       await adminPrograms.expectProgramManageStatusesPage(programName)
       await adminProgramStatuses.expectCreateStatusModalWithError(
         'This field is required',
@@ -71,14 +68,100 @@ describe('modify program statuses', () => {
     })
 
     it('fails to create status with an existing name', async () => {
-      await adminProgramStatuses.addStatus('Existing status')
+      await adminProgramStatuses.createStatus('Existing status')
       await adminPrograms.expectProgramManageStatusesPage(programName)
-      await adminProgramStatuses.addStatus('Existing status')
+      await adminProgramStatuses.createStatus('Existing status')
       await adminPrograms.expectProgramManageStatusesPage(programName)
       await adminProgramStatuses.expectCreateStatusModalWithError(
         'A status with name Existing status already exists',
       )
       await dismissModal(pageObject)
+    })
+  })
+
+  describe('edit existing statuses', () => {
+    const programName = 'test program edit statuses'
+    const firstStatusName = 'First status'
+    const secondStatusName = 'Second status'
+
+    beforeAll(async () => {
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.gotoDraftProgramManageStatusesPage(programName)
+
+      // Create two existing statuses.
+      await adminProgramStatuses.createStatus(firstStatusName)
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.createStatus(secondStatusName)
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectStatusExists(firstStatusName, false)
+      await adminProgramStatuses.expectStatusExists(secondStatusName, false)
+    })
+
+    it('fails to edit status when providing an existing status name', async () => {
+      await adminProgramStatuses.editStatus(firstStatusName, secondStatusName)
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectEditStatusModalWithError(
+        `A status with name ${secondStatusName} already exists`,
+      )
+      await dismissModal(pageObject)
+    })
+
+    it('fails to edit status with an empty name', async () => {
+      await adminProgramStatuses.editStatus(firstStatusName, '')
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectEditStatusModalWithError(
+        'This field is required',
+      )
+      await dismissModal(pageObject)
+    })
+
+    it('edits an existing status name', async () => {
+      await adminProgramStatuses.editStatus(
+        secondStatusName,
+        'Updated status name',
+      )
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectStatusExists(
+        'Updated status name',
+        false,
+      )
+      await adminProgramStatuses.expectStatusNotExists(secondStatusName)
+    })
+
+    it('edits an existing status and configures email', async () => {
+      await adminProgramStatuses.editStatus(
+        firstStatusName,
+        firstStatusName,
+        'An email body',
+      )
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectStatusExists(firstStatusName, true)
+    })
+  })
+
+  describe('delete existing status', () => {
+    const programName = 'test program delete status'
+    const firstStatusName = 'First status'
+    const secondStatusName = 'Second status'
+
+    beforeAll(async () => {
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.gotoDraftProgramManageStatusesPage(programName)
+
+      // Create two existing statuses.
+      await adminProgramStatuses.createStatus(firstStatusName)
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.createStatus(secondStatusName)
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectStatusExists(firstStatusName, false)
+      await adminProgramStatuses.expectStatusExists(secondStatusName, false)
+    })
+
+    it('deletes an existing status', async () => {
+      await adminProgramStatuses.deleteStatus(firstStatusName)
+      await adminPrograms.expectProgramManageStatusesPage(programName)
+      await adminProgramStatuses.expectStatusNotExists(firstStatusName)
+      await adminProgramStatuses.expectStatusExists(secondStatusName, false)
     })
   })
 })
--- browser-test/src/support/admin_program_statuses.ts
+++ browser-test/src/support/admin_program_statuses.ts
@@ -14,25 +14,6 @@ export class AdminProgramStatuses {
     ).toContain('No statuses have been created yet')
   }
 
-  async addStatus(statusName: string, emailBody = '') {
-    await this.page.click('button:has-text("Create a new status")')
-    await waitForAnyModal(this.page)
-
-    await this.page.fill('text="Status name (required)"', statusName)
-    await this.page.fill('text="Applicant status change email"', emailBody)
-
-    await this.page.click('button:has-text("Confirm")')
-    await waitForPageJsLoad(this.page)
-  }
-
-  async expectCreateStatusModalWithError(expectErrorContains: string) {
-    const modal = await waitForAnyModal(this.page)
-    expect(await modal.innerText()).toContain('Create a new status')
-    if (!!expectErrorContains) {
-      expect(await modal.innerText()).toContain(expectErrorContains)
-    }
-  }
-
   async expectStatusExists(statusName: string, expectEmailExists: boolean) {
     const statusLocator = this.page.locator(
       this.programStatusItemSelector(statusName),
@@ -47,6 +28,83 @@ export class AdminProgramStatuses {
         'Applicant notification email added',
       )
     }
+  }
+
+  async expectStatusNotExists(statusName: string) {
+    const statusLocator = this.page.locator(
+      this.programStatusItemSelector(statusName),
+    )
+    expect(await statusLocator.isVisible()).toEqual(false)
+  }
+
+  async createStatus(statusName: string, emailBody = '') {
+    await this.page.click('button:has-text("Create a new status")')
+
+    const modal = await waitForAnyModal(this.page)
+    expect(await modal.innerText()).toContain('Create a new status')
+
+    const statusFieldHandle = (await modal.$('text="Status name (required)"'))!
+    await statusFieldHandle.fill(statusName)
+    const emailFieldHandle = (await modal.$(
+      'text="Applicant status change email"',
+    ))!
+    await emailFieldHandle.fill(emailBody)
+
+    const confirmHandle = (await modal.$('button:has-text("Confirm")'))!
+    await confirmHandle.click()
+    await waitForPageJsLoad(this.page)
+  }
+
+  async expectCreateStatusModalWithError(expectErrorContains: string) {
+    const modal = await waitForAnyModal(this.page)
+    expect(await modal.innerText()).toContain('Create a new status')
+    if (!!expectErrorContains) {
+      expect(await modal.innerText()).toContain(expectErrorContains)
+    }
+  }
+
+  async editStatus(
+    originalStatusName: string,
+    editedStatusName: string,
+    editedEmailBody = '',
+  ) {
+    await this.page.click(
+      this.programStatusItemSelector(originalStatusName) +
+        ' button:has-text("Edit")',
+    )
+
+    const modal = await waitForAnyModal(this.page)
+    expect(await modal.innerText()).toContain('Edit this status')
+
+    const statusFieldHandle = (await modal.$('text="Status name (required)"'))!
+    await statusFieldHandle.fill(editedStatusName)
+    const emailFieldHandle = (await modal.$(
+      'text="Applicant status change email"',
+    ))!
+    await emailFieldHandle.fill(editedEmailBody)
+
+    const confirmHandle = (await modal.$('button:has-text("Confirm")'))!
+    await confirmHandle.click()
+    await waitForPageJsLoad(this.page)
+  }
+
+  async expectEditStatusModalWithError(expectErrorContains: string) {
+    const modal = await waitForAnyModal(this.page)
+    expect(await modal.innerText()).toContain('Edit this status')
+    if (!!expectErrorContains) {
+      expect(await modal.innerText()).toContain(expectErrorContains)
+    }
+  }
+
+  async deleteStatus(statusName: string) {
+    await this.page.click(
+      this.programStatusItemSelector(statusName) + ' button:has-text("Delete")',
+    )
+    const modal = await waitForAnyModal(this.page)
+
+    const deleteHandle = (await modal.$('button:has-text("Delete")'))!
+    await deleteHandle.click()
+    await waitForPageJsLoad(this.page)
   }
 
   programStatusItemSelector(statusName: string): string {
