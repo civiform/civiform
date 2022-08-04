2aac1ec231f81625c5ec5739c99cebda4ce3b5d9
Mikita Belahlazau <nbeloglazov@google.com>
2022-08-04 15:04:01 -0700

    Add stuff.

--- cloud/aws/templates/aws_oidc/bin/destroy.py
+++ cloud/aws/templates/aws_oidc/bin/destroy.py
@@ -17,5 +17,8 @@ class Destroy(AwsSetupTemplate):
             self._make_backend_override()
 
     def post_terraform_destroy(self):
-        # Don't destroy s3 state bucket as it's marked as indestructible.
-        pass
+        # when config is dev then the state is stored locally and no clean up
+        # required
+        if not self.config.is_dev():
+            print('Not destroying S3 bucket that contains terraform state. ' + 'You have to destroy it manually:')
+            print(f'https://s3.console.aws.amazon.com/s3/buckets/{self.config.app_prefix}-backendstate')
