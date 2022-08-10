c0c66cf21754fdc7647211b8ee1721bfcf4883d2
Mikita Belahlazau <nbeloglazov@google.com>
2022-08-10 14:56:34 -0700

    Improve code.

--- cloud/aws/templates/aws_oidc/bin/resources.py
+++ cloud/aws/templates/aws_oidc/bin/resources.py
@@ -8,15 +8,18 @@ Resource names use pattern {app_prefix}-{name} and this file contains only the
 second part, name.
 """
 
-# Defined in file cloud/aws/templates/aws_oidc/secrets.tf
+# Defined in cloud/aws/templates/aws_oidc/secrets.tf
 ADFS_CLIENT_ID = 'adfs_client_id'
 ADFS_SECRET = 'adfs_secret'
 APPLICANT_OIDC_CLIENT_ID = 'applicant_oidc_client_id'
 APPLICANT_OIDC_CLIENT_SECRET = 'applicant_oidc_client_secret'
 POSTGRES_PASSWORD = 'postgres_password'
 
-# Defined in file cloud/aws/templates/aws_oidc/main.tf
+# Defined in cloud/aws/templates/aws_oidc/main.tf
 DATABASE = 'civiform-db'
 
-# Defined by fargate modules file cloud/aws/templates/aws_oidc/app.tf
+# Defined by fargate modules in cloud/aws/templates/aws_oidc/app.tf
 FARGATE_SERVICE = 'service'
+
+# Defined in cloud/aws/modules/setup/backend_storage.tf
+S3_TERRAFORM_STATE_BUCKET = 'backendstate'
