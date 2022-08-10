017716689f0aba8a7c4e6b1243bfc787331ffd4b
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-08-10 13:28:00 -0700

    Fix Dev Seed test to clean up it's db connections

--- server/test/controllers/dev/DatabaseSeedControllerTest.java
+++ server/test/controllers/dev/DatabaseSeedControllerTest.java
@@ -1,30 +1,37 @@
 package controllers.dev;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static play.api.test.CSRFTokenHelper.addCSRFToken;
-import static play.mvc.Http.Status.NOT_FOUND;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.fakeRequest;
+  import static org.assertj.core.api.Assertions.assertThat;
+  import static play.api.test.CSRFTokenHelper.addCSRFToken;
+  import static play.mvc.Http.Status.NOT_FOUND;
+  import static play.mvc.Http.Status.OK;
+  import static play.test.Helpers.contentAsString;
+  import static play.test.Helpers.fakeRequest;
 
-import org.junit.Before;
-import org.junit.Test;
-import play.Mode;
-import play.inject.guice.GuiceApplicationBuilder;
-import play.mvc.Result;
-import repository.ResetPostgres;
+  import java.util.Optional;
+  import org.junit.After;
+  import org.junit.Test;
+  import play.Application;
+  import play.Mode;
+  import play.inject.guice.GuiceApplicationBuilder;
+  import play.mvc.Result;
+  import play.test.Helpers;
 
-public class DatabaseSeedControllerTest extends ResetPostgres {
+public class DatabaseSeedControllerTest {
 
+  private Optional<Application> maybeApp = Optional.empty();
   private DatabaseSeedController controller;
 
-  @Before
-  public void setupController() {
-    controller = createControllerInMode(Mode.DEV);
+  @After
+  public void stopApplication() {
+    if (maybeApp.isPresent()) {
+      Helpers.stop(maybeApp.get());
+      maybeApp = Optional.empty();
+    }
   }
 
   @Test
   public void seedAndClearDatabase_displaysCorrectInformation() {
+    setupControllerInMode(Mode.DEV);
     // Navigate to index before seeding - should not have the fake program.
     Result result = controller.index(addCSRFToken(fakeRequest()).build());
     assertThat(result.status()).isEqualTo(OK);
@@ -43,7 +50,7 @@ public class DatabaseSeedControllerTest extends ResetPostgres {
 
   @Test
   public void index_inNonDevMode_returnsNotFound() {
-    DatabaseSeedController controller = createControllerInMode(Mode.TEST);
+    setupControllerInMode(Mode.TEST);
     Result result = controller.index(fakeRequest().build());
 
     assertThat(result.status()).isEqualTo(NOT_FOUND);
@@ -51,7 +58,7 @@ public class DatabaseSeedControllerTest extends ResetPostgres {
 
   @Test
   public void seed_inNonDevMode_returnsNotFound() {
-    DatabaseSeedController controller = createControllerInMode(Mode.TEST);
+    setupControllerInMode(Mode.TEST);
     Result result = controller.seed();
 
     assertThat(result.status()).isEqualTo(NOT_FOUND);
@@ -59,17 +66,14 @@ public class DatabaseSeedControllerTest extends ResetPostgres {
 
   @Test
   public void clear_inNonDevMode_returnsNotFound() {
-    DatabaseSeedController controller = createControllerInMode(Mode.TEST);
+    setupControllerInMode(Mode.TEST);
     Result result = controller.clear();
 
     assertThat(result.status()).isEqualTo(NOT_FOUND);
   }
 
-  private DatabaseSeedController createControllerInMode(Mode mode) {
-    return new GuiceApplicationBuilder()
-        .in(mode)
-        .build()
-        .injector()
-        .instanceOf(DatabaseSeedController.class);
+  private void setupControllerInMode(Mode mode) {
+    maybeApp = Optional.of(new GuiceApplicationBuilder().in(mode).build());
+    controller = maybeApp.get().injector().instanceOf(DatabaseSeedController.class);
   }
 }
