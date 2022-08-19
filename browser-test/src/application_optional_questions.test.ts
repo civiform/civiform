6e3e3af98f62bfee10621adaedd838eb03447682
Mikita Belahlazau <nbeloglazov@google.com>
2022-08-19 16:06:11 -0700

    Fix

--- browser-test/src/application_optional_questions.test.ts
+++ browser-test/src/application_optional_questions.test.ts
@@ -9,85 +9,51 @@ import {
   selectApplicantLanguage,
   ApplicantQuestions,
 } from './support'
+import {QuestionType} from './support/admin_questions'
 
 describe('optional application flow', () => {
-  it('program with all question types', async () => {
-    const {browser, page} = await startSession()
+  for (const type of Object.values(QuestionType)) {
+    it(`program with optional ${type} question`, async () => {
+      const {browser, page} = await startSession()
 
-    await loginAsAdmin(page)
-    const adminQuestions = new AdminQuestions(page)
-    const adminPrograms = new AdminPrograms(page)
+      await loginAsAdmin(page)
+      const adminQuestions = new AdminQuestions(page)
+      const adminPrograms = new AdminPrograms(page)
 
-    const questions = await adminQuestions.addAllNonSingleBlockQuestionTypes(
-      'optional-',
-    )
-    await adminQuestions.addFileUploadQuestion({
-      questionName: 'optional-file-upload',
-    })
+      const questionName = `optional-${type}`
+      await adminQuestions.addQuestionForType(type, questionName)
 
-    const programName = 'Optional Questions Program 1'
-    await adminPrograms.addProgram(programName)
-    await adminPrograms.editProgramBlockWithOptional(
-      programName,
-      'first description',
-      [],
-      'optional-file-upload',
-    )
-
-    for (let i = 0; i < questions.length; i++) {
-      await adminPrograms.addProgramBlockWithOptional(
+      const programName = `Optional Questions Program for question ${type}`
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.editProgramBlockWithOptional(
         programName,
-        'description',
+        'first description',
         [],
-        questions[i],
+        questionName,
       )
-    }
 
-    // Program names cannot be substrings of each other otherwise our text
-    // selectors can match the wrong one.
-    const programName2 = 'Optional Questions Program 2'
-    await adminPrograms.addProgram(programName2)
-    await adminPrograms.editProgramBlockWithOptional(
-      programName2,
-      'first description',
-      [],
-      'optional-file-upload',
-    )
+      await adminPrograms.gotoAdminProgramsPage()
+      await adminPrograms.expectDraftProgram(programName)
+      await adminPrograms.publishAllPrograms()
+      await adminPrograms.expectActiveProgram(programName)
 
-    await adminPrograms.gotoAdminProgramsPage()
-    await adminPrograms.expectDraftProgram(programName)
-    await adminPrograms.expectDraftProgram(programName2)
+      await logout(page)
+      await loginAsTestUser(page)
+      await selectApplicantLanguage(page, 'English')
 
-    await adminPrograms.publishAllPrograms()
-    await adminPrograms.expectActiveProgram(programName)
-    await adminPrograms.expectActiveProgram(programName2)
+      const applicantQuestions = new ApplicantQuestions(page)
+      await applicantQuestions.applyProgram(programName)
 
-    await logout(page)
-    await loginAsTestUser(page)
-    await selectApplicantLanguage(page, 'English')
+      // Skip first block without uploading a file
+      if (type === QuestionType.FILE_UPLOAD) {
+        await applicantQuestions.clickSkip()
+      } else {
+        await applicantQuestions.clickNext()
+      }
 
-    const applicantQuestions = new ApplicantQuestions(page)
-    await applicantQuestions.applyProgram(programName)
-
-    // Skip first block without uploading a file
-    await applicantQuestions.clickSkip()
-    // Skip blocks 2-12 without answering any questions
-    for (let i = 2; i <= 12; i++) {
-      await applicantQuestions.clickNext()
-    }
-
-    // Submit the first program
-    await applicantQuestions.submitFromReviewPage()
-    await applicantQuestions.returnToProgramsFromSubmissionPage()
-
-    // Complete the second program
-    await applicantQuestions.applyProgram(programName2)
-
-    // Skip Screen 1 when it pops up to be answered again
-    await applicantQuestions.clickSkip()
-
-    // Submit from review page
-    await applicantQuestions.submitFromReviewPage()
-    await endSession(browser)
-  })
+      // Submit the first program
+      await applicantQuestions.submitFromReviewPage()
+      await endSession(browser)
+    })
+  }
 })
