bb1f8ac8408e5b047d6970d0d556feeb609c8ff9
Brett Clouser <clouser@google.com>
2022-08-22 11:02:32 -0700

    Adding 'Create new question' dropdown to program builder.

---
+++ server/app/views/components/CreateQuestionButton.java
@@ -0,0 +1,64 @@
+package views.components;
+
+import static j2html.TagCreator.a;
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.p;
+
+import j2html.tags.specialized.ATag;
+import j2html.tags.specialized.DivTag;
+import services.question.types.QuestionType;
+import views.style.StyleUtils;
+import views.style.Styles;
+
+/**
+ * Contains a helper method to render the create question button with it's corresponding dropdown.
+ */
+public final class CreateQuestionButton {
+  /** Renders the "Create new question" button with a dropdown for each question type. */
+  public static DivTag renderCreateQuestionButton() {
+    String parentId = "create-question-button";
+    String dropdownId = parentId + "-dropdown";
+    DivTag linkButton =
+        new LinkElement().setId(parentId).setText("Create new question").asButtonNoHref();
+    DivTag dropdown =
+        div()
+            .withId(dropdownId)
+            .withClasses(
+                Styles.Z_50,
+                Styles.BORDER,
+                Styles.BG_WHITE,
+                Styles.TEXT_GRAY_600,
+                Styles.SHADOW_LG,
+                Styles.ABSOLUTE,
+                Styles.MT_3,
+                Styles.HIDDEN);
+
+    for (QuestionType type : QuestionType.values()) {
+      String typeString = type.toString().toLowerCase();
+      String link = controllers.admin.routes.AdminQuestionController.newOne(typeString).url();
+      ATag linkTag =
+          a().withHref(link)
+              .withId(String.format("create-%s-question", typeString))
+              .withClasses(
+                  Styles.BLOCK,
+                  Styles.P_3,
+                  Styles.BG_WHITE,
+                  Styles.TEXT_GRAY_600,
+                  StyleUtils.hover(Styles.BG_GRAY_100, Styles.TEXT_GRAY_800))
+              .with(
+                  Icons.questionTypeSvg(type)
+                      .withClasses(
+                          Styles.INLINE_BLOCK, Styles.H_6, Styles.W_6, Styles.MR_1, Styles.TEXT_SM))
+              .with(
+                  p(type.getLabel())
+                      .withClasses(
+                          Styles.ML_2,
+                          Styles.MR_4,
+                          Styles.INLINE,
+                          Styles.TEXT_SM,
+                          Styles.UPPERCASE));
+      dropdown.with(linkTag);
+    }
+    return linkButton.with(dropdown);
+  }
+}
--- server/app/views/components/QuestionBank.java
+++ server/app/views/components/QuestionBank.java
@@ -85,7 +85,7 @@ public class QuestionBank {
     innerDiv.with(contentDiv);
 
     H1Tag headerDiv = h1("Add Question").withClasses(Styles.MX_2, Styles._MB_3, Styles.TEXT_XL);
-    contentDiv.withId("question-bank-questions").with(headerDiv);
+    contentDiv.withId("question-bank-questions").with(div().with(headerDiv));
 
     InputTag filterInput =
         input()
@@ -108,8 +108,20 @@ public class QuestionBank {
     SvgTag filterIcon = Icons.svg(Icons.SEARCH).withClasses(Styles.H_4, Styles.W_4);
     DivTag filterIconDiv =
         div().withClasses(Styles.ABSOLUTE, Styles.ML_4, Styles.MT_3, Styles.MR_4).with(filterIcon);
-    DivTag filterDiv = div().withClasses(Styles.RELATIVE).with(filterIconDiv).with(filterInput);
+    DivTag filterDiv =
+        div().withClasses(Styles.MB_2, Styles.RELATIVE).with(filterIconDiv, filterInput);
     contentDiv.with(filterDiv);
+    contentDiv.with(
+        div()
+            .with(
+                div()
+                    .with(
+                        p("Not finding a question you're looking for in this list?"),
+                        div()
+                            .withClass(Styles.FLEX)
+                            .with(
+                                div().withClass(Styles.FLEX_GROW),
+                                CreateQuestionButton.renderCreateQuestionButton()))));
 
     ImmutableList<QuestionDefinition> filteredQuestions = filterQuestions();
 
