3335a650c2d80e9c0c1b33cf103fe2f5d7bf847f
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-08-23 13:57:17 -0400

    fmt

--- browser-test/src/admin_application_statuses.test.ts
+++ browser-test/src/admin_application_statuses.test.ts
@@ -16,124 +16,118 @@ import {
 import {Page} from 'playwright'
 
 describe('view program statuses', () => {
-    let pageObject: Page
-    let adminPrograms: AdminPrograms
-    let applicantQuestions: ApplicantQuestions
-    let adminProgramStatuses: AdminProgramStatuses
+  let pageObject: Page
+  let adminPrograms: AdminPrograms
+  let applicantQuestions: ApplicantQuestions
+  let adminProgramStatuses: AdminProgramStatuses
 
+  beforeAll(async () => {
+    const {page} = await startSession()
+    pageObject = page
+    adminPrograms = new AdminPrograms(pageObject)
+    applicantQuestions = new ApplicantQuestions(pageObject)
+    adminProgramStatuses = new AdminProgramStatuses(pageObject)
+    await enableFeatureFlag(pageObject, 'application_status_tracking_enabled')
+    // Need to navigate back to the Admin page after enabling the feature flag.
+    await gotoEndpoint(pageObject, '')
+  })
+
+  afterAll(async () => {
+    await disableFeatureFlag(pageObject, 'application_status_tracking_enabled')
+  })
+
+  describe('without program statuses', () => {
+    const programWithoutStatusesName = 'test program without statuses'
     beforeAll(async () => {
-      const {page} = await startSession()
-      pageObject = page
-      adminPrograms = new AdminPrograms(pageObject)
-      applicantQuestions = new ApplicantQuestions(pageObject)
-      adminProgramStatuses = new AdminProgramStatuses(pageObject)
-      await enableFeatureFlag(pageObject, 'application_status_tracking_enabled')
-      // Need to navigate back to the Admin page after enabling the feature flag.
-      await gotoEndpoint(pageObject, '')
+      await loginAsAdmin(pageObject)
+
+      // Add a program, no questions are needed.
+      await adminPrograms.addProgram(programWithoutStatusesName)
+      await adminPrograms.publishProgram(programWithoutStatusesName)
+      await adminPrograms.expectActiveProgram(programWithoutStatusesName)
+
+      await logout(pageObject)
+      await loginAsGuest(pageObject)
+      await selectApplicantLanguage(pageObject, 'English')
+
+      // Submit an application.
+      await applicantQuestions.clickApplyProgramButton(
+        programWithoutStatusesName,
+      )
+      await applicantQuestions.submitFromPreviewPage()
+
+      await logout(pageObject)
     })
 
     afterAll(async () => {
-      await disableFeatureFlag(
-        pageObject,
-        'application_status_tracking_enabled',
+      await logout(pageObject)
+    })
+
+    it('does not Show status options', async () => {
+      await loginAsProgramAdmin(pageObject)
+
+      await adminPrograms.viewApplications(programWithoutStatusesName)
+
+      await adminPrograms.viewApplicationForApplicant(userDisplayName())
+
+      expect(await adminPrograms.isStatusSelectorVisible()).toBe(false)
+    })
+  })
+
+  describe('with program statuses', () => {
+    const programWithStatusesName = 'test program with statuses'
+    const statusName = 'Status 1'
+    beforeAll(async () => {
+      await loginAsAdmin(pageObject)
+
+      // Add a program, no questions are needed.
+      await adminPrograms.addProgram(programWithStatusesName)
+      await adminPrograms.gotoDraftProgramManageStatusesPage(
+        programWithStatusesName,
       )
+      await adminProgramStatuses.createStatus(statusName)
+      await adminPrograms.publishProgram(programWithStatusesName)
+      await adminPrograms.expectActiveProgram(programWithStatusesName)
+
+      await logout(pageObject)
+      await loginAsGuest(pageObject)
+      await selectApplicantLanguage(pageObject, 'English')
+
+      // Submit an application.
+      await applicantQuestions.clickApplyProgramButton(programWithStatusesName)
+      await applicantQuestions.submitFromPreviewPage()
+
+      await logout(pageObject)
+      await loginAsProgramAdmin(pageObject)
+
+      await adminPrograms.viewApplications(programWithStatusesName)
+      await adminPrograms.viewApplicationForApplicant(userDisplayName())
     })
 
-    describe('without program statuses', () => {
-      const programWithoutStatusesName = 'test program without statuses'
-      beforeAll(async () => {
-        await loginAsAdmin(pageObject)
-
-        // Add a program, no questions are needed.
-        await adminPrograms.addProgram(programWithoutStatusesName)
-        await adminPrograms.publishProgram(programWithoutStatusesName)
-        await adminPrograms.expectActiveProgram(programWithoutStatusesName)
-
-        await logout(pageObject)
-        await loginAsGuest(pageObject)
-        await selectApplicantLanguage(pageObject, 'English')
-
-        // Submit an application.
-        await applicantQuestions.clickApplyProgramButton(
-          programWithoutStatusesName,
-        )
-        await applicantQuestions.submitFromPreviewPage()
-
-        await logout(pageObject)
-      })
-
-      afterAll(async () => {
-        await logout(pageObject)
-      })
-
-      it('does not Show status options', async () => {
-        await loginAsProgramAdmin(pageObject)
-
-        await adminPrograms.viewApplications(programWithoutStatusesName)
-
-        await adminPrograms.viewApplicationForApplicant(userDisplayName())
-
-        expect(await adminPrograms.isStatusSelectorVisible()).toBe(false)
-      })
+    afterAll(async () => {
+      await logout(pageObject)
     })
 
-    describe('with program statuses', () => {
-      const programWithStatusesName = 'test program with statuses'
-      const statusName = 'Status 1'
-      beforeAll(async () => {
-        await loginAsAdmin(pageObject)
-
-        // Add a program, no questions are needed.
-        await adminPrograms.addProgram(programWithStatusesName)
-        await adminPrograms.gotoDraftProgramManageStatusesPage(
-          programWithStatusesName,
-        )
-        await adminProgramStatuses.createStatus(statusName)
-        await adminPrograms.publishProgram(programWithStatusesName)
-        await adminPrograms.expectActiveProgram(programWithStatusesName)
-
-        await logout(pageObject)
-        await loginAsGuest(pageObject)
-        await selectApplicantLanguage(pageObject, 'English')
-
-        // Submit an application.
-        await applicantQuestions.clickApplyProgramButton(
-          programWithStatusesName,
-        )
-        await applicantQuestions.submitFromPreviewPage()
-
-        await logout(pageObject)
-        await loginAsProgramAdmin(pageObject)
-
-        await adminPrograms.viewApplications(programWithStatusesName)
-        await adminPrograms.viewApplicationForApplicant(userDisplayName())
-      })
-
-      afterAll(async () => {
-        await logout(pageObject)
-      })
-
-      it('shows status selector', async () => {
-        expect(await adminPrograms.isStatusSelectorVisible()).toBe(true)
-      })
-
-      it('shows default option as placeholder', async () => {
-        expect(await adminPrograms.getStatusOption()).toBe('Choose an option:')
-      })
+    it('shows status selector', async () => {
+      expect(await adminPrograms.isStatusSelectorVisible()).toBe(true)
     })
 
-    describe('when a status is changed, a confirmation dialog is shown', () => {
-      it('when rejecting, the selected status is not changed', async () => {
-        await adminPrograms.setStatusOptionAndDismissModal(statusName)
-        expect(await adminPrograms.getStatusOption()).toBe('Choose an option:')
-      })
-
-      it('when confirmed, the page is redirected with a success toast', async () => {
-        await adminPrograms.setStatusOptionAndConfirmModal(statusName)
-        expect(await adminPrograms.getStatusOption()).toBe('Choose an option:')
-        await adminPrograms.expectUpdateStatusToast()
-        // TODO(#3020): Assert that the selected status has been updated.
-      })
+    it('shows default option as placeholder', async () => {
+      expect(await adminPrograms.getStatusOption()).toBe('Choose an option:')
     })
-  },
-)
+  })
+
+  describe('when a status is changed, a confirmation dialog is shown', () => {
+    it('when rejecting, the selected status is not changed', async () => {
+      await adminPrograms.setStatusOptionAndDismissModal(statusName)
+      expect(await adminPrograms.getStatusOption()).toBe('Choose an option:')
+    })
+
+    it('when confirmed, the page is redirected with a success toast', async () => {
+      await adminPrograms.setStatusOptionAndConfirmModal(statusName)
+      expect(await adminPrograms.getStatusOption()).toBe('Choose an option:')
+      await adminPrograms.expectUpdateStatusToast()
+      // TODO(#3020): Assert that the selected status has been updated.
+    })
+  })
+})
