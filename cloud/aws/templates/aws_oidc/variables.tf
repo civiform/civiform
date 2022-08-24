a0eee13d2d3c0903f48b662418a0fbfbfb39f008
Kiana McNellis <kianam@google.com>
2022-08-24 16:50:32 -0600

    tests don't work if params don't have default

--- cloud/aws/templates/aws_oidc/variables.tf
+++ cloud/aws/templates/aws_oidc/variables.tf
@@ -204,6 +204,7 @@ variable "civiform_applicant_idp" {
 variable "applicant_oidc_provider_logout" {
   type        = bool
   description = "If the applicant OIDC logout should also perform a central logout from the auth provider"
+  default     = false
 }
 
 variable "applicant_oidc_post_logout_redirect_param" {
