c122e71f2fc52f75b150dd760b4008e02ff7c423
Jeannie Fu <jeanniefu@google.com>
2022-08-25 16:49:31 -0700

    Remove extra comment

--- server/app/views/questiontypes/EnumeratorQuestionRenderer.java
+++ server/app/views/questiontypes/EnumeratorQuestionRenderer.java
@@ -41,7 +41,6 @@ public final class EnumeratorQuestionRenderer extends ApplicantQuestionRendererI
           Styles.GAP_4,
           Styles.MB_4);
 
-  // DELETE: double check how extra enumerators render
   public EnumeratorQuestionRenderer(ApplicantQuestion question) {
     super(question, InputFieldType.COMPOSITE);
   }
