3cdc23be590117cbae2c768f725e436f71114152
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-08-25 14:40:57 -0400

    Remove uniqueness from application_events indexes.

--- server/test/repository/ApplicationEventRepositoryTest.java
+++ server/test/repository/ApplicationEventRepositoryTest.java
@@ -52,7 +52,48 @@ public class ApplicationEventRepositoryTest extends ResetPostgres {
   }
 
   @Test
-  public void getEvents() {
+  public void insertMultipleEventsOnApplication() {
+    Instant startInstant = Instant.now();
+    Program program = resourceCreator.insertActiveProgram("Program");
+    Account actor = resourceCreator.insertAccount();
+    Applicant applicant = resourceCreator.insertApplicant();
+    Application application = resourceCreator.insertActiveApplication(applicant,
+      program);
+
+    ApplicationEventDetails details =
+      ApplicationEventDetails.builder()
+        .setEventType(ApplicationEventDetails.Type.STATUS_CHANGE)
+        .setStatusEvent(
+          StatusEvent.builder().setStatusText("Status").setEmailSent(true)
+            .build()).build();
+
+    ApplicationEvent event1 =
+      new ApplicationEvent(
+        application, actor, ApplicationEventDetails.Type.STATUS_CHANGE,
+        details);
+    ApplicationEvent insertedEvent1 = repo.insertSync(event1);
+
+    ApplicationEvent event2 =
+      new ApplicationEvent(
+        application, actor, ApplicationEventDetails.Type.STATUS_CHANGE,
+        details);
+
+    ApplicationEvent insertedEvent2 = repo.insertSync(event2);
+
+    // Evaluate.
+    assertThat(insertedEvent1.id).isNotEqualTo(insertedEvent2.id);
+    // Generated values.
+    assertThat(insertedEvent2.id).isNotNull();
+    assertThat(insertedEvent2.getCreateTime()).isAfter(startInstant);
+    // Pass through values.
+    assertThat(insertedEvent2.getApplication()).isEqualTo(application);
+    assertThat(insertedEvent2.getCreator()).isEqualTo(actor);
+    assertThat(insertedEvent2.getDetails()).isEqualTo(details);
+    assertThat(insertedEvent2.getEventType()).isEqualTo(
+      ApplicationEventDetails.Type.STATUS_CHANGE);
+  }
+    @Test
+    public void getEvents() {
     // Setup
     Instant startInstant = Instant.now();
     Program program = resourceCreator.insertActiveProgram("Program");
