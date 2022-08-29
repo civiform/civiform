5f480650f2d8d0eac9aff90fcb34c4c1fb702ee5
Sami Abdul-Wahid <syntapy@pm.me>
2022-08-29 16:55:18 -0700

    Add 404 page (#1751)
    * 1st attempt at adding 404 pg error handler
    
    * Basic mock 404 handler
    
    * basic j2html 404 page
    
    * Basic 404 page but needs polishing
    
    * Attempt lang + copy index view styling
    
    * Taylored style adapted from applicant program index
    
    * Tweak image, add notes from product-ux meeting
    
    * Drafted error handler but isnt calling / rendering
    
    * Logged in 404pg header
    
    * Shows login button on 404 pg
    
    * Better styled 404pg login button
    
    * Add login icon
    
    * Login button flushed right
    
    * Add most of 404pg content
    
    * Login icon + button somewhat better positioned
    
    * Finally got main content centered
    
    * Center main content, match text with design
    
    * Removed footer without changing other code
    
    * Add footer back in and center it
    
    * Refactor to reuse code that adds body & footer
    
    * Basic logged out test passing
    
    * Style links
    
    * Vertical spacing styling + alt img text
    
    * Better alignment of login icon
    
    * Remove unused style
    
    * Responsive header / description spacings
    
    * Pass RequestHeader not Request to view methods
    
    * Update browser test to try real notfound page
    
    * Move error handler to root package
    
    * fmt
    
    * package-lock.json was modified
    
    * Build civiform dev img locally
    
    * Add basic error handler
    
    * Error handler linked to 404 page!
    
    * Remove unused ErrorController + commented out ErrorHandler
    
    * fmt
    
    * Add comments to 404 page content
    
    * Isolate login btn div mess into login btn method
    
    * package-lock.json was modified
    
    * Move ErrorHandler back to root package
    
    * Fix vertical positioning of login button
    
    * Modify/add/delete comments, reorder navbar methods
    
    * Prevent line breaks in last 2 or 3 words
    
    * Inline description paragraphs on mobile
    
    * 3 words last line of tile
    
    * Fix & test login aspect
    
    * revert bin/build-dev
    
    * fmt
    
    * Remove unused NotFoundPage.java
    
    * Minor refactor of page code
    
    * Try revert style login button styl but not perfect
    
    * Revert to older way of doing login / logout btn
    
    * Fix browser test
    
    * fmt
    
    * Use svg file for login icon
    
    * Remove unneeded comment
    
    * Add spanish translation
    
    * Spanish accents + spanish term for homepage
    
    * clean up login button spacing code
    
    * fmt
    
    * More space between login icon & button
    
    * Add "-Werror" back into to build.sbt
    
    * fmt
    
    * fix some post-merge bugs
    
    * rm unsplash imgs + unused seattle logos
    
    * edit title / description in en + es
    
    * fix browser tests
    
    * fmt
    
    * rm unused items from services/MessageKey.java
    
    * undelete blue Seattle-logo png img
    
    * move 404 msgs around in conf/messages.*
    
    * use better way to get 'Guest' display name
    
    * fmt
    
    * some more specific tag types in ApplicantLayout
    
    * rm mock 404 error page method in browser test
    
    * add test for spanish 404 page
    
    * rm used imports in browser test
    
    * rm mockNotFound method from DevController
    
    * remove navbar
    
    * fmt
    
    * change ContainerTag to type-specific tags in NotFound page
    
    * fmt
    
    * move space to messages files
    
    * tweek spacings
    
    * bion's comments
    
    * remove comma/space from messages
    
    * fmt
    
    * bion's comments

---
+++ server/app/views/errors/NotFound.java
@@ -0,0 +1,70 @@
+package views.errors;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static j2html.TagCreator.a;
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.h1;
+import static j2html.TagCreator.p;
+import static j2html.TagCreator.span;
+
+import com.google.inject.Inject;
+import j2html.tags.specialized.DivTag;
+import j2html.tags.specialized.H1Tag;
+import play.i18n.Messages;
+import play.mvc.Http;
+import play.twirl.api.Content;
+import services.MessageKey;
+import views.BaseHtmlView;
+import views.HtmlBundle;
+import views.LanguageSelector;
+import views.applicant.ApplicantLayout;
+import views.style.BaseStyles;
+import views.style.ErrorStyles;
+import views.style.Styles;
+
+public final class NotFound extends BaseHtmlView {
+
+  private final ApplicantLayout layout;
+  private final LanguageSelector languageSelector;
+
+  @Inject
+  public NotFound(ApplicantLayout layout, LanguageSelector languageSelector) {
+    this.layout = checkNotNull(layout);
+    this.languageSelector = checkNotNull(languageSelector);
+  }
+
+  public Content render(Http.RequestHeader request, Messages messages) {
+    HtmlBundle bundle = addBodyFooter(request, messages);
+    return layout.render(bundle);
+  }
+
+  private H1Tag h1Content(Messages messages) {
+    return h1(span(messages.at(MessageKey.ERROR_NOT_FOUND_TITLE.getKeyName())))
+        .withClasses(ErrorStyles.H1_NOT_FOUND);
+  }
+
+  private DivTag descriptionContent(Messages messages) {
+    return div(p(
+                span(messages.at(MessageKey.ERROR_NOT_FOUND_DESCRIPTION.getKeyName())),
+                a(messages.at(MessageKey.ERROR_NOT_FOUND_DESCRIPTION_LINK.getKeyName()))
+                    .withHref("/")
+                    .withClasses(BaseStyles.LINK_TEXT, BaseStyles.LINK_HOVER_TEXT))
+            .withClasses(ErrorStyles.P_MOBILE_INLINE))
+        .withClasses(ErrorStyles.P_DESCRIPTION);
+  }
+
+  /** Page returned on 404 error */
+  private DivTag mainContent(Messages messages) {
+    return div(h1Content(messages), descriptionContent(messages))
+        .withClasses(Styles.TEXT_CENTER, Styles.MAX_W_SCREEN_SM, Styles.W_5_6, Styles.MX_AUTO);
+  }
+
+  private HtmlBundle addBodyFooter(Http.RequestHeader request, Messages messages) {
+    HtmlBundle bundle = layout.getBundle();
+    String language = languageSelector.getPreferredLangage(request).code();
+    bundle.setLanguage(language);
+    bundle.addMainContent(mainContent(messages));
+
+    return bundle;
+  }
+}
