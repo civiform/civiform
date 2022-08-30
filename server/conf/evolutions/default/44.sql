8c6e25217b87e4489411a600c85905d5e7a5a4ac
Brett Clouser <clouser@google.com>
2022-08-30 10:37:03 -0700

    Consolidating triggers.

--- server/conf/evolutions/default/44.sql
+++ server/conf/evolutions/default/44.sql
@@ -1,19 +1,37 @@
-# --- Fix indexes made in evolution 42 to not be unique
+# --- Add database triggers to set the latest_status column on creating an application_event or
+# --- updating an application.
 
 # --- !Ups
 
 alter table applications add column if not exists latest_status varchar;
 
+CREATE OR REPLACE FUNCTION retrieve_latest_status(app_id applications.id%TYPE) RETURNS text AS $$
+  SELECT
+      (CASE WHEN details->'status_event'->>'status_text' = ''
+        THEN NULL
+        ELSE details->'status_event'->>'status_text'
+       END) AS latest_status
+      FROM application_events
+      WHERE
+        application_id = app_id AND
+        details->>'event_type' = 'STATUS_CHANGE'
+      ORDER BY create_time DESC
+      LIMIT 1
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION process_application_change() RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.latest_status := retrieve_latest_status(NEW.id);;
+    RETURN NEW;;
+  END;;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION process_status_change() RETURNS TRIGGER AS $$
   BEGIN
     IF ((NEW.details->>'event_type') = 'STATUS_CHANGE') THEN
       UPDATE applications
-        SET latest_status = (
-          CASE WHEN NEW.details->'status_event'->>'status_text' = ''
-            THEN NULL
-            ELSE NEW.details->'status_event'->>'status_text'
-          END)
-        WHERE id = NEW.application_id;;
+        SET latest_status = retrieve_latest_status(NEW.application_id)
+      WHERE id = NEW.application_id;;
     END IF;;
     RETURN NULL;; -- result is ignored since this is an AFTER
   END;;
@@ -22,24 +40,6 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER status_change
 AFTER INSERT OR UPDATE ON application_events
   FOR EACH ROW EXECUTE FUNCTION process_status_change();
-
-CREATE OR REPLACE FUNCTION process_application_change() RETURNS TRIGGER AS $$
-  BEGIN
-    NEW.latest_status := (SELECT
-      (CASE WHEN details->'status_event'->>'status_text' = ''
-        THEN NULL
-        ELSE details->'status_event'->>'status_text'
-       END) AS latest_status
-      FROM application_events
-      WHERE
-        application_id = NEW.id AND
-        details->>'event_type' = 'STATUS_CHANGE'
-      ORDER BY create_time DESC
-      LIMIT 1);;
-
-    RETURN NEW;;
-  END;;
-$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER application_change BEFORE INSERT OR UPDATE ON applications
     FOR EACH ROW EXECUTE FUNCTION process_application_change();
