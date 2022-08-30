52e9c349d4acd173c1ec9e39402e3fd8f385b6c3
Brett Clouser <clouser@google.com>
2022-08-30 13:53:16 -0700

    More comments.

--- server/conf/evolutions/default/44.sql
+++ server/conf/evolutions/default/44.sql
@@ -1,10 +1,15 @@
 # --- Add database triggers to set the latest_status column on creating an application_event or
 # --- updating an application.
+# --- Relevant documentation:
+# --- Trigger functions: https://www.postgresql.org/docs/current/sql-createtrigger.html
+# --- Create trigger statement: https://www.postgresql.org/docs/current/sql-createtrigger.html
 
 # --- !Ups
 
 alter table applications add column if not exists latest_status varchar;
 
+-- The latest status associated with an application is set to NULL if no application event entries
+-- exist. Otherwise, the text from the latest event corresponding to a status change is used.
 CREATE OR REPLACE FUNCTION retrieve_latest_status(app_id applications.id%TYPE) RETURNS text AS $$
   SELECT
       (CASE WHEN details->'status_event'->>'status_text' = ''
@@ -19,6 +24,8 @@ CREATE OR REPLACE FUNCTION retrieve_latest_status(app_id applications.id%TYPE) R
       LIMIT 1
 $$ LANGUAGE SQL;
 
+-- A function that sets the "latest_status" column anytime an application is updated.
+-- Note: The "NEW" variable corresponds to the application record to be inserted / updated.
 CREATE OR REPLACE FUNCTION process_application_change() RETURNS TRIGGER AS $$
   BEGIN
     NEW.latest_status := retrieve_latest_status(NEW.id);;
@@ -26,6 +33,12 @@ CREATE OR REPLACE FUNCTION process_application_change() RETURNS TRIGGER AS $$
   END;;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER application_change BEFORE INSERT OR UPDATE ON applications
+    FOR EACH ROW EXECUTE FUNCTION process_application_change();
+
+-- A function that sets the "latest_status" column on the referenced application record anytime
+-- an event record is updated.
+-- Note: The "NEW" variable corresponds to the application event record to be inserted / updated.
 CREATE OR REPLACE FUNCTION process_application_event_change() RETURNS TRIGGER AS $$
   BEGIN
     IF ((NEW.details->>'event_type') = 'STATUS_CHANGE') THEN
@@ -33,7 +46,7 @@ CREATE OR REPLACE FUNCTION process_application_event_change() RETURNS TRIGGER AS
         SET latest_status = retrieve_latest_status(NEW.application_id)
       WHERE id = NEW.application_id;;
     END IF;;
-    RETURN NULL;; -- result is ignored since this is an AFTER
+    RETURN NULL;; -- result is ignored for triggers that execute after an insert
   END;;
 $$ LANGUAGE plpgsql;
 
@@ -41,13 +54,10 @@ CREATE TRIGGER application_event_change
 AFTER INSERT OR UPDATE ON application_events
   FOR EACH ROW EXECUTE FUNCTION process_application_event_change();
 
-CREATE TRIGGER application_change BEFORE INSERT OR UPDATE ON applications
-    FOR EACH ROW EXECUTE FUNCTION process_application_change();
-
 # --- !Downs
 
-DROP TRIGGER application_change ON applications;
 DROP TRIGGER application_event_change ON application_events;
+DROP TRIGGER application_change ON applications;
 DROP FUNCTION IF EXISTS process_application_event_change;
 DROP FUNCTION IF EXISTS process_application_change;
 DROP FUNCTION IF EXISTS retrieve_latest_status;
