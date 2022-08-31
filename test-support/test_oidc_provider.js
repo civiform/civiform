c8ef09146e80aee84f1b95e16c75808983eadb2e
Brett Clouser <clouser@google.com>
2022-08-31 12:04:42 -0700

    Add support for civiform:9000 as a redirect URL.

--- test-support/test_oidc_provider.js
+++ test-support/test_oidc_provider.js
@@ -7,14 +7,15 @@ const configuration = {
       response_types: ['id_token'],
       response_mode: ['form_post'],
       grant_types: ['implicit'],
-      // "native" because we're on localhost.
-      application_type: 'native',
+      application_type: 'web',
       scopes: ['openid', 'profile'],
       redirect_uris: [
         'http://localhost:9000/callback/OidcClient',
         'http://localhost:9000/callback/AdClient',
         'http://localhost:19001/callback/OidcClient',
         'http://localhost:19001/callback/AdClient',
+        'http://civiform:9000/callback/OidcClient',
+        'http://civiform:9000/callback/AdClient',
       ],
     },
   ],
@@ -30,7 +31,7 @@ const configuration = {
           user_emailid: id + '@example.com',
           // lie about verification for tests.
           email_verified: true,
-          user_displayname: 'first middle last',
+          user_displayname: 'Test middlename User',
         }
       },
     }
@@ -43,6 +44,20 @@ const configuration = {
 
 const oidcPort = process.env.OIDC_PORT || 3380
 const oidc = new Provider('http://localhost:' + oidcPort, configuration)
+
+const {invalidate: orig} = oidc.Client.Schema.prototype
+
+// By default, redirect URLs must be https and cannot refer to localhost. While these are correct
+// for production implementations, this is intended to be used ONLY for testing. For browser tests,
+// internal Docker networking is used, which uses HTTP. For running the server locally, redirects
+// are pointed towards localhost.
+// From https://github.com/panva/node-oidc-provider/blob/0fcc112e0a95b3b2dae4eba6da812253277567c9/recipes/implicit_http_localhost.md
+oidc.Client.Schema.prototype.invalidate = function invalidate(message, code) {
+  if (code === 'implicit-force-https' || code === 'implicit-forbid-localhost') {
+    return
+  }
+  orig.call(this, message)
+}
 
 process.on('SIGINT', () => {
   console.info('Interrupted')
