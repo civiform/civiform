da9d3c7a7c59c0a38120f95e943df253bc83a845
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2022-08-31 10:16:33 -0700

    Adding updateDob funtionality for TIs (#3292)
    * Adding updateDob funtionality for TIs
    
    * more tests
    
    * fixing breaking unit tests
    
    * PR review comments update
    
    * fixing typo
    
    * fixing testcase names
    
    * error handling of ApplicantNotFoundException
    
    * fixing unit tests
    
    * PR updates

--- server/test/controllers/ti/TrustedIntermediaryControllerTest.java
+++ server/test/controllers/ti/TrustedIntermediaryControllerTest.java
@@ -5,9 +5,12 @@ import static play.api.test.CSRFTokenHelper.addCSRFToken;
 import static play.mvc.Http.Status.SEE_OTHER;
 
 import auth.ProfileFactory;
+import auth.ProfileUtils;
 import com.google.common.collect.ImmutableMap;
 import controllers.WithMockedProfiles;
+import forms.AddApplicantToTrustedIntermediaryGroupForm;
 import java.util.Optional;
+import models.Account;
 import models.Applicant;
 import models.TrustedIntermediaryGroup;
 import org.junit.Before;
@@ -16,11 +19,13 @@ import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
 import repository.UserRepository;
+import services.applicant.exception.ApplicantNotFoundException;
 
 public class TrustedIntermediaryControllerTest extends WithMockedProfiles {
   private UserRepository repo;
   private TrustedIntermediaryController tiController;
   private ProfileFactory profileFactory;
+  private ProfileUtils profileUtils;
 
   @Before
   public void setup() {
@@ -30,6 +35,7 @@ public class TrustedIntermediaryControllerTest extends WithMockedProfiles {
     createTIWithMockedProfile(managedApplicant);
     repo = instanceOf(UserRepository.class);
     profileFactory.createFakeTrustedIntermediary();
+    profileUtils = instanceOf(ProfileUtils.class);
   }
 
   @Test
@@ -49,8 +55,10 @@ public class TrustedIntermediaryControllerTest extends WithMockedProfiles {
                         "sample1@fake.com",
                         "dob",
                         "")));
-    TrustedIntermediaryGroup group = repo.listTrustedIntermediaryGroups().get(0);
-    Result result = tiController.addApplicant(group.id + 1, requestBuilder.build());
+    Http.Request request = requestBuilder.build();
+    TrustedIntermediaryGroup trustedIntermediaryGroup =
+        repo.getTrustedIntermediaryGroup(profileUtils.currentUserProfile(request).get()).get();
+    Result result = tiController.addApplicant(trustedIntermediaryGroup.id, request);
     assertThat(result.status()).isEqualTo(SEE_OTHER);
     assertThat(result.flash().get("error").get().trim()).isEqualTo("Date of Birth required");
   }
@@ -79,5 +87,30 @@ public class TrustedIntermediaryControllerTest extends WithMockedProfiles {
         repo.lookupApplicantByEmail("sample2@fake.com").toCompletableFuture().join();
     assertThat(testApplicant.get().getApplicantData().getDateOfBirth().get().toString())
         .isEqualTo("2022-07-18");
+  }
+
+  @Test
+  public void testUpdateDOBFunctionWithExistingDob() throws ApplicantNotFoundException {
+
+    AddApplicantToTrustedIntermediaryGroupForm form =
+        new AddApplicantToTrustedIntermediaryGroupForm();
+    form.setEmailAddress("sample3@example.com");
+    form.setFirstName("foo");
+    form.setLastName("bar");
+    form.setDob("2022-07-10");
+
+    Http.RequestBuilder requestBuilder =
+        addCSRFToken(Helpers.fakeRequest().bodyForm(ImmutableMap.of("dob", "2022-05-05")));
+    Http.Request request = requestBuilder.build();
+    TrustedIntermediaryGroup trustedIntermediaryGroup =
+        repo.getTrustedIntermediaryGroup(profileUtils.currentUserProfile(request).get()).get();
+    repo.createNewApplicantForTrustedIntermediaryGroup(form, trustedIntermediaryGroup);
+    Optional<Account> account = repo.lookupAccountByEmail("sample3@example.com");
+    Result result = tiController.updateDateOfBirth(account.get().id, request);
+    assertThat(result.status()).isEqualTo(SEE_OTHER);
+    Optional<Applicant> applicant =
+        repo.lookupAccountByEmail("sample3@example.com").get().newestApplicant();
+    assertThat(applicant.get().getApplicantData().getDateOfBirth().get().toString())
+        .isEqualTo("2022-05-05");
   }
 }
