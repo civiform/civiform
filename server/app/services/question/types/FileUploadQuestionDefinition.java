5bc4314e36da0b4b2d02f60c2bfc8fdeb8ab94d9
Brett Clouser <clouser@google.com>
2022-09-06 18:15:52 -0700

    Add LastModifiedTime to QuestionDefinition (#3353)
    * Tightening visibility.
    
    * Adding lastModifiedTime to the QuestionDefinition constructors.
    
    * Updating unit tests and removing method from Question in favor of using QuestionDefinition method.
    
    * Removing not useful test since the test question bank does not persist state.
    
    * PR feedback.

--- server/app/services/question/types/FileUploadQuestionDefinition.java
+++ server/app/services/question/types/FileUploadQuestionDefinition.java
@@ -2,12 +2,13 @@ package services.question.types;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.auto.value.AutoValue;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.OptionalLong;
 import services.LocalizedStrings;
 
 /** Defines a file upload question. */
-public class FileUploadQuestionDefinition extends QuestionDefinition {
+public final class FileUploadQuestionDefinition extends QuestionDefinition {
 
   public FileUploadQuestionDefinition(
       OptionalLong id,
@@ -15,7 +16,8 @@ public class FileUploadQuestionDefinition extends QuestionDefinition {
       Optional<Long> enumeratorId,
       String description,
       LocalizedStrings questionText,
-      LocalizedStrings questionHelpText) {
+      LocalizedStrings questionHelpText,
+      Optional<Instant> lastModifiedTime) {
     super(
         id,
         name,
@@ -23,7 +25,8 @@ public class FileUploadQuestionDefinition extends QuestionDefinition {
         description,
         questionText,
         questionHelpText,
-        FileUploadValidationPredicates.create());
+        FileUploadValidationPredicates.create(),
+        lastModifiedTime);
   }
 
   public FileUploadQuestionDefinition(
