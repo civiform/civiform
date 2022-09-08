484f6d568d0fa1136909c72d3f9f5bf588afa638
Brett Clouser <clouser@google.com>
2022-09-08 13:36:43 -0700

    Add another test that an application still shows even if it doesn't appear in the filtered list after updating a status.

--- browser-test/src/admin_application_statuses.test.ts
+++ browser-test/src/admin_application_statuses.test.ts
@@ -363,5 +363,29 @@ describe('view program statuses', () => {
       })
       await adminPrograms.expectApplicationCount(1)
     })
+
+    it('updating status to something no longer in the filter still displays the application on reload', async () => {
+      const {adminPrograms} = ctx
+
+      await adminPrograms.viewApplications(programForFilteringName)
+      await adminPrograms.filterProgramApplications({
+        applicationStatusOption: approvedStatusName,
+      })
+      await adminPrograms.viewApplicationForApplicant('Guest')
+      const modal = await adminPrograms.setStatusOptionAndAwaitModal(
+        rejectedStatusName,
+      )
+      await adminPrograms.confirmStatusUpdateModal(modal)
+
+      // The application should no longer be in the list, since it's status is no longer
+      // "approved". However, it should still be displayed in the viewer since admins may want to
+      // easily revert the status update.
+      await adminPrograms.expectApplicationCount(0)
+      const applicationText = await adminPrograms
+        .applicationFrameLocator()
+        .locator('#application-view')
+        .innerText()
+      expect(applicationText).toContain('Guest')
+    })
   })
 })
