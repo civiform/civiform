928cd3c8d54697deded2f074fc54e3afa3aa1ded
Mikita Belahlazau <nbeloglazov@google.com>
2022-09-09 12:51:55 -0700

    Fix

--- server/app/filters/DisableCachingFilter.java
+++ server/app/filters/DisableCachingFilter.java
@@ -41,9 +41,8 @@ public class DisableCachingFilter extends EssentialFilter {
                       final Integer status = result.status();
                       final String path = request.uri().toLowerCase();
 
-
                       if ((!environment.isDev() // Must revalidate assets in dev mode
-                         || this.enableCacheForBrowserTests)
+                              || this.enableCacheForBrowserTests)
                           && ASSET_PATH_PREFIXES.stream().anyMatch(path::startsWith)
                           && OK_STATUS_CODES.contains(status)) {
                         // In prod/staging, static assets are fingerprinted,
