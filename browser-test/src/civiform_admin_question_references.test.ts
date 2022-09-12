8c869f06aeb4fae8c03904508e024e174e564d3b
Mikita Belahlazau <nbeloglazov@google.com>
2022-09-12 11:17:26 -0700

    Redesign admin question list page (#3378)
    1. Show question text and help text as main question info. Question name and description are now shown as "admin notes".
    2. Add "draft" and "active" rows for questions.
    3. Render questions as a list rather than a table.
    4. Remove translated languages text.

--- browser-test/src/civiform_admin_question_references.test.ts
+++ browser-test/src/civiform_admin_question_references.test.ts
@@ -10,7 +10,8 @@ describe('view program references from question view', () => {
     await adminQuestions.addAddressQuestion({questionName})
     await adminQuestions.expectQuestionProgramReferencesText({
       questionName,
-      expectedProgramReferencesText: 'Used across 0 active & 0 draft programs',
+      expectedProgramReferencesText: 'Used across 0 programs',
+      version: 'draft',
     })
   })
 
@@ -32,7 +33,8 @@ describe('view program references from question view', () => {
     await adminQuestions.gotoAdminQuestionsPage()
     await adminQuestions.expectQuestionProgramReferencesText({
       questionName,
-      expectedProgramReferencesText: 'Used across 0 active & 1 draft programs',
+      expectedProgramReferencesText: 'Used across 1 draft programs',
+      version: 'draft',
     })
 
     // Publish and add a reference from a new program in the draft version.
@@ -48,6 +50,7 @@ describe('view program references from question view', () => {
     await adminQuestions.expectQuestionProgramReferencesText({
       questionName,
       expectedProgramReferencesText: 'Used across 1 active & 2 draft programs',
+      version: 'active',
     })
 
     await adminQuestions.expectProgramReferencesModalContains({
