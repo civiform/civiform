72d7f989259fb1fabcdf704d023e2b19e6f61c5a
Mikita Belahlazau <nbeloglazov@google.com>
2022-09-13 16:08:52 -0700

    Fix

--- server/app/views/admin/programs/ProgramBlockView.java
+++ server/app/views/admin/programs/ProgramBlockView.java
@@ -5,21 +5,21 @@ import static j2html.TagCreator.div;
 import j2html.tags.specialized.DivTag;
 import services.program.ProgramDefinition;
 import views.BaseHtmlView;
+import views.ViewUtils;
+import views.ViewUtils.BadgeStatus;
 import views.style.Styles;
 
 abstract class ProgramBlockView extends BaseHtmlView {
   /** Renders a div with internal/admin program information. */
   protected final DivTag renderProgramInfo(ProgramDefinition programDefinition) {
-    DivTag programStatus =
-        div("Draft").withId("program-status").withClasses(Styles.TEXT_XS, Styles.UPPERCASE);
     DivTag programTitle =
         div(programDefinition.adminName())
             .withId("program-title")
-            .withClasses(Styles.TEXT_3XL, Styles.PB_3);
+            .withClasses(Styles.TEXT_3XL, Styles.PB_3, Styles.MT_5);
     DivTag programDescription =
         div(programDefinition.adminDescription()).withClasses(Styles.TEXT_SM);
 
-    return div(programStatus, programTitle, programDescription)
+    return div(ViewUtils.makeBadge(BadgeStatus.DRAFT), programTitle, programDescription)
         .withClasses(
             Styles.BG_GRAY_100,
             Styles.TEXT_GRAY_800,
