aba87b69e99dac1f506bac418a716431fff5461e
Bion Johnson <bion@users.noreply.github.com>
2022-09-14 14:17:18 -0700

    AWS monitoring stack (#3321)
    * Metrics scraper ECS container definition
    
    * Scraper logs to its own log file
    
    * Machine sizes, roles
    
    * Permissions, images
    
    * fmt
    
    * Address PR comments
    
    * Rename adot-config.yaml -> otel-config.yaml

--- cloud/aws/templates/aws_oidc/app.tf
+++ cloud/aws/templates/aws_oidc/app.tf
@@ -19,81 +19,24 @@ module "aws_cw_logs" {
   }
 }
 
-module "td" {
-  source          = "cn-terraform/ecs-fargate-task-definition/aws"
-  version         = "1.0.30"
-  name_prefix     = "${var.app_prefix}-civiform"
-  container_name  = "${var.app_prefix}-civiform"
-  container_image = "${var.civiform_image_repo}:${var.image_tag}"
+module "aws_scraper_logs" {
+  source    = "cn-terraform/cloudwatch-logs/aws"
+  version   = "1.0.12"
+  logs_path = "${var.app_prefix}-civiform-scraper-logs/"
+  tags = {
+    Name = "${var.app_prefix} Scraper Logs"
+    Type = "Scraper Logs"
+  }
+}
 
-  port_mappings = [
-    {
-      containerPort = var.port
-      hostPort      = var.port
-      protocol      = "tcp"
-    },
-    {
-      containerPort = 443
-      hostPort      = 443
-      protocol      = "tcp"
-    },
-  ]
+module "civiform_server_container_def" {
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
 
-  ecs_task_execution_role_custom_policies = [
-    jsonencode(
-      {
-        "Version" : "2012-10-17",
-        "Statement" : [
-          {
-            "Effect" : "Allow",
-            "Action" : [
-              "secretsmanager:GetSecretValue"
-            ],
-            "Resource" : [
-              aws_secretsmanager_secret.postgres_username_secret.arn,
-              aws_secretsmanager_secret.postgres_password_secret.arn,
-              aws_secretsmanager_secret.app_secret_key_secret.arn,
-              aws_secretsmanager_secret.api_secret_salt_secret.arn,
-              aws_secretsmanager_secret.adfs_secret_secret.arn,
-              aws_secretsmanager_secret.adfs_client_id_secret.arn,
-              aws_secretsmanager_secret.applicant_oidc_client_secret_secret.arn,
-              aws_secretsmanager_secret.applicant_oidc_client_id_secret.arn,
-            ]
-          },
-          {
-            "Effect" : "Allow",
-            "Action" : [
-              "kms:Encrypt",
-              "kms:Decrypt",
-              "kms:ReEncrypt*",
-              "kms:GenerateDataKey*",
-              "kms:DescribeKey"
-            ],
-            "Resource" : [aws_kms_key.civiform_kms_key.arn, aws_kms_key.file_storage_key.arn]
-          },
-          {
-            "Effect" : "Allow",
-            "Action" : [
-              "s3:*"
-            ],
-            "Resource" : [
-              aws_s3_bucket.civiform_files_s3.arn,
-              "${aws_s3_bucket.civiform_files_s3.arn}/*",
-            ]
-          },
-          {
-            "Effect" : "Allow",
-            "Action" : [
-              "ses:*"
-            ],
-            "Resource" : [
-              for email in module.email_service : email.email_arn
-            ]
-          },
-        ]
-      }
-    )
-  ]
+  container_name               = "${var.app_prefix}-civiform"
+  container_image              = "${var.civiform_image_repo}:${var.image_tag}"
+  container_memory             = 4096
+  container_memory_reservation = 2048
 
   secrets = [
     {
@@ -172,6 +115,20 @@ module "td" {
     CIVIFORM_API_KEYS_BAN_GLOBAL_SUBNET          = var.civiform_api_keys_ban_global_subnet
     CIVIFORM_SERVER_METRICS_ENABLED              = var.civiform_server_metrics_enabled
   }
+
+  port_mappings = [
+    {
+      containerPort = var.port
+      hostPort      = var.port
+      protocol      = "tcp"
+    },
+    {
+      containerPort = 443
+      hostPort      = 443
+      protocol      = "tcp"
+    },
+  ]
+
   log_configuration = {
     logDriver = "awslogs"
     options = {
@@ -188,10 +145,167 @@ module "td" {
     }
     secretOptions = null
   }
+}
+
+module "civiform_metrics_scraper_container_def" {
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  container_name               = "${var.app_prefix}-metrics-scraper"
+  container_image              = var.scraper_image
+  container_memory             = 2048
+  container_memory_reservation = 1024
+
+  map_environment = {
+    PROMETHEUS_WRITE_ENDPOINT = "${aws_prometheus_workspace.metrics.prometheus_endpoint}api/v1/remote_write"
+    AWS_REGION                = var.aws_region
+  }
+
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      "awslogs-region"        = var.aws_region
+      "awslogs-stream-prefix" = "ecs"
+      "awslogs-group"         = module.aws_scraper_logs.logs_path
+      "awslogs-create-group"  = "true"
+      # Use https://docs.docker.com/config/containers/logging/awslogs/#awslogs-multiline-pattern
+      # Logs are streamed via container's stdout. Each line is considered a
+      # separate log messsage. To collect stacktraces, which take multiple line,
+      # to a single event we consider all lines which start with a whitespace character to be
+      # part of the previous line and not a standalone event.
+      "awslogs-multiline-pattern" = "^[^\\s]"
+    }
+    secretOptions = null
+  }
+}
+
+locals {
+  name_prefix = "${var.app_prefix}-civiform"
+
   tags = {
     Name = "${var.app_prefix} Civiform EC2 Task Definition"
     Type = "Civiform EC2 Task Definition"
   }
+
+  civiform_ecs_task_execution_role_custom_policies = [
+    jsonencode(
+      {
+        "Version" : "2012-10-17",
+        "Statement" : [
+          {
+            "Effect" : "Allow",
+            "Action" : [
+              "secretsmanager:GetSecretValue"
+            ],
+            "Resource" : [
+              aws_secretsmanager_secret.postgres_username_secret.arn,
+              aws_secretsmanager_secret.postgres_password_secret.arn,
+              aws_secretsmanager_secret.app_secret_key_secret.arn,
+              aws_secretsmanager_secret.api_secret_salt_secret.arn,
+              aws_secretsmanager_secret.adfs_secret_secret.arn,
+              aws_secretsmanager_secret.adfs_client_id_secret.arn,
+              aws_secretsmanager_secret.applicant_oidc_client_secret_secret.arn,
+              aws_secretsmanager_secret.applicant_oidc_client_id_secret.arn,
+            ]
+          },
+          {
+            "Effect" : "Allow",
+            "Action" : [
+              "kms:Encrypt",
+              "kms:Decrypt",
+              "kms:ReEncrypt*",
+              "kms:GenerateDataKey*",
+              "kms:DescribeKey"
+            ],
+            "Resource" : [aws_kms_key.civiform_kms_key.arn, aws_kms_key.file_storage_key.arn]
+          },
+          {
+            "Effect" : "Allow",
+            "Action" : [
+              "s3:*"
+            ],
+            "Resource" : [
+              aws_s3_bucket.civiform_files_s3.arn,
+              "${aws_s3_bucket.civiform_files_s3.arn}/*",
+            ]
+          },
+          {
+            "Effect" : "Allow",
+            "Action" : [
+              "ses:*"
+            ],
+            "Resource" : [
+              for email in module.email_service : email.email_arn
+            ]
+          },
+          {
+            "Effect" : "Allow",
+            "Action" : [
+              "aps:RemoteWrite"
+            ],
+            "Resource" : "*"
+          }
+        ]
+      }
+    )
+  ]
+}
+
+resource "aws_iam_role" "civiform_ecs_task_execution_role" {
+  name               = "${local.name_prefix}-ecs-task-execution-role"
+  assume_role_policy = <<JSON
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs-tasks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole",
+            "Sid": ""
+        }
+      ]
+    }
+JSON
+  tags               = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "civiform_ecs_task_execution_role_policy_attach" {
+  role       = aws_iam_role.civiform_ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_policy" "civiform_ecs_task_execution_role_custom_policy" {
+  count       = length(local.civiform_ecs_task_execution_role_custom_policies)
+  name        = "${local.name_prefix}-ecs-task-execution-role-custom-policy-${count.index}"
+  description = "A custom policy for ${local.name_prefix}-ecs-task-execution-role IAM Role"
+  policy      = local.civiform_ecs_task_execution_role_custom_policies[count.index]
+  tags        = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "civiform_ecs_task_execution_role_custom_policy" {
+  count      = length(local.civiform_ecs_task_execution_role_custom_policies)
+  role       = aws_iam_role.civiform_ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.civiform_ecs_task_execution_role_custom_policy[count.index].arn
+}
+
+resource "aws_ecs_task_definition" "td" {
+  family = "${local.name_prefix}-td"
+
+  cpu    = 1024
+  memory = 6144
+
+  container_definitions = jsonencode([
+    module.civiform_server_container_def.json_map_object,
+    module.civiform_metrics_scraper_container_def.json_map_object
+  ])
+
+  task_role_arn            = aws_iam_role.civiform_ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.civiform_ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  tags                     = local.tags
 }
 
 module "ecs_fargate_service" {
@@ -202,7 +316,7 @@ module "ecs_fargate_service" {
   default_certificate_arn = var.ssl_certificate_arn
   ssl_policy              = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
   vpc_id                  = module.vpc.vpc_id
-  task_definition_arn     = module.td.aws_ecs_task_definition_td_arn
+  task_definition_arn     = aws_ecs_task_definition.td.arn
   container_name          = "${var.app_prefix}-civiform"
   ecs_cluster_name        = module.ecs_cluster.aws_ecs_cluster_cluster_name
   ecs_cluster_arn         = module.ecs_cluster.aws_ecs_cluster_cluster_arn
@@ -238,5 +352,26 @@ module "ecs_fargate_service" {
   tags = {
     Name = "${var.app_prefix} Civiform Fargate Service"
     Type = "Civiform Fargate Service"
+  }
+}
+
+resource "aws_lb_listener_rule" "block_external_traffic_to_metrics_rule" {
+  count        = length(module.ecs_fargate_service.lb_https_listeners_arns)
+  listener_arn = module.ecs_fargate_service.lb_https_listeners_arns[count.index]
+
+  action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Forbidden"
+      status_code  = "403"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/metrics"]
+    }
   }
 }
