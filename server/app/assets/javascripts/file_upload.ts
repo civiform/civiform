fe50bd746cf3f50056acd7d1813a04a4cf41bdb2
Jeannie Fu <jeanniefu@google.com>
2022-09-14 16:43:56 -0700

    bin/fmt

--- server/app/assets/javascripts/file_upload.ts
+++ server/app/assets/javascripts/file_upload.ts
@@ -43,7 +43,8 @@ function validateFileUploadQuestions(formEl: Element): boolean {
         const ariaDescribedBy = fileInput.getAttribute('aria-describedBy')
         fileInput.setAttribute(
           'aria-describedBy',
-          `${errorId} ${ariaDescribedBy}`)
+          `${errorId} ${ariaDescribedBy}`,
+        )
       }
     }
     isAllValid = isAllValid && isValid
