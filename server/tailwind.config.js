43d75a32820765f254ddbbb5b7507cccddf5c69c
Sami Abdul-Wahid <syntapy@pm.me>
2022-09-15 20:25:22 -0700

    update tailwind.config.js

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,43 +1,9 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './app/assets/javascripts/*.ts',
-      './app/views/style/Styles.java',
-      './app/views/style/BaseStyles.java',
-    ],
-    extract: {
-      java: (content) => {
-        return content
-      },
-    },
-    transform: {
-      java: (content) => {
-        const output = []
-        for (const match of content.matchAll(/"([\w-/:.]+)"/g)) {
-          output.push(match[1])
-          // We don't know which, if any, of these prefixes are in use for any class in particular.
-          // We therefore have to use every combination of them.
-          for (const prefix of [
-            'even',
-            'focus',
-            'focus-within',
-            'hover',
-            'disabled',
-            'sm',
-            'md',
-            'lg',
-            'xl',
-            '2xl',
-          ]) {
-            output.push(prefix + ':' + match[1])
-          }
-        }
-        return output
-      },
-    },
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './app/assets/javascripts/*.ts',
+    './app/views/style/Styles.java',
+    './app/views/style/BaseStyles.java',
+  ],
   theme: {
     extend: {
       colors: {
@@ -56,13 +22,6 @@ module.exports = {
           DEFAULT: '#4a148c',
         },
       },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['disabled', 'odd'],
-      textColor: ['disabled'],
-      opacity: ['disabled'],
     },
   },
   plugins: [require('@tailwindcss/line-clamp')],
