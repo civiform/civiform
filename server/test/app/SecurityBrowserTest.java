cc102c063f317713cbdb7813c71d777b17484ba6
Kiana McNellis <kiana@kmcnellis.com>
2022-09-20 15:40:32 -0500

    dev oidc needs correct logout params

--- server/test/app/SecurityBrowserTest.java
+++ server/test/app/SecurityBrowserTest.java
@@ -16,17 +16,9 @@ import repository.UserRepository;
 import services.WellKnownPaths;
 import support.CfTestHelpers;
 import org.fluentlenium.core.domain.FluentWebElement;
-import org.fluentlenium.core.wait.FluentWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SecurityBrowserTest extends BaseBrowserTest {
   private static UserRepository userRepository;
-  private static final Logger logger = LoggerFactory.getLogger(SecurityBrowserTest.class);
 
   @Override
   protected Application provideApplication() {
@@ -126,19 +118,12 @@ public class SecurityBrowserTest extends BaseBrowserTest {
     assertThat(browser.pageSource()).contains("You are logged in.");
     logout();
     assertThat(browser.url())
-        .contains("session/end?returnToURL=http%3A%2F%2Flocalhost%3A19001%2F&clientId=foo")
+        .contains("session/end?post_logout_redirect_uri=http%3A%2F%2Flocalhost%3A19001%2F&client_id=foo")
         .as("redirects to login provider");
     assertThat(browser.pageSource().contains("Do you want to sign-out from")).as("Confirm logout from dev-oidc");
     FluentWebElement continueButton = browser.$("button").first();
     assertThat(continueButton.textContent()).contains("Yes");
     continueButton.click();
-    // assertThat(browser.url())
-        // .contains("/session/end/success");
-    browser.waitUntil((WebDriver d) -> {
-      logger.warn("url " + d.getCurrentUrl());
-      logger.warn("d " + d.toString());
-      return d.getCurrentUrl().contains("/loginForm");
-      });
     assertThat(browser.url())
         .contains("/loginForm");
 
