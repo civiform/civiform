8fb0605d6166240fff483a654d28444ba5b62ed8
Sami Abdul-Wahid <syntapy@pm.me>
2022-09-20 09:52:57 -0700

    fmt

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -9,34 +9,34 @@ module.exports = {
     // since we generate those dynamically. See:
     //  https://tailwindcss.com/docs/content-configuration#customizing-extraction-logic
     //
-    // However, this results in a very large 3.1MB CSS file so we'll need to convert to 
-    // style literals to use the default extractor (as a separate PR) before this gets 
-    // pushed to prod. See: 
+    // However, this results in a very large 3.1MB CSS file so we'll need to convert to
+    // style literals to use the default extractor (as a separate PR) before this gets
+    // pushed to prod. See:
     //  https://github.com/civiform/civiform/pull/2630
     extract: {
-        java: (content) => {
-          const output = []
-          for (const match of content.matchAll(/"([\w-/:.]+)"/g)) {
-            output.push(match[1])
-            // We don't know which, if any, of these prefixes are in use for any class in particular.
-            // We therefore have to use every combination of them.
-            for (const prefix of [
-              'even',
-              'focus',
-              'focus-within',
-              'hover',
-              'disabled',
-              'sm',
-              'md',
-              'lg',
-              'xl',
-              '2xl',
-            ]) {
-              output.push(prefix + ':' + match[1])
-            }
+      java: (content) => {
+        const output = []
+        for (const match of content.matchAll(/"([\w-/:.]+)"/g)) {
+          output.push(match[1])
+          // We don't know which, if any, of these prefixes are in use for any class in particular.
+          // We therefore have to use every combination of them.
+          for (const prefix of [
+            'even',
+            'focus',
+            'focus-within',
+            'hover',
+            'disabled',
+            'sm',
+            'md',
+            'lg',
+            'xl',
+            '2xl',
+          ]) {
+            output.push(prefix + ':' + match[1])
           }
-          return output
-        },
+        }
+        return output
+      },
     },
   },
   theme: {
