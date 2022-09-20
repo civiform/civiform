140239e423ac4f4cb607d152510ac6844ae21668
Leyla Azizova <azizoval@google.com>
2022-09-20 14:27:42 -0700

    comment

--- server/app/featureflags/FeatureFlags.java
+++ server/app/featureflags/FeatureFlags.java
@@ -34,7 +34,11 @@ public final class FeatureFlags {
         && config.getBoolean(FEATURE_FLAG_OVERRIDES_ENABLED);
   }
 
-  /** If the Status Tracking feature is enabled. */
+  /**
+   * If the Status Tracking feature is enabled.
+   *
+   * <p>Allows for overrides set in {@code request}.
+   */
   public boolean isStatusTrackingEnabled(Request request) {
     return getFlagEnabled(request, APPLICATION_STATUS_TRACKING_ENABLED);
   }
