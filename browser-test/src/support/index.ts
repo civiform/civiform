f8348b435567a083c7b512d8aff12637d72ba4a1
Sami Abdul-Wahid <syntapy@pm.me>
2022-09-21 12:00:41 -0700

    set screenshot test failure threshold to zero

--- browser-test/src/support/index.ts
+++ browser-test/src/support/index.ts
@@ -461,7 +461,7 @@ export const validateScreenshot = async (
     allowSizeMismatch: true,
     // threshold is 1% it's pretty wide but there is some noise that we can't
     // explain
-    failureThreshold: 0.01,
+    failureThreshold: 0,
     failureThresholdType: 'percent',
     customSnapshotsDir: 'image_snapshots',
     customDiffDir: 'diff_output',
