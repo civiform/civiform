civiform/civiform

commit b0d4869dde139786cf9507992c9a29c1a42417f5

Conditionally adding application status to applicant-facing view. (#3447)
### Description

If an application status exists, display it in the applicant-facing program list.

### Screenshots
![Screen Shot 2022-09-20 at 12 19 10 PM](https://user-images.githubusercontent.com/1870301/191345642-15b3aec4-6086-4b42-9031-98361e38174c.png)

## Release notes:

If an application status exists, display it in the applicant-facing program list.

### Checklist

- [X] Added the correct label: < feature | bug | dependencies | infrastructure | ignore-for-release >
- [X] Created tests which fail without the change (if possible)
- [ ] Extended the README / documentation, if necessary

### Issue(s)

Fixes #3270

browser-test/src/support/index.ts
@@ -449,6 +449,7 @@ export const validateScreenshot = async (
       // regexp will break and it will show up in screenshots.
       date.textContent = date
         .textContent!.replace(/\d{4}\/\d{2}\/\d{2}/, '2030/01/01')
+        .replace(/\d{1,2}\/\d{1,2}\/\d{2}/, '1/1/30')
         .replace(/\d{1,2}:\d{2} (PM|AM)/, '11:22 PM')
     }
   })

browser-test/src/applicant_application_statuses.test.ts
@@ -1,0 +1,56 @@
+import {
+  createTestContext,
+  enableFeatureFlag,
+  loginAsAdmin,
+  loginAsProgramAdmin,
+  loginAsTestUser,
+  logout,
+  selectApplicantLanguage,
+  testUserDisplayName,
+  validateAccessibility,
+  validateScreenshot,
+} from './support'
+
+describe('with program statuses', () => {
+  const ctx = createTestContext(/* clearDb= */ false)
+
+  const programName = 'applicant-with-statuses-program'
+  const approvedStatusName = 'Approved'
+
+  beforeAll(async () => {
+    const {page, adminPrograms, adminProgramStatuses, applicantQuestions} = ctx
+    await loginAsAdmin(page)
+
+    await enableFeatureFlag(page, 'application_status_tracking_enabled')
+    await adminPrograms.addProgram(programName)
+    await adminPrograms.gotoDraftProgramManageStatusesPage(programName)
+    await adminProgramStatuses.createStatus(approvedStatusName)
+    await adminPrograms.publishProgram(programName)
+    await adminPrograms.expectActiveProgram(programName)
+    await logout(page)
+
+    // Submit an application as a test user so that we can navigate back to the applications page.
+    await loginAsTestUser(page)
+    await selectApplicantLanguage(page, 'English')
+    await applicantQuestions.clickApplyProgramButton(programName)
+    await applicantQuestions.submitFromPreviewPage()
+    await logout(page)
+
+    // Navigate to the submitted application as the program admin and set a status.
+    await loginAsProgramAdmin(page)
+    await adminPrograms.viewApplications(programName)
+    await adminPrograms.viewApplicationForApplicant(testUserDisplayName())
+    const modal = await adminPrograms.setStatusOptionAndAwaitModal(
+      approvedStatusName,
+    )
+    await adminPrograms.confirmStatusUpdateModal(modal)
+    await logout(page)
+  })
+
+  it('displays status and passes accessibility checks', async () => {
+    const {page} = ctx
+    await loginAsTestUser(page)
+    await validateAccessibility(page)
+    await validateScreenshot(page, 'program-list-with-status')
+  })
+})

server/app/repository/ApplicationRepository.java
@@ -251,6 +251,7 @@ public final class ApplicationRepository {
               .query()
               // Eagerly fetch the program in a SQL join.
               .fetch("program")
+              .fetch("applicationEvents")
               .findSet()
               .stream()
               .collect(ImmutableSet.toImmutableSet());

server/app/services/applicant/ApplicantService.java
@@ -52,6 +52,7 @@ import services.program.PathNotInBlockException;
 import services.program.ProgramDefinition;
 import services.program.ProgramNotFoundException;
 import services.program.ProgramService;
+import services.program.StatusDefinitions;
 import services.question.exceptions.UnsupportedScalarTypeException;
 import services.question.types.ScalarType;
 
@@ -551,14 +552,32 @@ public final class ApplicantService {
               maybeSubmittedApp.map(Application::getSubmitTime);
           if (maybeDraftApp.isPresent()) {
             inProgressPrograms.add(
-                ApplicantProgramData.create(
-                    maybeDraftApp.get().getProgram().getProgramDefinition(),
-                    latestSubmittedApplicationTime));
+                ApplicantProgramData.builder()
+                    .setProgram(maybeDraftApp.get().getProgram().getProgramDefinition())
+                    .setLatestSubmittedApplicationTime(latestSubmittedApplicationTime)
+                    .build());
             programNamesWithApplications.add(programName);
           } else if (maybeSubmittedApp.isPresent() && activeProgramNames.containsKey(programName)) {
-            ProgramDefinition programDef = activeProgramNames.get(programName);
+            // When extracting the application status, the definitions associated with the program
+            // version at the time of submission are used. However, when clicking "reapply", we use
+            // the latest program version below.
+            ProgramDefinition applicationProgramVersion =
+                maybeSubmittedApp.get().getProgram().getProgramDefinition();
+            Optional<String> maybeLatestStatus = maybeSubmittedApp.get().getLatestStatus();
+            Optional<StatusDefinitions.Status> maybeCurrentStatus =
+                maybeLatestStatus.isPresent()
+                    ? applicationProgramVersion.statusDefinitions().getStatuses().stream()
+                        .filter(
+                            programStatus ->
+                                programStatus.statusText().equals(maybeLatestStatus.get()))
+                        .findFirst()
+                    : Optional.empty();
             submittedPrograms.add(
-                ApplicantProgramData.create(programDef, latestSubmittedApplicationTime));
+                ApplicantProgramData.builder()
+                    .setProgram(activeProgramNames.get(programName))
+                    .setLatestSubmittedApplicationTime(latestSubmittedApplicationTime)
+                    .setLatestSubmittedApplicationStatus(maybeCurrentStatus)
+                    .build());
             programNamesWithApplications.add(programName);
           }
         });
@@ -568,7 +587,9 @@ public final class ApplicantService {
     unappliedActivePrograms.forEach(
         programName -> {
           unappliedPrograms.add(
-              ApplicantProgramData.create(activeProgramNames.get(programName), Optional.empty()));
+              ApplicantProgramData.builder()
+                  .setProgram(activeProgramNames.get(programName))
+                  .build());
         });
 
     // Ensure each list is ordered by database ID for consistent ordering.
@@ -632,10 +653,21 @@ public final class ApplicantService {
 
     public abstract Optional<Instant> latestSubmittedApplicationTime();
 
-    static ApplicantProgramData create(
-        ProgramDefinition program, Optional<Instant> latestSubmittedApplicationTime) {
-      return new AutoValue_ApplicantService_ApplicantProgramData(
-          program, latestSubmittedApplicationTime);
+    public abstract Optional<StatusDefinitions.Status> latestSubmittedApplicationStatus();
+
+    static Builder builder() {
+      return new AutoValue_ApplicantService_ApplicantProgramData.Builder();
+    }
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder setProgram(ProgramDefinition v);
+
+      abstract Builder setLatestSubmittedApplicationTime(Optional<Instant> v);
+
+      abstract Builder setLatestSubmittedApplicationStatus(Optional<StatusDefinitions.Status> v);
+
+      abstract ApplicantProgramData build();
     }
   }
 

server/app/views/applicant/ProgramIndexView.java
@@ -9,6 +9,7 @@ import static j2html.TagCreator.h2;
 import static j2html.TagCreator.h3;
 import static j2html.TagCreator.h4;
 import static j2html.TagCreator.img;
+import static j2html.TagCreator.p;
 import static j2html.TagCreator.span;
 import static j2html.TagCreator.text;
 
@@ -20,6 +21,7 @@ import j2html.tags.specialized.ATag;
 import j2html.tags.specialized.DivTag;
 import j2html.tags.specialized.H1Tag;
 import j2html.tags.specialized.ImgTag;
+import j2html.tags.specialized.PTag;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -37,6 +39,7 @@ import play.twirl.api.Content;
 import services.MessageKey;
 import services.applicant.ApplicantService;
 import services.program.ProgramDefinition;
+import services.program.StatusDefinitions;
 import views.BaseHtmlView;
 import views.HtmlBundle;
 import views.TranslationUtils;
@@ -280,8 +283,13 @@ public final class ProgramIndexView extends BaseHtmlView {
     DivTag programData =
         div()
             .withId(baseId + "-data")
-            .withClasses(Styles.W_FULL, Styles.PX_4, Styles.OVERFLOW_AUTO)
-            .with(title, description);
+            .withClasses(Styles.W_FULL, Styles.PX_4, Styles.OVERFLOW_AUTO);
+    if (cardData.latestSubmittedApplicationStatus().isPresent()) {
+      programData.with(
+          programCardApplicationStatus(
+              preferredLocale, cardData.latestSubmittedApplicationStatus().get()));
+    }
+    programData.with(title, description);
 
     // Add info link.
     String infoUrl =
@@ -371,6 +379,20 @@ public final class ProgramIndexView extends BaseHtmlView {
         .with(actionDiv);
   }
 
+  private PTag programCardApplicationStatus(
+      Locale preferredLocale, StatusDefinitions.Status status) {
+    return p().withClasses(
+            Styles.BORDER,
+            Styles.ROUNDED_LG,
+            Styles.PX_2,
+            Styles.PY_1,
+            Styles.MB_4,
+            Styles.BG_BLUE_100)
+        .with(
+            span(status.localizedStatusText().getOrDefault(preferredLocale))
+                .withClasses(Styles.TEXT_XS, Styles.FONT_MEDIUM));
+  }
+
   private DivTag programCardSubmittedDate(Messages messages, Instant submittedDate) {
     TranslationUtils.TranslatedStringSplitResult translateResult =
         TranslationUtils.splitTranslatedSingleArgString(messages, MessageKey.SUBMITTED_DATE);
@@ -388,7 +410,8 @@ public final class ProgramIndexView extends BaseHtmlView {
                 // SHORT will print dates as 1/2/2022.
                 FormatStyle.SHORT)
             .format(dateTime);
-    submittedComponents.add(span(formattedSubmitTime).withClass(Styles.FONT_SEMIBOLD));
+    submittedComponents.add(
+        span(formattedSubmitTime).withClasses(ReferenceClasses.BT_DATE, Styles.FONT_SEMIBOLD));
 
     if (!afterContent.isEmpty()) {
       submittedComponents.add(text(afterContent));

commit 2c60b58891f5b21467087e00ae25ed541cd2deb6

Setting tests to fail if screenshots have any differences rather than the previous value of 1% (#3486)
### Description

While here, there were two additional fixes due to needing screenshot updates:
  * Add a `cf-application-id` class that can be used to replace the dynamic application ID shown when the applicant submits an application. This is replaced prior to a screenshot being taken
  * Fixing issue where the new date format regexp introduced by #3447 errantly matched the date format from the previous regexp

If we determine that screenshots are flaky with threshold=0, we can increase it again. Hopefully, we can add code to deflake rather than increasing the threshold. Additionally, another source of very small rendering differences could be when the underlying Playwright binary updates the set of browsers it uses. A new version of Chromium wouldn't produce a large difference in screenshots, but it could produce a few pixels of difference, especially around anti-aliasing.

Note: There was a single screenshot failure `enumerator-only-active-snap.png` that seemed to have only 4 pixels of difference near rounded corners for the program card and the "Create new question" button. I'm suspicious that this is the anti-aliasing issue described above.

## Release notes:

N/A

### Testing

Ran CI on this PR 5 times and confirmed that there weren't any failures due to screenshot differences.

Run 1: https://github.com/civiform/civiform/actions/runs/3108260833/jobs/5037261281
Run 2: https://github.com/civiform/civiform/actions/runs/3108260833/jobs/5037729580
Run 3: https://github.com/civiform/civiform/actions/runs/3108260833/jobs/5038019647
Run 4: https://github.com/civiform/civiform/actions/runs/3108260833/jobs/5038283397
Run 5: https://github.com/civiform/civiform/actions/runs/3108260833/jobs/5038557633 (failed with standard flake, rerun was successful: https://github.com/civiform/civiform/actions/runs/3108260833/jobs/5038869395)
### Checklist

server/app/views/applicant/ApplicantUpsellCreateAccountView.java
@@ -20,6 +20,7 @@ import views.HtmlBundle;
 import views.components.LinkElement;
 import views.components.ToastMessage;
 import views.style.ApplicantStyles;
+import views.style.ReferenceClasses;
 import views.style.StyleUtils;
 import views.style.Styles;
 
@@ -101,7 +102,7 @@ public final class ApplicantUpsellCreateAccountView extends BaseHtmlView {
             .with(
                 div(messages.at(
                         MessageKey.CONTENT_CONFIRMED.getKeyName(), programTitle, applicationId))
-                    .withClasses(Styles.TEXT_LG));
+                    .withClasses(ReferenceClasses.BT_APPLICATION_ID, Styles.TEXT_LG));
 
     // Don't show "create an account" upsell box to TIs, or anyone with an email address already.
     if (Strings.isNullOrEmpty(account.getEmailAddress()) && account.getMemberOfGroup().isEmpty()) {

server/app/views/style/ReferenceClasses.java
@@ -126,7 +126,8 @@ public final class ReferenceClasses {
   public static final String MODAL = "cf-modal";
   public static final String MODAL_CLOSE = "cf-modal-close";
   public static final String MODAL_DISPLAY_ON_LOAD = "cf-modal-display-on-load";
-  // This class marks fields that contain dates. It is used by browser tests
-  // to replace content with static date when taking screenshot.
+  // These classes mark fields that need dynamic replacement by browser tests when
+  // taking a screenshot (e.g. dates, application IDs).
   public static final String BT_DATE = "cf-bt-date";
+  public static final String BT_APPLICATION_ID = "cf-application-id";
 }

browser-test/src/support/index.ts
@@ -439,18 +439,28 @@ export const validateScreenshot = async (
   if (DISABLE_SCREENSHOTS) {
     return
   }
-  // To make screenshots stable go through all date fields (elements that have cf-bt-date class)
-  // and replace date/time with fixed text.
   const page = 'page' in element ? element.page() : element
   await page.evaluate(() => {
+    // To make screenshots stable go through all date fields (elements that have cf-bt-date class)
+    // and replace date/time with fixed text.
     for (const date of Array.from(document.querySelectorAll('.cf-bt-date'))) {
       // Use regexp replacement instead of full replacement to make sure that format of the text
       // matches what we expect. In case underlying format changes to "September 20, 2022" then
       // regexp will break and it will show up in screenshots.
       date.textContent = date
         .textContent!.replace(/\d{4}\/\d{2}\/\d{2}/, '2030/01/01')
-        .replace(/\d{1,2}\/\d{1,2}\/\d{2}/, '1/1/30')
+        .replace(/^(\d{1,2}\/\d{1,2}\/\d{2})$/, '1/1/30')
         .replace(/\d{1,2}:\d{2} (PM|AM)/, '11:22 PM')
+    }
+    // Go through all application ID fields (elements that have cf-application-id class) and
+    // replace the ID with a stable value.
+    for (const applicationId of Array.from(
+      document.querySelectorAll('.cf-application-id'),
+    )) {
+      applicationId.textContent = applicationId.textContent!.replace(
+        /\d+/,
+        '1234',
+      )
     }
   })
   expect(screenshotFileName).toMatch(/[a-z0-9-]+/)
@@ -460,9 +470,7 @@ export const validateScreenshot = async (
     }),
   ).toMatchImageSnapshot({
     allowSizeMismatch: true,
-    // threshold is 1% it's pretty wide but there is some noise that we can't
-    // explain
-    failureThreshold: 0.01,
+    failureThreshold: 0,
     failureThresholdType: 'percent',
     customSnapshotsDir: 'image_snapshots',
     customDiffDir: 'diff_output',