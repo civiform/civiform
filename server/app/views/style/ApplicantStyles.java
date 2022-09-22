civiform/civiform

commit b0d4869dde139786cf9507992c9a29c1a42417f5

Conditionally adding application status to applicant-facing view. (#3447)
### Description

If an application status exists, display it in the applicant-facing program list.

### Screenshots
![Screen Shot 2022-09-20 at 12 19 10 PM](https://user-images.githubusercontent.com/1870301/191345642-15b3aec4-6086-4b42-9031-98361e38174c.png)

## Release notes:

If an application status exists, display it in the applicant-facing program list.

### Checklist

- [X] Added the correct label: < feature | bug | dependencies | infrastructure | ignore-for-release >
- [X] Created tests which fail without the change (if possible)
- [ ] Extended the README / documentation, if necessary

### Issue(s)

Fixes #3270

server/app/views/style/ApplicantStyles.java
@@ -49,8 +49,8 @@ public final class ApplicantStyles {
   public static final String PROGRAM_CARD =
       StyleUtils.joinStyles(
           Styles.W_FULL,
-          StyleUtils.responsiveSmall(Styles.W_72),
-          Styles.H_72,
+          StyleUtils.responsiveSmall(Styles.W_96),
+          Styles.H_80,
           Styles.BG_WHITE,
           Styles.ROUNDED_XL,
           Styles.SHADOW_MD,

browser-test/src/applicant_application_statuses.test.ts
@@ -1,0 +1,56 @@
+import {
+  createTestContext,
+  enableFeatureFlag,
+  loginAsAdmin,
+  loginAsProgramAdmin,
+  loginAsTestUser,
+  logout,
+  selectApplicantLanguage,
+  testUserDisplayName,
+  validateAccessibility,
+  validateScreenshot,
+} from './support'
+
+describe('with program statuses', () => {
+  const ctx = createTestContext(/* clearDb= */ false)
+
+  const programName = 'applicant-with-statuses-program'
+  const approvedStatusName = 'Approved'
+
+  beforeAll(async () => {
+    const {page, adminPrograms, adminProgramStatuses, applicantQuestions} = ctx
+    await loginAsAdmin(page)
+
+    await enableFeatureFlag(page, 'application_status_tracking_enabled')
+    await adminPrograms.addProgram(programName)
+    await adminPrograms.gotoDraftProgramManageStatusesPage(programName)
+    await adminProgramStatuses.createStatus(approvedStatusName)
+    await adminPrograms.publishProgram(programName)
+    await adminPrograms.expectActiveProgram(programName)
+    await logout(page)
+
+    // Submit an application as a test user so that we can navigate back to the applications page.
+    await loginAsTestUser(page)
+    await selectApplicantLanguage(page, 'English')
+    await applicantQuestions.clickApplyProgramButton(programName)
+    await applicantQuestions.submitFromPreviewPage()
+    await logout(page)
+
+    // Navigate to the submitted application as the program admin and set a status.
+    await loginAsProgramAdmin(page)
+    await adminPrograms.viewApplications(programName)
+    await adminPrograms.viewApplicationForApplicant(testUserDisplayName())
+    const modal = await adminPrograms.setStatusOptionAndAwaitModal(
+      approvedStatusName,
+    )
+    await adminPrograms.confirmStatusUpdateModal(modal)
+    await logout(page)
+  })
+
+  it('displays status and passes accessibility checks', async () => {
+    const {page} = ctx
+    await loginAsTestUser(page)
+    await validateAccessibility(page)
+    await validateScreenshot(page, 'program-list-with-status')
+  })
+})

browser-test/src/support/index.ts
@@ -449,6 +449,7 @@ export const validateScreenshot = async (
       // regexp will break and it will show up in screenshots.
       date.textContent = date
         .textContent!.replace(/\d{4}\/\d{2}\/\d{2}/, '2030/01/01')
+        .replace(/\d{1,2}\/\d{1,2}\/\d{2}/, '1/1/30')
         .replace(/\d{1,2}:\d{2} (PM|AM)/, '11:22 PM')
     }
   })

server/app/repository/ApplicationRepository.java
@@ -251,6 +251,7 @@ public final class ApplicationRepository {
               .query()
               // Eagerly fetch the program in a SQL join.
               .fetch("program")
+              .fetch("applicationEvents")
               .findSet()
               .stream()
               .collect(ImmutableSet.toImmutableSet());

server/app/services/applicant/ApplicantService.java
@@ -52,6 +52,7 @@ import services.program.PathNotInBlockException;
 import services.program.ProgramDefinition;
 import services.program.ProgramNotFoundException;
 import services.program.ProgramService;
+import services.program.StatusDefinitions;
 import services.question.exceptions.UnsupportedScalarTypeException;
 import services.question.types.ScalarType;
 
@@ -551,14 +552,32 @@ public final class ApplicantService {
               maybeSubmittedApp.map(Application::getSubmitTime);
           if (maybeDraftApp.isPresent()) {
             inProgressPrograms.add(
-                ApplicantProgramData.create(
-                    maybeDraftApp.get().getProgram().getProgramDefinition(),
-                    latestSubmittedApplicationTime));
+                ApplicantProgramData.builder()
+                    .setProgram(maybeDraftApp.get().getProgram().getProgramDefinition())
+                    .setLatestSubmittedApplicationTime(latestSubmittedApplicationTime)
+                    .build());
             programNamesWithApplications.add(programName);
           } else if (maybeSubmittedApp.isPresent() && activeProgramNames.containsKey(programName)) {
-            ProgramDefinition programDef = activeProgramNames.get(programName);
+            // When extracting the application status, the definitions associated with the program
+            // version at the time of submission are used. However, when clicking "reapply", we use
+            // the latest program version below.
+            ProgramDefinition applicationProgramVersion =
+                maybeSubmittedApp.get().getProgram().getProgramDefinition();
+            Optional<String> maybeLatestStatus = maybeSubmittedApp.get().getLatestStatus();
+            Optional<StatusDefinitions.Status> maybeCurrentStatus =
+                maybeLatestStatus.isPresent()
+                    ? applicationProgramVersion.statusDefinitions().getStatuses().stream()
+                        .filter(
+                            programStatus ->
+                                programStatus.statusText().equals(maybeLatestStatus.get()))
+                        .findFirst()
+                    : Optional.empty();
             submittedPrograms.add(
-                ApplicantProgramData.create(programDef, latestSubmittedApplicationTime));
+                ApplicantProgramData.builder()
+                    .setProgram(activeProgramNames.get(programName))
+                    .setLatestSubmittedApplicationTime(latestSubmittedApplicationTime)
+                    .setLatestSubmittedApplicationStatus(maybeCurrentStatus)
+                    .build());
             programNamesWithApplications.add(programName);
           }
         });
@@ -568,7 +587,9 @@ public final class ApplicantService {
     unappliedActivePrograms.forEach(
         programName -> {
           unappliedPrograms.add(
-              ApplicantProgramData.create(activeProgramNames.get(programName), Optional.empty()));
+              ApplicantProgramData.builder()
+                  .setProgram(activeProgramNames.get(programName))
+                  .build());
         });
 
     // Ensure each list is ordered by database ID for consistent ordering.
@@ -632,10 +653,21 @@ public final class ApplicantService {
 
     public abstract Optional<Instant> latestSubmittedApplicationTime();
 
-    static ApplicantProgramData create(
-        ProgramDefinition program, Optional<Instant> latestSubmittedApplicationTime) {
-      return new AutoValue_ApplicantService_ApplicantProgramData(
-          program, latestSubmittedApplicationTime);
+    public abstract Optional<StatusDefinitions.Status> latestSubmittedApplicationStatus();
+
+    static Builder builder() {
+      return new AutoValue_ApplicantService_ApplicantProgramData.Builder();
+    }
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder setProgram(ProgramDefinition v);
+
+      abstract Builder setLatestSubmittedApplicationTime(Optional<Instant> v);
+
+      abstract Builder setLatestSubmittedApplicationStatus(Optional<StatusDefinitions.Status> v);
+
+      abstract ApplicantProgramData build();
     }
   }
 

commit 1fc68c260602ccca680e848c7a4ff19a82af50ec

Resize applicant-facing program card width  (#3493)
### Description

This is fallout from https://github.com/civiform/civiform/pull/3447, where the program card was resized from `w-72` to `w-96`. The previous hard-coded value was closely tied to the grid layout we set for each card that renders. This reverts the width back to `w-72`, since the increase in size wasn't strictly necessary.

### Screenshots

Before:
![Screen Shot 2022-09-22 at 1 13 59 PM](https://user-images.githubusercontent.com/1870301/191845064-7e459e8a-9503-48b6-b329-aef997514389.png)

After:
![Screen Shot 2022-09-22 at 1 36 51 PM](https://user-images.githubusercontent.com/1870301/191846203-e4e7caca-3d83-479a-ace1-2f190a162678.png)

## Release notes:

Fixes issue where applicant-facing program list items overlapped on smaller screen sizes.

### Checklist

- [X] Added the correct label: < feature | bug | dependencies | infrastructure | ignore-for-release >
- [ ] Created tests which fail without the change (if possible)
- [ ] Extended the README / documentation, if necessary

### Issue(s)
#3492

server/app/views/style/ApplicantStyles.java
@@ -49,7 +49,9 @@ public final class ApplicantStyles {
   public static final String PROGRAM_CARD =
       StyleUtils.joinStyles(
           Styles.W_FULL,
-          StyleUtils.responsiveSmall(Styles.W_96),
+          // This width is closely tied to the grid layout in ProgramIndexView.java. Increasing the
+          // card size may cause them to overlap on smaller screen sizes.
+          StyleUtils.responsiveSmall(Styles.W_72),
           Styles.H_80,
           Styles.BG_WHITE,
           Styles.ROUNDED_XL,