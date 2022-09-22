civiform/civiform

commit ca7024022015be90344738652590fbf852d6cf6e

Applicant-facing program index page contains info for previously submitted applications (#2695)
* Expose dev-oidc port to unblock local testing of login.

* Retrieving active applications. Adding a new 'Submitted' section and changing button/section titles.

* Add submitted date to cards.

* Allow submitted date to still bold the date while handling cases where the date may occur before the 'submitted' text in translated languages.

* Moving responsibility for finding relevant programs into ApplicantServiceImpl. Also including programs that have a draft / submitted application but are hidden from the index.

* bin/fmt

* Move all program splitting into ApplicantService for easier testing. This method was already highly specific, so there's no reason to split the logic across it and ProgramIndexView.

* bin/fmt

* Fix issue where already submitted applications weren't being rendered in the correct section.

* bin/fmt

* Adding distinct messages / TODOs while we await translations.

* Mark classes as final.

* Adding unit tests.

* Preventing runtime exception for multiple active programs with the same name.

* Using more fluent interfaces.

* Splitting the placeholder splitting into a separate helper.

* Adding TranslationUtils test. Also used a randomly generated placeholder for replacement.

* Improving test coverage for already-submitted applications.

* Restoring filtering based on relevant programs.

* Move duplicate checking into separate function. Also make it print the list of duplicate programs instead of printing one by one.

* Add comments / reorganize.

* Use the new section titles.

* PR feedback.

* Hiding submitted applications for hidden programs.

* bin/fmt

* PR feedback for typos.

* Rename variables for readability.

* Fixing method name.

* Sorting by (submit_time, created_time) rather than database ID when finding the most recent application.

server/app/repository/ApplicationRepository.java
@@ -4,6 +4,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import io.ebean.DB;
 import io.ebean.Database;
 import io.ebean.ExpressionList;
@@ -26,7 +27,7 @@ import services.program.ProgramNotFoundException;
  * ApplicationRepository performs complicated operations on {@link Application} that often involve
  * other EBean models or asynchronous handling.
  */
-public class ApplicationRepository {
+public final class ApplicationRepository {
   private final ProgramRepository programRepository;
   private final UserRepository userRepository;
   private final Database database;
@@ -231,6 +232,29 @@ public class ApplicationRepository {
   public CompletionStage<Optional<Application>> getApplication(long applicationId) {
     return supplyAsync(
         () -> database.find(Application.class).setId(applicationId).findOneOrEmpty(),
+        executionContext.current());
+  }
+
+  /**
+   * Get all applications with the specified {@link LifecyleStage}s for an applicant.
+   *
+   * <p>The {@link Program} associated with the application is eagerly loaded.
+   */
+  public CompletionStage<ImmutableSet<Application>> getApplicationsForApplicant(
+      long applicantId, ImmutableSet<LifecycleStage> stages) {
+    return supplyAsync(
+        () -> {
+          return database
+              .find(Application.class)
+              .where()
+              .isIn("lifecycle_stage", stages)
+              .query()
+              // Eagerly fetch the program in a SQL join.
+              .fetch("program")
+              .findSet()
+              .stream()
+              .collect(ImmutableSet.toImmutableSet());
+        },
         executionContext.current());
   }
 }

commit e5588b9c48ace3eb974e3ff615885d5ec4ccb364

Fix call for program index page to properly filter to the set associated with the applicant (#3487)
### Description
This was a regression introduced by #2695.

## Release notes:

Fixes issue where the applicant-facing program index page errantly categorized programs as in-progress / submitted if any applicant had an in-progress / submitted application for that program.

In addition, the application submission date and current application status could be shown for these programs, but no other application data would have been visible.

### Checklist

- [X] Added the correct label: < feature | bug | dependencies | infrastructure | ignore-for-release >
- [X] Created tests which fail without the change (if possible)
- [ ] Extended the README / documentation, if necessary

### Issue(s)

Fixes #<issue_number>; Fixes #<issue_number>; Fixes #<issue_number>...

server/app/repository/ApplicationRepository.java
@@ -247,6 +247,7 @@ public final class ApplicationRepository {
           return database
               .find(Application.class)
               .where()
+              .eq("applicant.id", applicantId)
               .isIn("lifecycle_stage", stages)
               .query()
               // Eagerly fetch the program in a SQL join.

server/test/repository/ApplicationRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import java.time.Instant;
 import java.util.Optional;
 import models.Applicant;
@@ -35,7 +36,7 @@ public class ApplicationRepositoryTest extends ResetPostgres {
   @Test
   public void submitApplication_updatesOtherApplicationVersions() {
     Applicant applicant = saveApplicant("Alice");
-    Program program = createDraftProgram("Program");
+    Program program = createProgram("Program");
 
     Application appOne =
         repo.submitApplication(applicant, program, Optional.empty()).toCompletableFuture().join();
@@ -74,8 +75,8 @@ public class ApplicationRepositoryTest extends ResetPostgres {
     Applicant applicant1 = saveApplicant("Alice");
     Applicant applicant2 = saveApplicant("Bob");
 
-    Program program1 = createDraftProgram("Program");
-    Program program2 = createDraftProgram("OtherProgram");
+    Program program1 = createProgram("Program");
+    Program program2 = createProgram("OtherProgram");
 
     repo.createOrUpdateDraft(applicant1, program1).toCompletableFuture().join();
 
@@ -92,7 +93,7 @@ public class ApplicationRepositoryTest extends ResetPostgres {
   @Test
   public void createOrUpdateDraftApplication_updatesExistingDraft() {
     Applicant applicant = saveApplicant("Alice");
-    Program program = createDraftProgram("Program");
+    Program program = createProgram("Program");
     Application appDraft1 =
         repo.createOrUpdateDraft(applicant, program).toCompletableFuture().join();
     Application appDraft2 =
@@ -106,7 +107,7 @@ public class ApplicationRepositoryTest extends ResetPostgres {
   @Test
   public void submitApplication_twoDraftsThrowsException() {
     Applicant applicant = saveApplicant("Alice");
-    Program program = createDraftProgram("Program");
+    Program program = createProgram("Program");
     Application appDraft1 = Application.create(applicant, program, LifecycleStage.DRAFT);
     appDraft1.save();
     Application appDraft2 = Application.create(applicant, program, LifecycleStage.DRAFT);
@@ -140,7 +141,7 @@ public class ApplicationRepositoryTest extends ResetPostgres {
   @Test
   public void submitApplication_noDrafts() {
     Applicant applicant = saveApplicant("Alice");
-    Program program = createDraftProgram("Program");
+    Program program = createProgram("Program");
     Application app =
         repo.submitApplication(applicant, program, Optional.empty()).toCompletableFuture().join();
     assertThat(repo.getApplication(app.id).toCompletableFuture().join().get().getLifecycleStage())
@@ -165,8 +166,8 @@ public class ApplicationRepositoryTest extends ResetPostgres {
 
   @Test
   public void getApplications() {
-    Program programOne = createDraftProgram("first");
-    Program programTwo = createDraftProgram("second");
+    Program programOne = createProgram("first");
+    Program programTwo = createProgram("second");
 
     Instant yesterday = dateConverter.parseIso8601DateToStartOfDateInstant("2022-01-02");
     Instant today = dateConverter.parseIso8601DateToStartOfDateInstant("2022-01-03");
@@ -216,6 +217,98 @@ public class ApplicationRepositoryTest extends ResetPostgres {
     assertThat(repo.getApplications(restrictiveFilter)).isEmpty();
   }
 
+  @Test
+  public void getApplicationsForApplicant() throws Exception {
+    Applicant applicant = saveApplicant("Applicant");
+
+    Program program = createProgram("Program");
+    Application appDraft1 = Application.create(applicant, program, LifecycleStage.DRAFT);
+    appDraft1.save();
+    Application appDraft2 = Application.create(applicant, program, LifecycleStage.DRAFT);
+    appDraft2.save();
+    Application appActive1 = Application.create(applicant, program, LifecycleStage.ACTIVE);
+    appActive1.save();
+    Application appActive2 = Application.create(applicant, program, LifecycleStage.ACTIVE);
+    appActive2.save();
+    Application appObsolete1 = Application.create(applicant, program, LifecycleStage.OBSOLETE);
+    appObsolete1.save();
+    Application appObsolete2 = Application.create(applicant, program, LifecycleStage.OBSOLETE);
+    appObsolete2.save();
+
+    ImmutableSet<Application> result =
+        repo.getApplicationsForApplicant(
+                applicant.id,
+                ImmutableSet.of(
+                    LifecycleStage.DRAFT, LifecycleStage.ACTIVE, LifecycleStage.OBSOLETE))
+            .toCompletableFuture()
+            .get();
+    assertThat(result.stream().map(a -> a.id))
+        .containsExactlyInAnyOrder(
+            appDraft1.id,
+            appDraft2.id,
+            appActive1.id,
+            appActive2.id,
+            appObsolete1.id,
+            appObsolete2.id);
+
+    result =
+        repo.getApplicationsForApplicant(applicant.id, ImmutableSet.of(LifecycleStage.DRAFT))
+            .toCompletableFuture()
+            .get();
+    assertThat(result.stream().map(a -> a.id))
+        .containsExactlyInAnyOrder(appDraft1.id, appDraft2.id);
+
+    result =
+        repo.getApplicationsForApplicant(applicant.id, ImmutableSet.of())
+            .toCompletableFuture()
+            .get();
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  public void getApplicationsForApplicant_filtersById() throws Exception {
+    Applicant primaryApplicant = saveApplicant("Applicant");
+    Applicant otherApplicant = saveApplicant("Other");
+
+    Program program = createProgram("Program");
+    Application primaryApplicantDraftApp =
+        Application.create(primaryApplicant, program, LifecycleStage.DRAFT);
+    primaryApplicantDraftApp.save();
+    Application otherApplicantActiveApp =
+        Application.create(otherApplicant, program, LifecycleStage.ACTIVE);
+    otherApplicantActiveApp.save();
+
+    ImmutableSet<Application> result =
+        repo.getApplicationsForApplicant(
+                otherApplicant.id,
+                ImmutableSet.of(
+                    LifecycleStage.DRAFT, LifecycleStage.ACTIVE, LifecycleStage.OBSOLETE))
+            .toCompletableFuture()
+            .get();
+    assertThat(result.stream().map(a -> a.id))
+        .containsExactlyInAnyOrder(otherApplicantActiveApp.id);
+
+    result =
+        repo.getApplicationsForApplicant(
+                primaryApplicant.id,
+                ImmutableSet.of(
+                    LifecycleStage.DRAFT, LifecycleStage.ACTIVE, LifecycleStage.OBSOLETE))
+            .toCompletableFuture()
+            .get();
+    assertThat(result.stream().map(a -> a.id))
+        .containsExactlyInAnyOrder(primaryApplicantDraftApp.id);
+
+    // Unknown applicant.
+    result =
+        repo.getApplicationsForApplicant(
+                Integer.MAX_VALUE,
+                ImmutableSet.of(
+                    LifecycleStage.DRAFT, LifecycleStage.ACTIVE, LifecycleStage.OBSOLETE))
+            .toCompletableFuture()
+            .get();
+    assertThat(result).isEmpty();
+  }
+
   private Applicant saveApplicant(String name) {
     Applicant applicant = new Applicant();
     applicant.getApplicantData().putString(Path.create("$.applicant.name"), name);
@@ -223,7 +316,7 @@ public class ApplicationRepositoryTest extends ResetPostgres {
     return applicant;
   }
 
-  private Program createDraftProgram(String name) {
+  private Program createProgram(String name) {
     Program program =
         new Program(
             name,

server/test/services/applicant/ApplicantServiceTest.java
@@ -959,6 +959,44 @@ public class ApplicantServiceTest extends ResetPostgres {
   }
 
   @Test
+  public void relevantProgramsForApplicant_otherApplicant() {
+    Applicant primaryApplicant = subject.createApplicant(1L).toCompletableFuture().join();
+    Applicant otherApplicant = subject.createApplicant(2L).toCompletableFuture().join();
+    Program programForDraft =
+        ProgramBuilder.newActiveProgram("program_for_draft")
+            .withBlock()
+            .withRequiredQuestion(testQuestionBank.applicantName())
+            .build();
+    Program programForSubmitted =
+        ProgramBuilder.newActiveProgram("program_for_submitted")
+            .withBlock()
+            .withRequiredQuestion(testQuestionBank.applicantFavoriteColor())
+            .build();
+    Program programForUnapplied =
+        ProgramBuilder.newActiveProgram("program_for_unapplied").withBlock().build();
+
+    applicationRepository
+        .createOrUpdateDraft(primaryApplicant.id, programForDraft.id)
+        .toCompletableFuture()
+        .join();
+    applicationRepository
+        .submitApplication(primaryApplicant.id, programForSubmitted.id, Optional.empty())
+        .toCompletableFuture()
+        .join();
+
+    ApplicantService.ApplicationPrograms result =
+        subject.relevantProgramsForApplicant(otherApplicant.id).toCompletableFuture().join();
+
+    assertThat(result.inProgress()).isEmpty();
+    assertThat(result.submitted()).isEmpty();
+    assertThat(result.unapplied().stream().map(p -> p.program().id()))
+        .containsExactlyInAnyOrder(
+            programForDraft.id, programForUnapplied.id, programForSubmitted.id);
+    assertThat(result.unapplied().stream().map(p -> p.latestSubmittedApplicationStatus()))
+        .containsExactly(Optional.empty(), Optional.empty(), Optional.empty());
+  }
+
+  @Test
   public void relevantProgramsForApplicant_withNewerProgramVersion() {
     Applicant applicant = subject.createApplicant(1L).toCompletableFuture().join();
 