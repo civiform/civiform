44b94a6f64bb53079235df2a4554847d23214ca4
Brett Clouser <clouser@google.com>
2022-09-26 09:43:44 -0700

    Add browser tests asserting that the "notify" checkbox is shown and an email is sent to the applicant (#3484)
    ### Description
    
    For local browser tests, we run localstack, which has an endpoint including all emails sent since the process has started. When running in a non-prober environment, we can assert the email contents and ensure one is sent when a status has been changed.
    
    ## Release notes:
    
    Adding automated testing that emails are appropriately sent when an application status has been changed.
    
    ### Checklist
    
    - [X] Added the correct label: < feature | bug | dependencies | infrastructure | ignore-for-release >
    - [X] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary
    
    ### Issue(s)
    
    Fixes #3297

--- browser-test/src/support/config.ts
+++ browser-test/src/support/config.ts
@@ -1,5 +1,6 @@
 export const {
   BASE_URL = 'http://civiform:9000',
+  LOCALSTACK_URL = 'http://localstack:4566',
   TEST_USER_AUTH_STRATEGY = '',
   TEST_USER_LOGIN = '',
   TEST_USER_PASSWORD = '',
