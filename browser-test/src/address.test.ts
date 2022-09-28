183dc297fcc3df2541d7efdaf26ce00f4c30f167
Mikita Belahlazau <nbeloglazov@google.com>
2022-09-28 16:11:41 -0700

    Fix

--- browser-test/src/address.test.ts
+++ browser-test/src/address.test.ts
@@ -37,8 +37,6 @@ describe('address applicant flow', () => {
       await applicantQuestions.applyProgram(programName)
 
       await validateScreenshot(page, 'address')
-      await validateScreenshot(page, 'address-bla-bla')
-      await validateScreenshot(page, 'address-bla-bla-bla')
     })
 
     it('validate screenshot with errors', async () => {
