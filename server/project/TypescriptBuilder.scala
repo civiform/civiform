c1148c13f3fcf2d7703d586795fc78f91727d62f
Mikita Belahlazau <nbeloglazov@google.com>
2022-09-28 15:37:44 -0700

    Add more docs and slightly rewrite code.

--- server/project/TypescriptBuilder.scala
+++ server/project/TypescriptBuilder.scala
@@ -27,7 +27,7 @@ import scala.sys.process.{Process, ProcessLogger}
   * pipeline stage. Read about Asset Pipeline tasks
   * https://github.com/sbt/sbt-web#asset-pipeline The task implements Asset
   * Pipeline task and not Source File task because the latter doesn't allow us
-  * to keep ∂irectory structure: we want that file `assets/javascripts/foo.ts`
+  * to keep directory structure: we want that file `assets/javascripts/foo.ts`
   * be compiled to `assets/javascripts/foo.js`. So we are using Pipeline.Stage
   * for that.
   *
@@ -38,6 +38,12 @@ object TypescriptBuilder extends AutoPlugin {
   override def trigger = allRequirements
 
   object autoImport {
+    // compileTypescript is the entry point of this plugin. It is a task
+    // which is called by sbt-web when it prepares assets. This task
+    // receives a seq of all assets (images, css, TS files), compiles TS files
+    // and returns seq of all input assets plus newly compiled JS files.
+    //
+    // This is task declaration. Implementation is below.
     val compileTypescript = taskKey[Pipeline.Stage](
       "Compiles typescript from assets/javascript folder"
     )
@@ -47,22 +53,40 @@ object TypescriptBuilder extends AutoPlugin {
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     compileTypescript := { inputFiles =>
       val streamsVal = (Assets / streams).value: @sbtUnchecked
-      // dir for compiled JS files
+      // targetDir will contain compiled JS files.
       val targetDir = new File(webTarget.value, "typescript/javascripts")
       val cacheDir = new File(streamsVal.cacheDirectory, "run")
-      recompileTypescriptIfFilesChanged(
+      val compiledJsFiles = recompileTypescriptIfFilesChanged(
         inputFiles,
         targetDir,
         cacheDir,
         streamsVal.log
       )
-      // get list of compiled JS files and maps from targetDir folder and return them together with input files.
+      // transform compiles JS files to PathMapping and add "javascripts" as part of the file name.
+      // That way JS files fille be added in /assets/javascripts/*.js in final output.
       val jsFiles: Seq[PathMapping] =
-        targetDir.listFiles().map({ f => (f, "javascripts/" + f.getName) })
+        compiledJsFiles.map({ f => (f, "javascripts/" + f.getName) })
+
       inputFiles ++ jsFiles
     }
   )
 
+  /** Given list of all assets (css, image, TS files) figures out whether any TS
+    * files have been changed and recompiles them. If no files have been changed
+    * assumes JS files have been compiled at earlier iterations and returns
+    * them.
+    *
+    * @param inputFiles
+    *   All assets files passed by sbt-web.
+    * @param targetDir
+    *   Directory that contains compiled JS files.
+    * @param cacheDir
+    *   Directory that contains cache file, used by syncIncremental.
+    * @param log
+    *   Logger object to output compilation errors to sbt console.
+    * @return
+    *   Seq of compiled JS files and sourcemaps.
+    */
   def recompileTypescriptIfFilesChanged(
     inputFiles: Seq[(File, String)],
     targetDir: File,
@@ -81,20 +105,34 @@ object TypescriptBuilder extends AutoPlugin {
     // we are using sbt-web syncIncremental function. It's somewhat complicated. For our
     // use case we just want to see if any of TS files changed and if so - recompile
     // everything.
-    incremental.syncIncremental(cacheDir, tsFiles)({ modifiedFiles =>
-      if (modifiedFiles.nonEmpty) {
+    val res = incremental.syncIncremental(cacheDir, tsFiles)({ modifiedFiles =>
+      if (modifiedFiles.nonEmpty || !targetDir.exists()) {
         log.info("Typescript files changed. Recompiling...")
         compileTypescriptInternal(targetDir, log)
       }
+      val compiledJsFiles: Seq[File] = targetDir.listFiles()
       // ignore opResults. We are not using full functionality of syncIncremental
       // so we always return dummy opResults with OpSuccess for each TS file.
       val opResults: Map[File, OpResult] = modifiedFiles
         .map(f => f -> OpSuccess(Set.empty[File], Set.empty[File]))
         .toMap
-      (opResults, ())
+
+      (opResults, compiledJsFiles)
     })(fileHasher)
+
+    res._2
   }
 
+  /** Function that compiles TS files. It relies on tsconfig.json to contain all
+    * necessary settings including which source files to compile. Note that we
+    * don't pass input source files. Instead we simply compile everything in
+    * server/app/assets/javascripts folder (check server/tsconfig.json).
+    *
+    * @param targetDir
+    *   Directory in which compiled JS and sourcemap files will be added.
+    * @param log
+    *   Logger object to output compilation errors to sbt console.
+    */
   def compileTypescriptInternal(targetDir: File, log: ManagedLogger) = {
     val compilationCommand =
       "npx tsc --pretty --project tsconfig.json --outDir " + targetDir
