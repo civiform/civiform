ad24d1b302124bfabf092f6a0b32a777beb7a758
Brett Clouser <clouser@google.com>
2022-09-29 11:35:20 -0700

    Adding a role and aria-label for the icon shown next to the external site link (#3564)
    ### Description
    
    ## Release notes:
    
    Adding accessibility attributes to the icon shown next to the "External site" link in the applicant program listing page.
    
    ### Checklist
    
    - [X] Added the correct label: < feature | bug | dependencies | infrastructure | ignore-for-release >
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary
    
    ### Issue(s)
    
    Fixes #3553

--- server/app/services/MessageKey.java
+++ server/app/services/MessageKey.java
@@ -59,6 +59,7 @@ public enum MessageKey {
   ENUMERATOR_VALIDATION_DUPLICATE_ENTITY_NAME("validation.duplicateEntityName"),
   ENUMERATOR_VALIDATION_ENTITY_REQUIRED("validation.entityNameRequired"),
   EXTERNAL_LINK("link.externalLink"),
+  EXTERNAL_LINK_OPENS_IN_NEW_TAB("link.externalLinkOpensInNewTab"),
   FILEUPLOAD_VALIDATION_FILE_REQUIRED("validation.fileRequired"),
   FOOTER_SUPPORT_LINK_DESCRIPTION("footer.supportLinkDescription"),
   GUEST("guest"),
