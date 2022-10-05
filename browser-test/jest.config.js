5b3a60cc3784b1fcba9fc1046e9508c16e024d60
Mikita Belahlazau <nbeloglazov@google.com>
2022-10-04 19:46:14 -0700

    Fix

--- browser-test/jest.config.js
+++ browser-test/jest.config.js
@@ -2,13 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
-    '^.+\\.(ts)$': 'ts-jest',
+    '^.+\\.(ts)$': ['ts-jest', {
+      tsconfig: 'src/tsconfig.json',
+    }],
   },
   globalSetup: './src/delete_database.ts',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'src/tsconfig.json',
-    },
-  },
   setupFilesAfterEnv: ['./src/support/setup-jest.ts'],
 }
