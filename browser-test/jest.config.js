079f8c878ecec8ad238af7a2e4ff1dc1f8973922
Mikita Belahlazau <nbeloglazov@google.com>
2022-10-04 20:49:38 -0700

    Format

--- browser-test/jest.config.js
+++ browser-test/jest.config.js
@@ -2,9 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
-    '^.+\\.(ts)$': ['ts-jest', {
-      tsconfig: 'src/tsconfig.json',
-    }],
+    '^.+\\.(ts)$': [
+      'ts-jest',
+      {
+        tsconfig: 'src/tsconfig.json',
+      },
+    ],
   },
   globalSetup: './src/delete_database.ts',
   setupFilesAfterEnv: ['./src/support/setup-jest.ts'],
