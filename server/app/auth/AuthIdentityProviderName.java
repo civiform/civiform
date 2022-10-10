fa093592dadd27c8f4c34da0ff729c8573cef8ac
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-10-10 15:31:57 -0700

    Refactor AuthIdentityProviderName logic to share logic that detects type from Config (#3624)
    There are multiple places in the codebase where AuthIdentityProviderName is inferred from Config. And I'm about to add another place in a PR. This PR reduces code duplication by making sure auth.application_idp config variable is read only in single place in codebase.

--- server/app/auth/AuthIdentityProviderName.java
+++ server/app/auth/AuthIdentityProviderName.java
@@ -1,7 +1,8 @@
 package auth;
 
+import com.typesafe.config.Config;
 import java.util.Arrays;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 /** Enum representing supported options for auth IDP for applicants and admins. */
 public enum AuthIdentityProviderName {
@@ -12,17 +13,34 @@ public enum AuthIdentityProviderName {
   LOGIN_GOV_APPLICANT("login-gov"),
   DISABLED_APPLICANT("disabled");
 
+  public static String AUTH_APPLICANT_CONFIG_PATH = "auth.applicant_idp";
+
   private final String authIdentityProviderNameString;
 
   AuthIdentityProviderName(String authIdentityProviderNameString) {
     this.authIdentityProviderNameString = authIdentityProviderNameString;
   }
 
-  /** Returns the enum associated with the provided string value. */
-  public static Optional<AuthIdentityProviderName> forString(String string) {
-    return Arrays.stream(AuthIdentityProviderName.values())
-        .filter(provider -> provider.getString().equals(string))
-        .findFirst();
+  public static AuthIdentityProviderName fromConfig(Config config) {
+    if (!config.hasPath(AUTH_APPLICANT_CONFIG_PATH)) {
+      // return IDCS if no config is specified.
+      return AuthIdentityProviderName.IDCS_APPLICANT;
+    }
+    String providerName = config.getString(AUTH_APPLICANT_CONFIG_PATH);
+    for (var provider : AuthIdentityProviderName.values()) {
+      if (provider.getString().equals(providerName)) {
+        return provider;
+      }
+    }
+    String supportedOptions =
+        Arrays.stream(AuthIdentityProviderName.values())
+            .map(AuthIdentityProviderName::getString)
+            .collect(Collectors.joining(", "));
+    throw new IllegalArgumentException(
+        "Unsupported auth.applicant_idp value: "
+            + providerName
+            + ". Supported values are "
+            + supportedOptions);
   }
 
   /** Returns the string value associated with the enum. */
