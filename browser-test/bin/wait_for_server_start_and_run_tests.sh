b9466eacbb5bcbdc6db4d2beeaeeffa6874b242b
Mikita Belahlazau <nbeloglazov@google.com>
2022-10-14 18:30:12 -0700

    Fix

--- browser-test/bin/wait_for_server_start_and_run_tests.sh
+++ browser-test/bin/wait_for_server_start_and_run_tests.sh
@@ -4,15 +4,37 @@ set -euo pipefail
 
 START_TIME=$(date +%s)
 DEADLINE=$(($START_TIME + 500))
-SERVER_URL="http://civiform:9000"
+
+# Allow callers to override BASE_URL if they want (e.g. for run-browser-tests-local).
+# Defaults civiform:9000 when running from within docker.
+export BASE_URL="${BASE_URL:-http://civiform:9000}"
+
+export TEST_USER_AUTH_STRATEGY=fake-oidc
+export TEST_USER_LOGIN=testuser
+export TEST_USER_PASSWORD=anotsecretpassword
+# The display name returned by test_oidc_provider.js is <username>@example.com.
+export TEST_USER_DISPLAY_NAME=testuser@example.com
+
+if ! output="$(node -v)"; then
+  echo output
+  echo "You must have node installed locally to run this command. Go to https://nodejs.org/en/download/package-manager/ for installation instructions."
+  exit 1
+fi
+
+# Sanity check that this script was called from within browser-test directory.
+if [ ! -d "image_snapshots" ]; then
+  echo output
+  echo "$(basename "$0") must be run from within browser-test directory."
+  exit 1
+fi
 
 # Install any new packages not built into the image
 # Also saves any package-lock.json changes back to your local filesystem.
 npm install --force --quiet
 
-echo "Polling to check server start"
+echo "Polling to check server start. Server url: ${BASE_URL}"
 
-until $(curl --output /dev/null --silent --head --fail --max-time 2 "${SERVER_URL}"); do
+until $(curl --output /dev/null --silent --head --fail --max-time 2 "${BASE_URL}"); do
   if (($(date +%s) > "${DEADLINE}")); then
     echo "Deadline exceeded waiting for server start"
     exit 1
@@ -30,7 +52,7 @@ for arg; do
 done
 
 if (($debug == 1)); then
-  DEBUG="pw:api" BASE_URL="${SERVER_URL}" npm test "$@"
+  DEBUG="pw:api" npm test "$@"
 else
-  BASE_URL="${SERVER_URL}" npm test "$@"
+  npm test "$@"
 fi
