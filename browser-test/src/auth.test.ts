196fa91fc215c46fc3826fb029539f7e497d0a0e
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-10-20 10:21:24 -0700

    Fix auth tests running on staging (#3685)
    The main fix is to use already existing testUserDisplayName() function when checking "Logged in as ..." text on the page. Originally this test used testUserEmail() for that but it's not versatile enough and doesn't handle stagings.
    
    Also different auth strategy require modifications in logout flow as well which are reflected in this PR.

--- browser-test/src/auth.test.ts
+++ browser-test/src/auth.test.ts
@@ -4,8 +4,11 @@ import {
   loginAsGuest,
   selectApplicantLanguage,
   validateScreenshot,
-  testUserEmail,
+  testUserDisplayName,
+  AuthStrategy,
 } from './support'
+import {TEST_USER_AUTH_STRATEGY} from './support/config'
+
 describe('applicant auth', () => {
   const ctx = createTestContext()
 
@@ -17,7 +20,7 @@ describe('applicant auth', () => {
     await validateScreenshot(page, 'logged-in')
 
     expect(await ctx.page.textContent('html')).toContain(
-      'Logged in as ' + testUserEmail(),
+      `Logged in as ${testUserDisplayName()}`,
     )
     expect(await ctx.page.textContent('html')).toContain('Logout')
   })
@@ -32,32 +35,40 @@ describe('applicant auth', () => {
     expect(await ctx.page.textContent('html')).toContain('Logout')
   })
 
-  it('applicant can confirm central provider logout', async () => {
-    const {page} = ctx
-    await loginAsTestUser(page)
-    await selectApplicantLanguage(page, 'English')
-    expect(await ctx.page.textContent('html')).toContain(
-      'Logged in as ' + testUserEmail(),
-    )
+  // so far only fake-oidc provider requires user to click "Yes" to confirm
+  // logout. AWS staging uses Auth0 which doesn't. And Seattle staging uses
+  // IDCS which at the moment doesn't have central logout enabled.
+  if (TEST_USER_AUTH_STRATEGY === AuthStrategy.FAKE_OIDC) {
+    it('applicant can confirm central provider logout', async () => {
+      const {page} = ctx
+      await loginAsTestUser(page)
+      await selectApplicantLanguage(page, 'English')
+      expect(await ctx.page.textContent('html')).toContain(
+        `Logged in as ${testUserDisplayName()}`,
+      )
 
-    await page.click('text=Logout')
+      await page.click('text=Logout')
 
-    await validateScreenshot(page, 'central-provider-logout')
-    expect(await ctx.page.textContent('html')).toContain(
-      'Do you want to sign-out from',
-    )
-  })
+      await validateScreenshot(page, 'central-provider-logout')
+      expect(await ctx.page.textContent('html')).toContain(
+        'Do you want to sign-out from',
+      )
+    })
+  }
 
   it('applicant can logout', async () => {
     const {page} = ctx
     await loginAsTestUser(page)
     await selectApplicantLanguage(page, 'English')
     expect(await ctx.page.textContent('html')).toContain(
-      'Logged in as ' + testUserEmail(),
+      `Logged in as ${testUserDisplayName()}`,
     )
 
     await page.click('text=Logout')
-    await page.click('button:has-text("Yes")')
+    // At the moment only fake OIDC has confirmation page during logout.
+    if (TEST_USER_AUTH_STRATEGY === AuthStrategy.FAKE_OIDC) {
+      await page.click('button:has-text("Yes")')
+    }
 
     expect(await ctx.page.textContent('html')).toContain('Continue as guest')
   })
