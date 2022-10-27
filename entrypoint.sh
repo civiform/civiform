dce14a0067b91e7670f33c8ebfb986dc5f3f501f
Mikita Belahlazau <nbeloglazov@google.com>
2022-10-26 23:00:16 -0700

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@ echo "whoami"
 whoami
 echo "USER $USER"
 npm install
-npx tailwindcss build -i ./app/assets/stylesheets/styles.css -o ./public/stylesheets/tailwind.css
+ls -l public
+ls -l public/stylesheets
+ls -l public/stylesheets/tailwind.css
+
+#npx tailwindcss build -i ./app/assets/stylesheets/styles.css -o ./public/stylesheets/tailwind.css
 # sbt "$@"
