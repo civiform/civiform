64dfb3c3638f1ff2fec10d587d595f7f983f045e
Mikita Belahlazau <nbeloglazov@google.com>
2022-10-26 23:14:46 -0700

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ echo "USER $USER"
 npm install
 ls -l public
 touch public/stylesheets/tailwind.css
-
-#npx tailwindcss build -i ./app/assets/stylesheets/styles.css -o ./public/stylesheets/tailwind.css
+ls -l public/stylesheets/
+npx tailwindcss build -i ./app/assets/stylesheets/styles.css -o ./public/stylesheets/tailwind.css
 # sbt "$@"
