5343eac2b06648b3bfbe8539756b06dba854ed3e
Mikita Belahlazau <nbeloglazov@google.com>
2022-10-26 23:42:02 -0700

    Revert

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
-echo "whoami"
-whoami
-echo "USER $USER"
+
 npm install
-ls -l public
-touch public/stylesheets/tailwind.css
-ls -l public/stylesheets/
-npx tailwindcss build -i ./app/assets/stylesheets/styles.css -o ./public/stylesheets/tailwind.css
-# sbt "$@"
+sbt "$@"
