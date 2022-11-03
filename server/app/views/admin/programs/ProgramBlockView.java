f581af5d9e427337be99c33fff52e9419a621776
Mikita Belahlazau <nbeloglazov@google.com>
2022-11-02 17:50:13 -0700

    Commit

--- server/app/views/admin/programs/ProgramBlockView.java
+++ server/app/views/admin/programs/ProgramBlockView.java
@@ -1,6 +1,7 @@
 package views.admin.programs;
 
 import static j2html.TagCreator.div;
+import static j2html.TagCreator.span;
 
 import j2html.tags.specialized.ButtonTag;
 import j2html.tags.specialized.DivTag;
@@ -13,22 +14,30 @@ import views.style.AdminStyles;
 
 abstract class ProgramBlockView extends BaseHtmlView {
   /** Renders a div with internal/admin program information. */
-  protected final DivTag renderProgramInfo(ProgramDefinition programDefinition) {
-    DivTag programTitle =
-        div(programDefinition.adminName()).withId("program-title").withClasses("text-3xl", "pb-3");
-    DivTag programDescription = div(programDefinition.adminDescription()).withClasses("text-sm");
+  protected final DivTag renderProgramInfo(ProgramDefinition program) {
+    DivTag title =
+        div(program.localizedName().getDefault())
+            .withId("program-title")
+            .withClasses("text-3xl", "pb-3");
+    DivTag description = div(program.localizedDescription().getDefault()).withClasses("text-sm");
+    DivTag adminNote =
+        div()
+            .withClasses("text-sm")
+            .with(span("Admin note: ").withClasses("font-semibold"))
+            .with(span(program.adminDescription()));
 
     ButtonTag editDetailsButton =
         ViewUtils.makeSvgTextButton("Edit program details", Icons.EDIT)
             .withClasses(AdminStyles.SECONDARY_BUTTON_STYLES, "my-5");
     asRedirectElement(
         editDetailsButton,
-        controllers.admin.routes.AdminProgramController.edit(programDefinition.id()).url());
+        controllers.admin.routes.AdminProgramController.edit(program.id()).url());
 
     return div(
             ViewUtils.makeBadge(BadgeStatus.DRAFT),
-            programTitle,
-            programDescription,
+            title,
+            description,
+            adminNote,
             editDetailsButton)
         .withClasses("bg-gray-100", "text-gray-800", "shadow-md", "p-8", "pt-4", "-mx-2");
   }
