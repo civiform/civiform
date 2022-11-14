97e484b9c4859938b2734281399dcb1b4ad9e485
Leyla <azizoval@google.com>
2022-11-14 09:25:15 -0800

    if register_uri is set show create account button (#3796)
    * if register_uri is set show create account button
    
    * add register_uri to browswer tests
    
    * validate accessibility
    
    * validate accesibility

--- browser-test/src/landing_page.test.ts
+++ browser-test/src/landing_page.test.ts
@@ -1,10 +1,19 @@
-import {createTestContext, validateScreenshot} from './support'
+import {
+  createTestContext,
+  validateAccessibility,
+  validateScreenshot,
+} from './support'
 
 describe('the landing page', () => {
   const ctx = createTestContext()
 
   it('it has login options', async () => {
     expect(await ctx.page.textContent('html')).toContain('Continue as guest')
+    expect(await ctx.page.textContent('html')).toContain('Create account')
     await validateScreenshot(ctx.page, 'landing-page')
+  })
+
+  it('has no accessibility violations', async () => {
+    await validateAccessibility(ctx.page)
   })
 })
