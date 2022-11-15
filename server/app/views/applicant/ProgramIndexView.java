civiform/civiform

commit 95156e772d8c82e359b04ad571bacfa0f5a62f35

Add pencil icon to edit button on program overview page (#3805)
* Implement

* udpate

server/app/views/components/LinkElement.java
@@ -1,5 +1,6 @@
 package views.components;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static j2html.TagCreator.a;
 import static j2html.TagCreator.button;
 import static j2html.TagCreator.div;
@@ -17,6 +18,7 @@ import j2html.tags.attributes.ITarget;
 import j2html.tags.specialized.ATag;
 import j2html.tags.specialized.DivTag;
 import j2html.tags.specialized.FormTag;
+import java.util.Optional;
 import play.filters.csrf.CSRF;
 import play.mvc.Http;
 import scala.Option;
@@ -60,7 +62,8 @@ public final class LinkElement {
           StyleUtils.focus("outline-none", "ring-2"));
 
   private static final String DEFAULT_LINK_STYLES =
-      StyleUtils.joinStyles(BaseStyles.LINK_TEXT, BaseStyles.LINK_HOVER_TEXT);
+      StyleUtils.joinStyles(
+          BaseStyles.LINK_TEXT, BaseStyles.LINK_HOVER_TEXT, "inline-flex", "items-center");
 
   private static final String BUTTON_LOOKS_LIKE_LINK_STYLES =
       StyleUtils.joinStyles(
@@ -79,6 +82,7 @@ public final class LinkElement {
   private String styles = "";
   private String onsubmit = "";
   private boolean doesOpenInNewTab = false;
+  private Optional<Icons> icon = Optional.empty();
 
   public LinkElement setId(String id) {
     this.id = id;
@@ -110,8 +114,15 @@ public final class LinkElement {
     return this;
   }
 
+  public LinkElement setIcon(Icons icon) {
+    this.icon = Optional.of(checkNotNull(icon));
+    return this;
+  }
+
   public ATag asAnchorText() {
-    ATag tag = a(text);
+    ATag tag = a();
+    this.icon.ifPresent(icon -> tag.with(Icons.svg(icon).withClasses("mr-2")));
+    tag.withText(this.text);
     return tag.withCondId(!Strings.isNullOrEmpty(id), id)
         .withCondHref(!Strings.isNullOrEmpty(href), href)
         .withCondTarget(doesOpenInNewTab, "_blank")

server/app/views/applicant/ApplicantProgramSummaryView.java
@@ -12,7 +12,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import controllers.applicant.routes;
 import j2html.tags.ContainerTag;
-import j2html.tags.specialized.ATag;
 import j2html.tags.specialized.DivTag;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -28,6 +27,7 @@ import services.applicant.RepeatedEntity;
 import views.ApplicantUtils;
 import views.BaseHtmlView;
 import views.HtmlBundle;
+import views.components.Icons;
 import views.components.LinkElement;
 import views.components.ToastMessage;
 import views.style.ApplicantStyles;
@@ -192,15 +192,17 @@ public final class ApplicantProgramSummaryView extends BaseHtmlView {
                       applicantId, data.programId(), data.blockId())
                   .url();
 
-      ATag editAction =
+      LinkElement editElement =
           new LinkElement()
               .setHref(editLink)
               .setText(editText)
-              .setStyles("bottom-0", "right-0", "text-blue-600", StyleUtils.hover("text-blue-700"))
-              .asAnchorText()
-              .attr("aria-label", ariaLabel);
+              .setStyles("bottom-0", "right-0", "text-blue-600", StyleUtils.hover("text-blue-700"));
+      if (data.isAnswered()) {
+        editElement.setIcon(Icons.EDIT);
+      }
       DivTag editContent =
-          div(editAction)
+          div()
+              .with(editElement.asAnchorText().attr("aria-label", ariaLabel))
               .withClasses(
                   "font-medium",
                   "break-normal",

commit 28565b58007a444d5472c9318d45494e25b7d3d1

Fix program card external link rendering (#3815)
It got broken by #3805 The reason is that link produced by LinkElement now always use display: inline-flex so that element inside the link (text and icon) are aligned properly. But it means that link is always inline. If callers want to make it block - they need to wrap it with div() as callers can't override LinkElement class.

server/app/views/applicant/ProgramIndexView.java
@@ -297,7 +297,7 @@ public final class ProgramIndexView extends BaseHtmlView {
     ATag infoLink =
         new LinkElement()
             .setId(baseId + "-info-link")
-            .setStyles("block", "my-2", "text-sm", "underline")
+            .setStyles("mb-2", "text-sm", "underline")
             .setText(messages.at(MessageKey.LINK_PROGRAM_DETAILS.getKeyName()))
             .setHref(infoUrl)
             .asAnchorText()
@@ -306,14 +306,14 @@ public final class ProgramIndexView extends BaseHtmlView {
                 messages.at(
                     MessageKey.LINK_PROGRAM_DETAILS_SR.getKeyName(),
                     program.localizedName().getOrDefault(preferredLocale)));
-    programData.with(infoLink);
+    programData.with(div(infoLink));
 
     // Add external link if it is set.
     if (!program.externalLink().isEmpty()) {
       ATag externalLink =
           new LinkElement()
               .setId(baseId + "-external-link")
-              .setStyles("block", "my-2", "text-sm", "underline")
+              .setStyles("mb-2", "text-sm", "underline")
               .setText(messages.at(MessageKey.EXTERNAL_LINK.getKeyName()))
               .setHref(program.externalLink())
               .opensInNewTab()
@@ -327,7 +327,7 @@ public final class ProgramIndexView extends BaseHtmlView {
                       .withClasses(
                           "shrink-0", "h-5", "w-auto", "inline", "ml-1", "align-text-top"));
 
-      programData.with(externalLink);
+      programData.with(div(externalLink));
     }
 
     if (cardData.latestSubmittedApplicationTime().isPresent()) {

server/app/views/components/LinkElement.java
@@ -121,7 +121,7 @@ public final class LinkElement {
 
   public ATag asAnchorText() {
     ATag tag = a();
-    this.icon.ifPresent(icon -> tag.with(Icons.svg(icon).withClasses("mr-2")));
+    this.icon.ifPresent(icon -> tag.with(Icons.svg(icon).withClasses("mr-2", "w-5", "h-5")));
     tag.withText(this.text);
     return tag.withCondId(!Strings.isNullOrEmpty(id), id)
         .withCondHref(!Strings.isNullOrEmpty(href), href)

browser-test/src/application_navigation.test.ts
@@ -2,6 +2,7 @@ import {
   createTestContext,
   loginAsAdmin,
   loginAsGuest,
+  logout,
   selectApplicantLanguage,
   validateAccessibility,
   validateScreenshot,
@@ -133,12 +134,25 @@ describe('Applicant navigation flow', () => {
     })
 
     it('verify program list page', async () => {
-      const {page} = ctx
+      const {page, adminPrograms} = ctx
+      await loginAsAdmin(page)
+      // create second program that has an external link.
+      const programWithExternalLink = 'Program with external link'
+      await adminPrograms.addProgram(
+        programWithExternalLink,
+        'Program description',
+        'https://external.com',
+      )
+      await adminPrograms.publishProgram(programWithExternalLink)
+      await logout(page)
       await loginAsGuest(page)
       await selectApplicantLanguage(page, 'English')
 
       // Verify we are on program list page.
       expect(await page.innerText('h1')).toContain('Get benefits')
+      expect(
+        await page.locator('a:has-text("External site")').getAttribute('href'),
+      ).toEqual('https://external.com')
       await validateAccessibility(page)
       await validateScreenshot(page, 'program-list-page')
     })