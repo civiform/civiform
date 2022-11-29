eb311307e1f69a63a8b03009338b79de9a154f63
Mikita Belahlazau <nbeloglazov@google.com>
2022-11-30 00:22:44 +0300

    implement

---
+++ server/app/assets/javascripts/applicant_entry_point.ts
@@ -0,0 +1,11 @@
+/**
+ * Entry point for applicant bundle. Applicant bundle is served on all pages
+ * that are used by applicants and trusted intermediaries.
+ */
+
+import './main';
+import './accordion';
+import './enumerator';
+import './radio';
+import './toast';
+import './modal';
