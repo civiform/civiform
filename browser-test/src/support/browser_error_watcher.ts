091af42f87d0c7253abbf06b009f9978b8c83c40
Mikita Belahlazau <nbeloglazov@google.com>
2022-12-02 21:18:12 +0300

    Fix

--- browser-test/src/support/browser_error_watcher.ts
+++ browser-test/src/support/browser_error_watcher.ts
@@ -13,11 +13,19 @@ interface ErrorOnPage {
  */
 export class BrowserErrorWatcher {
   private readonly errors: ErrorOnPage[] = []
+  private readonly downloadUrls = new Set<string>()
 
   constructor(page: Page) {
     // Catch JS errors on page.
     page.on('pageerror', (error) => {
       this.errors.push({message: error.stack || error.message, url: page.url()})
+    })
+
+    // For some reason all download file requests end up as net::ERR_ABORTED.
+    // To exclude it from our error detection we record them here and check
+    // later.
+    page.on('download', (download) => {
+      this.downloadUrls.add(download.url())
     })
 
     // Catch requests failed due to network problems.
@@ -29,26 +37,32 @@ export class BrowserErrorWatcher {
     })
 
     // Catch requests failed due to 4xx or 5xx responses.
-    page.on('requestfinished', async (request) => {
-      const statusCode = (await request.response())?.status() || 200
-      if (statusCode >= 400 && statusCode < 600) {
-        this.errors.push({
-          message: `Got response with status code ${statusCode}`,
-          url: request.url(),
-        })
-      }
+    page.on('requestfinished', (request) => {
+      void request.response().then((response) => {
+        if (response == null) return
+        const statusCode = response.status()
+        if (statusCode >= 400 && statusCode < 600) {
+          this.errors.push({
+            message: `Got response with status code ${statusCode}`,
+            url: request.url(),
+          })
+        }
+      })
     })
   }
 
   failIfContainsErrors() {
-    if (this.errors.length === 0) {
+    const errorsToReport = this.errors.filter(
+      (error) => !this.downloadUrls.has(error.url),
+    )
+    if (errorsToReport.length === 0) {
       return
     }
-    const errorMessages = this.errors
+    const errorMessages = errorsToReport
       .map((error) => `Page: ${error.url}\nError: ${error.message}`)
       .join('\n\n')
     throw new Error(
-      `Detected ${this.errors.length} errors in browser during test run:\n\n${errorMessages}`,
+      `Detected ${errorsToReport.length} errors in browser during test run:\n\n${errorMessages}`,
     )
   }
 }
