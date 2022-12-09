590071e6bf74dfa4d34e79f1cd0350364c2bdfef
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-12-09 14:08:10 -0800

    Add initial JS bundling support (#3861)
    Adds additional step to sbt asserts building: building JS bundles. At the moment JS bundles are not used. It adds two bundles: applicant.bundle.js and admin.bundle.js. In follow up PRs I'll add a feature flag which controls whether we serve JS bundles or individual JS files.

---
+++ server/app/assets/javascripts/admin_entry_point.ts
@@ -0,0 +1,18 @@
+/**
+ * Entry point for admin bundle. Admin bundle is served on all pages that are
+ * accessed by CiviForm and Program admins.
+ */
+
+import './main'
+import './accordion'
+import './radio'
+import './toast'
+import './admin_application_view'
+import './admin_applications'
+import './admin_programs'
+import './admin_validation'
+import './dev_icons'
+import './enumerator'
+import './modal'
+import './questionBank'
+import './preview'
