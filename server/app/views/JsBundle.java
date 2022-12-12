0b2197d514902673b1d77d373569a33b0ea0f59b
Mikita Belahlazau <nbeloglazov@google.com>
2022-12-12 14:49:40 -0800

    Fix

--- server/app/views/JsBundle.java
+++ server/app/views/JsBundle.java
@@ -1,7 +1,23 @@
 package views;
 
+/**
+ * JS bundle is a bundled minified JS file containing client-side CiviForm code. Each page must
+ * include only one JS bundle. Each bundle compiled from an entry point that imports all necessary
+ * dependencies.
+ */
 public enum JsBundle {
+  /**
+   * Bundle used on admin pages: CiviForm admin and program admin pages.
+   *
+   * <p>Entry point is admin_entry_point.ts
+   */
   ADMIN("admin.bundle"),
+
+  /**
+   * Bundle used on applicant pages: pages that applicants and trusted intermediaries see.
+   *
+   * <p>Entry point is applicant_entry_point.ts
+   */
   APPLICANT("applicant.bundle");
 
   private final String jsPath;
