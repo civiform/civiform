131b768290d178f4151f1dcaad1c00d5ba51364b
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-12-13 12:38:03 -0500

    Alpha order

--- server/app/featureflags/FeatureFlags.java
+++ server/app/featureflags/FeatureFlags.java
@@ -83,8 +83,8 @@ public final class FeatureFlags {
   public ImmutableMap<String, Boolean> getAllFlags(Request request) {
     return ImmutableMap.of(
         ALLOW_CIVIFORM_ADMIN_ACCESS_PROGRAMS, allowCiviformAdminAccessPrograms(request),
-        PROGRAM_ELIGIBILITY_CONDITIONS_ENABLED, isProgramEligibilityConditionsEnabled(request),
-        APPLICATION_STATUS_TRACKING_ENABLED, isStatusTrackingEnabled(request));
+        APPLICATION_STATUS_TRACKING_ENABLED, isStatusTrackingEnabled(request),
+        PROGRAM_ELIGIBILITY_CONDITIONS_ENABLED, isProgramEligibilityConditionsEnabled(request));
   }
 
   /**
