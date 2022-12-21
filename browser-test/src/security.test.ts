df977269401d94150775a751b6bae802330a87ee
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-12-20 16:38:37 -0800

    Migrate SecurityBrowserTest.java to TS browser tests (#3957)
    SecurityBrowserTest.java uses legacy JS/HTML interpreter which in the past blocked certain improvements like serving modern JS to users containing ES6+ features such as let, const, async/await. This PR migrates tests from that file to more robust browser test TS codebase.
    
    Checklist

--- browser-test/src/security.test.ts
+++ browser-test/src/security.test.ts
@@ -4,6 +4,7 @@ import {
   loginAsAdmin,
   loginAsGuest,
 } from './support'
+import {BASE_URL} from './support/config'
 
 describe('applicant security', () => {
   const ctx = createTestContext()
@@ -26,5 +27,42 @@ describe('applicant security', () => {
     await loginAsAdmin(page)
     const response = await gotoEndpoint(page, '/applicants/1234567/programs')
     expect(response!.status()).toBe(401)
+  })
+
+  it('applicant cannot access admin pages', async () => {
+    const {page} = ctx
+    // this test visits page that returns 401 which triggers BrowserErrorWatcher.
+    // Silencing error on that page.
+    ctx.browserErrorWatcher.ignoreErrorsFromUrl(/\/admin\/programs/)
+    await loginAsGuest(page)
+    const response = await gotoEndpoint(page, '/admin/programs')
+    expect(response!.status()).toBe(403)
+  })
+
+  it('redirects to login form when not logged in', async () => {
+    const {page} = ctx
+    await page.goto(BASE_URL)
+    expect(await page.innerHTML('body')).toMatch(
+      /Please log in with your (.*) account/,
+    )
+  })
+
+  it('redirects to program dashboard when logged in as admin', async () => {
+    const {page} = ctx
+    await loginAsAdmin(page)
+    await page.goto(BASE_URL)
+    expect(await page.innerHTML('body')).toMatch(/Program dashboard/)
+    expect(await page.innerHTML('body')).toMatch(
+      /Create, edit and publish programs/,
+    )
+  })
+
+  it('redirects to language page when logged in as guest', async () => {
+    const {page} = ctx
+    await loginAsGuest(page)
+    await page.goto(BASE_URL)
+    expect(await page.innerHTML('body')).toMatch(
+      /Please select your preferred language/,
+    )
   })
 })
