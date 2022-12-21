fd814d0f92345ab22626d0cf123e0daa508cfacd
Mikita Belahlazau <nbeloglazov@google.com>
2022-12-21 12:27:14 -0800

    Implement

--- server/app/assets/javascripts/admin_programs.ts
+++ server/app/assets/javascripts/admin_programs.ts
@@ -1,70 +1,66 @@
-class AdminPrograms {
-  private static PROGRAM_CARDS_SELECTOR = '.cf-admin-program-card'
-  private static PROGRAM_LINK_ATTRIBUTE = 'data-copyable-program-link'
+import {showToastMessage} from './toast'
 
-  static attachCopyProgramLinkListeners() {
-    const withCopyableProgramLink = Array.from(
-      document.querySelectorAll(
-        `${AdminPrograms.PROGRAM_CARDS_SELECTOR} [${AdminPrograms.PROGRAM_LINK_ATTRIBUTE}]`,
-      ),
-    )
-    withCopyableProgramLink.forEach((el) => {
-      const programLink = el.getAttribute(AdminPrograms.PROGRAM_LINK_ATTRIBUTE)
-      if (!programLink) {
-        console.warn(
-          `Empty ${AdminPrograms.PROGRAM_LINK_ATTRIBUTE} for element`,
-        )
-        return
-      }
-      el.addEventListener('click', () => {
-        void AdminPrograms.copyProgramLinkToClipboard(programLink)
-      })
+const PROGRAM_CARDS_SELECTOR = '.cf-admin-program-card'
+const PROGRAM_LINK_ATTRIBUTE = 'data-copyable-program-link'
+
+function attachCopyProgramLinkListeners() {
+  const withCopyableProgramLink = Array.from(
+    document.querySelectorAll(
+      `${PROGRAM_CARDS_SELECTOR} [${PROGRAM_LINK_ATTRIBUTE}]`,
+    ),
+  )
+  withCopyableProgramLink.forEach((el) => {
+    const programLink = el.getAttribute(PROGRAM_LINK_ATTRIBUTE)
+    if (!programLink) {
+      console.warn(`Empty ${PROGRAM_LINK_ATTRIBUTE} for element`)
+      return
+    }
+    el.addEventListener('click', () => {
+      void copyProgramLinkToClipboard(programLink)
     })
-  }
+  })
+}
 
-  /**
-   * Attempts to copy the given content to the clipboard.
-   * @param {string} content
-   * @return {Promise<boolean>} indicating whether the content was copied to the clipboard
-   */
-  static async tryCopyToClipboard(content: string): Promise<boolean> {
-    if (!window.navigator['clipboard']) {
-      return false
-    }
-    try {
-      await window.navigator['clipboard'].writeText(content)
-      return true
-    } catch {
-      return false
-    }
+/**
+ * Attempts to copy the given content to the clipboard.
+ * @param {string} content
+ * @return {Promise<boolean>} indicating whether the content was copied to the clipboard
+ */
+async function tryCopyToClipboard(content: string): Promise<boolean> {
+  if (!window.navigator['clipboard']) {
+    return false
   }
+  try {
+    await window.navigator['clipboard'].writeText(content)
+    return true
+  } catch {
+    return false
+  }
+}
 
-  static async copyProgramLinkToClipboard(programLink: string) {
-    const succeeded = await AdminPrograms.tryCopyToClipboard(programLink)
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const toastController = window['toastController'] as any
-    if (succeeded) {
-      toastController.showToastMessage({
-        id: `program-link-${Math.random()}`,
-        content: 'Program link copied to clipboard',
-        duration: 3000,
-        type: 'success',
-        canDismiss: true,
-        canIgnore: false,
-      })
-    } else {
-      toastController.showToastMessage({
-        id: `program-link-${Math.random()}`,
-        content: `Could not copy program link to clipboard: ${programLink}`,
-        duration: -1,
-        type: 'warning',
-        canDismiss: true,
-        canIgnore: false,
-      })
-    }
+async function copyProgramLinkToClipboard(programLink: string) {
+  const succeeded = await tryCopyToClipboard(programLink)
+  if (succeeded) {
+    showToastMessage({
+      id: `program-link-${Math.random()}`,
+      content: 'Program link copied to clipboard',
+      duration: 3000,
+      type: 'success',
+      canDismiss: true,
+      canIgnore: false,
+    })
+  } else {
+    showToastMessage({
+      id: `program-link-${Math.random()}`,
+      content: `Could not copy program link to clipboard: ${programLink}`,
+      duration: -1,
+      type: 'warning',
+      canDismiss: true,
+      canIgnore: false,
+    })
   }
 }
 
 export function init() {
-  AdminPrograms.attachCopyProgramLinkListeners()
+  attachCopyProgramLinkListeners()
 }
--- server/app/assets/javascripts/toast.ts
+++ server/app/assets/javascripts/toast.ts
@@ -6,226 +6,215 @@
  *  - dismiss a toast messages based on a user action or after a specified timeout.
  *  - permanently dismiss toast messags (using localStorage)
  */
-class ToastController {
-  static containerId = 'toast-container'
-  static messageClass = 'cf-toast'
-  static messageDataClass = 'cf-toast-data'
 
-  static infoSvgPath =
-    'M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0' +
-    ' 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
-  static errorSvgPath =
-    'M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0' +
-    ' 102 0V6a1 1 0 00-1-1z'
-  static successSvgPath = 'M5 13l4 4L19 7'
-  static warningSvgPath =
-    'M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742' +
-    ' 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012' +
-    ' 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z'
+const CONTAINER_ID = 'toast-container'
+const MESSAGE_CLASS = 'cf-toast'
+const MESSAGE_DATA_CLASS = 'cf-toast-data'
+const INFO_SVG_PATH =
+  'M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0' +
+  ' 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+const ERROR_SVG_PATH =
+  'M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0' +
+  ' 102 0V6a1 1 0 00-1-1z'
+const SUCCESS_SVG_PATH = 'M5 13l4 4L19 7'
+const WARNING_SVG_PATH =
+  'M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742' +
+  ' 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012' +
+  ' 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z'
 
-  toastContainer: Element
+export function init() {
+  const toastContainer = document.createElement('div')
+  toastContainer.setAttribute('id', CONTAINER_ID)
+  toastContainer.classList.add(
+    'absolute',
+    'hidden',
+    'left-1/2',
+    'top-0',
+    'transform',
+    '-translate-x-1/2',
+    'z-20',
+  )
+  document.body.appendChild(toastContainer)
+  maybeShowToasts()
+}
 
-  constructor() {
-    this.toastContainer = document.createElement('div')
-    this.toastContainer.setAttribute('id', ToastController.containerId)
-    this.toastContainer.classList.add(
+export function showToastMessage(message: ToastMessage) {
+  const inIgnoreList = localStorage.getItem(message.id + '-dismissed')
+  if (message.canIgnore && inIgnoreList) {
+    return
+  }
+
+  const toastMessage = document.createElement('div')
+  toastMessage.setAttribute('id', message.id)
+  toastMessage.setAttribute('ignorable', String(message.canIgnore))
+  toastMessage.classList.add(MESSAGE_CLASS)
+  toastMessage.classList.add(
+    'bg-opacity-90',
+    'duration-300',
+    'flex',
+    'flex-row',
+    'max-w-md',
+    'px-2',
+    'py-2',
+    'my-3',
+    'relative',
+    'rounded-sm',
+    'shadow-lg',
+    'transition-opacity',
+    'transform',
+    'text-gray-700',
+  )
+
+  if (message.type === 'alert') {
+    toastMessage.classList.add('bg-gray-200', 'border-gray-300')
+  } else if (message.type === 'error') {
+    toastMessage.classList.add('bg-red-400', 'border-red-500')
+  } else if (message.type === 'success') {
+    toastMessage.classList.add('bg-emerald-200', 'border-emerald-300')
+  } else if (message.type === 'warning') {
+    toastMessage.classList.add('bg-amber-200', 'border-amber-300')
+  }
+
+  toastMessage.appendChild(getToastIcon(message.type))
+
+  // Add the content string.
+  const contentContainer = document.createElement('span')
+  contentContainer.textContent = message.content
+  toastMessage.appendChild(contentContainer)
+
+  // Maybe add dismiss button.
+  if (message.canDismiss) {
+    const dismissButton = document.createElement('div')
+    dismissButton.setAttribute('id', message.id + '-dismiss')
+    dismissButton.classList.add(
       'absolute',
-      'hidden',
-      'left-1/2',
-      'top-0',
-      'transform',
-      '-translate-x-1/2',
-      'z-20',
+      'font-bold',
+      'pl-6',
+      'opacity-40',
+      'right-4',
+      'top-2',
+      'cursor-pointer',
+      'hover:opacity-100',
     )
-    document.body.appendChild(this.toastContainer)
-
-    this.maybeShowToasts()
+    dismissButton.textContent = 'x'
+    dismissButton.addEventListener('click', dismissClicked)
+    toastMessage.appendChild(dismissButton)
+    toastMessage.classList.add('pr-8')
   }
 
-  showToastMessage(message: ToastMessage) {
-    const inIgnoreList = localStorage.getItem(message.id + '-dismissed')
-    if (message.canIgnore && inIgnoreList) {
-      return
+  const toastContainer = document.getElementById(CONTAINER_ID)
+  if (toastContainer) {
+    toastContainer.appendChild(toastMessage)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    toastContainer!.classList.remove('hidden')
+    if (message.duration > 0) {
+      setTimeout(() => {
+        dismissToast(message.id, /* dismissClicked = */ false)
+      }, message.duration)
     }
+  }
+}
 
-    const toastMessage = document.createElement('div')
-    toastMessage.setAttribute('id', message.id)
-    toastMessage.setAttribute('ignorable', String(message.canIgnore))
-    toastMessage.classList.add(ToastController.messageClass)
-    toastMessage.classList.add(
-      'bg-opacity-90',
-      'duration-300',
-      'flex',
-      'flex-row',
-      'max-w-md',
-      'px-2',
-      'py-2',
-      'my-3',
-      'relative',
-      'rounded-sm',
-      'shadow-lg',
-      'transition-opacity',
-      'transform',
-      'text-gray-700',
-    )
+function getToastIcon(type: string): Element {
+  const svgContainer = document.createElement('div')
+  svgContainer.classList.add('flex-none', 'pr-2')
 
-    if (message.type === 'alert') {
-      toastMessage.classList.add('bg-gray-200', 'border-gray-300')
-    } else if (message.type === 'error') {
-      toastMessage.classList.add('bg-red-400', 'border-red-500')
-    } else if (message.type === 'success') {
-      toastMessage.classList.add('bg-emerald-200', 'border-emerald-300')
-    } else if (message.type === 'warning') {
-      toastMessage.classList.add('bg-amber-200', 'border-amber-300')
-    }
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.setAttribute('fill', 'currentColor')
+  svg.setAttribute('fill-rule', 'evenodd')
+  svg.setAttribute('viewBox', '0 0 20 20')
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+  svg.classList.add('inline-block', 'h-6', 'w-6')
+  svgContainer.appendChild(svg)
 
-    toastMessage.appendChild(this.getToastIcon(message.type))
+  const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  svg.appendChild(svgPath)
 
-    // Add the content string.
-    const contentContainer = document.createElement('span')
-    contentContainer.textContent = message.content
-    toastMessage.appendChild(contentContainer)
+  if (type === 'alert') {
+    svgPath.setAttribute('d', INFO_SVG_PATH)
+  } else if (type === 'error') {
+    svgPath.setAttribute('d', ERROR_SVG_PATH)
+  } else if (type === 'success') {
+    svg.setAttribute('fill', 'none')
+    svg.setAttribute('stroke', 'currentColor')
+    svg.setAttribute('stroke-width', '2')
+    svgPath.setAttribute('d', SUCCESS_SVG_PATH)
+  } else if (type === 'warning') {
+    svgPath.setAttribute('d', WARNING_SVG_PATH)
+  }
+  return svgContainer
+}
 
-    // Maybe add dismiss button.
-    if (message.canDismiss) {
-      const dismissButton = document.createElement('div')
-      dismissButton.setAttribute('id', message.id + '-dismiss')
-      dismissButton.classList.add(
-        'absolute',
-        'font-bold',
-        'pl-6',
-        'opacity-40',
-        'right-4',
-        'top-2',
-        'cursor-pointer',
-        'hover:opacity-100',
-      )
-      dismissButton.textContent = 'x'
-      dismissButton.addEventListener('click', this.dismissClicked)
-      toastMessage.appendChild(dismissButton)
-      toastMessage.classList.add('pr-8')
-    }
-
-    const toastContainer = document.getElementById(ToastController.containerId)
-    if (toastContainer) {
-      toastContainer.appendChild(toastMessage)
+/** If a toast message is present, create it and add it to the container. */
+function maybeShowToasts() {
+  const messages = Array.from(
+    document.querySelectorAll('.' + MESSAGE_DATA_CLASS),
+  )
+  messages.forEach((element) => {
+    const message: ToastMessage = {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      toastContainer!.classList.remove('hidden')
-      if (message.duration > 0) {
-        setTimeout(
-          ToastController.dismissToast,
-          message.duration,
-          message.id,
-          /* dismissClicked = */ false,
-        )
-      }
-    }
-  }
-
-  private getToastIcon(type: string): Element {
-    const svgContainer = document.createElement('div')
-    svgContainer.classList.add('flex-none', 'pr-2')
-
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    svg.setAttribute('fill', 'currentColor')
-    svg.setAttribute('fill-rule', 'evenodd')
-    svg.setAttribute('viewBox', '0 0 20 20')
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
-    svg.classList.add('inline-block', 'h-6', 'w-6')
-    svgContainer.appendChild(svg)
-
-    const svgPath = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'path',
-    )
-    svg.appendChild(svgPath)
-
-    if (type === 'alert') {
-      svgPath.setAttribute('d', ToastController.infoSvgPath)
-    } else if (type === 'error') {
-      svgPath.setAttribute('d', ToastController.errorSvgPath)
-    } else if (type === 'success') {
-      svg.setAttribute('fill', 'none')
-      svg.setAttribute('stroke', 'currentColor')
-      svg.setAttribute('stroke-width', '2')
-      svgPath.setAttribute('d', ToastController.successSvgPath)
-    } else if (type === 'warning') {
-      svgPath.setAttribute('d', ToastController.warningSvgPath)
-    }
-    return svgContainer
-  }
-
-  /** If a toast message is present, create it and add it to the container. */
-  maybeShowToasts() {
-    const messages = Array.from(
-      document.querySelectorAll('.' + ToastController.messageDataClass),
-    )
-    messages.forEach((element) => {
-      const message: ToastMessage = {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        id: element.getAttribute('id')!,
-        canDismiss: element.getAttribute('canDismiss') === 'true',
-        canIgnore: element.getAttribute('canIgnore') === 'true',
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        content: element.textContent!,
-        duration: Number(element.getAttribute('toastDuration')),
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        type: element.getAttribute('toastType')!.toLowerCase(),
-      }
-      element.remove()
-      this.showToastMessage(message)
-    })
-  }
-
-  /**
-   *  Hide warning message and throw an indicator in local storage to not show.
-   *  @param {Event} event The event that triggered this action.
-   *  */
-  dismissClicked(event: Event) {
-    const target = event.target as Element
-    const toast = target.closest('.' + ToastController.messageClass)
-    if (toast && toast.hasAttribute('id')) {
+      id: element.getAttribute('id')!,
+      canDismiss: element.getAttribute('canDismiss') === 'true',
+      canIgnore: element.getAttribute('canIgnore') === 'true',
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const toastId = toast.getAttribute('id')!
-      ToastController.dismissToast(toastId, /* dismissClicked = */ true)
+      content: element.textContent!,
+      duration: Number(element.getAttribute('toastDuration')),
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      type: element.getAttribute('toastType')!.toLowerCase(),
     }
+    element.remove()
+    showToastMessage(message)
+  })
+}
+
+/**
+ *  Hide warning message and throw an indicator in local storage to not show.
+ *  @param {Event} event The event that triggered this action.
+ *  */
+function dismissClicked(event: Event) {
+  const target = event.target as Element
+  const toast = target.closest('.' + MESSAGE_CLASS)
+  if (toast && toast.hasAttribute('id')) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const toastId = toast.getAttribute('id')!
+    dismissToast(toastId, /* dismissClicked = */ true)
+  }
+}
+
+/**
+ * If toastMessage has a storageId and was dismissed by the user (not a timeout)
+ * then we add the id to localStorage so that it isn't displayed again.
+ * @param {string} toastId The html id of the toast message
+ * @param {boolean} dismissClicked Whether to add indicator in local storage to not show
+ *  */
+function dismissToast(toastId: string, dismissClicked: boolean) {
+  const toastMessage = document.getElementById(toastId)
+  if (toastMessage) {
+    if (dismissClicked && toastMessage.getAttribute('ignorable')) {
+      localStorage.setItem(toastId + '-dismissed', 'true')
+    }
+
+    // Dismiss the toast with the given id.
+    toastMessage.classList.add('opacity-0')
+    cleanupToast(toastId)
+  }
+}
+
+/**
+ * Removes a toast from the DOM and hides the toast container if it is now empty.
+ * @param {string} toastId The html id of the toast message
+ * */
+function cleanupToast(toastId: string) {
+  const toastMessage = document.getElementById(toastId)
+  if (toastMessage) {
+    // Remove toast message.
+    toastMessage.remove()
   }
 
-  /**
-   * If toastMessage has a storageId and was dismissed by the user (not a timeout)
-   * then we add the id to localStorage so that it isn't displayed again.
-   * @param {string} toastId The html id of the toast message
-   * @param {boolean} dismissClicked Whether to add indicator in local storage to not show
-   *  */
-  static dismissToast(toastId: string, dismissClicked: boolean) {
-    const toastMessage = document.getElementById(toastId)
-    if (toastMessage) {
-      if (dismissClicked && toastMessage.getAttribute('ignorable')) {
-        localStorage.setItem(toastId + '-dismissed', 'true')
-      }
-
-      // Dismiss the toast with the given id.
-      toastMessage.classList.add('opacity-0')
-      ToastController.cleanupToast(toastId)
-    }
-  }
-
-  /**
-   * Removes a toast from the DOM and hides the toast container if it is now empty.
-   * @param {string} toastId The html id of the toast message
-   * */
-  static cleanupToast(toastId: string) {
-    const toastMessage = document.getElementById(toastId)
-    if (toastMessage) {
-      // Remove toast message.
-      toastMessage.remove()
-    }
-
-    /** Hide toast container if there are no toast messages active. */
-    const toastContainer = document.getElementById(this.containerId)
-    if (toastContainer && toastContainer.children.length == 0) {
-      toastContainer.classList.add('hidden')
-    }
+  /** Hide toast container if there are no toast messages active. */
+  const toastContainer = document.getElementById(CONTAINER_ID)
+  if (toastContainer && toastContainer.children.length == 0) {
+    toastContainer.classList.add('hidden')
   }
 }
 
@@ -236,11 +225,4 @@ type ToastMessage = {
   content: string
   duration: number
   type: string
-}
-
-export function init() {
-  // toastController is used from other JS files. They read it from the window object.
-  // TODO(#3864): refactor to export instance of controller instead of setting
-  // it on window object.
-  window['toastController'] = new ToastController()
 }
