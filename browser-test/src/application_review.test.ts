10d5271997da07933c285a4caae70510aae0b5e7
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2023-01-19 14:40:27 -0800

    CiviForm Logo as "home" navigation to program page (#4075)

--- browser-test/src/application_review.test.ts
+++ browser-test/src/application_review.test.ts
@@ -7,6 +7,8 @@ import {
   logout,
   selectApplicantLanguage,
   testUserDisplayName,
+  waitForPageJsLoad,
+  validateScreenshot,
 } from './support'
 
 describe('Program admin review of submitted applications', () => {
@@ -241,6 +243,11 @@ describe('Program admin review of submitted applications', () => {
       'favorite-trees-q',
       'pine; cherry',
     )
+    // Expect CF logo to route ProgramAdmins back to their homepage
+    await page.click('text=CF')
+    await waitForPageJsLoad(page)
+
+    await validateScreenshot(page, 'applications-page')
   })
 
   it('program applications listed most recent first', async () => {
