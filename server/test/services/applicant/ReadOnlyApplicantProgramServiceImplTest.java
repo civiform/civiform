8ac99f60f745abcb0cfa734205fba44cf99d58b4
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2023-01-23 16:21:51 -0500

    Update ReadOnlyApplicantProgramServiceImplTest.java

--- server/test/services/applicant/ReadOnlyApplicantProgramServiceImplTest.java
+++ server/test/services/applicant/ReadOnlyApplicantProgramServiceImplTest.java
@@ -1067,6 +1067,44 @@ public class ReadOnlyApplicantProgramServiceImplTest extends ResetPostgres {
     assertThat(subject.getBlockIndex("not a real block id")).isEqualTo(-1);
   }
 
+  @Test
+  @Parameters({"5, true", "1, false"})
+  public void getIsApplicationEligible(long answer, boolean expectedResult) {
+    // Create a program with an eligibility condition == 5 and answer it with different values.
+    QuestionDefinition numberQuestionDefinition =
+        testQuestionBank.applicantJugglingNumber().getQuestionDefinition();
+    PredicateDefinition predicate =
+        PredicateDefinition.create(
+            PredicateExpressionNode.create(
+                LeafOperationExpressionNode.create(
+                    numberQuestionDefinition.getId(),
+                    Scalar.NUMBER,
+                    Operator.EQUAL_TO,
+                    PredicateValue.of("5"))),
+            PredicateAction.SHOW_BLOCK);
+
+    EligibilityDefinition eligibilityDefinition =
+        EligibilityDefinition.builder().setPredicate(predicate).build();
+    programDefinition =
+        ProgramBuilder.newDraftProgram("My Program")
+            .withBlock("Block one")
+            .withRequiredQuestionDefinition(numberQuestionDefinition)
+            .withEligibilityDefinition(eligibilityDefinition)
+            .buildDefinition();
+
+    // Answer the questions
+    answerNameQuestion(programDefinition.id());
+    QuestionAnswerer.answerNumberQuestion(
+        applicantData,
+        ApplicantData.APPLICANT_PATH.join(numberQuestionDefinition.getQuestionPathSegment()),
+        answer);
+
+    // Test the summary data
+    ReadOnlyApplicantProgramService subject =
+        new ReadOnlyApplicantProgramServiceImpl(applicantData, programDefinition, FAKE_BASE_URL);
+    assertThat(subject.isApplicationEligible()).isEqualTo(expectedResult);
+  }
+
   private void answerNameQuestion(long programId) {
     Path path = Path.create("applicant.applicant_name");
     QuestionAnswerer.answerNameQuestion(applicantData, path, "Alice", "Middle", "Last");
