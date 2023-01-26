cce521f8810aa8a8d3231bd4a62494921b41aef2
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2023-01-26 15:26:49 -0500

    averages

--- server/app/controllers/monitoring/MetricsController.java
+++ server/app/controllers/monitoring/MetricsController.java
@@ -4,11 +4,16 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.typesafe.config.Config;
 import controllers.CiviFormController;
+import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.UncheckedIOException;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Inject;
 import play.mvc.Result;
 
@@ -44,6 +49,45 @@ public final class MetricsController extends CiviFormController {
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
+
+    var counts = new HashMap<String,  Double>() ;
+    var totalTimes = new HashMap<String,  Double>() ;
+    Enumeration<Collector.MetricFamilySamples> enumer = collectorRegistry.metricFamilySamples();
+    while (enumer.hasMoreElements()) {
+      var samples = enumer.nextElement();
+      for (var sample: samples.samples) {
+        var name = sample.name;
+        var labels = new HashMap<String, String>();
+        for(int i = 0; i < sample.labelNames.size(); ++i) {
+          labels.put(sample.labelNames.get(i), sample.labelValues.get(i));
+        }
+        System.out.println(name);
+
+        if (name.endsWith("_sum")) {
+          double value = sample.value;
+          var subName = name.substring(0, name.length() - 4);
+          var measure = String.format("%s_%s_%s_%s_%s", subName, labels.get("RouteActionMethod"), labels.get("Path"), labels.get("Status"), labels.get("Verb"));
+          totalTimes.put(measure, value);
+        }
+        if (name.endsWith("_count")) {
+          double value = sample.value;
+          var subName = name.substring(0, name.length() - 6);
+          var measure = String.format("%s_%s_%s_%s_%s", subName, labels.get("RouteActionMethod"), labels.get("Path"), labels.get("Status"), labels.get("Verb"));
+          counts.put(measure, value);
+        }
+      }
+    }
+    var metricByTime = new HashMap<Double, String>();
+    counts.keySet().stream().sorted().forEach(
+      key -> {
+        double count = counts.get(key);
+        double totalTime = totalTimes.get(key);
+        var metric = String.format("%s: num %f avg ms: %f\n", key, count, totalTime/count);
+        metricByTime.put(totalTime/count, metric);
+      }
+    );
+    metricByTime.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey)).map(Map.Entry::getValue).forEach(writer::write);
+
 
     return ok(writer.toString()).as(TextFormat.CONTENT_TYPE_004);
   }
