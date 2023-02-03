d548b0f6f4895465bf86c924e8e0431bfc574554
Sean Albert <smabert@gmail.com>
2023-02-03 15:52:35 -0700

    javadoc for EsriServiceAreaInclusion

--- server/app/services/geo/esri/EsriServiceAreaInclusion.java
+++ server/app/services/geo/esri/EsriServiceAreaInclusion.java
@@ -3,16 +3,20 @@ package services.geo.esri;
 import com.google.auto.value.AutoValue;
 import java.time.Instant;
 
+/** Represents a service area and its inclusion state for an address location. */
 @AutoValue
 public abstract class EsriServiceAreaInclusion {
   public static Builder builder() {
     return new AutoValue_EsriServiceAreaInclusion.Builder();
   }
 
+  /** returns the service area that was checked for inclusion. */
   public abstract String getArea();
 
+  /** returns the inclusion state of the service area. */
   public abstract EsriServiceAreaState getState();
 
+  /** return the timestamp for when the inclusion check was made. */
   public abstract Instant getTimeStamp();
 
   @AutoValue.Builder
