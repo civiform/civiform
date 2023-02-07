a8d8f1abc85bb87843a3b8f405d63a1d91342f04
Avritt Rohwer <avritt@google.com>
2023-02-07 14:27:56 -0800

    Browser test not needed

--- browser-test/src/landing_page.test.ts
+++ browser-test/src/landing_page.test.ts
@@ -1,65 +1,23 @@
 import {
   AuthStrategy,
   createTestContext,
-  disableFeatureFlag,
-  enableFeatureFlag,
   validateAccessibility,
   validateScreenshot,
-  TestContext,
 } from './support'
 import {TEST_USER_AUTH_STRATEGY} from './support/config'
 
-const sharedTests = (ctx: TestContext, screenshotName: string) => {
+describe('the landing page', () => {
+  const ctx = createTestContext()
+
   it('it has login options', async () => {
     expect(await ctx.page.textContent('html')).toContain('Continue as guest')
     if (TEST_USER_AUTH_STRATEGY !== AuthStrategy.AWS_STAGING) {
       expect(await ctx.page.textContent('html')).toContain('Create account')
     }
-  })
-
-  it('it matches screenshot', async () => {
-    await validateScreenshot(ctx.page, screenshotName)
+    await validateScreenshot(ctx.page, 'landing-page')
   })
 
   it('has no accessibility violations', async () => {
     await validateAccessibility(ctx.page)
-  })
-}
-
-describe('the landing page', () => {
-  const ctx = createTestContext()
-
-  describe('without image tag feature flag', () => {
-    beforeAll(async () => {
-      await disableFeatureFlag(
-        ctx.page,
-        'show_civiform_image_tag_on_landing_page',
-      )
-    })
-
-    sharedTests(ctx, 'landing-page-no-version')
-
-    it('it does not have civiform version', async () => {
-      expect(await ctx.page.textContent('html')).not.toContain(
-        'CiviForm version: dev',
-      )
-    })
-  })
-
-  describe('with image tag feature flag', () => {
-    beforeAll(async () => {
-      await enableFeatureFlag(
-        ctx.page,
-        'show_civiform_image_tag_on_landing_page',
-      )
-    })
-
-    sharedTests(ctx, 'landing-page-with-version')
-
-    it('it has civiform version', async () => {
-      expect(await ctx.page.textContent('html')).toContain(
-        'CiviForm version: dev',
-      )
-    })
   })
 })
