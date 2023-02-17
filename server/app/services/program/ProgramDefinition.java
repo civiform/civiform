1cb18f1a95b667afe5f7c8ab647e41cc7dbb959b
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-02-17 10:14:39 -0500

    Show eligibility status in Program Admin application view (#4227)

--- server/app/services/program/ProgramDefinition.java
+++ server/app/services/program/ProgramDefinition.java
@@ -100,6 +100,12 @@ public abstract class ProgramDefinition {
     return orderedProgramDefinition;
   }
 
+  /** Returns whether a program has eligibility conditions applied. */
+  public boolean hasEligibilityEnabled() {
+    return blockDefinitions().stream()
+        .anyMatch(blockDef -> blockDef.eligibilityDefinition().isPresent());
+  }
+
   private ImmutableList<BlockDefinition> orderBlockDefinitionsInner(
       ImmutableList<BlockDefinition> currentLevel) {
     ImmutableList.Builder<BlockDefinition> blockDefinitionBuilder = ImmutableList.builder();
