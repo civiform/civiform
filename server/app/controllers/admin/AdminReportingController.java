46f96001341428d12feb2f0d7168f00bd293278f
Bion Johnson <bion@users.noreply.github.com>
2023-03-07 14:34:12 -0800

    Program-specific reporting pages (#4331)

--- server/app/controllers/admin/AdminReportingController.java
+++ server/app/controllers/admin/AdminReportingController.java
@@ -8,25 +8,66 @@ import javax.inject.Inject;
 import org.pac4j.play.java.Secure;
 import play.mvc.Http;
 import play.mvc.Result;
+import services.program.ProgramService;
 import services.reporting.ReportingService;
 import views.admin.reporting.AdminReportingIndexView;
+import views.admin.reporting.AdminReportingShowView;
 
 /** Controller for displaying reporting data to the admin roles. */
 public final class AdminReportingController extends CiviFormController {
 
   private final AdminReportingIndexView adminReportingIndexView;
+  private final AdminReportingShowView adminReportingShowView;
+  private final ProgramService programService;
   private final ReportingService reportingService;
 
   @Inject
   public AdminReportingController(
-      AdminReportingIndexView adminReportingIndexView, ReportingService reportingService) {
+      AdminReportingIndexView adminReportingIndexView,
+      AdminReportingShowView adminReportingShowView,
+      ProgramService programService,
+      ReportingService reportingService) {
     this.adminReportingIndexView = Preconditions.checkNotNull(adminReportingIndexView);
+    this.adminReportingShowView = Preconditions.checkNotNull(adminReportingShowView);
+    this.programService = Preconditions.checkNotNull(programService);
     this.reportingService = Preconditions.checkNotNull(reportingService);
   }
 
   @Secure(authorizers = Authorizers.Labels.ANY_ADMIN)
   public Result index() {
     return ok(adminReportingIndexView.render(reportingService.getMonthlyStats()));
+  }
+
+  @Secure(authorizers = Authorizers.Labels.ANY_ADMIN)
+  public Result show(String programSlug) {
+    String programName =
+        programService
+            .getActiveProgramDefinitionAsync(programSlug)
+            .toCompletableFuture()
+            .join()
+            .adminName();
+    return ok(
+        adminReportingShowView.render(
+            programSlug, programName, reportingService.getMonthlyStats()));
+  }
+
+  @Secure(authorizers = Authorizers.Labels.ANY_ADMIN)
+  public Result downloadProgramCsv(String programSlug) {
+    String programName =
+        programService
+            .getActiveProgramDefinitionAsync(programSlug)
+            .toCompletableFuture()
+            .join()
+            .adminName();
+
+    String csv = reportingService.applicationsToProgramByMonthCsv(programName);
+
+    return ok(csv)
+        .as(Http.MimeTypes.BINARY)
+        .withHeader(
+            "Content-Disposition",
+            String.format(
+                "attachment; filename=\"%s\"", String.format("CiviForm_%s.csv", programSlug)));
   }
 
   /** Identifiers for the different data sets available for download. */
