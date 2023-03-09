767107e1063b497bd1a1ac5b8d9165d65599b797
Michael Zetune <mzetune@google.com>
2023-03-09 15:32:47 -0500

    Delete removal of post_logout_redirect_uri in CustomOidcLogoutRequest

--- server/app/auth/oidc/CustomOidcLogoutRequest.java
+++ server/app/auth/oidc/CustomOidcLogoutRequest.java
@@ -64,8 +64,6 @@ public final class CustomOidcLogoutRequest extends LogoutRequest {
 
     Map<String, List<String>> params = super.toParameters();
 
-    // Remove post_logout_redirect_uri and replace with custom logic.
-    params.remove("post_logout_redirect_uri");
     if (postLogoutRedirectURI != null && !postLogoutRedirectParam.isEmpty()) {
       params.put(
           postLogoutRedirectParam, Collections.singletonList(postLogoutRedirectURI.toString()));
