cff8f739ad4a03ba1a0df043688912e9d6905a1d
Sean Albert <smabert@gmail.com>
2023-03-12 16:05:25 -0700

    formatting

--- server/test/controllers/admin/AdminProgramControllerTest.java
+++ server/test/controllers/admin/AdminProgramControllerTest.java
@@ -213,7 +213,8 @@ public class AdminProgramControllerTest extends ResetPostgres {
     // Redirect is to the blocks edit page.
     assertThat(result.status()).isEqualTo(SEE_OTHER);
     assertThat(result.redirectLocation())
-        .hasValue(controllers.admin.routes.AdminProgramBlocksController.index(newDraft.get().id).url());
+        .hasValue(
+            controllers.admin.routes.AdminProgramBlocksController.index(newDraft.get().id).url());
   }
 
   @Test
@@ -230,7 +231,8 @@ public class AdminProgramControllerTest extends ResetPostgres {
     // Redirect is to the blocks edit page.
     assertThat(result.status()).isEqualTo(SEE_OTHER);
     assertThat(result.redirectLocation())
-        .hasValue(controllers.admin.routes.AdminProgramBlocksController.index(draftProgram.id).url());
+        .hasValue(
+            controllers.admin.routes.AdminProgramBlocksController.index(draftProgram.id).url());
 
     Program updatedDraft =
         programRepository.lookupProgram(draftProgram.id).toCompletableFuture().join().get();
