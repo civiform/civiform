8d120fb683590b7e73cfa3cc49e3d9601d1d5dc4
Austin Valeske <avaleske@google.com>
2023-03-22 18:53:08 -0400

    Switch to using inheritance to handle the different upsell views

--- server/app/views/applicant/ApplicantCommonIntakeUpsellCreateAccountView.java
+++ server/app/views/applicant/ApplicantCommonIntakeUpsellCreateAccountView.java
@@ -1,45 +1,31 @@
 package views.applicant;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static j2html.TagCreator.div;
 import static j2html.TagCreator.each;
-import static j2html.TagCreator.h1;
-import static j2html.TagCreator.h2;
 import static j2html.TagCreator.h3;
 import static j2html.TagCreator.p;
 import static j2html.TagCreator.section;
 
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
-import controllers.routes;
 import j2html.tags.specialized.DivTag;
 import java.util.Locale;
 import java.util.Optional;
-
-import j2html.tags.specialized.SectionTag;
 import models.Account;
 import play.i18n.Messages;
 import play.mvc.Http;
 import play.twirl.api.Content;
-import services.MessageKey;
 import services.applicant.ApplicantService;
-import views.BaseHtmlView;
-import views.HtmlBundle;
-import views.components.LinkElement;
 import views.components.ToastMessage;
-import views.style.ApplicantStyles;
 import views.style.ReferenceClasses;
-import views.style.StyleUtils;
 
 /** Renders a confirmation page after application submission, for the common intake form. */
-public final class ApplicantCommonIntakeUpsellCreateAccountView extends BaseHtmlView {
-
-  private final ApplicantLayout layout;
+public final class ApplicantCommonIntakeUpsellCreateAccountView
+    extends UpsellCreateAccountBaseView {
 
   @Inject
   public ApplicantCommonIntakeUpsellCreateAccountView(ApplicantLayout layout) {
-    this.layout = checkNotNull(layout);
+    super(layout);
   }
 
   /** Renders a sign-up page with a baked-in redirect. */
@@ -54,29 +40,12 @@ public final class ApplicantCommonIntakeUpsellCreateAccountView extends BaseHtml
     //  String title = messages.at(MessageKey.TITLE_APPLICATION_CONFIRMATION.getKeyName());
     String title = "Benefits you may qualify for";
 
-    HtmlBundle bundle = layout.getBundle().setTitle(title);
-
     DivTag content =
-        div()
-            .withClasses(ApplicantStyles.PROGRAM_INFORMATION_BOX)
-            .with(h1(title).withClasses("text-3xl", "text-black", "font-bold", "mb-4"))
-            .with(
-                eligibleProgramsSection(eligiblePrograms, messages.lang().toLocale())
-                    .withClasses(ReferenceClasses.BT_APPLICATION_ID, "mb-4"));
+        eligibleProgramsSection(eligiblePrograms, messages.lang().toLocale())
+            .withClasses(ReferenceClasses.BT_APPLICATION_ID, "mb-4");
 
-    content.with(addCreateAccountSection(redirectTo, account, messages));
-
-    bannerMessage.ifPresent(bundle::addToastMessages);
-
-    bundle.addMainStyles(ApplicantStyles.MAIN_PROGRAM_APPLICATION).addMainContent(content);
-
-    return layout.renderWithNav(request, applicantName, messages, bundle);
-  }
-
-  private SectionTag addCreateAccountSection(String redirectTo, Account account, Messages messages) {
-
-
-
+    return super.renderContainer(
+        request, redirectTo, account, applicantName, messages, title, content, bannerMessage);
   }
 
   private DivTag eligibleProgramsSection(
