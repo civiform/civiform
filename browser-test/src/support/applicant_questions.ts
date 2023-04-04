d9d55ac53a65c80ef4a310cc9099a5981b0ad98b
Michael Zetune <30369272+MichaelZetune@users.noreply.github.com>
2023-04-04 17:04:53 -0400

    Prompt guests to log in with modal on application confirmation screen (#4500)

--- browser-test/src/support/applicant_questions.ts
+++ browser-test/src/support/applicant_questions.ts
@@ -188,6 +188,10 @@ export class ApplicantQuestions {
     await waitForPageJsLoad(this.page)
   }
 
+  async clickApplyToAnotherProgramButton() {
+    await this.page.click('button:has-text("Apply to another program")')
+  }
+
   async expectProgramPublic(programName: string, description: string) {
     const tableInnerText = await this.page.innerText('main')
 
@@ -345,7 +349,15 @@ export class ApplicantQuestions {
     expect(await this.page.innerText('h1')).toContain(
       'Application confirmation',
     )
-    await this.page.click('text="Apply to another program"')
+    await this.clickApplyToAnotherProgramButton()
+
+    // If we are logged in as a guest, we will get a prompt to log
+    // in before going back to the programs page. Bypass this to stay
+    // logged in as a guest.
+    const pageContent = await this.page.textContent('html')
+    if (pageContent!.includes('Continue without an account')) {
+      await this.page.click('text="Continue without an account"')
+    }
     await waitForPageJsLoad(this.page)
 
     // Ensure that we redirected to the programs list page.
