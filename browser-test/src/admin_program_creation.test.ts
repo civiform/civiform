173a194ad24a994aef7ff907a75fdb214c1b4192
Reema Al-Marzoog <reemaa@google.com>
2023-04-20 16:47:13 -0400

    Wait for response

--- browser-test/src/admin_program_creation.test.ts
+++ browser-test/src/admin_program_creation.test.ts
@@ -170,7 +170,11 @@ describe('program creation', () => {
     // the input value shows what it will be set to when clicked
     expect(await addressCorrectionInput.inputValue()).toBe('true')
 
+    const responsePromise = page.waitForResponse((response) => {
+      return response.url().includes('setAddressCorrectionEnabled')
+    })
     await adminPrograms.clickAddressCorrectionToggle()
+    await responsePromise
 
     expect(await addressCorrectionInput.inputValue()).toBe('false')
 
