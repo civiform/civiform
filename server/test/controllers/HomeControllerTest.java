33a0ffe2a6999bb4a683928abc6a4012a921357b
Michael Zetune <mzetune@google.com>
2023-04-27 15:32:08 -0400

    Fix HomeControllerTest

--- server/test/controllers/HomeControllerTest.java
+++ server/test/controllers/HomeControllerTest.java
@@ -5,38 +5,23 @@ import static play.api.test.Helpers.testServerPort;
 import static play.test.Helpers.fakeRequest;
 import static play.test.Helpers.route;
 
-import featureflags.FeatureFlag;
-import java.util.Optional;
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.pac4j.core.context.HttpConstants;
 import play.mvc.Http;
 import play.mvc.Result;
 import repository.ResetPostgres;
 
-@RunWith(JUnitParamsRunner.class)
 public class HomeControllerTest extends ResetPostgres {
 
   @Test
-  @Parameters({"true", "false"})
-  public void testUnauthenticatedSecurePage(boolean bypassLoginLanguageScreens) {
+  public void testUnauthenticatedSecurePage() {
     Http.RequestBuilder request =
         fakeRequest(routes.HomeController.securePlayIndex())
-            .header(Http.HeaderNames.HOST, "localhost:" + testServerPort())
-            .session(
-                FeatureFlag.BYPASS_LOGIN_LANGUAGE_SCREENS.toString(),
-                Boolean.toString(bypassLoginLanguageScreens));
+            .header(Http.HeaderNames.HOST, "localhost:" + testServerPort());
     Result result = route(app, request);
     assertThat(result.status()).isNotEqualTo(HttpConstants.OK);
 
-    if (bypassLoginLanguageScreens) {
-      assertThat(result.redirectLocation().get()).isEqualTo(routes.HomeController.index().url());
-    } else {
-      assertThat(result.redirectLocation())
-          .contains(routes.HomeController.loginForm(Optional.of("login")).url());
-    }
+    assertThat(result.redirectLocation().get()).isEqualTo(routes.HomeController.index().url());
   }
 
   @Test
