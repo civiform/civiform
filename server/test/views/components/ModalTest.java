dbf77cbf13c0de3f4adae0b15dbe3b86b44a6fe2
Michael Zetune <mzetune@google.com>
2023-05-05 13:36:00 -0400

    Add ModalTest.java

---
+++ server/test/views/components/ModalTest.java
@@ -0,0 +1,71 @@
+package views.components;
+
+import static j2html.TagCreator.div;
+import static j2html.TagCreator.text;
+import static org.assertj.core.api.Assertions.assertThat;
+import static views.components.Modal.RepeatOpenBehavior.Group.*;
+
+import org.junit.Test;
+
+public class ModalTest {
+
+  private static final Modal.Builder MODAL_BUILDER =
+      Modal.builder()
+          .setModalId("modal-id")
+          .setContent(div().with(text("content")))
+          .setModalTitle("Modal Title");
+
+  @Test
+  public void buildsModal_withIdTitleAndContent() {
+    Modal modal = MODAL_BUILDER.build();
+    String rendered = modal.getContainerTag().render();
+
+    assertThat(rendered).contains("<div id=\"modal-id\"");
+    assertThat(rendered).contains("<div>content</div>");
+    assertThat(rendered).contains("Modal Title");
+  }
+
+  @Test
+  public void buildsModal_withSetWidth() {
+    Modal modal = MODAL_BUILDER.setWidth(Modal.Width.FOURTH).build();
+    String rendered = modal.getContainerTag().render();
+
+    assertThat(rendered).contains("w-1/4");
+  }
+
+  @Test
+  public void buildsModal_withRepeatOpenBehavior_alwaysShow() {
+    Modal modal =
+        MODAL_BUILDER.setRepeatOpenBehavior(Modal.RepeatOpenBehavior.alwaysShow()).build();
+    String rendered = modal.getContainerTag().render();
+
+    assertThat(rendered).doesNotContain("only-show-once-group");
+  }
+
+  @Test
+  public void buildsModal_withRepeatOpenBehavior_onlyShowOnceNoBypassUrl() {
+    Modal modal =
+        MODAL_BUILDER
+            .setRepeatOpenBehavior(
+                Modal.RepeatOpenBehavior.showOnlyOnce(PROGRAMS_INDEX_LOGIN_PROMPT))
+            .build();
+    String rendered = modal.getContainerTag().render();
+
+    assertThat(rendered).contains("only-show-once-group=\"programs_index_login_prompt\"");
+    assertThat(rendered).doesNotContain("bypass-url");
+  }
+
+  @Test
+  public void buildsModal_withRepeatOpenBehavior_onlyShowOnceWithBypassUrl() {
+    Modal modal =
+        MODAL_BUILDER
+            .setRepeatOpenBehavior(
+                Modal.RepeatOpenBehavior.showOnlyOnce(
+                    PROGRAMS_INDEX_LOGIN_PROMPT, "https://bypassurl.com"))
+            .build();
+    String rendered = modal.getContainerTag().render();
+
+    assertThat(rendered).contains("only-show-once-group=\"programs_index_login_prompt\"");
+    assertThat(rendered).contains("bypass-url=\"https://bypassurl.com\"");
+  }
+}
