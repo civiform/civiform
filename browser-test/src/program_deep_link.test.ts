3aacc4246227725b096a554923cf4a8a7ca58124
Michael Zetune <mzetune@google.com>
2023-05-08 11:55:14 -0400

    Write tests and add screenshot

--- browser-test/src/program_deep_link.test.ts
+++ browser-test/src/program_deep_link.test.ts
@@ -1,23 +1,27 @@
 import {
   createTestContext,
+  enableFeatureFlag,
   gotoEndpoint,
   loginAsAdmin,
   loginAsGuest,
   loginAsTestUser,
   logout,
   selectApplicantLanguage,
+  validateScreenshot,
 } from './support'
 
 describe('navigating to a deep link', () => {
   const ctx = createTestContext()
 
-  it('as a guest user or registered user', async () => {
+  const questionText = 'What is your address?'
+
+  beforeEach(async () => {
     const {page, adminQuestions, adminPrograms} = ctx
+
+    await enableFeatureFlag(page, 'bypass_login_language_screens')
 
     // Arrange
     await loginAsAdmin(page)
-
-    const questionText = 'What is your address?'
 
     await adminQuestions.addAddressQuestion({
       questionName: 'Test address question',
@@ -36,11 +40,38 @@ describe('navigating to a deep link', () => {
     await adminPrograms.expectActiveProgram(programName)
 
     await logout(page)
+  })
+
+  it('shows a login prompt for guest users', async () => {
+    const {page} = ctx
+
+    await gotoEndpoint(page, '/programs/test-deep-link')
+    await loginAsGuest(page)
+    expect(await page.innerText('html')).toContain('Continue to application')
+    await validateScreenshot(
+      page,
+      'login-prompt-for-guest-users-using-program-slug',
+    )
+  })
+
+  it('does not show login prompt for logged in users', async () => {
+    const {page} = ctx
+
+    await gotoEndpoint(page, '/programs/test-deep-link')
+    await loginAsTestUser(page, 'button:has-text("Log in")')
+    expect(await page.innerText('html')).not.toContain(
+      'Continue to application',
+    )
+  })
+
+  it('as a guest user or registered user', async () => {
+    const {page} = ctx
 
     // Exercise guest path
     // Act
     await gotoEndpoint(page, '/programs/test-deep-link')
     await loginAsGuest(page)
+    await page.click('text="Continue to application"')
     await selectApplicantLanguage(page, 'English')
 
     // Assert
@@ -53,13 +84,8 @@ describe('navigating to a deep link', () => {
 
     // Exercise test user path
     // Act
-
-    // TODO(#4705): make goToEndpoint execute before loginAsTestUser as it should,
-    // such that user can go to the slug, log in, and be taken back to the slug. Currently
-    // with BYPASS_LOGIN_LANGUAGE_SCREENS on, logging in always takes you back to the programs
-    // index page,and this behavior will take some work to override in a feasible way.
-    await loginAsTestUser(page)
     await gotoEndpoint(page, '/programs/test-deep-link')
+    await loginAsTestUser(page, 'button:has-text("Log in")')
     await selectApplicantLanguage(page, 'English')
 
     // Assert
