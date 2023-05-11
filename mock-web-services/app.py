b51c68f55d0cb7f4edef5f63559c0d546cb31dd4
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-05-10 17:14:03 -0700

    Adding docstrings to python files

--- mock-web-services/app.py
+++ mock-web-services/app.py
@@ -2,28 +2,46 @@ from flask import Flask, Response, request, json
 from esri import EsriMockWebService
 
 app = Flask(__name__)
-esriWS = EsriMockWebService(app)
+esri_ws = EsriMockWebService(app)
 
 
 @app.route("/esri/findAddressCandidates")
-def findAddressCandidates():
+def find_address_candidates():
+    """
+    Endpoint to mock Esri findAddressCandidates. We use the address as a key in
+    determining which type of response this will return.
+
+    Valid options are:
+        * Address In Area
+        * Legit Address
+        * Bogus Address
+    """
     try:
         address = request.args.get("address")
-        return esriWS.findAddressCandidates(address)
+        return esri_ws.find_address_candidates(address)
     except Exception as e:
         print(e)
-        return Response("Bad request", status=400)
+        return Response("Bad request. " + e, status=400)
 
 
 @app.route("/esri/serviceAreaFeatures")
-def serviceAreaFeatures():
+def service_area_features():
+    """
+    Endpoint to mock Esri service area validation. We use the latitude as a
+    key in determining which type of response this will return.
+
+    Valid options are:
+        * 100.0
+        * 101.0
+        * 102.0
+    """
     try:
         geometry = json.loads(request.args.get("geometry").replace("'", '"'))
         latitude = geometry.get("y")
-        return esriWS.serviceAreaFeatures(latitude)
+        return esri_ws.service_area_features(latitude)
     except Exception as e:
         print(e)
-        return Response("Bad request", status=400)
+        return Response("Bad request. " + e, status=400)
 
 
 if __name__ == "__main__":
--- mock-web-services/webservice/__init__.py
+++ mock-web-services/webservice/__init__.py
@@ -1,18 +1,21 @@
 class WebService:
+    """Base class contain helper methods for returning responses."""
 
     def __init__(self, app):
         self.app = app
 
-    def readFile(self, filename):
-        with open(filename, "r") as file:
+    def read_file(self, file_path):
+        """Reads a file from path and returns it as a string."""
+        with open(file_path, "r") as file:
             jsonstr = file.read()
 
         return jsonstr
 
-    def returnJsonResponse(self, jsonstr):
-        return self.app.response_class(
-            response=jsonstr, mimetype="application/json")
+    def return_json_response(self, jsonstr):
+        """Return a string of json as the response as the correct mimetype."""
+        return self.app.response_class(response=jsonstr, mimetype="application/json")
 
-    def returnJsonResponseFromFile(self, filename):
-        jsonstr = self.readFile(filename)
-        return self.returnJsonResponse(jsonstr)
+    def return_json_response_from_file(self, file_path):
+        """Reads a file from path and returns a response formatted as json"""
+        jsonstr = self.read_file(file_path)
+        return self.return_json_response(jsonstr)
