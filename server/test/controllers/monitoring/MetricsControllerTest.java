18e7f0d4906ac85710ca57bcc5a5281405e6671a
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-05-12 13:34:09 -0400

    Create MetricsControllerTest.java

---
+++ server/test/controllers/monitoring/MetricsControllerTest.java
@@ -0,0 +1,82 @@
+package controllers.monitoring;
+
+import com.google.common.collect.ImmutableMap;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import controllers.WithMockedProfiles;
+import io.prometheus.client.CollectorRegistry;
+import models.Applicant;
+import models.Application;
+import models.LifecycleStage;
+import org.junit.Before;
+import org.junit.Test;
+import repository.VersionRepository;
+import services.program.ProgramDefinition;
+import support.ProgramBuilder;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static play.test.Helpers.*;
+
+public class MetricsControllerTest extends WithMockedProfiles {
+
+  @Before
+  public void setUp() {
+    resetDatabase();
+  }
+
+  @Test
+  public void getMetrics_returns404WhenMetricsNotEnabled() {
+    Config config =
+      ConfigFactory.parseMap(
+        ImmutableMap.<String, String>builder()
+          .put("server_metrics.enabled", "false")
+          .build());
+
+    MetricsController controller =
+      new MetricsController(
+        instanceOf(CollectorRegistry.class),
+        config);
+    assertThat(controller.getMetrics().status()).isEqualTo(404);
+  }
+
+  @Test
+  public void getMetrics_returnsMetricData() {
+    ProgramDefinition programDefinition =
+      ProgramBuilder.newActiveProgram("test program", "desc").buildDefinition();
+    VersionRepository versionRepository = instanceOf(VersionRepository.class);
+    Applicant applicant = createApplicantWithMockedProfile();
+    applicant.getApplicantData().setPreferredLocale(Locale.ENGLISH);
+    applicant.save();
+    Application app =
+      new Application(applicant, programDefinition.toProgram(), LifecycleStage.DRAFT);
+    app.save();
+    resourceCreator().insertDraftProgram(programDefinition.adminName());
+    versionRepository.publishNewSynchronizedVersion();
+
+    Config config =
+      ConfigFactory.parseMap(
+        ImmutableMap.<String, String>builder()
+          .put("server_metrics.enabled", "true")
+          .build());
+
+    MetricsController controller =
+      new MetricsController(
+        instanceOf(CollectorRegistry.class),
+        config);
+
+    String metricsContent = contentAsString(controller.getMetrics());
+
+    assertThat(controller.getMetrics().status()).isEqualTo(200);
+    assertThat(metricsContent).contains(getEbeanCountName("Program.findList", 2));
+    assertThat(metricsContent).contains(getEbeanCountName("Question.findList", 2));
+    assertThat(metricsContent).contains(getEbeanCountName("Version.byId", 2));
+    assertThat(metricsContent).contains("ebean_queries_mean_latency_micros");
+    assertThat(metricsContent).contains("ebean_queries_max_latency_micros");
+    assertThat(metricsContent).contains("ebean_queries_total_latency_micros");
+  }
+
+  private String getEbeanCountName(String queryName, int count) {
+    return String.format("ebean_queries_total{name=\"%s\",} %s.0", queryName, count);
+  }
+}
