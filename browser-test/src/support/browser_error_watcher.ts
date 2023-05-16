db2c7a72c7a5f35b31656bf986b390994fc97df0
Bion Johnson <bionj@google.com>
2023-05-16 14:00:36 -0700

    fmt

--- browser-test/src/support/browser_error_watcher.ts
+++ browser-test/src/support/browser_error_watcher.ts
@@ -64,7 +64,7 @@ export class BrowserErrorWatcher {
       // Console errors with the message net::ERR_ABORTED have been a source of test
       // flakiness. We were unable to find any cases of these errors indicating actual
       // problems with the app or test system so ignore them here.
-      .filter((error) => !error.message.includes("net::ERR_ABORTED"))
+      .filter((error) => !error.message.includes('net::ERR_ABORTED'))
       .filter((error) => {
         return !this.urlsToIgnore.some((regexp) => regexp.test(error.url))
       })
