f62a802b543b3937a0a36067694d1a2b8c07bd64
Bion Johnson <bion@users.noreply.github.com>
2023-06-26 22:23:44 +0200

    Only use applicant_portal_name if whitelabel_civic_entity_full_name is not present (#5136)

--- server/app/modules/MainModule.java
+++ server/app/modules/MainModule.java
@@ -64,11 +64,20 @@ public class MainModule extends AbstractModule {
   public String provideApplicantAuthProviderName(Config config) {
     checkNotNull(config);
 
-    if (config.hasPath("applicant_portal_name")
-        && !config.getString("applicant_portal_name").isBlank()) {
+    boolean applicantPortalNamePresent =
+        config.hasPath("applicant_portal_name")
+            && !config.getString("applicant_portal_name").isBlank();
+
+    boolean civicEntityNameIsPresent =
+        config.hasPath("whitelabel_civic_entity_full_name")
+            && !config.getString("whitelabel_civic_entity_full_name").isBlank();
+
+    if (applicantPortalNamePresent) {
       return config.getString("applicant_portal_name");
+    } else if (civicEntityNameIsPresent) {
+      return config.getString("whitelabel_civic_entity_full_name");
     }
 
-    return config.getString("whitelabel_civic_entity_full_name");
+    return "TestPortal";
   }
 }
