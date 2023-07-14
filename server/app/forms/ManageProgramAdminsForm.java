bea56081945ae9d9951cd47ff9098eb30d68dfed
Tom Smith <tomsmith@google.com>
2023-07-14 15:46:56 -0400

    Revise flow for adding/removing program admins (#5220)

--- server/app/forms/ManageProgramAdminsForm.java
+++ server/app/forms/ManageProgramAdminsForm.java
@@ -6,12 +6,16 @@ import java.util.List;
 /** A form for adding and removing program admins by email for a specific program. */
 public final class ManageProgramAdminsForm {
 
+  private String adminEmail;
+  // TODO: remove the fields below in favor of adminEmail. We want the interaction to operate on a
+  // single email address at a time.
   private List<String> addAdminEmails;
   private List<String> removeAdminEmails;
 
   public ManageProgramAdminsForm() {
     this.addAdminEmails = new ArrayList<>();
     this.removeAdminEmails = new ArrayList<>();
+    this.adminEmail = "";
   }
 
   public List<String> getAdminEmails() {
@@ -28,5 +32,13 @@ public final class ManageProgramAdminsForm {
 
   public void setRemoveAdminEmails(List<String> removeAdminEmails) {
     this.removeAdminEmails = removeAdminEmails;
+  }
+
+  public String getAdminEmail() {
+    return adminEmail;
+  }
+
+  public void setAdminEmail(String adminEmail) {
+    this.adminEmail = adminEmail;
   }
 }
