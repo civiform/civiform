4826e2ad305372fbe7515ed3b053f900de3087e9
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-07-25 19:47:53 -0400

    Update ProgramDefinitionTest.java

--- server/test/services/program/ProgramDefinitionTest.java
+++ server/test/services/program/ProgramDefinitionTest.java
@@ -5,12 +5,11 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import auth.ProgramAcls;
+import com.google.common.collect.ImmutableList;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
-
-import com.google.common.collect.ImmutableList;
 import models.DisplayMode;
 import models.Question;
 import org.junit.Test;
@@ -234,7 +233,7 @@ public class ProgramDefinitionTest extends ResetPostgres {
     assertThat(program.hasQuestion(questionA)).isTrue();
     assertThat(program.hasQuestion(questionB)).isTrue();
     assertThat(program.hasQuestion(questionC)).isFalse();
-    assertThat(program.getQuestionIdsInProgram().equals(ImmutableList.of(123L, 321L));
+    assertThat(program.getQuestionIdsInProgram()).equals(ImmutableList.of(123L, 321L));
   }
 
   @Test
