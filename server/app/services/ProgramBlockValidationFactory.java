6ecab570502f8b89fcdd666f2d09699932273bb1
SwathiMurthi <swathi.murthi@seattle.gov>
2023-07-26 08:38:42 -0700

    formatting

--- server/app/services/ProgramBlockValidationFactory.java
+++ server/app/services/ProgramBlockValidationFactory.java
@@ -7,10 +7,11 @@ import models.Version;
 import repository.VersionRepository;
 
 /**
- * Factory for creating `ProgramBlockValidation` instances. 
- * <p>Needed because - for every question in the question picker, we will need to check if the question is
- * tombstoned, resulting in n+1 DB queries. To avoid this, we create a factory object injecting a
- * VersionRepository and calling getDraftVersion() only once per request.
+ * Factory for creating `ProgramBlockValidation` instances.
+ *
+ * <p>Needed because - for every question in the question picker, we will need to check if the
+ * question is tombstoned, resulting in n+1 DB queries. To avoid this, we create a factory object
+ * injecting a VersionRepository and calling getDraftVersion() only once per request.
  */
 public final class ProgramBlockValidationFactory {
 
