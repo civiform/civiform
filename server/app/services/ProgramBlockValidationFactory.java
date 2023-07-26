100e34d3f32e3db2f94417fc294c189cd5844544
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2023-07-26 08:21:51 -0700

    Update server/app/services/ProgramBlockValidationFactory.java
    Co-authored-by: Bion Johnson <bion@users.noreply.github.com>

--- server/app/services/ProgramBlockValidationFactory.java
+++ server/app/services/ProgramBlockValidationFactory.java
@@ -7,10 +7,10 @@ import models.Version;
 import repository.VersionRepository;
 
 /**
- * Helper class for creating a ProgramBlockValidation Object.The reason this factory class is needed
- * because - for every question in the question picker, we will need to check if the question is
- * tombstoned, resulting in n+1 DB queries. To avoid it, we create a factory object injecting a
- * VersionRepository and calling getDraftVersion() only once during the object creation.
+ * Factory for creating `ProgramBlockValidation` instances. 
+ * <p>Needed because - for every question in the question picker, we will need to check if the question is
+ * tombstoned, resulting in n+1 DB queries. To avoid this, we create a factory object injecting a
+ * VersionRepository and calling getDraftVersion() only once per request.
  */
 public final class ProgramBlockValidationFactory {
 
