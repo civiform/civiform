c24687e05a6518a17272107625a47611b4169070
Michael Zetune <mzetune@google.com>
2023-07-26 10:43:12 -0400

    Make JsonPrettifier final

--- server/app/services/export/JsonPrettifier.java
+++ server/app/services/export/JsonPrettifier.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-public class JsonPrettifier {
+public final class JsonPrettifier {
   public static String asPrettyJsonString(String original) {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
