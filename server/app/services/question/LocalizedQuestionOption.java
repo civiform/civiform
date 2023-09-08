e615a8794ed92dec472c63b16cfeb146c264091d
Austin Valeske <avaleske@google.com>
2023-09-08 18:44:12 -0400

    Update question definitions and related files to use admin names

--- server/app/services/question/LocalizedQuestionOption.java
+++ server/app/services/question/LocalizedQuestionOption.java
@@ -12,8 +12,8 @@ public abstract class LocalizedQuestionOption {
 
   /** Create a LocalizedQuestionOption. */
   public static LocalizedQuestionOption create(
-      long id, long order, String optionText, Locale locale) {
-    return new AutoValue_LocalizedQuestionOption(id, order, optionText, locale);
+      long id, long order, String adminName, String optionText, Locale locale) {
+    return new AutoValue_LocalizedQuestionOption(id, order, adminName, optionText, locale);
   }
 
   /** The id for this option. */
@@ -21,6 +21,9 @@ public abstract class LocalizedQuestionOption {
 
   /** The order of the option. */
   public abstract long order();
+
+  /** The immutable identifier for this option, used to reference it in the API and predicates. */
+  public abstract String adminName();
 
   /** The text strings to display to the user. */
   public abstract String optionText();
