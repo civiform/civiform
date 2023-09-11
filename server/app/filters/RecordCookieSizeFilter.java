329a1b6d6d8e31a1654910065a9a25730127be19
Tom Smith <yotommy@gmail.com>
2023-09-11 17:05:12 -0400

    bin/fmt

--- server/app/filters/RecordCookieSizeFilter.java
+++ server/app/filters/RecordCookieSizeFilter.java
@@ -8,24 +8,26 @@ import play.routing.Router;
 
 public class RecordCookieSizeFilter extends EssentialFilter {
   private Histogram PLAY_SESSION_COOKIE_SIZE =
-    Histogram.build()
-      .name("play_session_cookie_size_bytes")
-      .linearBuckets(0, 512, 10)
-      .labelNames("action_method")
-      .help("Size of the PLAY_SESSION cookie in bytes")
-      .register();
+      Histogram.build()
+          .name("play_session_cookie_size_bytes")
+          .linearBuckets(0, 512, 10)
+          .labelNames("action_method")
+          .help("Size of the PLAY_SESSION cookie in bytes")
+          .register();
 
   @Override
   public EssentialAction apply(EssentialAction next) {
     return EssentialAction.of(
-      requestHeader -> {
-        HandlerDef handlerDef = requestHeader.attrs().get(Router.Attrs.HANDLER_DEF);
-        String actionMethod = handlerDef.controller() + "." + handlerDef.method();
-        requestHeader.getCookie("PLAY_SESSION").ifPresent(cookie -> {
-          PLAY_SESSION_COOKIE_SIZE.labels(actionMethod).observe(cookie.value().length());
+        requestHeader -> {
+          HandlerDef handlerDef = requestHeader.attrs().get(Router.Attrs.HANDLER_DEF);
+          String actionMethod = handlerDef.controller() + "." + handlerDef.method();
+          requestHeader
+              .getCookie("PLAY_SESSION")
+              .ifPresent(
+                  cookie -> {
+                    PLAY_SESSION_COOKIE_SIZE.labels(actionMethod).observe(cookie.value().length());
+                  });
+          return next.apply(requestHeader);
         });
-        return next.apply(requestHeader);
-      }
-    );
   }
 }
