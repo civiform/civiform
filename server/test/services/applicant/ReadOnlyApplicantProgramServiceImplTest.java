c158994fbe453f13183a42cc9945c1b7f96cd8f4
Bion Johnson <bionj@google.com>
2023-10-09 12:40:39 -0700

    Wups

--- server/test/services/applicant/ReadOnlyApplicantProgramServiceImplTest.java
+++ server/test/services/applicant/ReadOnlyApplicantProgramServiceImplTest.java
@@ -37,7 +37,7 @@ import services.question.types.ScalarType;
 import support.ProgramBuilder;
 
 @RunWith(JUnitParamsRunner.class)
-public class ReadOnlyApplicantProgramServiceTest extends ResetPostgres {
+public class ReadOnlyApplicantProgramServiceImplTest extends ResetPostgres {
   private static final String FAKE_BASE_URL = "http://fake-base-url";
 
   private QuestionDefinition nameQuestion;
