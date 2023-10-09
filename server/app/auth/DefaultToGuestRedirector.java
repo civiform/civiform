f256b82e196f0da8ae32273790203ce204b3ae90
Tom Smith <yotommy@gmail.com>
2023-10-09 10:16:29 -0400

    Exclude API calls

--- server/app/auth/DefaultToGuestRedirector.java
+++ server/app/auth/DefaultToGuestRedirector.java
@@ -3,6 +3,7 @@ package auth;
 import static controllers.CallbackController.REDIRECT_TO_SESSION_KEY;
 import static play.mvc.Results.redirect;
 
+import com.google.common.collect.ImmutableMap;
 import controllers.routes;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -17,11 +18,7 @@ public final class DefaultToGuestRedirector {
    * them, redirecting them to the original page afterward.
    */
   public static Result createGuestSessionAndRedirect(Http.Request request) {
-    System.out.println(
-        "XXX before creating guest session, the current session contains: "
-            + request.session().data());
     return redirect(routes.CallbackController.callback(GuestClient.CLIENT_NAME).url())
-        .addingToSession(request, REDIRECT_TO_SESSION_KEY, request.uri());
-    // .withSession(ImmutableMap.of(REDIRECT_TO_SESSION_KEY, request.uri()));
+        .withSession(ImmutableMap.of(REDIRECT_TO_SESSION_KEY, request.uri()));
   }
 }
