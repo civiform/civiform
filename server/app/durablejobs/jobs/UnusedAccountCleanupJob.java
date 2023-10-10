ba99793842f09faa676229b140375d9001143f25
Bion Johnson <bion@users.noreply.github.com>
2023-10-10 14:07:42 -0700

    Update server/app/durablejobs/jobs/UnusedAccountCleanupJob.java
    Co-authored-by: Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>

--- server/app/durablejobs/jobs/UnusedAccountCleanupJob.java
+++ server/app/durablejobs/jobs/UnusedAccountCleanupJob.java
@@ -12,7 +12,7 @@ import repository.AccountRepository;
 
 /** Destroys all guest accounts older than a set age that have not started any applications. */
 public final class UnusedAccountCleanupJob extends DurableJob {
-  private static final Logger LOGGER = LoggerFactory.getLogger(OldJobCleanupJob.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(UnusedAccountCleanupJob.class);
   private static final int UNUSED_ACCOUNT_MAX_AGE_IN_DAYS = 90;
 
   private final AccountRepository accountRepository;
