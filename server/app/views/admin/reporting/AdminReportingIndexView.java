441db66509d31434bb5f9b2f907137ddbd03ab22
zarifzm <zarif.mazumder@seattle.gov>
2023-10-13 14:36:11 -0700

    add name column

--- server/app/views/admin/reporting/AdminReportingIndexView.java
+++ server/app/views/admin/reporting/AdminReportingIndexView.java
@@ -128,6 +128,7 @@ public final class AdminReportingIndexView extends BaseHtmlView {
   public static final ImmutableList<ReportingTableRenderer.ReportingTableHeader>
       APPLICATION_COUNTS_BY_PROGRAM_HEADERS =
           ImmutableList.of(
+              ReportingTableRenderer.ReportingTableHeader.create("Name"),
               ReportingTableRenderer.ReportingTableHeader.create("Program"),
               ReportingTableRenderer.ReportingTableHeader.create(
                   "Submissions", "The total number of applications submitted to this program."),
