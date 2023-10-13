3915b2b4f75ca295445e85633479c504b2d514cd
Tom Smith <yotommy@gmail.com>
2023-10-13 11:06:25 -0400

    Create SerializedIdTokens class and wrap in IdTokens.

--- server/test/auth/oidc/IdTokensTest.java
+++ server/test/auth/oidc/IdTokensTest.java
@@ -1,16 +1,17 @@
-package auth;
+package auth.oidc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.jwt.PlainJWT;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import org.junit.Test;
 
 public class IdTokensTest {
@@ -26,8 +27,8 @@ public class IdTokensTest {
   @Test
   public void testStoreAndRemove() {
     Clock clock = Clock.fixed(Instant.ofEpochSecond(100), ZoneOffset.UTC);
-    Map<String, String> idTokenMap = new HashMap<>();
-    IdTokens idTokens = new IdTokens(clock, idTokenMap);
+    SerializedIdTokens serializedIdTokens = new SerializedIdTokens(new HashMap<>());
+    IdTokens idTokens = new IdTokens(clock, serializedIdTokens);
 
     // We can use fake values for the tokens since these operations don't need to parse them.
     idTokens.storeIdToken("s1", "token1");
@@ -43,12 +44,12 @@ public class IdTokensTest {
   @Test
   public void testPurgeExpiredTokens() {
     Clock clock = Clock.fixed(Instant.ofEpochSecond(100), ZoneOffset.UTC);
+    SerializedIdTokens serializedIdTokens = new SerializedIdTokens(new HashMap<>());
+    IdTokens idTokens = new IdTokens(clock, serializedIdTokens);
 
-    Map<String, String> idTokenMap = new HashMap<>();
-    idTokenMap.put("s1", getJwt(99).serialize());
-    idTokenMap.put("s2", getJwt(100).serialize());
-    idTokenMap.put("s3", getJwt(101).serialize());
-    IdTokens idTokens = new IdTokens(clock, idTokenMap);
+    idTokens.storeIdToken("s1", getJwt(99).serialize());
+    idTokens.storeIdToken("s2", getJwt(100).serialize());
+    idTokens.storeIdToken("s3", getJwt(101).serialize());
 
     assertThat(idTokens.getIdToken("s1")).isNotEmpty();
     assertThat(idTokens.getIdToken("s2")).isNotEmpty();
@@ -58,5 +59,7 @@ public class IdTokensTest {
     assertThat(idTokens.getIdToken("s1")).isEmpty();
     assertThat(idTokens.getIdToken("s2")).isEmpty();
     assertThat(idTokens.getIdToken("s3")).isNotEmpty();
+    assertThatCode(() -> JWTParser.parse(idTokens.getIdToken("s3").get()))
+        .doesNotThrowAnyException();
   }
 }
