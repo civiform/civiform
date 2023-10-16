120dde74aec8f7304f77146af5870173261da921
Rocky Fine <rocky.fine@exygy.com>
2023-10-16 15:55:11 -0400

    remove custom method and add softbreak(br)

--- server/app/views/CiviFormMarkdown.java
+++ server/app/views/CiviFormMarkdown.java
@@ -24,55 +24,6 @@ public final class CiviFormMarkdown {
     return RENDERER.render(markdownRootNode);
   }
 
-  /**
-   * This method removes the tags that wrap an HTML block. This is useful when you want to remove
-   * the
-   *
-   * <p>tags that are added by default by the CommonMark library to strings that do not start with a
-   * Markdown indicator. These inserted
-   *
-   * <p>tags can create paragraph breaks in places that are not intended. For example, in
-   * TextFormatter.createLinksAndEscapeText, we apply Markdown parsing to the strings in between
-   * urls. Allowing the default behavior of inserting
-   *
-   * <p>tags results in html that looks like this:
-   *
-   * <p>Markdown text: "The url www.kittens.com is the cutest url!"
-   *
-   * <p>Resulting rendered html:
-   *
-   * <p>The url <a>www.kittens.com</a>
-   *
-   * <p>is the cutest url! Logic for this method was pulled from this commit which is the
-   * recommended way to solve this problem:
-   * https://github.com/commonmark/commonmark-java/commit/8b0fd7c73afaf1756edb6412c15a75cc423a6ba9
-   */
-  public String renderWithoutWrappingTags(String markdown) {
-    Document document = new Document();
-    Node paragraphs = PARSER.parse(markdown);
-    Node paragraph = paragraphs.getFirstChild();
-
-    // paragraph is null when the string passed in is an empty space,
-    // for example the space between two URLs passed through TextFormatter.createLinksAndEscapeText
-    if (paragraph != null) {
-      Node child = paragraph.getFirstChild();
-      while (child != null) {
-        Node current = child;
-        child = current.getNext();
-        document.appendChild(current);
-      }
-    }
-
-    // PARSER.parse removes trailing spaces by default which is a problem when we have strings that
-    // are the middle of a phrase, or have a string that is an empty space (as in the case when it's
-    // the space between URLs). This adds back in spaces that may have been removed.
-    if (markdown.substring(markdown.length() - 1).equals(" ")) {
-      document.appendChild(new Text(" "));
-    }
-
-    return RENDERER.render(document);
-  }
-
   private static final List<Extension> extensions = Arrays.asList(AutolinkExtension.create());
 
   private static final Parser PARSER = Parser.builder().extensions(extensions).build();
@@ -81,6 +32,7 @@ public final class CiviFormMarkdown {
       HtmlRenderer.builder()
           .extensions(extensions)
           .attributeProviderFactory(context -> new CiviFormAttributeProvider())
+          .softbreak("<br/>")
           .build();
 
   /** Customizes HTML element attributes for the CiviForm UI. */
