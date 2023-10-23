4cf8925b4938fb6d42deed50ffde3d18d182abb6
SwathiMurthi <swathi.murthi@seattle.gov>
2023-10-23 15:29:59 -0700

    formatting

--- server/app/repository/ExportServiceRepository.java
+++ server/app/repository/ExportServiceRepository.java
@@ -33,51 +33,51 @@ public class ExportServiceRepository {
     String questionName = questionDefinition.getName();
     Map<Long, String> alloptionsMap = new HashMap<>();
     database
-      .sqlQuery(
-        "SELECT DISTINCT jsonb_array_elements(q.question_options)->>'adminName'AS"
-          + " AdminName,jsonb_array_elements(q.question_options)->>'id' AS Id FROM questions"
-          + " q where name = :currentQuestion::varchar")
-      .setParameter("currentQuestion", questionName)
-      .findList()
-      .stream()
-      .forEach(row -> alloptionsMap.put(row.getLong("id"), row.getString("AdminName")));
+        .sqlQuery(
+            "SELECT DISTINCT jsonb_array_elements(q.question_options)->>'adminName'AS"
+                + " AdminName,jsonb_array_elements(q.question_options)->>'id' AS Id FROM questions"
+                + " q where name = :currentQuestion::varchar")
+        .setParameter("currentQuestion", questionName)
+        .findList()
+        .stream()
+        .forEach(row -> alloptionsMap.put(row.getLong("id"), row.getString("AdminName")));
     Set<Long> allSelectedOptions = new HashSet<>();
     database
-      .sqlQuery(
-        "select json_array_elements(((object #>>"
-          + " '{}')::jsonb)::json#>'{applicant, "
-          + questionName
-          + " ,selections}') AS selections from"
-          + " applications;")
-      .findList()
-      .forEach(
-        row ->
-          allSelectedOptions.add(
-            Long.parseLong(row.getString("selections").replaceAll("^\"|\"$", ""))));
+        .sqlQuery(
+            "select json_array_elements(((object #>>"
+                + " '{}')::jsonb)::json#>'{applicant, "
+                + questionName
+                + " ,selections}') AS selections from"
+                + " applications;")
+        .findList()
+        .forEach(
+            row ->
+                allSelectedOptions.add(
+                    Long.parseLong(row.getString("selections").replaceAll("^\"|\"$", ""))));
     Map<Long, String> combinedList = new HashMap<>();
     alloptionsMap.keySet().stream()
-      .forEach(
-        e -> {
-          if (allSelectedOptions.contains(e)) {
-            combinedList.put(e, alloptionsMap.get(e));
-          }
-        });
+        .forEach(
+            e -> {
+              if (allSelectedOptions.contains(e)) {
+                combinedList.put(e, alloptionsMap.get(e));
+              }
+            });
 
     Version activeVersion = versionRepositoryProvider.get().getActiveVersion();
     MultiOptionQuestionDefinition currentQuestion =
-      (MultiOptionQuestionDefinition)
-        versionRepositoryProvider
-          .get()
-          .getQuestionByNameForVersion(questionName, activeVersion)
-          .get()
-          .getQuestionDefinition();
+        (MultiOptionQuestionDefinition)
+            versionRepositoryProvider
+                .get()
+                .getQuestionByNameForVersion(questionName, activeVersion)
+                .get()
+                .getQuestionDefinition();
     currentQuestion.getOptions().stream()
-      .forEach(
-        e -> {
-          if (!combinedList.containsKey(e.id())) {
-            combinedList.put(e.id(), e.adminName());
-          }
-        });
+        .forEach(
+            e -> {
+              if (!combinedList.containsKey(e.id())) {
+                combinedList.put(e.id(), e.adminName());
+              }
+            });
     return ImmutableMap.<Long, String>builder().putAll(combinedList).build();
   }
 }
--- server/test/repository/ExportServiceRepositoryTest.java
+++ server/test/repository/ExportServiceRepositoryTest.java
@@ -36,9 +36,9 @@ public class ExportServiceRepositoryTest extends ResetPostgres {
   @Test
   public void getMultiSelectedHeaders_NoPreviousQuestionVersion() {
     QuestionDefinition questionDefinition =
-      testQuestionBank.applicantKitchenTools().getQuestionDefinition();
+        testQuestionBank.applicantKitchenTools().getQuestionDefinition();
     ImmutableMap<Long, String> multiSelectHeaders =
-      repo.getMultiSelectedHeaders(questionDefinition);
+        repo.getMultiSelectedHeaders(questionDefinition);
     assertThat(multiSelectHeaders.size()).isEqualTo(3);
     assertThat(multiSelectHeaders).containsKey(1L);
     checkMap(multiSelectHeaders, 1L, "toaster");
@@ -48,29 +48,29 @@ public class ExportServiceRepositoryTest extends ResetPostgres {
 
   @Test
   public void getMultiSelectedHeaders_DraftVersionExcludedButPublishedVersionIncluded()
-    throws Exception {
+      throws Exception {
     Question questionWeather =
-      createMultiSelectQuestion("weather", "fall", "spring", "summer", LifecycleStage.ACTIVE);
+        createMultiSelectQuestion("weather", "fall", "spring", "summer", LifecycleStage.ACTIVE);
     MultiOptionQuestionDefinition multiOptionQuestionDefinition =
-      (MultiOptionQuestionDefinition) questionWeather.getQuestionDefinition();
+        (MultiOptionQuestionDefinition) questionWeather.getQuestionDefinition();
     QuestionOption newOption =
-      QuestionOption.create(4L, 4L, "winter", LocalizedStrings.of(Locale.US, "winter"));
+        QuestionOption.create(4L, 4L, "winter", LocalizedStrings.of(Locale.US, "winter"));
     ImmutableList<QuestionOption> currentOptions = multiOptionQuestionDefinition.getOptions();
     ImmutableList<QuestionOption> newOptionList =
-      ImmutableList.<QuestionOption>builder().addAll(currentOptions).add(newOption).build();
+        ImmutableList.<QuestionOption>builder().addAll(currentOptions).add(newOption).build();
     QuestionDefinition toUpdate =
-      new QuestionDefinitionBuilder(questionWeather.getQuestionDefinition())
-        .setQuestionOptions(newOptionList)
-        .build();
+        new QuestionDefinitionBuilder(questionWeather.getQuestionDefinition())
+            .setQuestionOptions(newOptionList)
+            .build();
     questionService.update(toUpdate);
 
     ImmutableMap<Long, String> multiSelectHeaders =
-      repo.getMultiSelectedHeaders(questionWeather.getQuestionDefinition());
+        repo.getMultiSelectedHeaders(questionWeather.getQuestionDefinition());
     // not draft versions are not part of the header list
     assertThat(multiSelectHeaders.size()).isEqualTo(3);
     versionRepository.publishNewSynchronizedVersion();
     ImmutableMap<Long, String> multiSelectHeaderUpdated =
-      repo.getMultiSelectedHeaders(questionWeather.getQuestionDefinition());
+        repo.getMultiSelectedHeaders(questionWeather.getQuestionDefinition());
     assertThat(multiSelectHeaderUpdated.size()).isEqualTo(4);
     checkMap(multiSelectHeaderUpdated, 1L, "fall");
     checkMap(multiSelectHeaderUpdated, 2L, "spring");
@@ -81,34 +81,34 @@ public class ExportServiceRepositoryTest extends ResetPostgres {
   @Test
   public void getMultiSelectedHeaders_ThrowsExceptionOnWrongQuestionType() {
     QuestionDefinition questionDefinition =
-      testQuestionBank.applicantAddress().getQuestionDefinition();
+        testQuestionBank.applicantAddress().getQuestionDefinition();
     assertThatThrownBy(() -> repo.getMultiSelectedHeaders(questionDefinition))
-      .isInstanceOf(RuntimeException.class)
-      .hasMessage("The Question Type is not checkbox");
+        .isInstanceOf(RuntimeException.class)
+        .hasMessage("The Question Type is not checkbox");
   }
 
   @Test
   public void getMultiSelectedHeaders_ThrowsExceptionOnUnpublishedQuestion() {
     QuestionDefinitionConfig config =
-      QuestionDefinitionConfig.builder()
-        .setName("test")
-        .setDescription("test")
-        .setQuestionText(LocalizedStrings.of(Locale.US, "test"))
-        .setQuestionHelpText(LocalizedStrings.of(Locale.US, "This is sample help text."))
-        .build();
+        QuestionDefinitionConfig.builder()
+            .setName("test")
+            .setDescription("test")
+            .setQuestionText(LocalizedStrings.of(Locale.US, "test"))
+            .setQuestionHelpText(LocalizedStrings.of(Locale.US, "This is sample help text."))
+            .build();
     ImmutableList<QuestionOption> questionOptions =
-      ImmutableList.of(
-        QuestionOption.create(1L, 1L, "option1", LocalizedStrings.of(Locale.US, "option1")),
-        QuestionOption.create(2L, 2L, "option2", LocalizedStrings.of(Locale.US, "option2")),
-        QuestionOption.create(3L, 3L, "option3", LocalizedStrings.of(Locale.US, "option3")));
+        ImmutableList.of(
+            QuestionOption.create(1L, 1L, "option1", LocalizedStrings.of(Locale.US, "option1")),
+            QuestionOption.create(2L, 2L, "option2", LocalizedStrings.of(Locale.US, "option2")),
+            QuestionOption.create(3L, 3L, "option3", LocalizedStrings.of(Locale.US, "option3")));
     QuestionDefinition definition =
-      new MultiOptionQuestionDefinition(
-        config,
-        questionOptions,
-        MultiOptionQuestionDefinition.MultiOptionQuestionType.CHECKBOX);
+        new MultiOptionQuestionDefinition(
+            config,
+            questionOptions,
+            MultiOptionQuestionDefinition.MultiOptionQuestionType.CHECKBOX);
     assertThatThrownBy(() -> repo.getMultiSelectedHeaders(definition))
-      .isInstanceOf(NoSuchElementException.class)
-      .hasMessage("No value present");
+        .isInstanceOf(NoSuchElementException.class)
+        .hasMessage("No value present");
   }
 
   private void checkMap(ImmutableMap<Long, String> multiSelectHeaders, Long key, String value) {
@@ -117,24 +117,24 @@ public class ExportServiceRepositoryTest extends ResetPostgres {
   }
 
   private Question createMultiSelectQuestion(
-    String name, String option1, String option2, String option3, LifecycleStage stage) {
+      String name, String option1, String option2, String option3, LifecycleStage stage) {
     QuestionDefinitionConfig config =
-      QuestionDefinitionConfig.builder()
-        .setName(name)
-        .setDescription(name)
-        .setQuestionText(LocalizedStrings.of(Locale.US, name))
-        .setQuestionHelpText(LocalizedStrings.of(Locale.US, "This is sample help text."))
-        .build();
+        QuestionDefinitionConfig.builder()
+            .setName(name)
+            .setDescription(name)
+            .setQuestionText(LocalizedStrings.of(Locale.US, name))
+            .setQuestionHelpText(LocalizedStrings.of(Locale.US, "This is sample help text."))
+            .build();
     ImmutableList<QuestionOption> questionOptions =
-      ImmutableList.of(
-        QuestionOption.create(1L, 1L, option1, LocalizedStrings.of(Locale.US, option1)),
-        QuestionOption.create(2L, 2L, option2, LocalizedStrings.of(Locale.US, option2)),
-        QuestionOption.create(3L, 3L, option3, LocalizedStrings.of(Locale.US, option3)));
+        ImmutableList.of(
+            QuestionOption.create(1L, 1L, option1, LocalizedStrings.of(Locale.US, option1)),
+            QuestionOption.create(2L, 2L, option2, LocalizedStrings.of(Locale.US, option2)),
+            QuestionOption.create(3L, 3L, option3, LocalizedStrings.of(Locale.US, option3)));
     QuestionDefinition definition =
-      new MultiOptionQuestionDefinition(
-        config,
-        questionOptions,
-        MultiOptionQuestionDefinition.MultiOptionQuestionType.CHECKBOX);
+        new MultiOptionQuestionDefinition(
+            config,
+            questionOptions,
+            MultiOptionQuestionDefinition.MultiOptionQuestionType.CHECKBOX);
     return testQuestionBank.maybeSave(definition, stage);
   }
 }
