d76198f3e49922fde54b504687777b8a9296f9f3
Rocky Fine <rocky.fine@exygy.com>
2023-10-24 15:08:15 -0400

    fix merge conflicts

--- server/test/views/components/TextFormatterTest.java
+++ server/test/views/components/TextFormatterTest.java
@@ -38,15 +38,14 @@ public class TextFormatterTest {
     List<String> contentArr = Splitter.on("</a>").splitToList(htmlContent);
     assertIsExternalUrlWithIcon(
         contentArr.get(0),
-        "<a href=\"http://internet.website\" class=\"text-sm text-blue-900 font-bold opacity-75"
-            + " underline hover:opacity-100\" target=\"_blank\" rel=\"nofollow noopener"
+        "<a href=\"http://internet.website\" class=\"text-blue-600 hover:text-blue-500 underline\""
+            + " target=\"_blank\" rel=\"nofollow noopener"
             + " noreferrer\">http://internet.website<svg",
         "</svg>");
     assertIsExternalUrlWithIcon(
         htmlContent,
-        "<a href=\"https://secure.website\" class=\"text-sm text-blue-900 font-bold opacity-75"
-            + " underline hover:opacity-100\" target=\"_blank\" rel=\"nofollow noopener"
-            + " noreferrer\">https://secure.website<svg",
+        "<a href=\"https://secure.website\" class=\"text-blue-600 hover:text-blue-500 underline\""
+            + " target=\"_blank\" rel=\"nofollow noopener noreferrer\">https://secure.website<svg",
         "</svg></a></p>\n");
   }
 
@@ -60,9 +59,8 @@ public class TextFormatterTest {
     String htmlContent = content.get(0).render();
     assertIsExternalUrlWithIcon(
         htmlContent,
-        "<a href=\"https://www.google.com\" class=\"text-sm text-blue-900 font-bold opacity-75"
-            + " underline hover:opacity-100\" target=\"_blank\" rel=\"nofollow noopener"
-            + " noreferrer\">this is a link",
+        "<a href=\"https://www.google.com\" class=\"text-blue-600 hover:text-blue-500 underline\""
+            + " target=\"_blank\" rel=\"nofollow noopener noreferrer\">this is a link",
         "</svg></a></p>\n");
   }
 
