396838377db76d17dde7329c760676ff82b305be
Rocky Fine <rocky.fine@exygy.com>
2023-11-07 10:27:09 -0500

    fix functionality to expand program description on click

--- server/app/assets/javascripts/main.ts
+++ server/app/assets/javascripts/main.ts
@@ -205,16 +205,15 @@ function moveMultiOptionQuestionDown(event: Event) {
  * @param {Event} event The event that triggered this action.
  */
 function removeLineClamp(event: Event) {
-  const target = event.target as HTMLElement
-  target.classList.add('line-clamp-none')
+  const div = event.currentTarget as HTMLElement
+  div.classList.add('line-clamp-none')
 }
 
 function attachLineClampListeners() {
   const applicationCardDescriptions = Array.from(
     document.querySelectorAll('.cf-application-card-description'),
   )
-  applicationCardDescriptions.forEach((el) =>
-    el.addEventListener('click', removeLineClamp),
+  applicationCardDescriptions.forEach((el) => el.addEventListener('click', removeLineClamp)
   )
 }
 
