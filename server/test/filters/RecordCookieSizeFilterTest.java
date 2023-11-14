fef925cd8c772efb9eb9fae8eab9518416d504f6
Tom Smith <yotommy@gmail.com>
2023-11-14 11:01:48 -0500

    Only populate cookie sizes if metrics are enabled

--- server/test/filters/RecordCookieSizeFilterTest.java
+++ server/test/filters/RecordCookieSizeFilterTest.java
@@ -5,6 +5,8 @@ import static play.test.Helpers.fakeRequest;
 
 import akka.stream.testkit.NoMaterializer$;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.typesafe.config.ConfigFactory;
 import io.prometheus.client.CollectorRegistry;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +17,11 @@ import play.mvc.EssentialAction;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Results;
+import services.settings.SettingDescription;
+import services.settings.SettingMode;
+import services.settings.SettingType;
+import services.settings.SettingsManifest;
+import services.settings.SettingsSection;
 
 public class RecordCookieSizeFilterTest {
   @Before
@@ -22,10 +29,46 @@ public class RecordCookieSizeFilterTest {
     CollectorRegistry.defaultRegistry.clear();
   }
 
+  private SettingsManifest createSettingsManifest(boolean metricsEnabled) {
+    return new SettingsManifest(
+        ImmutableMap.of(
+            "section1",
+            SettingsSection.create(
+                "section1",
+                "description1",
+                ImmutableList.of(),
+                ImmutableList.of(
+                    SettingDescription.create(
+                        "CIVIFORM_SERVER_METRICS_ENABLED",
+                        "CiviForm server metrics enabled",
+                        false,
+                        SettingType.BOOLEAN,
+                        SettingMode.ADMIN_WRITEABLE)))),
+        ConfigFactory.parseMap(ImmutableMap.of("civiform_server_metrics_enabled", metricsEnabled)));
+  }
+
+  private Result runFilterWithCookieSize(RecordCookieSizeFilter filter, int cookieSize)
+      throws Exception {
+    Http.Cookie playSessionCookie =
+        Http.Cookie.builder("PLAY_SESSION", "*".repeat(cookieSize)).build();
+    Http.Request request = fakeRequest().cookie(playSessionCookie).build();
+    return filter
+        .apply(EssentialAction.of(r -> Accumulator.done(Results.ok("OK"))))
+        .apply(request)
+        .run(NoMaterializer$.MODULE$)
+        .toCompletableFuture()
+        .get();
+  }
+
+  private double samplesBelowUpperBound(List<Integer> sizes, double upperBound) {
+    return sizes.stream().filter(n -> n <= upperBound).count();
+  }
+
   @Test
   public void testCookieSizesAreRecorded() throws Exception {
     // Run filter with several requests with various cookie sizes.
-    RecordCookieSizeFilter filter = new RecordCookieSizeFilter();
+    RecordCookieSizeFilter filter = new RecordCookieSizeFilter(() -> createSettingsManifest(true));
+
     List<Integer> cookieSizes = ImmutableList.of(1000, 2000, 2000, 3000, 3500, 4000);
     for (int cookieSize : cookieSizes) {
       assertThat(runFilterWithCookieSize(filter, cookieSize).status()).isEqualTo(200);
@@ -69,20 +112,17 @@ public class RecordCookieSizeFilterTest {
     }
   }
 
-  private Result runFilterWithCookieSize(RecordCookieSizeFilter filter, int cookieSize)
-      throws Exception {
-    Http.Cookie playSessionCookie =
-        Http.Cookie.builder("PLAY_SESSION", "*".repeat(cookieSize)).build();
-    Http.Request request = fakeRequest().cookie(playSessionCookie).build();
-    return filter
-        .apply(EssentialAction.of(r -> Accumulator.done(Results.ok("OK"))))
-        .apply(request)
-        .run(NoMaterializer$.MODULE$)
-        .toCompletableFuture()
-        .get();
-  }
+  @Test
+  public void testCookieSizesAreNotRecordedIfMetricsAreNotEnabled() throws Exception {
+    RecordCookieSizeFilter filter = new RecordCookieSizeFilter(() -> createSettingsManifest(false));
 
-  private double samplesBelowUpperBound(List<Integer> sizes, double upperBound) {
-    return sizes.stream().filter(n -> n <= upperBound).count();
+    assertThat(runFilterWithCookieSize(filter, 1000).status()).isEqualTo(200);
+
+    var metricFamilySamplesEnumeration = CollectorRegistry.defaultRegistry.metricFamilySamples();
+    assertThat(metricFamilySamplesEnumeration.hasMoreElements()).isTrue();
+    var metricFamilySamples = metricFamilySamplesEnumeration.nextElement();
+    assertThat(metricFamilySamples.name).isEqualTo("play_session_cookie_size_bytes");
+
+    assertThat(metricFamilySamples.samples.isEmpty()).isTrue();
   }
 }
