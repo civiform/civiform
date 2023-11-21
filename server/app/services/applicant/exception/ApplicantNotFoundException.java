730a8a451c5b47a108eb59bff41470d9de9e75e3
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-11-21 16:23:42 -0500

    Update ApplicantNotFoundException.java

--- server/app/services/applicant/exception/ApplicantNotFoundException.java
+++ server/app/services/applicant/exception/ApplicantNotFoundException.java
@@ -1,10 +1,8 @@
 package services.applicant.exception;
 
-import models.ApplicantModel;
-
 /**
- * ApplicantNotFoundException is thrown when the {@link ApplicantModel} cannot be found by the
- * specified ID.
+ * ApplicantNotFoundException is thrown when the {@link models.ApplicantModel} cannot be found by
+ * the specified ID.
  */
 public final class ApplicantNotFoundException extends Exception {
   public ApplicantNotFoundException(long applicantId) {
