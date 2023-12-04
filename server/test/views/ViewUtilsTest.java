46954d911822f33f6b82151a7a9670934af3b48a
Rocky Fine <rocky.fine@exygy.com>
2023-12-04 14:24:41 -0500

    added ViewUtils test

--- server/test/views/ViewUtilsTest.java
+++ server/test/views/ViewUtilsTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import controllers.AssetsFinder;
 import j2html.tags.specialized.LinkTag;
 import j2html.tags.specialized.ScriptTag;
+import j2html.tags.specialized.DivTag;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,6 +15,8 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 import services.DateConverter;
+import views.style.StyleUtils;
+import java.util.Optional;
 
 public class ViewUtilsTest {
 
@@ -46,5 +49,17 @@ public class ViewUtilsTest {
 
     assertThat(result.render())
         .isEqualTo("<link href=\"/full/asset/path.css\" rel=\"stylesheet\">");
+  }
+
+  @Test
+  public void makeAlert_createsAlertComponentWithTheCorrectClasses() {
+    DivTag alertComponent = ViewUtils.makeAlert("some text", StyleUtils.joinStyles(ViewUtils.ALERT_INFO, ViewUtils.ALERT_SLIM), Optional.of("title"));
+    assertThat(alertComponent.render()).isEqualTo("<div class=\"usa-alert usa-alert--info usa-alert--slim\"><div class=\"usa-alert__body\"><h4 class=\"usa-alert__heading\">title</h4><p class=\"usa-alert__text\">some text</p></div></div>");
+  }
+
+  @Test
+  public void makeAlert_doesNotIncludeTitleIfNoneIsPresent() {
+    DivTag alertComponent = ViewUtils.makeAlert("some text", ViewUtils.ALERT_WARNING, Optional.empty());
+    assertThat(alertComponent.render()).isEqualTo("<div class=\"usa-alert usa-alert--warning\"><div class=\"usa-alert__body\"><p class=\"usa-alert__text\">some text</p></div></div>");
   }
 }
