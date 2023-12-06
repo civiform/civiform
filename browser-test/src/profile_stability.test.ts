610db0f133bfbb40e42540f2bdd8854a3234741b
Tom Smith <yotommy@gmail.com>
2023-12-06 14:56:27 -0500

    Code review round 1

--- browser-test/src/profile_stability.test.ts
+++ browser-test/src/profile_stability.test.ts
@@ -1,30 +1,43 @@
 import {createTestContext} from './support'
 import {BASE_URL, FROZEN_PLAY_SESSION_COOKIE_VALUE} from './support/config'
 
-describe('developer tools', () => {
+describe('user HTTP sessions', () => {
   const ctx = createTestContext()
+
+  interface Profile {
+    id: string
+    roles: string[]
+    clientName: string
+  }
 
   // This test ensures that an existing user profile, stored in a browser cookie,
   // is recognized and properly deserialized by the server.
   //
   // This guards against changes that unexpectedly affect serialization.
   it('recognizes the profile from a frozen cookie', async () => {
-    const hostname = new URL(BASE_URL).hostname
+    const {hostname} = new URL(BASE_URL)
     const frozenCookie = {
       name: 'PLAY_SESSION',
       value: FROZEN_PLAY_SESSION_COOKIE_VALUE,
       domain: hostname,
-      expires: 2147483647,
       path: '/',
     }
 
     await ctx.page.context().addCookies([frozenCookie])
     await ctx.page.goto(BASE_URL + '/dev/profile')
-    const content = await ctx.page.content()
 
-    // The field order is non-deterministic, so just check some key fields.
-    expect(content).toContain('"id" : "1"')
-    expect(content).toContain('"roles" : [ "ROLE_APPLICANT" ]')
-    expect(content).toContain('"clientName" : "GuestClient"')
+    const content = await ctx.page.content()
+    // Pull out the JSON from the page using multiline matching.
+    const matches = content.match(/{.*}/s)
+
+    expect(matches).not.toBeNull()
+
+    if (matches) {
+      const profile = JSON.parse(matches[0]) as Profile
+      expect(profile.id).toEqual('1')
+      expect(profile.roles.length).toEqual(1)
+      expect(profile.roles[0]).toEqual('ROLE_APPLICANT')
+      expect(profile.clientName).toEqual('GuestClient')
+    }
   })
 })
