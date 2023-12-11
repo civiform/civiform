7942bc7a473790888766f51f662d2b5b24d5334c
Tom Smith <yotommy@gmail.com>
2023-12-11 09:37:04 -0500

    Allow profile stability test to run as a prober test

--- browser-test/src/profile_stability.test.ts
+++ browser-test/src/profile_stability.test.ts
@@ -1,5 +1,6 @@
-import {createTestContext, isHermeticTestEnvironment} from './support'
-import {BASE_URL, FROZEN_PLAY_SESSION_COOKIE_VALUE} from './support/config'
+import {createTestContext} from './support'
+import {BASE_URL} from './support/config'
+import {FrozenPlaySessionCookie} from './support/cookies'
 
 describe('user HTTP sessions', () => {
   const ctx = createTestContext()
@@ -15,32 +16,26 @@ describe('user HTTP sessions', () => {
   //
   // This guards against changes that unexpectedly affect serialization.
   it('recognizes the profile from a frozen cookie', async () => {
-    // TODO(#6069): Get this to work as a prober test.
-    if (isHermeticTestEnvironment()) {
-      const {hostname} = new URL(BASE_URL)
-      const frozenCookie = {
-        name: 'PLAY_SESSION',
-        value: FROZEN_PLAY_SESSION_COOKIE_VALUE,
-        domain: hostname,
-        path: '/',
-      }
+    await ctx.page.context().addCookies([FrozenPlaySessionCookie.getCookie()])
 
-      await ctx.page.context().addCookies([frozenCookie])
-      await ctx.page.goto(BASE_URL + '/dev/profile')
+    await ctx.page.goto(BASE_URL + '/dev/profile')
 
-      const content = await ctx.page.content()
-      // Pull out the JSON from the page using multiline matching.
-      const matches = content.match(/{.*}/s)
+    const content = await ctx.page.content()
+    // Pull out the JSON from the page using multiline matching.
+    const matches = content.match(/{.*}/s)
 
-      expect(matches).not.toBeNull()
+    expect(matches).not.toBeNull()
 
-      if (matches) {
-        const profile = JSON.parse(matches[0]) as Profile
-        expect(profile.id).toEqual('1')
-        expect(profile.roles.length).toEqual(1)
-        expect(profile.roles[0]).toEqual('ROLE_APPLICANT')
-        expect(profile.clientName).toEqual('GuestClient')
-      }
+    if (matches) {
+      const profile = JSON.parse(matches[0]) as Profile
+      // The profile id is a string containing a number. The id varies
+      // in different environments, so we just ensure it is present and
+      // holds a positive integer.
+      expect(profile.id.length).toBeGreaterThanOrEqual(1)
+      expect(Number(profile.id)).toBeGreaterThanOrEqual(1)
+      expect(profile.roles.length).toEqual(1)
+      expect(profile.roles[0]).toEqual('ROLE_APPLICANT')
+      expect(profile.clientName).toEqual('GuestClient')
     }
   })
 })
