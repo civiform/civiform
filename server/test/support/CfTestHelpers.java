7726c7a2a0bba9a4c8f036706ac1c01d193e3b9e
Caitlin Shkuratov <caitlinshk@google.com>
2023-12-14 14:58:54 -0500

    Remove CfTestHelpers 2

--- server/test/support/CfTestHelpers.java
+++ server/test/support/CfTestHelpers.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import javax.inject.Provider;
 import org.mockito.MockedStatic;
 import org.pac4j.oidc.client.OidcClient;
