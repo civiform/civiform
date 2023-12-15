4b10b82f6e2d54627dbd558148ddd9040a5c2617
azhangggggg <annayang@google.com>
2023-12-14 23:45:18 -0800

    formatted long string in screen name and description

--- server/app/views/admin/programs/ProgramBlocksView.java
+++ server/app/views/admin/programs/ProgramBlocksView.java
@@ -426,8 +426,9 @@ public final class ProgramBlocksView extends ProgramBaseView {
 
     DivTag blockInfoDisplay =
         div()
-            .with(div(blockForm.getName()).withClasses("text-xl", "font-bold", "py-2"))
-            .with(div(blockForm.getDescription()).withClasses("text-lg", "max-w-prose"))
+            .with(div(blockForm.getName()).withClasses("text-xl", "font-bold", "py-2", "break-all"))
+            .with(
+                div(blockForm.getDescription()).withClasses("text-lg", "max-w-prose", "break-all"))
             .withId("block-info-display-" + blockDefinition.id())
             .withClasses("my-4");
 
