a59e6eca775a763222abc604466f5ba5c43b21f6
Nick Burgan <nick.burgan@exygy.com>
2023-12-14 17:20:49 -0800

    QuestionForm

--- server/app/forms/QuestionForm.java
+++ server/app/forms/QuestionForm.java
@@ -2,6 +2,7 @@ package forms;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -11,6 +12,7 @@ import services.LocalizedStrings;
 import services.TranslationNotFoundException;
 import services.UrlUtils;
 import services.export.CsvExporterService;
+import services.question.PrimaryApplicantInfoTag;
 import services.question.types.QuestionDefinition;
 import services.question.types.QuestionDefinitionBuilder;
 import services.question.types.QuestionType;
@@ -28,6 +30,7 @@ public abstract class QuestionForm {
   private QuestionDefinition qd;
   private String redirectUrl;
   private boolean isUniversal;
+  private ImmutableList<PrimaryApplicantInfoTag> primaryApplicantInfoTags;
 
   protected QuestionForm() {
     questionName = "";
@@ -38,6 +41,7 @@ public abstract class QuestionForm {
     questionExportState = Optional.of("");
     redirectUrl = "";
     isUniversal = false;
+    primaryApplicantInfoTags = ImmutableList.of();
   }
 
   protected QuestionForm(QuestionDefinition qd) {
@@ -61,6 +65,7 @@ public abstract class QuestionForm {
     }
 
     isUniversal = qd.isUniversal();
+    primaryApplicantInfoTags = qd.getPrimaryApplicantInfoTags();
   }
 
   public final String getQuestionName() {
@@ -139,7 +144,8 @@ public abstract class QuestionForm {
             .setEnumeratorId(enumeratorId)
             .setQuestionText(questionTextMap)
             .setQuestionHelpText(questionHelpTextMap)
-            .setUniversal(isUniversal);
+            .setUniversal(isUniversal)
+            .setPrimaryApplicantInfoTags(primaryApplicantInfoTags);
     return builder;
   }
 
@@ -191,5 +197,50 @@ public abstract class QuestionForm {
   // Awkwardly named, but must match the field name
   public final void setIsUniversal(boolean universal) {
     this.isUniversal = universal;
+  }
+
+  public final ImmutableList<PrimaryApplicantInfoTag> primaryApplicantInfoTags() {
+    return this.primaryApplicantInfoTags;
+  }
+
+  private void setTagState(PrimaryApplicantInfoTag primaryApplicantInfoTag, boolean isSet) {
+    boolean present = this.primaryApplicantInfoTags.contains(primaryApplicantInfoTag);
+    if (isSet) {
+      this.primaryApplicantInfoTags =
+          present
+              ? this.primaryApplicantInfoTags
+              : new ImmutableList.Builder<PrimaryApplicantInfoTag>()
+                  .addAll(this.primaryApplicantInfoTags)
+                  .add(primaryApplicantInfoTag)
+                  .build();
+    } else {
+      this.primaryApplicantInfoTags =
+          present
+              ? this.primaryApplicantInfoTags().stream()
+                  .filter(t -> t != primaryApplicantInfoTag)
+                  .collect(ImmutableList.toImmutableList())
+              : this.primaryApplicantInfoTags;
+    }
+  }
+
+  // The names of these functions must match up to the fieldName value in the PrimaryApplicantInfoTag
+  public final void setPrimaryApplicantInfoDob(boolean val) {
+    setTagState(PrimaryApplicantInfoTag.APPLICANT_DOB, val);
+  }
+
+  public final void setPrimaryApplicantInfoEmail(boolean val) {
+    setTagState(PrimaryApplicantInfoTag.APPLICANT_EMAIL, val);
+  }
+
+  public final void setPrimaryApplicantInfoName(boolean val) {
+    setTagState(PrimaryApplicantInfoTag.APPLICANT_NAME, val);
+  }
+
+  public final void setPrimaryApplicantInfoPhone(boolean val) {
+    setTagState(PrimaryApplicantInfoTag.APPLICANT_PHONE, val);
+  }
+
+  public final void setEmergencyContactName(boolean val) {
+    setTagState(PrimaryApplicantInfoTag.EMERGENCY_CONTACT_NAME, val);
   }
 }
