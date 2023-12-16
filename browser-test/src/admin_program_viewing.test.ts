a4ecb9bd4dc426444d26411856ae6762cd6b1af7
azhangggggg <annayang@google.com>
2023-12-15 16:52:09 -0800

    added browser test

--- browser-test/src/admin_program_viewing.test.ts
+++ browser-test/src/admin_program_viewing.test.ts
@@ -144,4 +144,61 @@ describe('admin program view page', () => {
 
     await validateScreenshot(page, 'view-program-start-editing')
   })
+
+  it('view program, view multiple blocks, then start editing with extra long screen name and description', async () => {
+    const {page, adminPrograms, adminQuestions} = ctx
+    await loginAsAdmin(page)
+    await enableFeatureFlag(page, 'esri_address_correction_enabled')
+
+    const programName = 'Apc program'
+    await adminQuestions.addAddressQuestion({questionName: 'address-q'})
+    await adminQuestions.addDateQuestion({questionName: 'date-q'})
+    await adminQuestions.addEmailQuestion({questionName: 'email-q'})
+
+    await adminPrograms.addProgram(programName)
+    await adminPrograms.addProgramBlock(programName, 'screen 2 description', [])
+    await adminPrograms.editProgramBlock(
+      programName,
+      'dummy description oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
+      ['address-q', 'date-q', 'email-q'],
+    )
+    await adminPrograms.publishAllDrafts()
+
+    await adminPrograms.gotoViewActiveProgramPage(programName)
+
+    await adminPrograms.gotoToBlockInReadOnlyProgram('1')
+    await adminPrograms.expectReadOnlyProgramBlock('1')
+    await adminPrograms.gotoToBlockInReadOnlyProgram('2')
+    await adminPrograms.expectReadOnlyProgramBlock('2')
+
+    await adminPrograms.expectQuestionCardWithLabel(
+      'address-q',
+      'required question',
+    )
+    await adminPrograms.expectQuestionCardWithLabel(
+      'address-q',
+      'address correction: disabled',
+    )
+    await adminPrograms.expectQuestionCardWithLabel(
+      'date-q',
+      'required question',
+    )
+    await adminPrograms.expectQuestionCardWithLabel(
+      'email-q',
+      'required question',
+    )
+
+    await validateScreenshot(
+      page,
+      'view-program-block-2-long-screen-name-and-description',
+    )
+
+    await adminPrograms.gotoViewActiveProgramPageAndStartEditing(programName)
+    await adminPrograms.expectProgramBlockEditPage(programName)
+
+    await validateScreenshot(
+      page,
+      'view-program-start-editing-extra-long-screen-name-and-description',
+    )
+  })
 })
