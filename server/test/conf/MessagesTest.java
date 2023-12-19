8057ed5bfd4fdfcc35237323c5ce8a9c37c7d1b5
dkatzz <86739416+dkatzz@users.noreply.github.com>
2023-12-19 19:58:41 +0000

    fix

--- server/test/conf/MessagesTest.java
+++ server/test/conf/MessagesTest.java
@@ -133,26 +133,25 @@ public class MessagesTest {
     ImmutableList<String> messageKeys =
         Arrays.stream(MessageKey.values()).map(MessageKey::getKeyName).collect(toImmutableList());
 
+    ImmutableList<String> untranslatedKeys =
+        ImmutableList.of(
+            "content.adminFooterPromptNew",
+            "content.commonIntakeConfirmationNew",
+            "content.commonIntakeConfirmationTiNew",
+            "content.commonIntakeNoMatchingProgramsNew",
+            "content.commonIntakeNoMatchingProgramsTiNew",
+            "content.findPrograms",
+            "content.findProgramsDescription",
+            "content.generalLoginModalPromptNew",
+            "content.guestDescriptionNew",
+            "content.initialLoginModalPromptNew",
+            "content.saveTimeServices",
+            "title.commonIntakeConfirmationNew",
+            "title.commonIntakeConfirmationTiNew",
+            "title.getStartedSection");
+
     // TODO(#5893) remove when translations are completed.
-    ImmutableList<String> messageKeysAndUntranslatedKeys =
-        Stream.of(
-                messageKeys,
-                ImmutableList.of(
-                    "content.adminFooterPromptNew",
-                    "content.commonIntakeConfirmationNew",
-                    "content.commonIntakeConfirmationTiNew",
-                    "content.commonIntakeNoMatchingProgramsNew",
-                    "content.commonIntakeNoMatchingProgramsTiNew",
-                    "content.findPrograms",
-                    "content.findProgramsDescription",
-                    "content.generalLoginModalPromptNew",
-                    "content.guestDescriptionNew",
-                    "content.initialLoginModalPromptNew",
-                    "content.saveTimeServices",
-                    "title.commonIntakeConfirmationNew",
-                    "title.commonIntakeConfirmationTiNew",
-                    "title.getStartedSection"))
-            .collect(toImmutableList());
+    ImmutableList<String> messageKeysAndUntranslatedKeys = Stream.of(messageKeys, untranslatedKeys);
 
     assertThat(keysInPrimaryFile)
         .containsExactlyInAnyOrderElementsOf(messageKeysAndUntranslatedKeys);
