5c8f707ef21705917939903107a698b1cd576b4b
azhangggggg <annayang@google.com>
2024-01-09 18:44:58 -0800

    took pdf view change out of this PR

--- server/test/services/applicant/ReadOnlyApplicantProgramServiceImplTest.java
+++ server/test/services/applicant/ReadOnlyApplicantProgramServiceImplTest.java
@@ -884,7 +884,7 @@ public class ReadOnlyApplicantProgramServiceImplTest extends ResetPostgres {
   }
 
   @Test
-  public void getInProgressBlockAfter_thereExistsABlockAfter_returnsTheBlockAfterTheGivenBlock() {
+  public void getBlockAfter_thereExistsABlockAfter_returnsTheBlockAfterTheGivenBlock() {
     ReadOnlyApplicantProgramService subject =
         new ReadOnlyApplicantProgramServiceImpl(
             jsonPathPredicateGeneratorFactory, applicantData, programDefinition, FAKE_BASE_URL);
@@ -896,7 +896,7 @@ public class ReadOnlyApplicantProgramServiceImplTest extends ResetPostgres {
   }
 
   @Test
-  public void getInProgressBlockAfter_argIsLastBlock_returnsEmpty() {
+  public void getBlockAfter_argIsLastBlock_returnsEmpty() {
     ReadOnlyApplicantProgramService subject =
         new ReadOnlyApplicantProgramServiceImpl(
             jsonPathPredicateGeneratorFactory, applicantData, programDefinition, FAKE_BASE_URL);
@@ -907,7 +907,7 @@ public class ReadOnlyApplicantProgramServiceImplTest extends ResetPostgres {
   }
 
   @Test
-  public void getInProgressBlockAfter_emptyBlocks_returnsEmpty() {
+  public void getBlockAfter_emptyBlocks_returnsEmpty() {
     ReadOnlyApplicantProgramService subject =
         new ReadOnlyApplicantProgramServiceImpl(
             jsonPathPredicateGeneratorFactory,
