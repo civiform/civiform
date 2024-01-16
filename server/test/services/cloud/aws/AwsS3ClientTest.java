8c23f977c3bdc2dc83b40f169f539e07857263a0
Caitlin Shkuratov <caitlinshk@google.com>
2024-01-16 11:36:24 -0500

    Rename to AwsS3Client

--- server/test/services/cloud/aws/AwsS3ClientTest.java
+++ server/test/services/cloud/aws/AwsS3ClientTest.java
@@ -8,17 +8,17 @@ import repository.ResetPostgres;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 
-public class RealAwsS3ClientTest extends ResetPostgres {
+public class AwsS3ClientTest extends ResetPostgres {
   private static final URI endpointUri = URI.create("https://s3.us-east-2.amazonaws.com");
   private final Credentials credentials = instanceOf(Credentials.class);
-  private final RealAwsS3Client realAwsS3Client = new RealAwsS3Client();
+  private final AwsS3Client awsS3Client = new AwsS3Client();
 
   @Test
   public void deleteObject_noKeyInRequest_throws() {
     assertThatExceptionOfType(IllegalArgumentException.class)
         .isThrownBy(
             () ->
-                realAwsS3Client.deleteObject(
+                awsS3Client.deleteObject(
                     credentials,
                     Region.US_EAST_2,
                     endpointUri,
@@ -31,7 +31,7 @@ public class RealAwsS3ClientTest extends ResetPostgres {
     assertThatExceptionOfType(IllegalArgumentException.class)
         .isThrownBy(
             () ->
-                realAwsS3Client.deleteObject(
+                awsS3Client.deleteObject(
                     credentials,
                     Region.US_EAST_2,
                     endpointUri,
