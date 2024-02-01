66e0c645bf846b1b220680d18e7d6ff13a2f4eeb
Nick Burgan <nick.burgan@exygy.com>
2024-01-31 16:23:12 -0800

    JS for dynamically showing and hiding toggles and alerts for Primary Applicant Info

--- server/app/assets/javascripts/admin_question_edit.ts
+++ server/app/assets/javascripts/admin_question_edit.ts
@@ -1,4 +1,4 @@
-import {assertNotNull} from './util'
+import {addEventListenerToElements, assertNotNull} from './util'
 import {ModalController} from './modal'
 
 class AdminQuestionEdit {
@@ -8,6 +8,62 @@ class AdminQuestionEdit {
     if (modal !== null) {
       this.addCustomModalClickHandler(modal)
     }
+
+    const primaryApplicantInfoSection = document.getElementById(
+      'primary-applicant-info',
+    )
+    if (primaryApplicantInfoSection !== null) {
+      this.addUniversalToggleHandler(primaryApplicantInfoSection)
+      this.addEnumeratorDropdownHandler(primaryApplicantInfoSection)
+    }
+  }
+
+  addEnumeratorDropdownHandler(primaryApplicantInfoSection: HTMLElement) {
+    addEventListenerToElements(
+      '#question-enumerator-select',
+      'input',
+      (event: Event) => {
+        const target = event.target as HTMLInputElement
+        const setHidden = target.value !== ''
+        primaryApplicantInfoSection.toggleAttribute('hidden', setHidden)
+      },
+    )
+  }
+
+  addUniversalToggleHandler(primaryApplicantInfoSection: HTMLElement) {
+    const primaryApplicantInfoSubsections =
+      primaryApplicantInfoSection.querySelectorAll(
+        '.cf-primary-applicant-info-subsection',
+      )
+    const universalInput = document.getElementById(
+      'universal-toggle-input',
+    ) as HTMLInputElement
+    addEventListenerToElements('#universal-toggle', 'click', () => {
+      primaryApplicantInfoSubsections.forEach((subsection) => {
+        const toggle = subsection.querySelector('.cf-toggle') as HTMLElement
+        const input = toggle.querySelector(
+          '.cf-toggle-hidden-input',
+        ) as HTMLInputElement
+        const universalAlert = subsection.querySelector(
+          '.cf-primary-applicant-info-universal-alert',
+        ) as HTMLElement
+        const alreadySetAlert = subsection.querySelector(
+          '.cf-primary-applicant-info-tag-already-set-alert',
+        ) // May be null
+        // Do not toggle things if this is hidden because the
+        // "you've already set this action on a different question" alert is showing.
+        if (alreadySetAlert === null) {
+          // Unset the action when we unset universal
+          // Because the universal input doesn't change until after the click event,
+          // we're checking for true here.
+          if (input.value === 'true' && universalInput.value === 'true') {
+            toggle.click()
+          }
+          toggle.toggleAttribute('hidden')
+        }
+        universalAlert.toggleAttribute('hidden')
+      })
+    })
   }
 
   addCustomModalClickHandler(modal: HTMLElement) {
