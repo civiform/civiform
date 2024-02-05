e5ca9714142d56994a855e386169cfecb620c4a0
Nick Burgan <nick.burgan@exygy.com>
2024-02-05 13:40:51 -0800

    fix

--- server/conf/evolutions/default/62.sql
+++ server/conf/evolutions/default/62.sql
@@ -16,11 +16,19 @@ CREATE INDEX IF NOT EXISTS index_first_name ON applicants USING gin (first_name 
 CREATE INDEX IF NOT EXISTS index_middle_name ON applicants USING gin (middle_name gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS index_last_name ON applicants USING gin (last_name gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS index_email_address ON applicants (email_address);
-CREATE INDEX IF NOT EXISTS index_country_code ON applicants USING gin(country_code);
+CREATE INDEX IF NOT EXISTS index_country_code ON applicants USING gin (country_code);
 CREATE INDEX IF NOT EXISTS index_phone_number ON applicants USING gin (phone_number gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS index_date_of_birth ON applicants (date_of_birth);
 
 # --- !Downs
+DROP INDEX IF EXISTS index_first_name;
+DROP INDEX IF EXISTS index_middle_name;
+DROP INDEX IF EXISTS index_last_name;
+DROP INDEX IF EXISTS index_email_address;
+DROP INDEX IF EXISTS index_country_code;
+DROP INDEX IF EXISTS index_phone_number;
+DROP INDEX IF EXISTS index_date_of_birth;
+
 ALTER TABLE applicants DROP column IF EXISTS first_name;
 ALTER TABLE applicants DROP column IF EXISTS middle_name;
 ALTER TABLE applicants DROP column IF EXISTS last_name;
@@ -29,10 +37,5 @@ ALTER TABLE applicants DROP column IF EXISTS country_code;
 ALTER TABLE applicants DROP column IF EXISTS phone_number;
 ALTER TABLE applicants DROP column IF EXISTS date_of_birth;
 
-DROP INDEX IF EXISTS index_first_name;
-DROP INDEX IF EXISTS index_middle_name;
-DROP INDEX IF EXISTS index_last_name;
-DROP INDEX IF EXISTS index_email_address;
-DROP INDEX IF EXISTS index_country_code;
-DROP INDEX IF EXISTS index_phone_number;
-DROP INDEX IF EXISTS index_date_of_birth;
+DROP EXTENSION IF EXISTS pg_trgm;
+DROP EXTENSION IF EXISTS btree_gin;
