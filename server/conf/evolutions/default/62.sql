8a63de63691061629d768681ac4cb99dd7321add
Nick Burgan <nick.burgan@exygy.com>
2024-02-05 12:29:14 -0800

    Fix evolution for primary applicant info
    This evolution neglected to have the IF NOT EXISTS guard for creating the extensions and columns. This can be problematic for the extension if the database user does not have permission to create extensions. In this case, the extension will need to be added manually, but then this evolution would fail because it didn't have the IF NOT EXISTS on it.

--- server/conf/evolutions/default/62.sql
+++ server/conf/evolutions/default/62.sql
@@ -1,16 +1,16 @@
 # --- Add primary applicant information columns
 
 # --- !Ups
-CREATE EXTENSION pg_trgm;
-CREATE EXTENSION btree_gin;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS btree_gin;
 
-ALTER TABLE applicants add column first_name varchar;
-ALTER TABLE applicants add column middle_name varchar;
-ALTER TABLE applicants add column last_name varchar;
-ALTER TABLE applicants add column email_address varchar;
-ALTER TABLE applicants add column country_code varchar;
-ALTER TABLE applicants add column phone_number varchar;
-ALTER TABLE applicants add column date_of_birth date;
+ALTER TABLE applicants add column IF NOT EXISTS first_name varchar;
+ALTER TABLE applicants add column IF NOT EXISTS middle_name varchar;
+ALTER TABLE applicants add column IF NOT EXISTS last_name varchar;
+ALTER TABLE applicants add column IF NOT EXISTS email_address varchar;
+ALTER TABLE applicants add column IF NOT EXISTS country_code varchar;
+ALTER TABLE applicants add column IF NOT EXISTS phone_number varchar;
+ALTER TABLE applicants add column IF NOT EXISTS date_of_birth date;
 
 CREATE INDEX IF NOT EXISTS index_first_name ON applicants USING gin (first_name gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS index_middle_name ON applicants USING gin (middle_name gin_trgm_ops);
@@ -21,13 +21,13 @@ CREATE INDEX IF NOT EXISTS index_phone_number ON applicants USING gin (phone_num
 CREATE INDEX IF NOT EXISTS index_date_of_birth ON applicants (date_of_birth);
 
 # --- !Downs
-ALTER TABLE applicants drop column first_name;
-ALTER TABLE applicants drop column middle_name;
-ALTER TABLE applicants drop column last_name;
-ALTER TABLE applicants drop column email_address;
-ALTER TABLE applicants drop column country_code;
-ALTER TABLE applicants drop column phone_number;
-ALTER TABLE applicants drop column date_of_birth;
+ALTER TABLE applicants DROP column IF EXISTS first_name;
+ALTER TABLE applicants DROP column IF EXISTS middle_name;
+ALTER TABLE applicants DROP column IF EXISTS last_name;
+ALTER TABLE applicants DROP column IF EXISTS email_address;
+ALTER TABLE applicants DROP column IF EXISTS country_code;
+ALTER TABLE applicants DROP column IF EXISTS phone_number;
+ALTER TABLE applicants DROP column IF EXISTS date_of_birth;
 
 DROP INDEX IF EXISTS index_first_name;
 DROP INDEX IF EXISTS index_middle_name;
