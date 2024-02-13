da8b5cbef204266ecbd6ae0cbd4b262891b6bb19
Rocky Fine <rocky.fine@exygy.com>
2024-02-13 18:52:18 -0500

    fix padding and add browser test

--- server/app/views/applicant/ApplicantUpsellView.java
+++ server/app/views/applicant/ApplicantUpsellView.java
@@ -47,13 +47,15 @@ public abstract class ApplicantUpsellView extends BaseHtmlView {
       String authProviderName,
       ImmutableList<DomContent> actionButtons) {
     return div()
-        .withClasses(
-            ApplicantStyles.PROGRAM_INFORMATION_BOX, ApplicantStyles.MAIN_PROGRAM_APPLICATION)
+        .withClasses("w-5/6", StyleUtils.responsiveSmall("w-2/3"), "mx-auto")
         .with(
-            h1(title).withClasses("text-3xl", "text-black", "font-bold", "mb-4"),
-            confirmationSection,
-            createAccountManagementSection(
-                shouldUpsell, messages, authProviderName, actionButtons));
+            div()
+                .withClasses(ApplicantStyles.PROGRAM_INFORMATION_BOX)
+                .with(
+                    h1(title).withClasses("text-3xl", "text-black", "font-bold", "mb-4"),
+                    confirmationSection,
+                    createAccountManagementSection(
+                        shouldUpsell, messages, authProviderName, actionButtons)));
   }
 
   protected static HtmlBundle createHtmlBundle(
@@ -65,7 +67,10 @@ public abstract class ApplicantUpsellView extends BaseHtmlView {
       DivTag mainContent) {
     HtmlBundle bundle = layout.getBundle(request).setTitle(title);
     bannerMessage.ifPresent(bundle::addToastMessages);
-    bundle.addMainContent(mainContent).addModals(loginPromptModal);
+    bundle
+        .addMainStyles("my-8", StyleUtils.responsiveSmall("my-12"))
+        .addMainContent(mainContent)
+        .addModals(loginPromptModal);
     return bundle;
   }
 
