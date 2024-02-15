7cff855411bda6b0bbf6d31b02a539f9211d9a69
Caitlin Shkuratov <147177873+caitlinshk@users.noreply.github.com>
2024-02-15 16:57:01 -0500

    Update Modal.java Javadoc to say it's deprecated (#6661)
    Co-authored-by: Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>

--- server/app/views/components/Modal.java
+++ server/app/views/components/Modal.java
@@ -17,7 +17,15 @@ import views.style.BaseStyles;
 import views.style.ReferenceClasses;
 import views.style.StyleUtils;
 
-/** Utility class for rendering a modal box. */
+/**
+ * DEPRECATED
+ *
+ * <p>Utility class for rendering a modal box.
+ *
+ * <p>Note that this is deprecated in favor of the USWDS modal component. Prefer using {@link
+ * views.ViewUtils#makeUSWDSModal} instead of this class. See
+ * https://github.com/civiform/civiform/issues/6264.
+ */
 @AutoValue
 public abstract class Modal {
 
