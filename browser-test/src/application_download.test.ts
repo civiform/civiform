c0b8a78fc905bf5b76db7820faf563660149a8a7
azhangggggg <annayang@google.com>
2024-02-22 15:25:57 -0800

    updated delete button in browser test

--- browser-test/src/application_download.test.ts
+++ browser-test/src/application_download.test.ts
@@ -196,7 +196,7 @@ describe('normal application flow', () => {
     // Change export visibility of a question
     await adminQuestions.createNewVersion('dropdown-csv-download')
     await adminQuestions.gotoQuestionEditPage('dropdown-csv-download')
-    await page.click('#question-settings button:has-text("Delete"):visible')
+    await page.click('#question-settings .cf-multi-option-delete-button')
     await page.click('text=Update')
 
     // Add a new question so that the program has multiple versions
