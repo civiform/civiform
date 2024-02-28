aa9ba720f3dca8ee1b8f8c6a1749133b84c1987c
Greg Hyde <ghyde@google.com>
2024-02-28 10:38:00 -0800

    Create base class for applicant views

--- server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
+++ server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
@@ -1,7 +1,5 @@
 package views.applicant;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import com.google.inject.Inject;
 import controllers.AssetsFinder;
 import controllers.applicant.ApplicantRequestedAction;
@@ -13,11 +11,7 @@ import views.ApplicationBaseView;
 import views.html.helper.CSRF;
 
 /** Renders a page for answering questions in a program screen (block). */
-public final class NorthStarApplicantProgramBlockEditView {
-  private final TemplateEngine templateEngine;
-  private final ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory;
-  private final AssetsFinder assetsFinder;
-  private final ApplicantRoutes applicantRoutes;
+public final class NorthStarApplicantProgramBlockEditView extends NorthStarApplicantBaseView {
 
   @Inject
   NorthStarApplicantProgramBlockEditView(
@@ -25,20 +19,11 @@ public final class NorthStarApplicantProgramBlockEditView {
       ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory,
       AssetsFinder assetsFinder,
       ApplicantRoutes applicantRoutes) {
-    this.templateEngine = checkNotNull(templateEngine);
-    this.playThymeleafContextFactory = checkNotNull(playThymeleafContextFactory);
-    this.assetsFinder = checkNotNull(assetsFinder);
-    this.applicantRoutes = checkNotNull(applicantRoutes);
+    super(templateEngine, playThymeleafContextFactory, assetsFinder, applicantRoutes);
   }
 
   public String render(Request request, ApplicationBaseView.Params applicationParams) {
-    ThymeleafModule.PlayThymeleafContext context = playThymeleafContextFactory.create(request);
-    context.setVariable("tailwindStylesheet", assetsFinder.path("stylesheets/tailwind.css"));
-    context.setVariable("uswdsStylesheet", assetsFinder.path("dist/uswds.min.css"));
-    context.setVariable("adminJsBundle", assetsFinder.path("dist/admin.bundle.js"));
-    context.setVariable("uswdsJsInit", assetsFinder.path("javascripts/uswds/uswds-init.min.js"));
-    context.setVariable("uswdsJsBundle", assetsFinder.path("dist/uswds.bundle.js"));
-    context.setVariable("ApiDocsController", controllers.api.routes.ApiDocsController);
+    ThymeleafModule.PlayThymeleafContext context = createThymeleafContext(request);
     context.setVariable("formAction", getFormAction(applicationParams));
     context.setVariable("csrfToken", CSRF.getToken(request.asScala()).value());
     return templateEngine.process("applicant/ApplicantProgramBlockEditView", context);
