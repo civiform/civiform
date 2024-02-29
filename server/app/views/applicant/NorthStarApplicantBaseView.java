8627839000fad605140a884aae56097676218990
Greg Hyde <ghyde@google.com>
2024-02-29 12:46:00 -0800

    Ran bin/fmt

--- server/app/views/applicant/NorthStarApplicantBaseView.java
+++ server/app/views/applicant/NorthStarApplicantBaseView.java
@@ -9,30 +9,30 @@ import org.thymeleaf.TemplateEngine;
 import play.mvc.Http.Request;
 
 public abstract class NorthStarApplicantBaseView {
-    protected final TemplateEngine templateEngine;
-    protected final ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory;
-    protected final AssetsFinder assetsFinder;
-    protected final ApplicantRoutes applicantRoutes;
+  protected final TemplateEngine templateEngine;
+  protected final ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory;
+  protected final AssetsFinder assetsFinder;
+  protected final ApplicantRoutes applicantRoutes;
 
-    NorthStarApplicantBaseView(
-        TemplateEngine templateEngine,
-        ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory,
-        AssetsFinder assetsFinder,
-        ApplicantRoutes applicantRoutes) {
-      this.templateEngine = checkNotNull(templateEngine);
-      this.playThymeleafContextFactory = checkNotNull(playThymeleafContextFactory);
-      this.assetsFinder = checkNotNull(assetsFinder);
-      this.applicantRoutes = checkNotNull(applicantRoutes);
-    }
+  NorthStarApplicantBaseView(
+      TemplateEngine templateEngine,
+      ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory,
+      AssetsFinder assetsFinder,
+      ApplicantRoutes applicantRoutes) {
+    this.templateEngine = checkNotNull(templateEngine);
+    this.playThymeleafContextFactory = checkNotNull(playThymeleafContextFactory);
+    this.assetsFinder = checkNotNull(assetsFinder);
+    this.applicantRoutes = checkNotNull(applicantRoutes);
+  }
 
-    protected ThymeleafModule.PlayThymeleafContext createThymeleafContext(Request request) {
-        ThymeleafModule.PlayThymeleafContext context = playThymeleafContextFactory.create(request);
-        context.setVariable("tailwindStylesheet", assetsFinder.path("stylesheets/tailwind.css"));
-        context.setVariable("uswdsStylesheet", assetsFinder.path("dist/uswds.min.css"));
-        context.setVariable("adminJsBundle", assetsFinder.path("dist/admin.bundle.js"));
-        context.setVariable("uswdsJsInit", assetsFinder.path("javascripts/uswds/uswds-init.min.js"));
-        context.setVariable("uswdsJsBundle", assetsFinder.path("dist/uswds.bundle.js"));
-        context.setVariable("ApiDocsController", controllers.api.routes.ApiDocsController);
-        return context;
-    }
+  protected ThymeleafModule.PlayThymeleafContext createThymeleafContext(Request request) {
+    ThymeleafModule.PlayThymeleafContext context = playThymeleafContextFactory.create(request);
+    context.setVariable("tailwindStylesheet", assetsFinder.path("stylesheets/tailwind.css"));
+    context.setVariable("uswdsStylesheet", assetsFinder.path("dist/uswds.min.css"));
+    context.setVariable("adminJsBundle", assetsFinder.path("dist/admin.bundle.js"));
+    context.setVariable("uswdsJsInit", assetsFinder.path("javascripts/uswds/uswds-init.min.js"));
+    context.setVariable("uswdsJsBundle", assetsFinder.path("dist/uswds.bundle.js"));
+    context.setVariable("ApiDocsController", controllers.api.routes.ApiDocsController);
+    return context;
+  }
 }
