ca98340939aec9b4444a7d7f95a3b91c1b548bbb
Greg Hyde <ghyde@google.com>
2024-02-29 13:55:25 -0800

    Add continue/submit buttons

--- server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
+++ server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
@@ -8,7 +8,6 @@ import modules.ThymeleafModule;
 import org.thymeleaf.TemplateEngine;
 import play.mvc.Http.Request;
 import views.ApplicationBaseView;
-import views.html.helper.CSRF;
 
 /** Renders a page for answering questions in a program screen (block). */
 public final class NorthStarApplicantProgramBlockEditView extends NorthStarApplicantBaseView {
@@ -25,7 +24,6 @@ public final class NorthStarApplicantProgramBlockEditView extends NorthStarAppli
   public String render(Request request, ApplicationBaseView.Params applicationParams) {
     ThymeleafModule.PlayThymeleafContext context = createThymeleafContext(request);
     context.setVariable("formAction", getFormAction(applicationParams));
-    context.setVariable("csrfToken", CSRF.getToken(request.asScala()).value());
     return templateEngine.process("applicant/ApplicantProgramBlockEditTemplate", context);
   }
 
