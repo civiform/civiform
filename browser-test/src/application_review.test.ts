a296607a0aec4c440a6c242ae793bdff06ec51a9
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2024-03-08 16:28:13 -0800

    Update browser-test/src/application_review.test.ts
    Co-authored-by: Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>

--- browser-test/src/application_review.test.ts
+++ browser-test/src/application_review.test.ts
@@ -280,7 +280,7 @@ test.describe('Program admin review of submitted applications', () => {
       await validateScreenshot(page, 'reporting-page')
     }
 
-    await page.click(`text=${programName}`)
+    await page.getByRole('link', { name: programName })
 
     if (isHermeticTestEnvironment()) {
       await validateScreenshot(page, 'program-specific-reporting-page')
