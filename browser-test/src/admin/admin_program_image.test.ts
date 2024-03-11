dfca61c6d078c246e3f772bfe1dd7f7b00e1ec13
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-03-11 07:40:49 -0700

    Move browser tests to sub-directories (#6852)

---
+++ browser-test/src/admin/admin_program_image.test.ts
@@ -0,0 +1,524 @@
+import {test, expect} from '@playwright/test'
+import {
+  createTestContext,
+  dismissToast,
+  loginAsAdmin,
+  validateScreenshot,
+  validateToastMessage,
+} from '../support'
+
+test.describe('Admin can manage program image', () => {
+  const ctx = createTestContext()
+
+  test('views a program without an image', async () => {
+    const {page, adminPrograms} = ctx
+    await loginAsAdmin(page)
+
+    const programName = 'Program without image'
+    await adminPrograms.addProgram(programName)
+
+    await adminPrograms.goToProgramImagePage(programName)
+
+    await validateScreenshot(page, 'program-image-none')
+  })
+
+  test.describe('back button', () => {
+    test('back button redirects to block page if came from block page', async () => {
+      const {page, adminPrograms, adminProgramImage} = ctx
+      await loginAsAdmin(page)
+
+      const programName = 'program name'
+      await adminPrograms.addProgram(programName)
+      // Navigate from edit program blocks page -> edit program image page
+      await adminPrograms.gotoEditDraftProgramPage(programName)
+      await adminPrograms.goToProgramImagePage(programName)
+
+      await adminProgramImage.clickBackButton()
+
+      await adminPrograms.expectProgramBlockEditPage()
+    })
+
+    test('back button redirects to details page if came from create program page', async () => {
+      const {page, adminPrograms, adminProgramImage} = ctx
+      await loginAsAdmin(page)
+
+      // After creating a program, admin should be redirected to the program images page
+      const programName = 'Back Test Program'
+      await adminPrograms.addProgram(programName)
+      await adminProgramImage.expectProgramImagePage()
+
+      // WHEN back is clicked
+      await adminProgramImage.clickBackButton()
+
+      // THEN the admin goes back to the edit program details page for this new program
+      await adminPrograms.expectProgramEditPage(programName)
+    })
+
+    test('back button preserves location after interaction', async () => {
+      const {page, adminPrograms, adminProgramImage} = ctx
+      await loginAsAdmin(page)
+
+      const programName = 'Back Test Program'
+      await adminPrograms.addProgram(programName)
+      await adminProgramImage.expectProgramImagePage()
+
+      // When an admin submits an image or description on the page, the page reloads.
+      // This test verifies that the redirect URL for the back button is preserved
+      // even after those submission and page reloads.
+      await adminProgramImage.setImageDescriptionAndSubmit('description')
+      await adminProgramImage.setImageFileAndSubmit(
+        'src/assets/program-summary-image-wide.png',
+      )
+
+      await adminProgramImage.clickBackButton()
+
+      await adminPrograms.expectProgramEditPage(programName)
+    })
+  })
+
+  test.describe('continue button', () => {
+    test('continue button shows if from program creation page', async () => {
+      const {page, adminPrograms, adminProgramImage} = ctx
+      await loginAsAdmin(page)
+
+      const programName = 'Back Test Program'
+      await adminPrograms.addProgram(programName)
+      await adminProgramImage.expectProgramImagePage()
+
+      await adminProgramImage.expectHasContinueButton()
+      await validateScreenshot(page, 'program-image-with-continue')
+    })
+
+    test('continue button redirects to program blocks page', async () => {
+      const {page, adminPrograms, adminProgramImage} = ctx
+      await loginAsAdmin(page)
+
+      const programName = 'Back Test Program'
+      await adminPrograms.addProgram(programName)
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectHasContinueButton()
+
+      await adminProgramImage.clickContinueButton()
+
+      await adminPrograms.expectProgramBlockEditPage()
+    })
+
+    test('continue button hides if from edit program image page', async () => {
+      const {page, adminPrograms, adminProgramImage} = ctx
+      await loginAsAdmin(page)
+
+      const programName = 'program name'
+      await adminPrograms.addProgram(programName)
+      // Navigate from edit program blocks page -> edit program image page
+      await adminPrograms.gotoEditDraftProgramPage(programName)
+      await adminPrograms.goToProgramImagePage(programName)
+
+      await adminProgramImage.expectNoContinueButton()
+    })
+  })
+
+  test.describe('description', () => {
+    const programName = 'Test program'
+
+    test.beforeEach(async () => {
+      const {page, adminPrograms} = ctx
+      await loginAsAdmin(page)
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.goToProgramImagePage(programName)
+    })
+
+    test('sets new description', async () => {
+      const {page, adminProgramImage} = ctx
+
+      await adminProgramImage.setImageDescription('Fake image description')
+      await validateScreenshot(
+        page,
+        'program-image-with-description-before-save',
+      )
+
+      await adminProgramImage.submitImageDescription()
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('Fake image description')
+      await validateToastMessage(
+        page,
+        adminProgramImage.descriptionUpdatedToastMessage(
+          'Fake image description',
+        ),
+      )
+
+      await dismissToast(page)
+      await validateScreenshot(page, 'program-image-with-description')
+    })
+
+    test('updates existing description', async () => {
+      const {page, adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('Fake image description')
+
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'New image description',
+      )
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('New image description')
+      await validateToastMessage(
+        page,
+        adminProgramImage.descriptionUpdatedToastMessage(
+          'New image description',
+        ),
+      )
+    })
+
+    test('removes description with empty', async () => {
+      const {page, adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('Fake image description')
+
+      await adminProgramImage.setImageDescriptionAndSubmit('')
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('')
+      await validateToastMessage(
+        page,
+        adminProgramImage.descriptionClearedToastMessage(),
+      )
+    })
+
+    test('removes description with blank', async () => {
+      const {page, adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('Fake image description')
+
+      // WHEN a blank description is entered
+      await adminProgramImage.setImageDescriptionAndSubmit('   ')
+      await adminProgramImage.expectProgramImagePage()
+
+      // We internally set it to completely empty
+      await adminProgramImage.expectDescriptionIs('')
+      await validateToastMessage(
+        page,
+        adminProgramImage.descriptionClearedToastMessage(),
+      )
+    })
+
+    test('does not remove description if image present (description set to empty)', async () => {
+      const {page, adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Original description',
+      )
+      await adminProgramImage.setImageFileAndSubmit(
+        'src/assets/program-summary-image-wide.png',
+      )
+
+      await adminProgramImage.setImageDescriptionAndSubmit('')
+
+      await adminProgramImage.expectDescriptionIs('Original description')
+      await validateToastMessage(
+        page,
+        adminProgramImage.descriptionNotClearedToastMessage(),
+      )
+    })
+
+    test('does not remove description if image present (description set to blank)', async () => {
+      const {page, adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Original description',
+      )
+      await adminProgramImage.setImageFileAndSubmit(
+        'src/assets/program-summary-image-wide.png',
+      )
+
+      await adminProgramImage.setImageDescriptionAndSubmit('      ')
+
+      await adminProgramImage.expectDescriptionIs('Original description')
+      await validateToastMessage(
+        page,
+        adminProgramImage.descriptionNotClearedToastMessage(),
+      )
+    })
+
+    test('can remove description after deleting image', async () => {
+      const {adminProgramImage} = ctx
+
+      // Set a description and image
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Original description',
+      )
+      await adminProgramImage.setImageFileAndSubmit(
+        'src/assets/program-summary-image-wide.png',
+      )
+
+      // If the image is deleted
+      await adminProgramImage.clickDeleteImageButton()
+      await adminProgramImage.confirmDeleteImageButton()
+
+      // Then the description can also be deleted afterwards
+      await adminProgramImage.setImageDescriptionAndSubmit('')
+
+      await adminProgramImage.expectDescriptionIs('')
+    })
+
+    test('disables submit button after save', async () => {
+      const {adminProgramImage} = ctx
+
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      // On page reload the submit button should be disabled because no changes
+      // have been made to the description since submission.
+      await adminProgramImage.expectDisabledImageDescriptionSubmit()
+    })
+
+    test('disables submit button when no text change', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      // Set an identical description and verify submit is still disabled.
+      await adminProgramImage.setImageDescription('Fake image description')
+      await adminProgramImage.expectDisabledImageDescriptionSubmit()
+
+      // Set a new description then go back to the original description
+      // and verify submit is still disabled.
+      await adminProgramImage.setImageDescription('Something different')
+      await adminProgramImage.setImageDescription('Fake image description')
+      await adminProgramImage.expectDisabledImageDescriptionSubmit()
+    })
+
+    test('enables submit button when change', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      await adminProgramImage.setImageDescription('Something different')
+
+      await adminProgramImage.expectEnabledImageDescriptionSubmit()
+    })
+
+    test('enables submit button when text removed', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      // Verify that we enable the submit button if the admin
+      // updates the description to be empty.
+      await adminProgramImage.setImageDescription('')
+
+      await adminProgramImage.expectEnabledImageDescriptionSubmit()
+    })
+
+    test('disables translation button when no description', async () => {
+      const {adminProgramImage} = ctx
+
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('')
+
+      await adminProgramImage.expectDisabledTranslationButton()
+    })
+
+    test('enables translation button when description exists', async () => {
+      const {adminProgramImage} = ctx
+
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await adminProgramImage.expectProgramImagePage()
+
+      await adminProgramImage.expectEnabledTranslationButton()
+    })
+
+    test('translation button redirects to translations page', async () => {
+      const {adminPrograms, adminProgramImage} = ctx
+
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await adminProgramImage.expectProgramImagePage()
+
+      await adminProgramImage.clickTranslationButton()
+      await adminPrograms.expectProgramManageTranslationsPage(programName)
+    })
+  })
+
+  test.describe('image file upload', () => {
+    const programName = 'Test program'
+
+    test.beforeEach(async () => {
+      const {page, adminPrograms} = ctx
+      await loginAsAdmin(page)
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.goToProgramImagePage(programName)
+    })
+
+    test('form is correctly formatted', async () => {
+      const {page} = ctx
+
+      const formInputs = await page
+        .locator('#image-file-upload-form')
+        .locator('input')
+        .all()
+      const lastFormInput = formInputs[formInputs.length - 1]
+
+      // AWS requires that the <input type="file"> element to be the last <input> in the <form>
+      await expect(lastFormInput).toHaveAttribute('type', 'file')
+    })
+
+    test('shows uploaded image before submitting', async () => {
+      const {page, adminProgramImage} = ctx
+
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-wide.png',
+      )
+
+      await validateScreenshot(page, 'program-image-with-image-before-save')
+    })
+
+    test('prevents image upload when no description', async () => {
+      const {adminProgramImage} = ctx
+
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectDescriptionIs('')
+
+      await adminProgramImage.expectDisabledImageFileUpload()
+      await adminProgramImage.expectDisabledImageFileUploadSubmit()
+    })
+
+    test('disables submit button when no image', async () => {
+      const {adminProgramImage} = ctx
+      // The submit button will also be disabled if there's no description,
+      // which we don't want to test here. So, set a description to rule
+      // that out.
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      await adminProgramImage.expectDisabledImageFileUploadSubmit()
+    })
+
+    test('disables submit button when too large image', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-too-large.png',
+      )
+
+      await adminProgramImage.expectDisabledImageFileUploadSubmit()
+    })
+
+    test('enables submit button when small enough image', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-wide.png',
+      )
+
+      await adminProgramImage.expectEnabledImageFileUploadSubmit()
+    })
+
+    test('disables submit button when image removed', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-wide.png',
+      )
+      await adminProgramImage.expectEnabledImageFileUploadSubmit()
+
+      await adminProgramImage.setImageFile('')
+
+      await adminProgramImage.expectDisabledImageFileUploadSubmit()
+    })
+
+    test('shows error when too large image', async () => {
+      const {page, adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+      await dismissToast(page)
+
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-too-large.png',
+      )
+
+      await adminProgramImage.expectTooLargeErrorShown()
+      await validateScreenshot(page, 'program-image-with-too-large-error')
+    })
+
+    test('hides error when too large image replaced with smaller image', async () => {
+      const {adminProgramImage} = ctx
+      await adminProgramImage.setImageDescriptionAndSubmit(
+        'Fake image description',
+      )
+
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-too-large.png',
+      )
+      await adminProgramImage.expectTooLargeErrorShown()
+
+      await adminProgramImage.setImageFile(
+        'src/assets/program-summary-image-tall.png',
+      )
+
+      await adminProgramImage.expectTooLargeErrorHidden()
+    })
+
+    test('adds new image', async () => {
+      const {page, adminProgramImage} = ctx
+
+      await adminProgramImage.expectNoImagePreview()
+      await adminProgramImage.setImageFileAndSubmit(
+        'src/assets/program-summary-image-wide.png',
+      )
+      await adminProgramImage.expectProgramImagePage()
+      await validateToastMessage(
+        page,
+        adminProgramImage.imageUpdatedToastMessage(),
+      )
+
+      await dismissToast(page)
+      await validateScreenshot(page, 'program-image-with-image')
+      await adminProgramImage.expectImagePreview()
+    })
+
+    test('deletes existing image', async () => {
+      const {page, adminProgramImage} = ctx
+
+      await adminProgramImage.setImageFileAndSubmit(
+        'src/assets/program-summary-image-wide.png',
+      )
+      await dismissToast(page)
+      await adminProgramImage.expectProgramImagePage()
+      await adminProgramImage.expectImagePreview()
+
+      await adminProgramImage.clickDeleteImageButton()
+      await validateScreenshot(page, 'delete-image-confirmation-modal')
+
+      await adminProgramImage.confirmDeleteImageButton()
+
+      await adminProgramImage.expectProgramImagePage()
+      await validateToastMessage(
+        page,
+        adminProgramImage.imageRemovedToastMessage(),
+      )
+      await adminProgramImage.expectNoImagePreview()
+    })
+  })
+})
---
+++ browser-test/src/applicant/questions/number_input.test.ts
@@ -0,0 +1,171 @@
+import {test, expect} from '@playwright/test'
+import {
+  createTestContext,
+  loginAsAdmin,
+  logout,
+  validateAccessibility,
+  validateScreenshot,
+} from '../../support'
+
+test.describe('Number question for applicant flow', () => {
+  const ctx = createTestContext(/* clearDb= */ false)
+  const numberInputError = 'div.cf-question-number-error'
+
+  test.describe('single number question', () => {
+    const programName = 'Test program for single number'
+
+    test.beforeAll(async () => {
+      const {page, adminQuestions, adminPrograms} = ctx
+      // As admin, create program with single number question.
+      await loginAsAdmin(page)
+
+      await adminQuestions.addNumberQuestion({
+        questionName: 'fave-number-q',
+      })
+      await adminPrograms.addAndPublishProgramWithQuestions(
+        ['fave-number-q'],
+        programName,
+      )
+
+      await logout(page)
+    })
+
+    test('validate screenshot', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+
+      await validateScreenshot(page, 'number')
+    })
+
+    test('validate screenshot with errors', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.clickNext()
+
+      await validateScreenshot(page, 'number-errors')
+    })
+
+    test('with valid number submits successfully', async () => {
+      const {applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.answerNumberQuestion('8')
+      await applicantQuestions.clickNext()
+
+      await applicantQuestions.submitFromReviewPage()
+    })
+
+    test('with no input does not submit', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      // Leave field blank.
+      await applicantQuestions.clickNext()
+
+      const numberId = '.cf-question-number'
+      expect(await page.innerText(numberId)).toContain(
+        'This question is required.',
+      )
+    })
+
+    test('with non-numeric inputs does not submit', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      const testValues = ['12e3', '12E3', '-123', '1.23']
+
+      for (const testValue of testValues) {
+        await applicantQuestions.answerNumberQuestion(testValue)
+        await applicantQuestions.clickNext()
+
+        await expect(page.locator(numberInputError)).toBeVisible()
+        await applicantQuestions.answerNumberQuestion('')
+      }
+    })
+  })
+
+  test.describe('multiple number questions', () => {
+    const programName = 'Test program for multiple numbers'
+
+    test.beforeAll(async () => {
+      const {page, adminQuestions, adminPrograms} = ctx
+      await loginAsAdmin(page)
+
+      await adminQuestions.addNumberQuestion({
+        questionName: 'my-number-q',
+      })
+      await adminQuestions.addNumberQuestion({
+        questionName: 'your-number-q',
+      })
+
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.editProgramBlockWithOptional(
+        programName,
+        'Optional question block',
+        ['my-number-q'],
+        'your-number-q', // optional
+      )
+      await adminPrograms.publishAllDrafts()
+
+      await logout(page)
+    })
+
+    test('with valid numbers submits successfully', async () => {
+      const {applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.answerNumberQuestion('100', 0)
+      await applicantQuestions.answerNumberQuestion('33', 1)
+      await applicantQuestions.clickNext()
+
+      await applicantQuestions.submitFromReviewPage()
+    })
+
+    test('with unanswered optional question submits successfully', async () => {
+      const {applicantQuestions} = ctx
+      // Only answer required question.
+      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.answerNumberQuestion('33', 1)
+      await applicantQuestions.clickNext()
+
+      await applicantQuestions.submitFromReviewPage()
+    })
+
+    test('with first invalid does not submit', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.answerNumberQuestion('-10', 0)
+      await applicantQuestions.answerNumberQuestion('33', 1)
+      await applicantQuestions.clickNext()
+
+      // Fix me! ESLint: playwright/prefer-web-first-assertions
+      // Directly switching to the best practice method fails
+      // because of a locator stict mode violation. That is it
+      // returns multiple elements.
+      //
+      // Recommended prefer-web-first-assertions fix:
+      //   await expect(page.locator(numberInputError)).toBeVisible()
+      expect(await page.isHidden(numberInputError)).toEqual(false)
+    })
+
+    test('with second invalid does not submit', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.answerNumberQuestion('10', 0)
+      await applicantQuestions.answerNumberQuestion('-5', 1)
+      await applicantQuestions.clickNext()
+
+      // Fix me! ESLint: playwright/prefer-web-first-assertions
+      // Directly switching to the best practice method fails
+      // because of a locator stict mode violation. That is it
+      // returns multiple elements.
+      //
+      // Recommended prefer-web-first-assertions fix:
+      //   await expect(page.locator(numberInputError + ' >> nth=1')).toBeVisible()
+      expect(await page.isHidden(numberInputError + ' >> nth=1')).toEqual(false)
+    })
+
+    test('has no accessiblity violations', async () => {
+      const {page, applicantQuestions} = ctx
+      await applicantQuestions.applyProgram(programName)
+
+      await validateAccessibility(page)
+    })
+  })
+})
