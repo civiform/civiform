dc9a35c56342d83f00eca4132f1998a761b88009
swathi <swathi@Jacobs-MacBook-Pro.local>
2024-03-11 09:23:17 -0700

    PR feedback

--- browser-test/src/application_review.test.ts
+++ browser-test/src/application_review.test.ts
@@ -280,7 +280,7 @@ test.describe('Program admin review of submitted applications', () => {
       await validateScreenshot(page, 'reporting-page')
     }
 
-    await page.getByRole('link', { name: programName }).click()
+    await page.getByRole('link', {name: programName}).click()
 
     if (isHermeticTestEnvironment()) {
       await validateScreenshot(page, 'program-specific-reporting-page')
--- server/conf/evolutions/default/66.sql
+++ server/conf/evolutions/default/66.sql
@@ -1,11 +1,11 @@
 # --- !Ups
---Drop the existing view
+
 DROP MATERIALIZED VIEW IF EXISTS monthly_submissions_reporting_view;
--- create a new view
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS monthly_submissions_reporting_view AS
   SELECT
   programs.name AS program_name,
-  activeprogram.localized_name AS localized_name,
+  active_program.localized_name AS localized_name,
   date_trunc('month', applications.submit_time) AS submit_month,
   count(*),
   percentile_cont(0.25) WITHIN GROUP (
@@ -26,18 +26,14 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS monthly_submissions_reporting_view AS
     INNER JOIN versions_programs vp ON
     vp.programs_id = p.id
     INNER JOIN versions v ON
-    vp.versions_id = v.id WHERE v.lifecycle_stage IN ('active')) AS activeprogram
-  ON activeprogram.name = programs.name
+    vp.versions_id = v.id WHERE v.lifecycle_stage IN ('active')) AS active_program
+  ON active_program.name = programs.name
 WHERE applications.lifecycle_stage IN ('active', 'obsolete')
-GROUP BY programs.name, activeprogram.localized_name, DATE_TRUNC('month', applications.submit_time)
-ORDER BY programs.name,activeprogram.localized_name, DATE_TRUNC('month', applications.submit_time) DESC;
+GROUP BY programs.name, active_program.localized_name, DATE_TRUNC('month', applications.submit_time)
+ORDER BY programs.name,active_program.localized_name, DATE_TRUNC('month', applications.submit_time) DESC;
 
---Reindexing
-REINDEX INDEX index_applications_by_submit_time;
 
 # --- !Downs
-
-DROP INDEX IF EXISTS index_applications_by_submit_time;
 
 DROP MATERIALIZED VIEW IF EXISTS monthly_submissions_reporting_view;
 
