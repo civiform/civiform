e383bec07a3803f24efe47ba0518da31e8c41ecc
greg-hyde <156004543+greg-hyde@users.noreply.github.com>
2024-03-13 11:31:07 -0700

    Gated on feature flag, render blank page (#6701)

---
+++ server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
@@ -0,0 +1,43 @@
+package views.applicant;
+
+import com.google.inject.Inject;
+import controllers.AssetsFinder;
+import controllers.applicant.ApplicantRequestedAction;
+import controllers.applicant.ApplicantRoutes;
+import modules.ThymeleafModule;
+import org.thymeleaf.TemplateEngine;
+import play.mvc.Http.Request;
+import views.ApplicationBaseView;
+import views.html.helper.CSRF;
+
+/** Renders a page for answering questions in a program screen (block). */
+public final class NorthStarApplicantProgramBlockEditView extends NorthStarApplicantBaseView {
+
+  @Inject
+  NorthStarApplicantProgramBlockEditView(
+      TemplateEngine templateEngine,
+      ThymeleafModule.PlayThymeleafContextFactory playThymeleafContextFactory,
+      AssetsFinder assetsFinder,
+      ApplicantRoutes applicantRoutes) {
+    super(templateEngine, playThymeleafContextFactory, assetsFinder, applicantRoutes);
+  }
+
+  public String render(Request request, ApplicationBaseView.Params applicationParams) {
+    ThymeleafModule.PlayThymeleafContext context = createThymeleafContext(request);
+    context.setVariable("formAction", getFormAction(applicationParams));
+    context.setVariable("csrfToken", CSRF.getToken(request.asScala()).value());
+    return templateEngine.process("applicant/ApplicantProgramBlockEditTemplate", context);
+  }
+
+  private String getFormAction(ApplicationBaseView.Params params) {
+    return applicantRoutes
+        .updateBlock(
+            params.profile(),
+            params.applicantId(),
+            params.programId(),
+            params.block().getId(),
+            params.inReview(),
+            ApplicantRequestedAction.NEXT_BLOCK)
+        .url();
+  }
+}
