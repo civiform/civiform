a10aabc6c56ef22c6fd2910ce0086e92dc44d59e
Caitlin Shkuratov <caitlinshk@google.com>
2024-03-19 14:39:44 -0400

    sending program & question IDs to controller

--- server/app/controllers/admin/AdminImportExportController.java
+++ server/app/controllers/admin/AdminImportExportController.java
@@ -1,30 +1,141 @@
 package controllers.admin;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
 import auth.Authorizers;
 import auth.ProfileUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import controllers.CiviFormController;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
 import org.pac4j.play.java.Secure;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Http;
 import play.mvc.Result;
 import repository.VersionRepository;
+import services.program.ProgramDefinition;
+import services.program.ProgramNotFoundException;
+import services.program.ProgramService;
+import services.question.QuestionService;
+import services.question.exceptions.QuestionNotFoundException;
+import services.question.types.QuestionDefinition;
 import views.admin.importexport.AdminImportExportView;
+import views.admin.importexport.AdminProgramExportForm;
 
 /** TODO */
 public final class AdminImportExportController extends CiviFormController {
   private final AdminImportExportView adminImportExportView;
+  private final FormFactory formFactory;
+  private final ObjectMapper objectMapper;
+  private final ProgramService programService;
+  private final QuestionService questionService;
 
   @Inject
   public AdminImportExportController(
       AdminImportExportView adminImportExportView,
+      FormFactory formFactory,
+      ObjectMapper objectMapper,
       ProfileUtils profileUtils,
+      ProgramService programService,
+      QuestionService questionService,
       VersionRepository versionRepository) {
     super(profileUtils, versionRepository);
     this.adminImportExportView = adminImportExportView;
+    this.formFactory = checkNotNull(formFactory);
+    this.objectMapper = checkNotNull(objectMapper);
+    this.programService = checkNotNull(programService);
+    this.questionService = checkNotNull(questionService);
   }
 
   @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
-  public Result index(Http.Request request) {
-    return ok(adminImportExportView.render(request));
+  public CompletionStage<Result> index(Http.Request request) {
+    // TODO: active or draft?
+    return questionService
+        .getReadOnlyQuestionService()
+        .thenApplyAsync(
+            readOnlyQuestionService ->
+                ok(
+                    adminImportExportView.render(
+                        request,
+                        programService.getActiveAndDraftPrograms().getActivePrograms(),
+                        readOnlyQuestionService
+                            .getActiveAndDraftQuestions()
+                            .getActiveQuestions())));
+  }
+
+  @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
+  public CompletionStage<Result> exportPrograms(Http.Request request) {
+    Form<AdminProgramExportForm> form =
+        formFactory.form(AdminProgramExportForm.class).bindFromRequest(request);
+
+    List<Long> exportingIds = form.get().getProgramIds();
+    System.out.println("exported IDs=" + exportingIds);
+
+    // TODO: Need to fetch all the question definitions for the programs
+
+    ImmutableList<ProgramDefinition> programs =
+        exportingIds.stream()
+            .map(
+                programId -> {
+                  try {
+                    return programService.getFullProgramDefinition(programId);
+                  } catch (ProgramNotFoundException e) {
+                    throw new RuntimeException(e);
+                  }
+                })
+            .collect(ImmutableList.toImmutableList());
+
+    ImmutableSet.Builder<Long> questionsUsedByPrograms = ImmutableSet.builder();
+    programs.forEach(
+        programDefinition ->
+            questionsUsedByPrograms.addAll(programDefinition.getQuestionIdsInProgram()));
+
+    System.out.println("questions used= " + questionsUsedByPrograms.build());
+
+    ImmutableList<QuestionDefinition> questions =
+        questionsUsedByPrograms.build().stream()
+            .map(
+                questionId -> {
+                  try {
+                    return questionService
+                        .getReadOnlyQuestionServiceSync()
+                        .getQuestionDefinition(questionId);
+                  } catch (QuestionNotFoundException e) {
+                    throw new RuntimeException(e);
+                  }
+                })
+            .collect(ImmutableList.toImmutableList());
+
+    String programJson = getPrettyJson(programs);
+    String questionJson = getPrettyJson(questions);
+
+    return supplyAsync(
+        () -> redirect(controllers.admin.routes.AdminImportExportController.index()));
+    /*
+    return questionService.getReadOnlyQuestionService()
+            .thenApplyAsync(
+                    readOnlyQuestionService ->
+                            ok(adminImportExportView.render(request,
+                                    programService.getActiveAndDraftPrograms().getActivePrograms(),
+                                    readOnlyQuestionService.getActiveAndDraftQuestions().getActiveQuestions()))
+
+            );
+
+     */
+  }
+
+  // From DatabaseSeedView
+  private <T> String getPrettyJson(ImmutableList<T> list) {
+    try {
+      return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(list);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
--- server/app/views/admin/importexport/AdminImportExportView.java
+++ server/app/views/admin/importexport/AdminImportExportView.java
@@ -2,18 +2,34 @@ package views.admin.importexport;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static j2html.TagCreator.div;
+import static j2html.TagCreator.each;
+import static j2html.TagCreator.fieldset;
+import static j2html.TagCreator.form;
 import static j2html.TagCreator.h1;
+import static j2html.TagCreator.h2;
+import static j2html.TagCreator.p;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
+import controllers.admin.routes;
+import j2html.tags.DomContent;
 import j2html.tags.specialized.DivTag;
+import j2html.tags.specialized.FieldsetTag;
 import play.mvc.Http;
 import play.twirl.api.Content;
+import services.program.ProgramDefinition;
+import services.question.types.QuestionDefinition;
 import views.BaseHtmlView;
 import views.HtmlBundle;
 import views.admin.AdminLayout;
 import views.admin.AdminLayoutFactory;
+import views.components.ButtonStyles;
+import views.components.FieldWithLabel;
 
 public final class AdminImportExportView extends BaseHtmlView {
+  // TODO: Do the same as ProgramImageDescriptionForm
+  private static final String PROGRAMS_FIELD_NAME = "programIds[]";
+
   private final AdminLayout layout;
 
   @Inject
@@ -21,11 +37,62 @@ public final class AdminImportExportView extends BaseHtmlView {
     this.layout = checkNotNull(layoutFactory).getLayout(AdminLayout.NavPage.IMPORT_EXPORT);
   }
 
-  public Content render(Http.Request request) {
+  public Content render(
+      Http.Request request,
+      ImmutableList<ProgramDefinition> activePrograms,
+      ImmutableList<QuestionDefinition> activeQuestions) {
     String title = "Import and export questions and programs";
     DivTag contentDiv = div().with(h1(title));
 
+    contentDiv.with(programSelection(request, activePrograms));
+    contentDiv.with(questionSelection(activeQuestions));
+
     HtmlBundle bundle = layout.getBundle(request).setTitle(title).addMainContent(contentDiv);
     return layout.render(bundle);
+  }
+
+  private DomContent programSelection(
+      Http.Request request, ImmutableList<ProgramDefinition> activePrograms) {
+    DivTag mainDiv = div();
+    mainDiv.with(h2("Programs"));
+
+    AdminProgramExportForm form = new AdminProgramExportForm(activePrograms);
+
+    FieldsetTag fields = fieldset();
+
+    for (int i = 0; i < form.getProgramIds().size(); i++) {
+      System.out.println(
+          "program " + form.getProgramNames().get(i) + "  is ID=" + form.getProgramIds().get(i));
+      fields.with(
+          FieldWithLabel.checkbox()
+              .setFieldName(PROGRAMS_FIELD_NAME)
+              .setLabelText(form.getProgramNames().get(i))
+              .setValue(String.valueOf(form.getProgramIds().get(i)))
+              .getCheckboxTag());
+    }
+
+    return mainDiv.with(
+        form()
+            .withMethod("POST")
+            .withAction(routes.AdminImportExportController.exportPrograms().url())
+            .with(makeCsrfTokenInputTag(request), fields)
+            .with(submitButton("Export programs").withClass(ButtonStyles.SOLID_BLUE)));
+  }
+
+  private DomContent questionSelection(ImmutableList<QuestionDefinition> activeQuestions) {
+    DivTag mainDiv = div();
+    mainDiv.with(h2("Questions"));
+    mainDiv.with(
+        each(
+            activeQuestions,
+            question -> {
+              System.out.println(
+                  "question "
+                      + question.getQuestionText().getDefault()
+                      + "  has ID="
+                      + question.getId());
+              return p(question.getQuestionText().getDefault());
+            }));
+    return mainDiv;
   }
 }
