58003a896f6cc025aa7c95b4fb8aa489d9c3cea1
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-03-22 08:52:25 -0700

    Adding Swagger v2 models (#7006)

---
+++ server/app/services/openapi/v2/MimeType.java
@@ -0,0 +1,16 @@
+package services.openapi.v2;
+
+/** The available mime types to allow */
+public enum MimeType {
+  Json("application/json");
+
+  private final String code;
+
+  MimeType(String code) {
+    this.code = code;
+  }
+
+  public String getCode() {
+    return code;
+  }
+}
---
+++ server/app/services/openapi/v2/SecurityRequirement.java
@@ -0,0 +1,29 @@
+package services.openapi.v2;
+
+import com.google.auto.value.AutoValue;
+
+/**
+ * Lists the required security schemes to execute this operation. The object can have multiple
+ * security schemes declared in it which are all required (that is, there is a logical AND between
+ * the schemes).
+ *
+ * <p>The name used for each property MUST correspond to a security scheme declared in the Security
+ * Definitions.
+ *
+ * <p>https://swagger.io/specification/v2/#security-requirement-object
+ */
+@AutoValue
+public abstract class SecurityRequirement {
+  public abstract SecurityType getSecurityType();
+
+  public static SecurityRequirement.Builder builder(SecurityType securityType) {
+    return new AutoValue_SecurityRequirement.Builder().setSecurityType(securityType);
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    protected abstract SecurityRequirement.Builder setSecurityType(SecurityType securityType);
+
+    public abstract SecurityRequirement build();
+  }
+}
