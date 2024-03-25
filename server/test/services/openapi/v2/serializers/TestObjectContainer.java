d5623b47f3277b902d2bf063d2eeedfc56eafd7e
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-03-25 15:05:15 -0700

    Adding swagger v2 model serializers and tests (#7014)

---
+++ server/test/services/openapi/v2/serializers/TestObjectContainer.java
@@ -0,0 +1,41 @@
+package services.openapi.v2.serializers;
+
+import services.openapi.v2.Definition;
+import services.openapi.v2.Header;
+import services.openapi.v2.Response;
+
+/**
+ * Some serializers don't directly start as a new field and thus will throw JsonGenerationException:
+ * Can not write a field name, expecting a value
+ *
+ * <p>This class acts as a small container to test the serialization of a specific object
+ */
+public class TestObjectContainer {
+  private Header header;
+  private Response response;
+  private Definition schema;
+
+  public TestObjectContainer(Header header) {
+    this.header = header;
+  }
+
+  public TestObjectContainer(Response response) {
+    this.response = response;
+  }
+
+  public TestObjectContainer(Definition schema) {
+    this.schema = schema;
+  }
+
+  public Header getHeader() {
+    return header;
+  }
+
+  public Response getResponse() {
+    return response;
+  }
+
+  public Definition getSchema() {
+    return schema;
+  }
+}
