9738408ecd02a7a9336a3a770be1fe5269a19b3b
Melanie Barker <134556723+melanie-exygy@users.noreply.github.com>
2024-03-27 14:41:18 -0700

    Fix bug where TI "Applying For" banner appeared on add and edit client (#7060)

--- browser-test/src/trusted_intermediary.test.ts
+++ browser-test/src/trusted_intermediary.test.ts
@@ -41,6 +41,10 @@ test.describe('Trusted intermediaries', () => {
     await tiDashboard.expectSuccessAlertOnUpdate()
     await validateScreenshot(page.locator('main'), 'edit-client-success-alert')
 
+    // The 'You are applying for...' banner should only be present when the TI
+    // is actively applying for a client
+    await tiDashboard.expectApplyingForBannerNotPresent()
+
     await page.click('#ti-dashboard-link')
     await waitForPageJsLoad(page)
     await tiDashboard.expectDashboardContainClient(updatedClient)
@@ -61,6 +65,10 @@ test.describe('Trusted intermediaries', () => {
     }
     await page.getByRole('link', {name: 'Add new client'}).click()
     await waitForPageJsLoad(page)
+
+    // The 'You are applying for...' banner should only be present when the TI
+    // is actively applying for a client
+    await tiDashboard.expectApplyingForBannerNotPresent()
 
     await page.fill('#email-input', client.emailAddress)
     await page.fill('#first-name-input', client.firstName)
@@ -380,8 +388,9 @@ test.describe('Trusted intermediaries', () => {
   })
 
   test('ti landing page is the TI Dashboard', async () => {
-    const {page} = ctx
+    const {page, tiDashboard} = ctx
     await loginAsTrustedIntermediary(page)
+    await tiDashboard.expectApplyingForBannerNotPresent()
     await validateScreenshot(page, 'ti')
   })
 
