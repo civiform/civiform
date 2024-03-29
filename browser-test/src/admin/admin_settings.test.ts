d2839317edd1f0c380f6a0a747901c1054695002
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-03-28 18:08:27 -0700

    Migrate tests to use fixtures

--- browser-test/src/admin/admin_settings.test.ts
+++ browser-test/src/admin/admin_settings.test.ts
@@ -1,64 +1,57 @@
-import {test} from '@playwright/test'
-import {
-  createTestContext,
-  loginAsAdmin,
-  validateScreenshot,
-  AdminSettings,
-} from '../support'
+import {test} from '../support/civiform_fixtures'
+import {loginAsAdmin, validateScreenshot} from '../support'
 
-test.describe('Managing system-wide settings', () => {
-  const ctx = createTestContext()
+test.describe(
+  'Managing system-wide settings',
+  {tag: ['@uses-fixtures']},
+  () => {
+    test('Displays the settings page', async ({page, adminSettings}) => {
+      await loginAsAdmin(page)
 
-  test('Displays the settings page', async () => {
-    const {page} = ctx
-    await loginAsAdmin(page)
+      await test.step('Go to admin settings page and take screenshot', async () => {
+        await adminSettings.gotoAdminSettings()
+        await validateScreenshot(page, 'admin-settings-page')
+      })
 
-    const adminSettings = new AdminSettings(page)
-    await adminSettings.gotoAdminSettings()
+      await test.step('Jump to a specific section', async () => {
+        await page.click('a:has-text("Branding")')
+        await validateScreenshot(
+          page,
+          'admin-settings-page-scrolled',
+          /* fullPage= */ false,
+        )
+      })
 
-    await validateScreenshot(page, 'admin-settings-page')
+      await test.step('Set viewport to a narrow width and take screenshot', async () => {
+        // We know the header will start to wrap at smaller widths
+        await page.setViewportSize({
+          width: 768,
+          height: 720,
+        })
 
-    // Jump to a specfific section
-    await page.click('a:has-text("Branding")')
-    await validateScreenshot(
-      page,
-      'admin-settings-page-scrolled',
-      /* fullPage= */ false,
-    )
-  })
+        await page.reload()
 
-  test('Displays the settings page in a narrow viewport', async () => {
-    const {page} = ctx
-
-    // We know the header will start to wrap at smaller widths
-    await page.setViewportSize({
-      width: 768,
-      height: 720,
+        await validateScreenshot(page, 'admin-settings-page-narrow')
+      })
     })
 
-    await loginAsAdmin(page)
-    const adminSettings = new AdminSettings(page)
-    await adminSettings.gotoAdminSettings()
+    test('Updates settings on save', async ({page, adminSettings}) => {
+      await loginAsAdmin(page)
 
-    await validateScreenshot(page, 'admin-settings-page-narrow')
-  })
+      await adminSettings.gotoAdminSettings()
 
-  test('Updates settings on save', async () => {
-    const {page} = ctx
-    await loginAsAdmin(page)
+      await test.step('button check', async () => {
+        await adminSettings.disableSetting('CF_OPTIONAL_QUESTIONS')
+        await adminSettings.saveChanges()
+        await adminSettings.expectDisabled('CF_OPTIONAL_QUESTIONS')
 
-    const adminSettings = new AdminSettings(page)
-    await adminSettings.gotoAdminSettings()
+        await adminSettings.enableSetting('CF_OPTIONAL_QUESTIONS')
+        await adminSettings.saveChanges()
+        await adminSettings.expectEnabled('CF_OPTIONAL_QUESTIONS')
 
-    await adminSettings.disableSetting('CF_OPTIONAL_QUESTIONS')
-    await adminSettings.saveChanges()
-    await adminSettings.expectDisabled('CF_OPTIONAL_QUESTIONS')
-
-    await adminSettings.enableSetting('CF_OPTIONAL_QUESTIONS')
-    await adminSettings.saveChanges()
-    await adminSettings.expectEnabled('CF_OPTIONAL_QUESTIONS')
-
-    await adminSettings.enableSetting('CF_OPTIONAL_QUESTIONS')
-    await adminSettings.saveChanges(/* expectUpdated= */ false)
-  })
-})
+        await adminSettings.enableSetting('CF_OPTIONAL_QUESTIONS')
+        await adminSettings.saveChanges(/* expectUpdated= */ false)
+      })
+    })
+  },
+)
