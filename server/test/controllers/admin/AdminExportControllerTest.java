193e87ccbf16a2007e3306930e00c956123ee0c6
Caitlin Shkuratov <147177873+caitlinshk@users.noreply.github.com>
2024-04-10 17:16:13 -0400

    [Program Migration] Support export and import of program definitions (#7113)

--- server/test/controllers/admin/AdminExportControllerTest.java
+++ server/test/controllers/admin/AdminExportControllerTest.java
@@ -5,19 +5,23 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static play.api.test.CSRFTokenHelper.addCSRFToken;
+import static play.mvc.Http.Status.BAD_REQUEST;
 import static play.mvc.Http.Status.NOT_FOUND;
 import static play.mvc.Http.Status.OK;
+import static play.mvc.Http.Status.SEE_OTHER;
 import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.fakeRequest;
 
 import auth.ProfileUtils;
 import com.google.common.collect.ImmutableMap;
+import models.ProgramModel;
 import org.junit.Before;
 import org.junit.Test;
 import play.data.FormFactory;
 import play.mvc.Result;
 import repository.ResetPostgres;
 import repository.VersionRepository;
+import services.migration.ProgramMigrationService;
 import services.program.ProgramService;
 import services.settings.SettingsManifest;
 import support.ProgramBuilder;
@@ -34,6 +38,7 @@ public class AdminExportControllerTest extends ResetPostgres {
             instanceOf(AdminExportView.class),
             instanceOf(FormFactory.class),
             instanceOf(ProfileUtils.class),
+            instanceOf(ProgramMigrationService.class),
             instanceOf(ProgramService.class),
             mockSettingsManifest,
             instanceOf(VersionRepository.class));
@@ -76,15 +81,48 @@ public class AdminExportControllerTest extends ResetPostgres {
   }
 
   @Test
-  public void exportProgram_migrationEnabled_notFound_hasProgramId() {
+  public void exportProgram_noProgramSelected_redirectsToIndex() {
+    when(mockSettingsManifest.getProgramMigrationEnabled(any())).thenReturn(true);
+
+    Result result = controller.exportProgram(addCSRFToken(fakeRequest()).build());
+
+    assertThat(result.status()).isEqualTo(SEE_OTHER);
+    assertThat(result.redirectLocation()).hasValue(routes.AdminExportController.index().url());
+  }
+
+  @Test
+  public void exportProgram_invalidProgramId_badRequest() {
     when(mockSettingsManifest.getProgramMigrationEnabled(any())).thenReturn(true);
 
     Result result =
         controller.exportProgram(
-            addCSRFToken(fakeRequest().method("POST").bodyForm(ImmutableMap.of("programId", "3")))
+            addCSRFToken(
+                    fakeRequest()
+                        .method("POST")
+                        .bodyForm(ImmutableMap.of("programId", String.valueOf(Long.MAX_VALUE))))
                 .build());
 
-    assertThat(result.status()).isEqualTo(NOT_FOUND);
-    assertThat(contentAsString(result)).contains("Received ID: 3");
+    assertThat(result.status()).isEqualTo(BAD_REQUEST);
+    assertThat(contentAsString(result)).contains("ID " + Long.MAX_VALUE + " could not be found");
+  }
+
+  @Test
+  public void exportProgram_validProgram_downloadsJson() {
+    when(mockSettingsManifest.getProgramMigrationEnabled(any())).thenReturn(true);
+    ProgramModel activeProgram = ProgramBuilder.newActiveProgram("active-program-1").build();
+
+    Result result =
+        controller.exportProgram(
+            addCSRFToken(
+                    fakeRequest()
+                        .method("POST")
+                        .bodyForm(ImmutableMap.of("programId", String.valueOf(activeProgram.id))))
+                .build());
+
+    assertThat(result.status()).isEqualTo(OK);
+    assertThat(result.contentType().get()).isEqualTo("application/json");
+    assertThat(result.header("Content-Disposition")).isPresent();
+    assertThat(result.header("Content-Disposition").get())
+        .startsWith("attachment; filename=\"active-program-1-exported");
   }
 }
