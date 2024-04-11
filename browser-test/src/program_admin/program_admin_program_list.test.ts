7ba3a217eaf0419ec8e947601e4f32315b0c78cd
azhangggggg <annayang@google.com>
2024-04-11 16:54:25 -0700

    add a test without the disabled program feature flag enabled

--- browser-test/src/program_admin/program_admin_program_list.test.ts
+++ browser-test/src/program_admin/program_admin_program_list.test.ts
@@ -11,6 +11,24 @@ import {ProgramVisibility} from '../support/admin_programs'
 
 test.describe('Program admin program list', () => {
   const ctx = createTestContext()
+  test('shows all the programs that are active', async () => {
+    const {page, adminPrograms} = ctx
+
+    await test.step('log in as a CiviForm admin and publish multiple programs', async () => {
+      await loginAsAdmin(page)
+      await adminPrograms.addProgram('Program Name One')
+      await adminPrograms.addProgram('Program Name Two')
+      await adminPrograms.addProgram('Program Name Three')
+      await adminPrograms.publishAllDrafts()
+      await logout(page)
+    })
+
+    await test.step('log in as a program admin and view the program list, verify that all active programs are shown', async () => {
+      await loginAsProgramAdmin(page)
+      await validateScreenshot(page, 'program-admin-program-list')
+    })
+  })
+
   test('shows all the programs that are active, including the program with disabled visibility', async () => {
     const {page, adminPrograms} = ctx
     await enableFeatureFlag(page, 'disabled_visibility_condition_enabled')
