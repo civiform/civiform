30825aa6c4624d5411c6c655a6842bbd8c738aef
azhangggggg <annayang@google.com>
2024-04-11 16:43:06 -0700

    added test back

---
+++ browser-test/src/program_admin/program_admin_program_list.test.ts
@@ -0,0 +1,40 @@
+import {test} from '@playwright/test'
+import {
+  createTestContext,
+  enableFeatureFlag,
+  loginAsAdmin,
+  loginAsProgramAdmin,
+  logout,
+  validateScreenshot,
+} from '../support'
+import {ProgramVisibility} from '../support/admin_programs'
+
+test.describe('Program admin program list', () => {
+  const ctx = createTestContext()
+  test('shows all the programs that are active, including the program with disabled visibility', async () => {
+    const {page, adminPrograms} = ctx
+    await enableFeatureFlag(page, 'disabled_visibility_condition_enabled')
+
+    await test.step('log in as a CiviForm admin and publish multiple programs', async () => {
+      await loginAsAdmin(page)
+      await adminPrograms.addProgram(
+        'Disabled Program Name',
+        'Program Description',
+        'https://usa.gov',
+        ProgramVisibility.DISABLED,
+      )
+      await adminPrograms.addProgram('Program Name Two')
+      await adminPrograms.addProgram('Program Name Three')
+      await adminPrograms.publishAllDrafts()
+      await logout(page)
+    })
+
+    await test.step('log in as a Program admin and view the program list, verify that the disabled program is shown', async () => {
+      await loginAsProgramAdmin(page)
+      await validateScreenshot(
+        page,
+        'program-admin-program-list-visible-disabled-program',
+      )
+    })
+  })
+})
