bf34cb1aad9336e42d8dd8b7675a82af6de083fb
devin-civi <167117500+devin-civi@users.noreply.github.com>
2024-04-29 18:31:04 +0000

    Add and test the ability to log in to north start programs page.

--- server/app/views/applicant/ProgramCardsSectionParamsFactory.java
+++ server/app/views/applicant/ProgramCardsSectionParamsFactory.java
@@ -68,7 +68,7 @@ public final class ProgramCardsSectionParamsFactory {
       ProgramCardParams.Builder cardBuilder = ProgramCardParams.builder();
       ProgramDefinition program = programDatum.program();
 
-      String actionUrl = "~" + applicantRoutes.review(profile, applicantId, program.id()).url();
+      String actionUrl = applicantRoutes.review(profile, applicantId, program.id()).url();
 
       // Note this doesn't yet manage markdown, links and appropriate aria labels for links, and
       // whatever else our current cards do.
