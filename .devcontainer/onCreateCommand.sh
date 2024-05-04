3cf83e1d2280c70903b1ed30d82f5fd3c7780bd0
Austin Lee <avaleske@users.noreply.github.com>
2024-05-04 00:05:03 +0000

    remove debug comment

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -3,8 +3,6 @@
 # DOC: For installing additional packages and configuration in the devcontainer.
 # Runs before updateContentCommand and before user secrets are available in the container.
 
-echo "hello"
-
 # Install tmux
 sudo apt update
 sudo apt install -y tmux
