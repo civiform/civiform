5a80a7693804e4e0ecc591d52a7033436fc8f717
azhangggggg <149536150+azhangggggg@users.noreply.github.com>
2024-05-21 17:45:02 +0000

    deleted blockDisabledProgramAction from ApplicantProgramsController

--- server/app/controllers/applicant/ApplicantProgramsController.java
+++ server/app/controllers/applicant/ApplicantProgramsController.java
@@ -4,7 +4,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static controllers.CallbackController.REDIRECT_TO_SESSION_KEY;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
-import actions.BlockDisabledProgramAction;
 import auth.CiviFormProfile;
 import auth.ProfileUtils;
 import auth.controllers.MissingOptionalException;
@@ -21,7 +20,6 @@ import play.libs.concurrent.ClassLoaderExecutionContext;
 import play.mvc.Http;
 import play.mvc.Http.Request;
 import play.mvc.Result;
-import play.mvc.With;
 import repository.VersionRepository;
 import services.applicant.ApplicantPersonalInfo;
 import services.applicant.ApplicantService;
@@ -141,8 +139,6 @@ public final class ApplicantProgramsController extends CiviFormController {
             });
   }
 
-  @Secure
-  @With(BlockDisabledProgramAction.class)
   public CompletionStage<Result> index(Request request) {
     Optional<Long> applicantId = getApplicantId(request);
     if (applicantId.isEmpty()) {
