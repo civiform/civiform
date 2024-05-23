50d6602ff5e0a4648af61be9beab048b7568f7c8
Rocky Fine <rocky.fine@exygy.com>
2024-05-23 11:46:59 -0400

    added hidden textarea so json is sent

--- server/app/views/admin/migration/AdminExportViewPartial.java
+++ server/app/views/admin/migration/AdminExportViewPartial.java
@@ -48,13 +48,27 @@ public final class AdminExportViewPartial extends BaseHtmlView {
                     .with(makeCsrfTokenInputTag(request))
                     .with(
                         FieldWithLabel.textArea()
-                            .setFieldName(AdminProgramExportForm.PROGRAM_JSON_FIELD)
+                            // we want to set this to disabled to preven the admin from editing the
+                            // json
+                            // but disabled fields aren't included in the form body
+                            // so we need to include a hidden field that the user won't see
+                            // but that will be included in the form body.
                             .setDisabled(true)
                             .setValue(json)
                             .getTextareaTag())
-                    .with(submitButton("Download Json").withClass(ButtonStyles.SOLID_BLUE))
+                    .with(
+                        FieldWithLabel.textArea()
+                            .setFieldName(AdminProgramExportForm.PROGRAM_JSON_FIELD)
+                            .setValue(json)
+                            .getTextareaTag()
+                            .withClass("hidden"))
+                    .with(
+                        div()
+                            .with(
+                                button("Copy Json").withClasses(ButtonStyles.SOLID_BLUE, "mr-2"),
+                                submitButton("Download Json").withClass(ButtonStyles.SOLID_BLUE))
+                            .withClasses("flex"))
                     .withAction(routes.AdminExportController.downloadJson(adminName).url()))
-            .with(button("Copy json").withClass(ButtonStyles.SOLID_BLUE))
             .withClasses("mt-10", "mb-10");
 
     return programDiv;
