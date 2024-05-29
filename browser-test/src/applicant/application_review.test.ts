b344c63dcb7a6bbb23c698933a510fa3ef3bf2cd
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-05-28 22:51:17 -0700

    Update application_review.test.ts

--- browser-test/src/applicant/application_review.test.ts
+++ browser-test/src/applicant/application_review.test.ts
@@ -606,9 +606,12 @@ test.describe('Program admin review of submitted applications', () => {
       expect(csvContentPhoneSearch).not.toContain('threeFirst')
     })
 
-    // Creating a range of a couple days to test the filter. The localtime used in the UI vs the 
+     await test.step('Search by date and validate expected applications are returned', async () => {
+       await adminPrograms.filterProgramApplications({
+  
+    // Creating a range of a couple days to test the filter. The localtime used in the UI vs the
     // UTC time used on the server is difficult to mock correctly without getting too hacky.
-    // This will good enough to make sure the filters work, even if they don't check exact 
+    // This will good enough to make sure the filters work, even if they don't check exact
     // bounds of just today.
     await test.step('Search by date and validate expected applications are returned', async () => {
       await adminPrograms.filterProgramApplications({
