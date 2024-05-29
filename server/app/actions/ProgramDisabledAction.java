fa999815b58a2f88753df5bb5d8410ecc0502b2e
azhangggggg <149536150+azhangggggg@users.noreply.github.com>
2024-05-28 17:05:20 -0700

    [Disabled Programs] disabling program deep link (#7518)

---
+++ server/app/actions/ProgramDisabledAction.java
@@ -0,0 +1,52 @@
+package actions;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import controllers.routes;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import javax.inject.Inject;
+import models.DisplayMode;
+import play.mvc.Action;
+import play.mvc.Http.Request;
+import play.mvc.Result;
+import services.program.ProgramDefinition;
+import services.program.ProgramService;
+
+/**
+ * Action that ensures the program the user is trying to access is not disabled.
+ *
+ * <p>The action will redirect the request to the home page if the the program is disabled.
+ *
+ * <p>
+ */
+public class ProgramDisabledAction extends Action.Simple {
+  private final ProgramService programService;
+
+  @Inject
+  public ProgramDisabledAction(ProgramService programService) {
+    this.programService = checkNotNull(programService);
+  }
+
+  private boolean programIsDisabled(String programSlug) {
+    ProgramDefinition programDefiniton =
+        programService
+            .getActiveFullProgramDefinitionAsync(programSlug)
+            .toCompletableFuture()
+            .join();
+    return programDefiniton.displayMode() == DisplayMode.DISABLED;
+  }
+
+  @Override
+  public CompletionStage<Result> call(Request req) {
+    Optional<String> programSlugOptional = req.flash().get("redirected-from-program-slug");
+
+    if (programSlugOptional.isPresent() && programIsDisabled(programSlugOptional.get())) {
+      // TODO: Build an error page and redirect the user to the error page instead
+      return CompletableFuture.completedFuture(redirect(routes.HomeController.index()));
+    }
+
+    return delegate.call(req); // continute processing next step
+  }
+}
---
+++ server/test/actions/ProgramDisabledActionTest.java
@@ -0,0 +1,113 @@
+package actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import auth.ProgramAcls;
+import com.google.common.collect.ImmutableList;
+import controllers.routes;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import models.DisplayMode;
+import models.ProgramModel;
+import org.junit.Test;
+import play.inject.Injector;
+import play.mvc.Action;
+import play.mvc.Http.Request;
+import play.mvc.Result;
+import play.test.Helpers;
+import play.test.WithApplication;
+import repository.VersionRepository;
+import services.program.BlockDefinition;
+import services.program.ProgramService;
+import services.program.ProgramType;
+
+public class ProgramDisabledActionTest extends WithApplication {
+  private ProgramService ps;
+  private static Injector injector;
+  private static final BlockDefinition EMPTY_FIRST_BLOCK =
+      BlockDefinition.builder()
+          .setId(1)
+          .setName("Screen 1")
+          .setDescription("Screen 1 description")
+          .build();
+
+  @Test
+  public void testProgramSlugIsNotAvailable() {
+    ProgramService programService = instanceOf(ProgramService.class);
+    ProgramDisabledAction action = new ProgramDisabledAction(programService);
+    Request request = Helpers.fakeRequest().build();
+
+    // Set up a mock for the delegate action
+    Action.Simple delegateMock = mock(Action.Simple.class);
+    when(delegateMock.call(request)).thenReturn(CompletableFuture.completedFuture(null));
+    action.delegate = delegateMock;
+    Result result = action.call(request).toCompletableFuture().join();
+
+    // Verify that the delegate action was called
+    assertEquals(null, result);
+  }
+
+  @Test
+  public void testDisabledProgram() {
+    ProgramService programService = instanceOf(ProgramService.class);
+    VersionRepository versionRepository = instanceOf(VersionRepository.class);
+    ProgramDisabledAction action = new ProgramDisabledAction(programService);
+
+    Map<String, String> flashData = new HashMap<>();
+    flashData.put("redirected-from-program-slug", "disabledprogram1");
+    Request request = Helpers.fakeRequest().flash(flashData).build();
+    ProgramModel program =
+        new ProgramModel(
+            /* adminName */ "disabledprogram1",
+            /* adminDescription */ "description for a disabled program ",
+            /* defaultDisplayName */ "disabled program",
+            /* defaultDisplayDescription */ "description",
+            /* defaultConfirmationMessage */ "",
+            /* externalLink */ "",
+            /* displayMode */ DisplayMode.DISABLED.getValue(),
+            /* blockDefinitions */ ImmutableList.of(EMPTY_FIRST_BLOCK),
+            /* associatedVersion */ versionRepository.getActiveVersion(),
+            /* programType */ ProgramType.DEFAULT,
+            /* eligibilityIsGating= */ true,
+            /* ProgramAcls */ new ProgramAcls());
+    program.save();
+
+    Result result = action.call(request).toCompletableFuture().join();
+    assertEquals(result.redirectLocation().get(), routes.HomeController.index().url());
+  }
+
+  @Test
+  public void testNonDisabledProgram() {
+    ProgramService programService = instanceOf(ProgramService.class);
+    VersionRepository versionRepository = instanceOf(VersionRepository.class);
+    ProgramDisabledAction action = new ProgramDisabledAction(programService);
+    Map<String, String> flashData = new HashMap<>();
+    flashData.put("redirected-from-program-slug", "publicprogram1");
+    Request request = Helpers.fakeRequest().flash(flashData).build();
+    ProgramModel program =
+        new ProgramModel(
+            /* adminName */ "publicprogram1",
+            /* adminDescription */ "description for a public visibile program",
+            /* defaultDisplayName */ "public program",
+            /* defaultDisplayDescription */ "description",
+            /* defaultConfirmationMessage */ "",
+            /* externalLink */ "",
+            /* displayMode */ DisplayMode.PUBLIC.getValue(),
+            /* blockDefinitions */ ImmutableList.of(EMPTY_FIRST_BLOCK),
+            /* associatedVersion */ versionRepository.getActiveVersion(),
+            /* programType */ ProgramType.DEFAULT,
+            /* eligibilityIsGating= */ true,
+            /* ProgramAcls */ new ProgramAcls());
+    program.save();
+
+    Action.Simple delegateMock = mock(Action.Simple.class);
+    when(delegateMock.call(request)).thenReturn(CompletableFuture.completedFuture(null));
+    action.delegate = delegateMock;
+
+    Result result = action.call(request).toCompletableFuture().join();
+    assertEquals(null, result);
+  }
+}
