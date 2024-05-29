19bb18e82246863ad8e6906a23158cbf5d8ced2e
Don Sandbekkhaug <ssandbekkhaug@google.com>
2024-05-29 17:03:04 +0000

    Delete unnecessary parts of test setup

--- browser-test/src/applicant/northstar_upsell.test.ts
+++ browser-test/src/applicant/northstar_upsell.test.ts
@@ -9,31 +9,26 @@ import {
 
 test.describe('Upsell tests', {tag: ['@northstar']}, () => {
   const programName = 'Sample program'
-  // const approvedStatusName = 'Approved'
 
-  test.beforeEach(
-    async ({page, adminPrograms, adminProgramStatuses, applicantQuestions}) => {
-      await loginAsAdmin(page)
+  test.beforeEach(async ({page, adminPrograms, applicantQuestions}) => {
+    await loginAsAdmin(page)
 
-      await test.step('Setup: Publish program as admin', async () => {
-        await adminPrograms.addProgram(programName)
-        await adminPrograms.gotoDraftProgramManageStatusesPage(programName)
-        // await adminProgramStatuses.createStatus(approvedStatusName)
-        await adminPrograms.publishProgram(programName)
-        await adminPrograms.expectActiveProgram(programName)
-        await logout(page)
-      })
+    await test.step('Setup: Publish program as admin', async () => {
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.publishProgram(programName)
+      await adminPrograms.expectActiveProgram(programName)
+      await logout(page)
+    })
 
-      await loginAsTestUser(page)
+    await loginAsTestUser(page)
 
-      await enableFeatureFlag(page, 'north_star_applicant_ui')
+    await enableFeatureFlag(page, 'north_star_applicant_ui')
 
-      await test.step('Setup: submit application', async () => {
-        await applicantQuestions.clickApplyProgramButton(programName)
-        await applicantQuestions.submitFromReviewPage(true)
-      })
-    },
-  )
+    await test.step('Setup: submit application', async () => {
+      await applicantQuestions.clickApplyProgramButton(programName)
+      await applicantQuestions.submitFromReviewPage(true)
+    })
+  })
 
   test('view application submitted page', async ({page}) => {
     expect(await page.textContent('html')).toContain('Application confirmation')
