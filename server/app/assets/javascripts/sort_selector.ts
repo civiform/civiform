152e47666e20ec0cde7710e4868855f745ca12c9
jcma-google <99992917+jcma-google@users.noreply.github.com>
2024-05-30 17:14:07 +0000

    format

--- server/app/assets/javascripts/sort_selector.ts
+++ server/app/assets/javascripts/sort_selector.ts
@@ -1,7 +1,7 @@
 /**
- * Responsible for controlling the sorting mechanism used by QuestionBankController. Elements (tagged with elementSelector class)
- * have data attributes with the value to be compared and will sort list using the value. The data attribute is defined
- * in QuestionSortOption.
+ * Responsible for controlling the sorting mechanism used by QuestionBankController and TrustedIntermediaryController. 
+ * Elements (tagged with elementSelector class) have data attributes with the value to be compared and will sort 
+ * list using the value. The data attribute is defined in QuestionSortOption.
  *
  * E.g. Sort by Name A-Z will look up the value for data attribute tiname-asc and sort the list by that value.
  *
--- server/app/views/components/QuestionSortOption.java
+++ server/app/views/components/QuestionSortOption.java
@@ -29,29 +29,22 @@ public enum QuestionSortOption {
       /* displayStringDescending= */ Optional.of("Most members"));
 
   // Suffix added to the sort option value to indicate if sorting should be in
-  // ascending or
-  // descending order. The full value will be in the format
-  // "<data_attribute_name>-<asc|desc>", e.g.
-  // "adminname-desc".
+  // ascending or descending order. The full value will be in the format
+  // "<data_attribute_name>-<asc|desc>", e.g. "adminname-desc".
   private static final String ASCENDING_SUFFIX = "asc";
   private static final String DESCENDING_SUFFIX = "desc";
 
   // Name of the HTML data attribute for this sorting option. This is used as part
-  // of the value for
-  // the select tag option along with the ascending or descending suffix. It
-  // should also be used as
-  // the data attribute name in the HTML that renders the question. The value of
-  // the data attribute
-  // should contain the actual data to sort on.
+  // of the value for the select tag option along with the ascending or descending suffix. It
+  // should also be used as the data attribute name in the HTML that renders the question. The value of
+  // the data attribute should contain the actual data to sort on.
   private final String dataAttribute;
   // Display string for the select tag option for the option that sorts by this
-  // attribute in
-  // descending order. Not present if this attribute should not have a descending
+  // attribute in descending order. Not present if this attribute should not have a descending
   // option.
   private final Optional<String> displayStringDescending;
   // Display string for the select tag option for the option that sorts by this
-  // attribute in
-  // ascending order. Not present if this attribute should not have an ascending
+  // attribute in ascending order. Not present if this attribute should not have an ascending
   // option.
   private final Optional<String> displayStringAscending;
 
