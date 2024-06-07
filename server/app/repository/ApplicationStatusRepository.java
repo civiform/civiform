1ffea71a1a8235e16085784101b4598588fa40c4
swatkat1 <swathi.murthi@seattle.gov>
2024-06-07 09:55:57 -0700

    adding tests

--- server/app/repository/ApplicationStatusRepository.java
+++ server/app/repository/ApplicationStatusRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 import io.ebean.DB;
 import io.ebean.Database;
+import java.util.List;
 import java.util.Optional;
 import javax.inject.Inject;
 import models.ApplicationStatusesModel;
@@ -25,5 +26,16 @@ public final class ApplicationStatusRepository {
         .and()
         .eq("status_lifecycle_stage", StatusLifecycleStage.ACTIVE)
         .findOneOrEmpty();
+  }
+
+  public List<ApplicationStatusesModel> lookupAllObsoleteStatuses(String programName) {
+    return database
+        .find(ApplicationStatusesModel.class)
+        .setLabel("ApplicationStatusesModel.findByProgramName")
+        .where()
+        .eq("program_name", programName)
+        .and()
+        .eq("status_lifecycle_stage", StatusLifecycleStage.OBSOLETE)
+        .findList();
   }
 }
--- server/test/models/ApplicationStatusesModelTest.java
+++ server/test/models/ApplicationStatusesModelTest.java
@@ -3,7 +3,9 @@ package models;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 import repository.ApplicationStatusRepository;
@@ -27,37 +29,50 @@ public class ApplicationStatusesModelTest extends ResetPostgres {
 
   @Test
   public void canAddActiveApplicationStatuses() {
-    ProgramModel program = ProgramBuilder.newActiveProgram("test program", "description").build();
+    Long uniqueProgramId = new Random().nextLong();
+    ProgramModel program =
+        ProgramBuilder.newActiveProgram("Obsolete tests" + uniqueProgramId, "description").build();
     String programName = program.getProgramDefinition().adminName();
     StatusDefinitions statusDefinitions = new StatusDefinitions(ImmutableList.of(APPROVED_STATUS));
     ApplicationStatusesModel applicationStatusesModel =
         new ApplicationStatusesModel(programName, statusDefinitions, StatusLifecycleStage.ACTIVE);
+    applicationStatusesModel.save();
     applicationStatusesModel.setCreateTimeForTest("2041-01-01T00:00:00Z").save();
     Optional<ApplicationStatusesModel> mayBeStatus = repo.lookupActiveStatus(programName);
     assertThat(mayBeStatus).isNotEmpty();
     ApplicationStatusesModel applicationStatusesModel1 = mayBeStatus.get();
-    assertThat(applicationStatusesModel1.getCreateTime()).isEqualTo("2041-01-01T00:00:00Z");
-    assertThat(applicationStatusesModel1.getStatusDefinitions()).isEqualTo(statusDefinitions);
-    assertThat(applicationStatusesModel1.getProgramName()).isEqualTo(programName);
-    assertThat(applicationStatusesModel1.getStatusLifecycleStage())
-        .isEqualTo(StatusLifecycleStage.ACTIVE);
+    checkApplicationStatusRow(
+        applicationStatusesModel1, programName, "Approved", StatusLifecycleStage.ACTIVE);
   }
 
   @Test
   public void canAddOboseleteApplicationStatuses() {
-    ProgramModel program = ProgramBuilder.newActiveProgram("test program", "description").build();
+    Long uniqueProgramId = new Random().nextLong();
+    ProgramModel program =
+        ProgramBuilder.newActiveProgram("test program" + uniqueProgramId, "description").build();
     String programName = program.getProgramDefinition().adminName();
     StatusDefinitions statusDefinitions = new StatusDefinitions(ImmutableList.of(APPROVED_STATUS));
     ApplicationStatusesModel applicationStatusesModel =
         new ApplicationStatusesModel(programName, statusDefinitions, StatusLifecycleStage.OBSOLETE);
+    applicationStatusesModel.save();
     applicationStatusesModel.setCreateTimeForTest("2041-01-01T00:00:00Z").save();
-    Optional<ApplicationStatusesModel> mayBeStatus = repo.lookupActiveStatus(programName);
-    assertThat(mayBeStatus).isNotEmpty();
-    ApplicationStatusesModel applicationStatusesModel1 = mayBeStatus.get();
-    assertThat(applicationStatusesModel1.getCreateTime()).isEqualTo("2041-01-01T00:00:00Z");
-    assertThat(applicationStatusesModel1.getStatusDefinitions()).isEqualTo(statusDefinitions);
-    assertThat(applicationStatusesModel1.getProgramName()).isEqualTo(programName);
-    assertThat(applicationStatusesModel1.getStatusLifecycleStage())
-        .isEqualTo(StatusLifecycleStage.OBSOLETE);
+    List<ApplicationStatusesModel> mayBeStatuses = repo.lookupAllObsoleteStatuses(programName);
+    assertThat(mayBeStatuses).isNotEmpty();
+    assertThat(mayBeStatuses.size()).isEqualTo(1);
+    ApplicationStatusesModel applicationStatusesModel1 = mayBeStatuses.get(0);
+    checkApplicationStatusRow(
+        applicationStatusesModel1, programName, "Approved", StatusLifecycleStage.OBSOLETE);
+  }
+
+  public void checkApplicationStatusRow(
+      ApplicationStatusesModel applicationStatusesModel,
+      String programName,
+      String statusText,
+      StatusLifecycleStage statusLifecycleStage) {
+    assertThat(applicationStatusesModel.getCreateTime()).isEqualTo("2041-01-01T00:00:00Z");
+    assertThat(applicationStatusesModel.getStatusDefinitions().getStatuses().get(0).statusText())
+        .isEqualTo(statusText);
+    assertThat(applicationStatusesModel.getProgramName()).isEqualTo(programName);
+    assertThat(applicationStatusesModel.getStatusLifecycleStage()).isEqualTo(statusLifecycleStage);
   }
 }
