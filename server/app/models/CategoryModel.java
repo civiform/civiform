1a4c21c55a3062244c5851161276f1d33d423660
Melanie Barker <melanie.barker@exygy.com>
2024-06-12 16:03:39 -0700

    Creating Category model and repository

---
+++ server/app/models/CategoryModel.java
@@ -0,0 +1,80 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.ebean.annotation.DbJsonB;
+import io.ebean.annotation.WhenCreated;
+import io.ebean.annotation.WhenModified;
+import java.time.Instant;
+import java.util.List;
+import java.util.Locale;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import play.data.validation.Constraints;
+import services.LocalizedStrings;
+
+/**
+ * An EBean mapped entity representing a category of programs. For example, "Health" or "Education".
+ *
+ * <p>A program can have many categories and a category can be assigned to many programs. Each
+ * category contains a {@code LocalizedStrings} object with the translations of its name.
+ */
+@Entity
+@Table(name = "categories")
+public class CategoryModel extends BaseModel {
+
+  @WhenCreated private Instant createTime;
+
+  @WhenModified private Instant lastModifiedTime;
+
+  @DbJsonB @Constraints.Required private LocalizedStrings localizedName;
+
+  @Constraints.Required private LifecycleStage lifecycleStage;
+
+  @ManyToMany(mappedBy = "categories")
+  @JoinTable(
+      name = "programs_categories",
+      joinColumns = @JoinColumn(name = "categories_id"),
+      inverseJoinColumns = @JoinColumn(name = "programs_id"))
+  private List<ProgramModel> programs;
+
+  public CategoryModel(ImmutableMap<Locale, String> translations) {
+    this.localizedName = LocalizedStrings.create(translations);
+    this.lifecycleStage = LifecycleStage.ACTIVE;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getDefaultName() {
+    return localizedName.getDefault();
+  }
+
+  @JsonIgnore
+  public LocalizedStrings getLocalizedName() {
+    return localizedName;
+  }
+
+  public Instant getCreateTime() {
+    return createTime;
+  }
+
+  public Instant getLastModifiedTime() {
+    return lastModifiedTime;
+  }
+
+  @JsonIgnore
+  public LifecycleStage getLifecycleStage() {
+    return lifecycleStage;
+  }
+
+  @JsonIgnore
+  public ImmutableList<ProgramModel> getPrograms() {
+    return ImmutableList.copyOf(programs);
+  }
+}
---
+++ server/app/repository/CategoryRepository.java
@@ -0,0 +1,60 @@
+package repository;
+
+import com.google.common.collect.ImmutableList;
+import io.ebean.DB;
+import io.ebean.Database;
+import javax.inject.Inject;
+import models.CategoryModel;
+
+public final class CategoryRepository {
+
+  private static final QueryProfileLocationBuilder queryProfileLocationBuilder =
+      new QueryProfileLocationBuilder("CategoryRepository");
+
+  private final Database database;
+
+  @Inject
+  public CategoryRepository() {
+    this.database = DB.getDefault();
+  }
+
+  public ImmutableList<CategoryModel> findCategoriesByIds(ImmutableList<Long> ids) {
+    return ImmutableList.copyOf(
+        database
+            .find(CategoryModel.class)
+            .setLabel("CategoryModel.findByIds")
+            .setProfileLocation(queryProfileLocationBuilder.create("findCategoriesByIds"))
+            .where()
+            .idIn(ids)
+            .findList());
+  }
+
+  public ImmutableList<CategoryModel> listCategories() {
+    return ImmutableList.copyOf(
+        database
+            .find(CategoryModel.class)
+            .setLabel("CategoryModel.list")
+            .setProfileLocation(queryProfileLocationBuilder.create("listCategories"))
+            .findList());
+  }
+
+  public CategoryModel fetchOrInsertUniqueCategory(CategoryModel category) {
+    CategoryModel existing =
+        database
+            .find(CategoryModel.class)
+            .where()
+            .jsonEqualTo("localizedName", "translations.en_US", category.getDefaultName())
+            .findOne();
+    if (existing != null) {
+      return existing;
+    }
+    return insertCategory(category);
+  }
+
+  private CategoryModel insertCategory(CategoryModel category) {
+    category.id = null;
+    database.insert(category);
+    category.refresh();
+    return category;
+  }
+}
