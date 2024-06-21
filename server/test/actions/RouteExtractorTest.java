295fc51e5c8cabb8fbb19da174f58528b8c87e8b
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-06-21 10:26:30 -0700

    Adding route extractor (#7842)

---
+++ server/test/actions/RouteExtractorTest.java
@@ -0,0 +1,37 @@
+package actions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.Test;
+
+public final class RouteExtractorTest {
+  @Test
+  public void successfully_extracts_route_parameters() {
+    String routePattern = "/programs/$programId<[^/]+>/blocks/$blockId<[^/]+>/edit";
+    String path = "/programs/1/blocks/2/edit";
+    RouteExtractor routeExtractor = new RouteExtractor(routePattern, path);
+
+    assertThat(routeExtractor.size()).isEqualTo(2);
+    assertThat(routeExtractor.getParamLongValue("programId")).isEqualTo(1);
+    assertThat(routeExtractor.getParamLongValue("blockId")).isEqualTo(2);
+  }
+
+  @Test
+  public void has_no_route_parameters_configured() {
+    String routePattern = "/programs/path/edit";
+    String path = "/programs/path/edit";
+    RouteExtractor routeExtractor = new RouteExtractor(routePattern, path);
+
+    assertThat(routeExtractor.size()).isEqualTo(0);
+  }
+
+  @Test
+  public void asking_for_route_parameter_that_does_not_exist() {
+    String routePattern = "/programs/$programId<[^/]+>/blocks/$blockId<[^/]+>/edit";
+    String path = "/programs/1/blocks/2/edit";
+    RouteExtractor routeExtractor = new RouteExtractor(routePattern, path);
+
+    assertThatThrownBy(() -> routeExtractor.getParamLongValue("unexpectedId"));
+  }
+}
