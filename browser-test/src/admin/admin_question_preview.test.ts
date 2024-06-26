687a0066ca75ee59a1ebddb4b8cbb83554d7df01
Don Sandbekkhaug <ssandbekkhaug@google.com>
2024-06-26 15:14:37 +0000

    North Star question previews

--- browser-test/src/admin/admin_question_preview.test.ts
+++ browser-test/src/admin/admin_question_preview.test.ts
@@ -1,18 +1,165 @@
 import {expect, test} from '../support/civiform_fixtures'
-import {loginAsAdmin} from '../support'
+import {enableFeatureFlag, loginAsAdmin, validateScreenshot} from '../support'
 
-test.describe('Admin question preview', () => {
+test.describe('Admin question preview', {tag: ['@northstar']}, () => {
   const questionName = 'test-question'
+
+  test.beforeEach(async ({page}) => {
+    await loginAsAdmin(page)
+    await enableFeatureFlag(page, 'north_star_applicant_ui')
+  })
+
+  test('Preview address question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addAddressQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      // The address question needs extra time to render
+      await page.waitForSelector('[data-load-question="true"]')
+
+      await validateScreenshot(page, 'north-star-address-question')
+    })
+  })
+
+  // TODO(#7891): Support checkbox question previews
+
+  test('Preview currency question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addCurrencyQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-currency-question')
+    })
+  })
+
+  test('Preview date question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addDateQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-date-question')
+    })
+  })
+
+  test('Preview dropdown question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addDropdownQuestion({
+        questionName: questionName,
+        options: [
+          {adminName: 'a', text: 'a'},
+          {adminName: 'b', text: 'b'},
+          {adminName: 'c', text: 'c'},
+        ],
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-dropdown-question')
+    })
+  })
+
+  test('Preview email question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addEmailQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-email-question')
+    })
+  })
+
+  // TODO(#7859): Fully support enumerator question previews
+  test('Preview enumerator question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addEnumeratorQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-enumerator-question')
+    })
+  })
+
+  // TODO(#7849): Support file upload question previews
+
+  test('Preview ID question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addIdQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-id-question')
+    })
+  })
+
+  test('Preview name question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addNameQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-name-question')
+    })
+  })
+
+  test('Preview number question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addNumberQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-number-question')
+    })
+  })
 
   test('Edit existing radio button question options', async ({
     page,
     adminQuestions,
   }) => {
-    await test.step('Setup: add radio button question', async () => {
-      await loginAsAdmin(page)
+    await test.step('Create question', async () => {
       await adminQuestions.addRadioButtonQuestion({
         questionName: questionName,
-        options: [{adminName: 'a', text: 'Old Option'}],
+        options: [
+          {adminName: 'a', text: 'Old Option'},
+          {adminName: 'b', text: 'b'},
+          {adminName: 'c', text: 'c'},
+        ],
       })
     })
 
@@ -28,6 +175,50 @@ test.describe('Admin question preview', () => {
 
       const previewDiv = page.locator('#sample-question')
       await expect(previewDiv.locator('text=New Option')).toBeVisible()
+
+      await validateScreenshot(page, 'north-star-radio-question')
+    })
+  })
+
+  test('Preview static text', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addStaticQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-static-question')
+    })
+  })
+
+  test('Preview phone number question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addPhoneQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-phone-question')
+    })
+  })
+
+  test('Preview text question', async ({page, adminQuestions}) => {
+    await test.step('Create question', async () => {
+      await adminQuestions.addTextQuestion({
+        questionName: questionName,
+      })
+    })
+
+    await test.step('Expect preview renders properly', async () => {
+      await adminQuestions.gotoQuestionEditPage(questionName)
+
+      await validateScreenshot(page, 'north-star-text-question')
     })
   })
 })
---
+++ server/app/controllers/admin/NorthStarQuestionPreviewController.java
@@ -0,0 +1,74 @@
+package controllers.admin;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import auth.CiviFormProfile;
+import auth.ProfileUtils;
+import com.google.common.collect.ImmutableList;
+import controllers.CiviFormController;
+import java.util.Optional;
+import javax.inject.Inject;
+import org.pac4j.play.java.Secure;
+import play.i18n.Lang;
+import play.i18n.Messages;
+import play.i18n.MessagesApi;
+import play.mvc.Http;
+import play.mvc.Http.Request;
+import play.mvc.Result;
+import repository.VersionRepository;
+import services.applicant.ApplicantPersonalInfo;
+import services.applicant.ApplicantPersonalInfo.Representation;
+import services.question.exceptions.InvalidQuestionTypeException;
+import services.question.types.QuestionType;
+import services.settings.SettingsManifest;
+import views.admin.questions.NorthStarQuestionPreview;
+
+/** Controller for rendering inputs for questions. */
+public final class NorthStarQuestionPreviewController extends CiviFormController {
+  private final NorthStarQuestionPreview northStarQuestionPreview;
+  private final Messages messages;
+  private final SettingsManifest settingsManifest;
+
+  @Inject
+  public NorthStarQuestionPreviewController(
+      ProfileUtils profileUtils,
+      VersionRepository versionRepository,
+      NorthStarQuestionPreview northStarQuestionPreview,
+      MessagesApi messagesApi,
+      SettingsManifest settingsManifest) {
+    super(profileUtils, versionRepository);
+    this.northStarQuestionPreview = checkNotNull(northStarQuestionPreview);
+    this.messages = messagesApi.preferred(ImmutableList.of(Lang.defaultLang()));
+    this.settingsManifest = checkNotNull(settingsManifest);
+  }
+
+  @Secure
+  public Result sampleQuestion(Request request, String questionType) {
+    if (!settingsManifest.getNorthStarApplicantUi(request)) {
+      return notFound();
+    }
+
+    Representation representation = Representation.builder().build();
+    ApplicantPersonalInfo api = ApplicantPersonalInfo.ofGuestUser(representation);
+    Optional<CiviFormProfile> profile = profileUtils.currentUserProfile(request);
+
+    QuestionType questionTypeEnum;
+    try {
+      questionTypeEnum = QuestionType.fromLabel(questionType);
+    } catch (InvalidQuestionTypeException e) {
+      return badRequest("Invalid question type: " + questionType);
+    }
+
+    NorthStarQuestionPreview.Params params =
+        NorthStarQuestionPreview.Params.builder()
+            .setRequest(request)
+            .setApplicantId(0l)
+            .setApplicantPersonalInfo(api)
+            .setProfile(profile.orElse(null))
+            .setType(questionTypeEnum)
+            .setMessages(messages)
+            .build();
+    String content = northStarQuestionPreview.render(params);
+    return ok(content).as(Http.MimeTypes.HTML);
+  }
+}
