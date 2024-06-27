f9bc3d4b44667db2309a757050d44e0eb5b2166d
swatkat1 <swathi.murthi@seattle.gov>
2024-06-27 11:18:22 -0700

    reordering

--- server/conf/evolutions/default/70.sql
+++ server/conf/evolutions/default/70.sql
@@ -1,15 +1,5 @@
+-- Migrate statuses data both from obsolete and active programs to the Application_statuses table
 # --- !Ups
-# -- Migrate Status data both from active and obsolete program state from programs table to the Application_statuses table
-INSERT INTO application_statuses (program_name,status_definitions,status_definitions_lifecycle_stage,create_time)
-SELECT
-  p.name,
-  p.status_definitions,
-  'active' AS status_definitions_lifecycle_stage,
-  p.create_time
-FROM programs p
-       INNER JOIN versions_programs vp ON vp.programs_id = p.id
-       INNER JOIN versions v ON vp.versions_id = v.id
-WHERE v.lifecycle_stage IN ('active');
 
 INSERT INTO application_statuses (program_name,status_definitions,status_definitions_lifecycle_stage)
 SELECT
@@ -20,5 +10,16 @@ FROM programs p
        INNER JOIN versions_programs vp ON vp.programs_id = p.id
        INNER JOIN versions v ON vp.versions_id = v.id
 WHERE v.lifecycle_stage IN ('obsolete');
+
+INSERT INTO application_statuses (program_name,status_definitions,status_definitions_lifecycle_stage,create_time)
+SELECT
+  p.name,
+  p.status_definitions,
+  'active' AS status_definitions_lifecycle_stage,
+  p.create_time
+FROM programs p
+       INNER JOIN versions_programs vp ON vp.programs_id = p.id
+       INNER JOIN versions v ON vp.versions_id = v.id
+WHERE v.lifecycle_stage IN ('active');
 
 # --- !Downs
