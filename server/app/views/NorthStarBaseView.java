070ca7af64973d2ecde840b6a45621634255433e
Don Sandbekkhaug <ssandbekkhaug@google.com>
2024-07-10 12:44:31 -0500

    Separate SCSS files for North Star (#8010)

--- server/app/views/NorthStarBaseView.java
+++ server/app/views/NorthStarBaseView.java
@@ -58,6 +58,7 @@ public abstract class NorthStarBaseView {
     ThymeleafModule.PlayThymeleafContext context = playThymeleafContextFactory.create(request);
     context.setVariable("tailwindStylesheet", assetsFinder.path("stylesheets/tailwind.css"));
     context.setVariable("uswdsStylesheet", assetsFinder.path("dist/uswds.min.css"));
+    context.setVariable("northStarStylesheet", assetsFinder.path("dist/uswds_northstar.min.css"));
     context.setVariable("applicantJsBundle", assetsFinder.path("dist/applicant.bundle.js"));
     context.setVariable("uswdsJsInit", assetsFinder.path("javascripts/uswds/uswds-init.min.js"));
     context.setVariable("uswdsJsBundle", assetsFinder.path("dist/uswds.bundle.js"));
