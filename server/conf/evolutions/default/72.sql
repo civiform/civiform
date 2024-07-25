99a6632c22a6a1f718515e6193fac38c097cb4ce
azhangggggg <149536150+azhangggggg@users.noreply.github.com>
2024-07-25 17:46:32 +0000

    add suffix as a column in applicant table

--- server/conf/evolutions/default/72.sql
+++ server/conf/evolutions/default/72.sql
@@ -1,11 +1,7 @@
 # --- Add suffix column to applicants table
 
 # --- !Ups
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS btree_gin;
 ALTER TABLE applicants add column IF NOT EXISTS suffix varchar;
 
 # --- !Downs
 ALTER TABLE applicants DROP column IF EXISTS suffix;
-DROP EXTENSION IF EXISTS pg_trgm;
-DROP EXTENSION IF EXISTS btree_gin;
