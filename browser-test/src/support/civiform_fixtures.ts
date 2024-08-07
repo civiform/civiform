08cfa6d4eb0ed494e6316d5d2bd1d59e500e5418
jeffpw-goog <165937506+jeffpw-goog@users.noreply.github.com>
2024-08-07 18:21:18 +0000

    Fail browser tests on any CSP error

--- browser-test/src/support/civiform_fixtures.ts
+++ browser-test/src/support/civiform_fixtures.ts
@@ -86,6 +86,12 @@ export const test = base.extend<CiviformFixtures>({
   },
 
   page: async ({page, request}, use) => {
+    page.on('console', (msg) => {
+      if (msg.text().includes('Content Security Policy')) {
+        throw new Error(msg.text())
+      }
+    })
+
     // BeforeEach
     await test.step('Clear database', async () => {
       await request.post('/dev/seed/clear')
