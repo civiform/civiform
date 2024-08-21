8c61feada20fb7f02a0de668bbbb7677a8615aa5
Devin Taylor <167117500+devin-civi@users.noreply.github.com>
2024-08-21 14:15:50 -0700

    [MultipleFileUpload] Avoid duplicate file names (#8340)

--- server/app/assets/javascripts/file_upload.ts
+++ server/app/assets/javascripts/file_upload.ts
@@ -2,6 +2,13 @@ import {addEventListenerToElements, assertNotNull} from './util'
 import {isFileTooLarge} from './file_upload_util'
 
 const UPLOAD_ATTR = 'data-upload-text'
+const UPLOADED_FILE_ATTR = 'data-uploaded-files'
+// Matches a file name with a number "-<number>" at the end. For example "file-2.png"
+// Groups are: [1] The file name [2] The "-<number>" [3] - The file type, if it exists (e.g. .png), null otherwise.
+const FILE_NAME_DIGIT_SUFFIX_REGEX = /(.*)(-\d*)(\..*)?$/
+// Matches a file name with a file type at the end.
+// Groups are [1] The file name [2] The file type.
+const FILE_NAME_REGEX = /(.*)(\..*)$/
 
 export function init() {
   // Don't add extra logic if we don't have a block form with a
@@ -53,6 +60,70 @@ export function init() {
       blockForm.submit()
     }
   })
+
+  const uploadedFilesAttribute = blockForm
+    .querySelector(`[${UPLOADED_FILE_ATTR}]`)
+    ?.getAttribute(UPLOADED_FILE_ATTR)
+
+  if (uploadedFilesAttribute) {
+    const uploadedFilesArray = JSON.parse(uploadedFilesAttribute) as string[]
+    blockForm.addEventListener('formdata', (event) => {
+      const formData = event.formData
+      const file = formData.get('file') as File
+
+      const newName = getUniqueName(file.name, uploadedFilesArray)
+      if (file.name != newName) {
+        // Rename uploaded file, if a file with the same name has already been uploaded.
+        formData.delete('file')
+        formData.append('file', file, newName)
+      }
+    })
+  }
+}
+
+/**
+ * Returns a unique name.
+ *
+ * Note: This is only exported so we can test it. It should be considered private.
+ *
+ * @param name The name of the file which must be unique.
+ * @param existingNames Array of existing names.
+ * @returns unique name, based on the input name, which doesn't match any of the existing names. It does
+ * this by appending a "-2", before the file type, or if a number already exists, a -"n+1".
+ */
+export function getUniqueName(name: string, existingNames: string[]) {
+  while (existingNames.includes(name)) {
+    const fileNameWithDigitSuffixMatch = name.match(
+      FILE_NAME_DIGIT_SUFFIX_REGEX,
+    )
+    let numberToAppend = 2
+    if (fileNameWithDigitSuffixMatch) {
+      // File name already has a digit suffix. Increment that instead.
+      numberToAppend =
+        parseInt(fileNameWithDigitSuffixMatch[2].substring(1)) + 1
+      // Put number before the file type ('.png') suffix if the file type is present ([3] is not null)
+      name =
+        fileNameWithDigitSuffixMatch[1] +
+        '-' +
+        numberToAppend +
+        (fileNameWithDigitSuffixMatch[3] ?? '')
+    } else {
+      const fileNameWithoutDigitMatch = name.match(FILE_NAME_REGEX)
+      if (fileNameWithoutDigitMatch) {
+        // Put number before the file type ('.png').
+        name =
+          fileNameWithoutDigitMatch[1] +
+          '-' +
+          numberToAppend +
+          fileNameWithoutDigitMatch[2]
+      } else {
+        // Otherwise, just add it to the end.
+        name += '-' + numberToAppend
+      }
+    }
+  }
+
+  return name
 }
 
 function onActionButtonClicked(e: Event, blockForm: Element) {
