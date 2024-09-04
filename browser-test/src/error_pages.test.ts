a69edfea42bba99b1f9145a2c0d2e75beca51446
Rocky Fine <rocky.fine@exygy.com>
2024-09-04 16:29:54 -0400

    added tests

--- browser-test/src/error_pages.test.ts
+++ browser-test/src/error_pages.test.ts
@@ -1,5 +1,11 @@
 import {test, expect} from './support/civiform_fixtures'
-import {selectApplicantLanguage} from './support'
+import {
+  loginAsAdmin,
+  selectApplicantLanguage,
+  validateAccessibility,
+  validateScreenshot,
+  waitForPageJsLoad,
+} from './support'
 
 test.describe('Error pages', {tag: ['@parallel-candidate']}, () => {
   test('404 page', async ({page}) => {
@@ -22,6 +28,45 @@ test.describe('Error pages', {tag: ['@parallel-candidate']}, () => {
       await expect(
         page.getByRole('heading', {
           name: 'No Pudimos encontrar la página que intentó visitar',
+        }),
+      ).toBeAttached()
+    })
+  })
+
+  test('server error page', async ({page, adminSettings}) => {
+    await test.step('Server error page is shown', async () => {
+      await page.goto('/error?exceptionId=1')
+      await validateScreenshot(page, 'server-error-page')
+      await validateAccessibility(page)
+    })
+
+    await test.step('Home button takes you to the homepage', async () => {
+      await page
+        .getByRole('link', {
+          name: 'Visit homepage',
+        })
+        .click()
+      await waitForPageJsLoad(page)
+      await expect(page).toHaveURL(/.*programs/)
+      await expect(
+        page.getByRole('heading', {
+          name: 'Save time applying for programs and services',
+        }),
+      ).toBeAttached()
+    })
+
+    await test.step('Updating the support email address updates the error page', async () => {
+      await loginAsAdmin(page)
+      await adminSettings.gotoAdminSettings()
+      await adminSettings.setStringSetting(
+        'SUPPORT_EMAIL_ADDRESS',
+        'help@email.com',
+      )
+      await adminSettings.saveChanges()
+      await page.goto('/error?exceptionId=1')
+      await expect(
+        page.getByRole('link', {
+          name: 'help@email.com',
         }),
       ).toBeAttached()
     })
