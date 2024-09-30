ecd144ac381f2b7a479a9cd3f13ca3aa47827d8c
Erika <erikang20@gmail.com>
2024-09-30 21:14:08 +0000

    Updating strings

--- server/app/services/MessageKey.java
+++ server/app/services/MessageKey.java
@@ -106,6 +106,7 @@ public enum MessageKey {
   BUTTON_PRINT("button.print"), // North Star only
   BUTTON_BACK("button.back"),
   BUTTON_SAVE_AND_EXIT("button.saveAndExit"),
+  BUTTON_REVIEW_AND_EXIT_V2("button.reviewAndExit.v2"),
   BUTTON_REVIEW("button.review"),
   BUTTON_DELETE_FILE("button.deleteFile"),
   BUTTON_KEEP_ACCT("button.keepAcct"),
