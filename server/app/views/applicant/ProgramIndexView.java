5170ecf1af08032e0def7a3c06cd9c7b1690766e
Melanie Barker <134556723+melanie-exygy@users.noreply.github.com>
2024-10-02 14:00:56 -0700

    Replace filter chip id with CSS class for simplicity (#8768)

--- server/app/views/applicant/ProgramIndexView.java
+++ server/app/views/applicant/ProgramIndexView.java
@@ -603,7 +603,7 @@ public final class ProgramIndexView extends BaseHtmlView {
                         relevantCategories,
                         category ->
                             div()
-                                .withId("filter-chip-" + category.replace(' ', '-'))
+                                .withClass("filter-chip")
                                 .with(
                                     input()
                                         .withId("check-category-" + category.replace(' ', '-'))
