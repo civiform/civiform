68e59bf351af34bf02b319ecd10593af34c2eb00
Rocky Fine <rocky.fine@exygy.com>
2024-10-07 16:43:54 -0400

    evolution

---
+++ server/conf/evolutions/default/79.sql
@@ -0,0 +1,7 @@
+# --- !Ups
+
+alter table programs add column localized_short_description jsonb, add column application_steps jsonb[];
+
+# --- !Downs
+
+alter table programs drop column if exists localized_short_description, application_steps;
