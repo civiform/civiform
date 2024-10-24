b93b771981930e4a58f96cff37da096813ae61ba
swatkat1 <swathi.murthi@seattle.gov>
2024-10-24 14:42:27 -0700

    working tests

--- server/test/forms/admin/BulkStatusUpdateFormTest.java
+++ server/test/forms/admin/BulkStatusUpdateFormTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static support.FakeRequestBuilder.fakeRequestBuilder;
 
 import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 import play.data.FormFactory;
@@ -21,14 +24,20 @@ public class BulkStatusUpdateFormTest extends ResetPostgres {
 
   @Test
   public void bindFromRequest_hasAllFields() {
+    List<Long> idList = new ArrayList<>();
+    idList.add(1L);
+    idList.add(2L);
+    idList.add(3L);
+    var paramList = idList.stream().map(e -> String.valueOf(e)).collect(Collectors.toList());
+    ;
     ImmutableMap<String, String> requestData =
         ImmutableMap.of(
             "applicationsIds[0]",
-            "1",
+            paramList.get(0),
             "applicationsIds[1]",
-            "2",
+            paramList.get(1),
             "applicationsIds[2]",
-            "3",
+            paramList.get(2),
             "statusText",
             "approved",
             "shouldSendEmail",
@@ -38,9 +47,9 @@ public class BulkStatusUpdateFormTest extends ResetPostgres {
     BulkStatusUpdateForm form =
         formFactory.form(BulkStatusUpdateForm.class).bindFromRequest(request).get();
 
-    assertThat(form.getApplicationsIds().get(0)).isEqualTo("1");
-    assertThat(form.getApplicationsIds().get(1)).isEqualTo("2");
-    assertThat(form.getApplicationsIds().get(2)).isEqualTo("3");
+    assertThat(form.getApplicationsIds().get(0)).isEqualTo(1L);
+    assertThat(form.getApplicationsIds().get(1)).isEqualTo(2L);
+    assertThat(form.getApplicationsIds().get(2)).isEqualTo(3L);
     assertThat(form.getStatusText()).isEqualTo("approved");
     assertThat(form.sendEmail()).isFalse();
   }
