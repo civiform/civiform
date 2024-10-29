c902b8be930da08f05190bacc79ea3e2ce33edb2
azhangggggg <149536150+azhangggggg@users.noreply.github.com>
2024-10-29 21:21:35 +0000

    add test for eligibility message form

--- server/app/forms/admin/BlockEligibilityMessageForm.java
+++ server/app/forms/admin/BlockEligibilityMessageForm.java
@@ -4,25 +4,25 @@ import com.google.common.collect.ImmutableList;
 
 /** A form for setting the eligibility message of a block. */
 public final class BlockEligibilityMessageForm {
-    public static final String ELIGIBILITY_MESSAGE = "eligibilityMessage";
+  public static final String ELIGIBILITY_MESSAGE = "eligibilityMessage";
 
-    public static final ImmutableList<String> FIELD_NAMES = ImmutableList.of(ELIGIBILITY_MESSAGE);
+  public static final ImmutableList<String> FIELD_NAMES = ImmutableList.of(ELIGIBILITY_MESSAGE);
 
-    private String eligibilityMessage;
+  private String eligibilityMessage;
 
-    public BlockEligibilityMessageForm() {
-        this.eligibilityMessage = "";
-    }
+  public BlockEligibilityMessageForm() {
+    this.eligibilityMessage = "";
+  }
 
-    public BlockEligibilityMessageForm(String eligibilityMessage) {
-        this.eligibilityMessage = eligibilityMessage;
-    }
+  public BlockEligibilityMessageForm(String eligibilityMessage) {
+    this.eligibilityMessage = eligibilityMessage;
+  }
 
-    public String getEligibilityMessage() {
-        return this.eligibilityMessage;
-    }
+  public String getEligibilityMessage() {
+    return this.eligibilityMessage;
+  }
 
-    public void setEligibilityMessage(String eligibilityMessage) {
-        this.eligibilityMessage = eligibilityMessage;
-    }
+  public void setEligibilityMessage(String eligibilityMessage) {
+    this.eligibilityMessage = eligibilityMessage;
+  }
 }
