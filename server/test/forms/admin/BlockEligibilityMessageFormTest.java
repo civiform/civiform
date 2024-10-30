aab2280317796a20f9fcb33204839900c8eafcd2
azhangggggg <149536150+azhangggggg@users.noreply.github.com>
2024-10-30 15:12:04 -0700

    [Eligibility Message] build eligibility msg form (#9065)

---
+++ server/test/forms/admin/BlockEligibilityMessageFormTest.java
@@ -0,0 +1,34 @@
+package forms.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.FakeRequestBuilder.fakeRequestBuilder;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
+import org.junit.Test;
+import play.data.FormFactory;
+import play.mvc.Http;
+import repository.ResetPostgres;
+
+public class BlockEligibilityMessageFormTest extends ResetPostgres {
+
+  private FormFactory formFactory;
+
+  @Before
+  public void setup() {
+    formFactory = instanceOf(FormFactory.class);
+  }
+
+  @Test
+  public void bindFromRequest_hasEligibilityMessage() {
+    String eligibilityMsg = "This is a test eligibility message";
+    ImmutableMap<String, String> requestData =
+        ImmutableMap.of(BlockEligibilityMessageForm.ELIGIBILITY_MESSAGE, eligibilityMsg);
+    Http.Request request = fakeRequestBuilder().bodyForm(requestData).build();
+
+    BlockEligibilityMessageForm form =
+        formFactory.form(BlockEligibilityMessageForm.class).bindFromRequest(request).get();
+
+    assertThat(form.getEligibilityMessage()).isEqualTo(eligibilityMsg);
+  }
+}
