340dcda24418620f78d951835b8faacf9b744cca
Rocky Fine <rocky.fine@exygy.com>
2024-11-04 18:33:46 -0500

    add check for programs table and not null

--- server/conf/evolutions/default/81.sql
+++ server/conf/evolutions/default/81.sql
@@ -1,9 +1,17 @@
 # --- !Ups
 
-ALTER TABLE programs ADD COLUMN IF NOT EXISTS localized_short_description jsonb DEFAULT ('{"isRequired": true, "translations": {"en_US": ""}}')::jsonb;
-ALTER TABLE programs ADD COLUMN IF NOT EXISTS application_steps jsonb DEFAULT ('[]')::jsonb;
+ALTER TABLE IF EXISTS programs 
+ADD COLUMN IF NOT EXISTS localized_short_description jsonb NOT NULL
+DEFAULT ('{"isRequired": true, "translations": {"en_US": ""}}')::jsonb;
+
+ALTER TABLE IF EXISTS programs 
+ADD COLUMN IF NOT EXISTS application_steps jsonb NOT NULL
+DEFAULT ('[]')::jsonb;
 
 # --- !Downs
 
-ALTER TABLE programs DROP COLUMN IF EXISTS localized_short_description;
-ALTER TABLE programs DROP COLUMN IF EXISTS application_steps;
+ALTER TABLE IF EXISTS programs 
+DROP COLUMN IF EXISTS localized_short_description;
+
+ALTER TABLE IF EXISTS programs 
+DROP COLUMN IF EXISTS application_steps;
