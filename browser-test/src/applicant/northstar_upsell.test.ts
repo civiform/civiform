ba913f5b48fcf33e5f8db7e9a745d7a8da2e3997
Ricardo Lagos <lagosr@google.com>
2024-11-05 19:32:35 +0000

    Add playwright test verifications for proper enabling/disabling of the 'Download Application' link in the application submit upssell page.

--- browser-test/src/applicant/northstar_upsell.test.ts
+++ browser-test/src/applicant/northstar_upsell.test.ts
@@ -1,6 +1,7 @@
 import {expect, test} from '../support/civiform_fixtures'
 import {
   enableFeatureFlag,
+  disableFeatureFlag,
   loginAsAdmin,
   loginAsTestUser,
   logout,
@@ -51,6 +52,7 @@ test.describe('Upsell tests', {tag: ['@northstar']}, () => {
     await loginAsTestUser(page)
 
     await enableFeatureFlag(page, 'north_star_applicant_ui')
+    await enableFeatureFlag(page, 'application_exportable')
 
     await test.step('Submit application', async () => {
       await applicantQuestions.clickApplyProgramButton(programName)
@@ -60,6 +62,54 @@ test.describe('Upsell tests', {tag: ['@northstar']}, () => {
     await validateApplicationSubmittedPage(
       page,
       /* expectRelatedProgram= */ true,
+      /* expectedDownloadApplicationLink= */ true,
+      applicantQuestions,
+    )
+
+    await test.step('Validate screenshot and accessibility', async () => {
+      await validateScreenshot(
+        page,
+        'upsell-north-star-download',
+        /* fullPage= */ true,
+        /* mobileScreenshot= */ true,
+      )
+    })
+
+    await validateAccessibility(page)
+
+    await test.step('Validate that user can return to the homepage without logging in', async () => {
+      await applicantQuestions.clickBackToHomepageButton()
+      // Expect the login dialog did not appear, so the user should already see the homepage
+      await page.waitForURL('**/programs')
+      // Expect the user is still logged in
+      await expect(page.getByRole('banner')).toContainText(
+        'Logged in as testuser@example.com',
+      )
+    })
+  })
+
+  test('view application submitted page while logged in (no application download link)', async ({
+    page,
+    adminPrograms,
+    applicantQuestions,
+  }) => {
+    // Create a second program for the related programs section
+    await createRelatedProgram(page, adminPrograms)
+
+    await loginAsTestUser(page)
+
+    await enableFeatureFlag(page, 'north_star_applicant_ui')
+    await disableFeatureFlag(page, 'application_exportable')
+
+    await test.step('Submit application', async () => {
+      await applicantQuestions.clickApplyProgramButton(programName)
+      await applicantQuestions.clickSubmitApplication()
+    })
+
+    await validateApplicationSubmittedPage(
+      page,
+      /* expectRelatedProgram= */ true,
+      /* expectedDownloadApplicationLink= */ false,
       applicantQuestions,
     )
 
@@ -90,6 +140,7 @@ test.describe('Upsell tests', {tag: ['@northstar']}, () => {
     applicantQuestions,
   }) => {
     await enableFeatureFlag(page, 'north_star_applicant_ui')
+    await enableFeatureFlag(page, 'application_exportable')
 
     await test.step('Submit application', async () => {
       await applicantQuestions.clickApplyProgramButton(programName)
@@ -99,6 +150,41 @@ test.describe('Upsell tests', {tag: ['@northstar']}, () => {
     await validateApplicationSubmittedPage(
       page,
       /* expectRelatedProgram= */ false,
+      /* expectDownloadApplicationLink= */ true,
+      applicantQuestions,
+    )
+
+    await test.step('Validate that login dialog is shown when user clicks on apply to another program', async () => {
+      await applicantQuestions.clickBackToHomepageButton()
+      await expect(page.getByText('Create an account or sign in')).toBeVisible()
+
+      await validateScreenshot(
+        page,
+        'upsell-north-star-login-download',
+        /* fullPage= */ false,
+        /* mobileScreenshot= */ true,
+      )
+
+      await validateAccessibility(page)
+    })
+  })
+
+  test('view application submitted page while logged out (no application download link)', async ({
+    page,
+    applicantQuestions,
+  }) => {
+    await enableFeatureFlag(page, 'north_star_applicant_ui')
+    await disableFeatureFlag(page, 'application_exportable')
+
+    await test.step('Submit application', async () => {
+      await applicantQuestions.clickApplyProgramButton(programName)
+      await applicantQuestions.clickSubmitApplication()
+    })
+
+    await validateApplicationSubmittedPage(
+      page,
+      /* expectRelatedProgram= */ false,
+      /* expectDownloadApplicationLink= */ false,
       applicantQuestions,
     )
 
@@ -173,6 +259,7 @@ test.describe('Upsell tests', {tag: ['@northstar']}, () => {
   async function validateApplicationSubmittedPage(
     page: Page,
     expectRelatedProgram: boolean,
+    expectApplicationDownloadLink: boolean,
     applicantQuestions: ApplicantQuestions,
   ) {
     await test.step('Validate application submitted page', async () => {
@@ -188,6 +275,12 @@ test.describe('Upsell tests', {tag: ['@northstar']}, () => {
       ).toBeVisible()
       await expect(page.getByText('Your submission information')).toBeVisible()
       await expect(page.getByText(customConfirmationText)).toBeVisible()
+
+      if (expectApplicationDownloadLink) {
+        await expect(page.getByText('Download your application')).toBeVisible()
+      } else {
+        await expect(page.getByText('Download your application')).toBeHidden()
+      }
 
       if (expectRelatedProgram) {
         await expect(
