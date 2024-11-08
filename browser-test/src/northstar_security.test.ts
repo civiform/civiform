736433cf2ce248a9431dfb306801df1f247d195f
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-11-08 09:04:30 -0800

    Setup for ns

--- browser-test/src/northstar_security.test.ts
+++ browser-test/src/northstar_security.test.ts
@@ -1,36 +1,44 @@
 import {test, expect} from './support/civiform_fixtures'
-import {loginAsAdmin} from './support'
+import {loginAsAdmin, enableFeatureFlag} from './support'
 
-test.describe('applicant security', {tag: ['@parallel-candidate']}, () => {
-  test('applicant cannot access admin pages', async ({request}) => {
-    const response = await request.get('/admin/programs')
-    await expect(response).toBeOK()
-    // Redirected to a non-admin page
-    expect(response.url()).not.toContain('/admin')
-  })
+test.describe(
+  'applicant security',
+  {tag: ['@parallel-candidate', '@northstar']},
+  () => {
+    test.beforeEach(async ({page}) => {
+      await enableFeatureFlag(page, 'north_star_applicant_ui')
+    })
 
-  test('redirects to program index page when not logged in (guest)', async ({
-    page,
-  }) => {
-    await page.goto('/')
-    await expect(
-      page.getByRole('heading', {
-        name: 'Save time applying for programs and services',
-      }),
-    ).toBeAttached()
-  })
+    test('applicant cannot access admin pages', async ({request}) => {
+      const response = await request.get('/admin/programs')
+      await expect(response).toBeOK()
+      // Redirected to a non-admin page
+      expect(response.url()).not.toContain('/admin')
+    })
 
-  test('redirects to program dashboard when logged in as admin', async ({
-    page,
-  }) => {
-    await loginAsAdmin(page)
-    await page.goto('/')
+    test('redirects to program index page when not logged in (guest)', async ({
+      page,
+    }) => {
+      await page.goto('/')
+      await expect(
+        page.getByRole('heading', {
+          name: 'Save time applying for programs and services',
+        }),
+      ).toBeAttached()
+    })
 
-    await expect(
-      page.getByRole('heading', {name: 'Program dashboard'}),
-    ).toBeAttached()
-    await expect(
-      page.getByRole('heading', {name: 'Create, edit and publish programs'}),
-    ).toBeAttached()
-  })
-})
+    test('redirects to program dashboard when logged in as admin', async ({
+      page,
+    }) => {
+      await loginAsAdmin(page)
+      await page.goto('/')
+
+      await expect(
+        page.getByRole('heading', {name: 'Program dashboard'}),
+      ).toBeAttached()
+      await expect(
+        page.getByRole('heading', {name: 'Create, edit and publish programs'}),
+      ).toBeAttached()
+    })
+  },
+)
