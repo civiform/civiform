e1e6e8ec628a2c984a85fcbe3a36f02239cde88c
Don Sandbekkhaug <ssandbekkhaug@google.com>
2024-11-12 13:22:52 -0600

    Migrate text.test.ts to North Star (#9185)

--- browser-test/src/applicant/questions/northstar_text.test.ts
+++ browser-test/src/applicant/questions/northstar_text.test.ts
@@ -1,12 +1,17 @@
 import {test, expect} from '../../support/civiform_fixtures'
 import {
+  enableFeatureFlag,
   loginAsAdmin,
   logout,
   validateAccessibility,
   validateScreenshot,
 } from '../../support'
 
-test.describe('Text question for applicant flow', () => {
+test.describe('Text question for applicant flow', {tag: ['@northstar']}, () => {
+  test.beforeEach(async ({page}) => {
+    await enableFeatureFlag(page, 'north_star_applicant_ui')
+  })
+
   test.describe('single text question', () => {
     const programName = 'Test program for single text q'
 
@@ -28,79 +33,115 @@ test.describe('Text question for applicant flow', () => {
     })
 
     test('validate screenshot', async ({page, applicantQuestions}) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
 
-      await validateScreenshot(page, 'text')
+      await test.step('Screenshot without errors', async () => {
+        await validateScreenshot(
+          page.getByTestId('questionRoot'),
+          'text-north-star',
+          /* fullPage= */ false,
+          /* mobileScreenshot= */ false,
+        )
+      })
+
+      await test.step('Screenshot with errors', async () => {
+        await applicantQuestions.clickContinue()
+        await validateScreenshot(
+          page.getByTestId('questionRoot'),
+          'text-errors-north-star',
+          /* fullPage= */ false,
+          /* mobileScreenshot= */ false,
+        )
+      })
     })
 
-    test('validate screenshot with errors', async ({
+    test('has no accessiblity violations', async ({
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
-      await applicantQuestions.clickNext()
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
 
-      await validateScreenshot(page, 'text-errors')
+      await validateAccessibility(page)
     })
 
     test('with text submits successfully', async ({applicantQuestions}) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion('I love CiviForm!')
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
-      await applicantQuestions.submitFromReviewPage()
+      await applicantQuestions.submitFromReviewPage(
+        /* northStarEnabled= */ true,
+      )
     })
 
     test('with empty text does not submit', async ({
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
-
-      // Click next without inputting anything
-      await applicantQuestions.clickNext()
-
-      const textId = '.cf-question-text'
-      expect(await page.innerText(textId)).toContain(
-        'This question is required.',
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
       )
+
+      // Click "Continue" without inputting anything
+      await applicantQuestions.clickContinue()
+
+      await expect(
+        page.getByText('Must contain at least 5 characters.'),
+      ).toBeVisible()
     })
 
     test('with too short text does not submit', async ({
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
-      await applicantQuestions.answerTextQuestion('hi')
-      await applicantQuestions.clickNext()
-
-      const textId = '.cf-question-text'
-      expect(await page.innerText(textId)).toContain(
-        'Must contain at least 5 characters.',
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
       )
+      await applicantQuestions.answerTextQuestion('hi')
+      await applicantQuestions.clickContinue()
+
+      await expect(
+        page.getByText('Must contain at least 5 characters.'),
+      ).toBeVisible()
     })
 
     test('with too long text does not submit', async ({
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion(
         'A long string that exceeds the character limit',
       )
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
-      const textId = '.cf-question-text'
-      expect(await page.innerText(textId)).toContain(
-        'Must contain at most 20 characters.',
-      )
+      await expect(
+        page.getByText('Must contain at most 20 characters.'),
+      ).toBeVisible()
     })
 
     test('hitting enter on text does not trigger submission', async ({
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion('I love CiviForm!', 0)
 
       // Ensure that clicking enter while on text input doesn't trigger form
@@ -110,16 +151,16 @@ test.describe('Text question for applicant flow', () => {
       await expect(page.locator('input[type=text]')).toBeVisible()
 
       // Check that pressing Enter on button works.
-      await page.focus('button:has-text("Save and next")')
+      await page.focus('button:has-text("Continue")')
       await page.keyboard.press('Enter')
-      await applicantQuestions.expectReviewPage()
+      await applicantQuestions.expectReviewPage(/* northStarEnabled= */ true)
 
       // Go back to question and ensure that "Review" button is also clickable
       // via Enter.
       await applicantQuestions.clickEdit()
-      await page.focus('text="Review"')
+      await page.focus('text="Review and exit"')
       await page.keyboard.press('Enter')
-      await applicantQuestions.expectReviewPage()
+      await applicantQuestions.expectReviewPage(/* northStarEnabled= */ true)
     })
   })
 
@@ -147,7 +188,10 @@ test.describe('Text question for applicant flow', () => {
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       let largeString = ''
       for (let i = 0; i < 1000; i++) {
         largeString += '1234567890'
@@ -156,7 +200,7 @@ test.describe('Text question for applicant flow', () => {
         // 10k characters + extra characters that should be trimmed
         largeString + 'xxxxxxx',
       )
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
       // Scroll to bottom so end of text is in view.
       await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight))
@@ -165,7 +209,9 @@ test.describe('Text question for applicant flow', () => {
       await validateScreenshot(page, 'text-max')
 
       // Form should submit with partial text entry.
-      await applicantQuestions.submitFromReviewPage()
+      await applicantQuestions.submitFromReviewPage(
+        /* northStarEnabled= */ true,
+      )
     })
   })
 
@@ -201,36 +247,49 @@ test.describe('Text question for applicant flow', () => {
     test('with both selections submits successfully', async ({
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion('I love CiviForm!', 0)
       await applicantQuestions.answerTextQuestion('You love CiviForm!', 1)
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
-      await applicantQuestions.submitFromReviewPage()
+      await applicantQuestions.submitFromReviewPage(
+        /* northStarEnabled= */ true,
+      )
     })
 
     test('with unanswered optional question submits successfully', async ({
       applicantQuestions,
     }) => {
       // Only answer second question. First is optional.
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion('You love CiviForm!', 1)
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
-      await applicantQuestions.submitFromReviewPage()
+      await applicantQuestions.submitFromReviewPage(
+        /* northStarEnabled= */ true,
+      )
     })
 
     test('with first invalid does not submit', async ({
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion(
         'A long string that exceeds the character limit',
         0,
       )
       await applicantQuestions.answerTextQuestion('You love CiviForm!', 1)
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
       const textId = '.cf-question-text'
       expect(await page.innerText(textId)).toContain(
@@ -242,13 +301,16 @@ test.describe('Text question for applicant flow', () => {
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
       await applicantQuestions.answerTextQuestion('I love CiviForm!', 0)
       await applicantQuestions.answerTextQuestion(
         'A long string that exceeds the character limit',
         1,
       )
-      await applicantQuestions.clickNext()
+      await applicantQuestions.clickContinue()
 
       const textId = `.cf-question-text >> nth=1`
       expect(await page.innerText(textId)).toContain(
@@ -260,7 +322,10 @@ test.describe('Text question for applicant flow', () => {
       page,
       applicantQuestions,
     }) => {
-      await applicantQuestions.applyProgram(programName)
+      await applicantQuestions.applyProgram(
+        programName,
+        /* northStarEnabled= */ true,
+      )
 
       await validateAccessibility(page)
     })
