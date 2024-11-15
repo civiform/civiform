d0a00b1153d8b25150a40f93abefdf20b6d3604f
Rocky Fine <rocky.fine@exygy.com>
2024-11-15 15:32:45 -0500

    trim json before submitting

--- server/app/assets/javascripts/admin_import_view.ts
+++ server/app/assets/javascripts/admin_import_view.ts
@@ -3,6 +3,7 @@ import {attachRedirectToPageListeners} from './main'
 class AdminImportView {
   constructor() {
     this.addRedirectListeners()
+    this.addStringifyJsonListener()
   }
 
   addRedirectListeners() {
@@ -10,6 +11,22 @@ class AdminImportView {
       attachRedirectToPageListeners()
     })
   }
+
+  addStringifyJsonListener() {
+    document.body.addEventListener('htmx:configRequest', function (evt) {
+      const customEvent = evt as CustomEvent<HtmxRequest>
+      const formData = customEvent.detail.formData
+      const programJson = formData.get('programJson') as string
+      const trimmedProgramJson = JSON.stringify(JSON.parse(programJson))
+      customEvent.detail.formData.set('programJson', trimmedProgramJson)
+    })
+  }
+}
+
+// htmx does not provide Typescript definitions for its custom events, so we have to define them ourselves
+// For more info about fields on an htmx request see https://htmx.org/events/#htmx:configRequest
+interface HtmxRequest {
+  formData: FormData
 }
 
 export function init() {
--- server/app/parsers/ProgramMigrationParser.java
+++ server/app/parsers/ProgramMigrationParser.java
@@ -9,11 +9,12 @@ import play.mvc.BodyParser;
  * limit for buffering. Program json is often quite large and we want to allow it to be processed
  * without increasing the buffer limit for all HTTP requests. All other requests use the default
  * Play buffer limit which is 100KB. For more information see:
- * https://www.playframework.com/documentation/3.0.x/JavaBodyParsers#Writing-a-custom-max-length-body-parser
+ * https://www.playframework.com/documentation/3.0.x/JavaBodyParsers
  */
 public class ProgramMigrationParser extends BodyParser.FormUrlEncoded {
   @Inject
   public ProgramMigrationParser(DefaultHttpErrorHandler errorHandler) {
+    // Allows requests up to about 1MB
     super(1024 * 1024, errorHandler);
   }
 }
