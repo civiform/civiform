4a863cc7ac9caf07fe0d4630adeedcc4019fd94d
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-12-11 10:01:14 -0800

    Generate swagger2 without questions

---
+++ server/app/services/openapi/AbstractOpenApiSchemaGenerator.java
@@ -0,0 +1,31 @@
+package services.openapi;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.collect.ImmutableList;
+import io.swagger.models.Scheme;
+
+public class AbstractOpenApiSchemaGenerator {
+  protected final OpenApiSchemaSettings openApiSchemaSettings;
+
+  protected AbstractOpenApiSchemaGenerator(OpenApiSchemaSettings openApiSchemaSettings) {
+    this.openApiSchemaSettings = checkNotNull(openApiSchemaSettings);
+  }
+
+  /**
+   * Returns the list of schemes to allow in the swagger schema. Special case to allow http for
+   * non-prod environments for testing purposes.
+   */
+  protected ImmutableList<Scheme> getSchemes() {
+    if (openApiSchemaSettings.allowHttpScheme()) {
+      return ImmutableList.of(Scheme.HTTP, Scheme.HTTPS);
+    }
+
+    return ImmutableList.of(Scheme.HTTPS);
+  }
+
+  /** Gets the baseurl without scheme */
+  protected String getHostName() {
+    return openApiSchemaSettings.baseUrl().replace("https://", "").replace("http://", "");
+  }
+}
