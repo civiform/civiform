fff79d24a4d245d26d58317fc8d05d1b104d704e
dkatzz <86739416+dkatzz@users.noreply.github.com>
2025-01-08 19:27:10 +0000

    fmt

--- server/test/repository/AccountRepositoryTest.java
+++ server/test/repository/AccountRepositoryTest.java
@@ -2,7 +2,6 @@ package repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
