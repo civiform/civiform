9015d17086854eb3c5cc56938fe7417808f85396
Melanie Barker <melanie.barker@exygy.com>
2025-01-09 15:44:46 -0800

    Merging deep link and program overview tests

--- browser-test/src/applicant/northstar_program_overview.test.ts
+++ browser-test/src/applicant/northstar_program_overview.test.ts
@@ -5,20 +5,58 @@ import {
   logout,
   loginAsTrustedIntermediary,
   ClientInformation,
+  loginAsTestUser,
 } from '../support'
 
 test.describe('Applicant program overview', {tag: ['@northstar']}, () => {
   const programName = 'test'
+  const questionText = 'This is a text question'
 
-  test.beforeEach(async ({page, adminPrograms}) => {
+  test.beforeEach(async ({page, adminPrograms, adminQuestions}) => {
     await enableFeatureFlag(page, 'north_star_applicant_ui')
 
-    await test.step('create a new program', async () => {
+    await test.step('create a new program with one text question', async () => {
       await loginAsAdmin(page)
+      await adminQuestions.addTextQuestion({
+        questionName: 'text question',
+        questionText: questionText,
+      })
       await adminPrograms.addProgram(programName)
+      await adminPrograms.editProgramBlockUsingSpec(programName, {
+        description: 'First block',
+        questions: [{name: 'text question', isOptional: true}],
+      })
       await adminPrograms.gotoAdminProgramsPage()
       await adminPrograms.publishProgram(programName)
       await logout(page)
+    })
+  })
+
+  test.describe('after starting an application', () => {
+    test.beforeEach(async ({applicantQuestions}) => {
+      await applicantQuestions.applyProgram(programName, true)
+      await applicantQuestions.answerTextQuestion('first answer')
+      await applicantQuestions.clickContinue()
+    })
+    test('takes guests and logged in users to the program overview', async ({
+      page,
+      applicantQuestions,
+      programOverview,
+    }) => {
+      // Exercise guest path
+      await page.goto(`/programs/${programName}`)
+      await programOverview.expectProgramOverviewPage()
+
+      await logout(page)
+
+      // Exercise test user path
+      await loginAsTestUser(page)
+      await applicantQuestions.applyProgram(programName, true)
+      await applicantQuestions.answerTextQuestion('first answer')
+      await applicantQuestions.clickContinue()
+
+      await page.goto(`/programs/${programName}`)
+      await programOverview.expectProgramOverviewPage()
     })
   })
 
@@ -43,13 +81,25 @@ test.describe('Applicant program overview', {tag: ['@northstar']}, () => {
       await logout(page)
     })
 
-    await page.goto(`/programs/${disabledProgramName}`)
+    await test.step(`opens the deep link of the disabled program and gets redirected to an error info page`, async () => {
+      await page.goto(`/programs/${disabledProgramName}`)
+      expect(page.url()).toContain('/disabled')
+      await expect(
+        page.getByRole('heading', {
+          name: 'This program is no longer available',
+        }),
+      ).toBeVisible()
+    })
 
-    await expect(
-      page.getByRole('heading', {
-        name: 'This program is no longer available',
-      }),
-    ).toBeVisible()
+    await test.step(`clicks on visit homepage button and it takes me to home page`, async () => {
+      await page.click('#visit-home-page-button')
+      expect(page.url()).toContain('/programs')
+      await expect(
+        page.getByRole('heading', {
+          name: 'Apply to programs in one place',
+        }),
+      ).toBeVisible()
+    })
   })
 
   test('trusted intermediary can view program overview with applicant id in the URL', async ({
@@ -75,5 +125,22 @@ test.describe('Applicant program overview', {tag: ['@northstar']}, () => {
     await page.goto(`${url}/${programName}`)
 
     await programOverview.expectProgramOverviewPage()
+  })
+
+  test('Going to a deep link does not retain redirect in session', async ({
+    page,
+  }) => {
+    // Go to a deep link
+    await page.goto(`/programs/${programName}`)
+
+    // Logging out should not take us back to the program overview, but rather
+    // to the program index page.
+    await logout(page)
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Apply to programs in one place',
+      }),
+    ).toBeAttached()
   })
 })
