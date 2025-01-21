47d7397a636ac2c6a68d7c0ca7ddf3eb7eca5331
dkatzz <86739416+dkatzz@users.noreply.github.com>
2025-01-21 20:45:55 +0000

    update

--- server/app/controllers/LogoutAllSessionsController.java
+++ server/app/controllers/LogoutAllSessionsController.java
@@ -65,29 +65,25 @@ public class LogoutAllSessionsController extends Controller {
   }
 
   public CompletionStage<Result> logoutFromEmail(Http.Request request, String email) {
-    try {
-      accountRepository
-          .lookupAccountByEmailAsync(email)
-          .thenAccept(
-              maybeAccount -> {
-                if (maybeAccount.isPresent()) {
-                  AccountModel account = maybeAccount.get();
-                  logger.debug("Found account for back channel logout: {}", account.id);
-                  account.clearActiveSessions();
-                  account.save();
-                } else {
-                  logger.warn("No account found for back channel logout");
-                }
-              })
-          .exceptionally(
-              e -> {
-                logger.error(e.getMessage(), e);
-                return null;
-              });
-
-    } catch (RuntimeException e) {
-      logger.error("Error clearing session from account", e);
-    }
+    logger.info("Received back channel logout request from remote address: {}", request.remoteAddress());
+    accountRepository
+        .lookupAccountByEmailAsync(email)
+        .thenAccept(
+            maybeAccount -> {
+              if (maybeAccount.isPresent()) {
+                AccountModel account = maybeAccount.get();
+                logger.debug("Found account for back channel logout: {}", account.id);
+                account.clearActiveSessions();
+                account.save();
+              } else {
+                logger.warn("No account found for back channel logout");
+              }
+            })
+        .exceptionally(
+            e -> {
+              logger.error(e.getMessage(), e);
+              return null;
+            });
 
     // Redirect to the landing page
     return CompletableFuture.completedFuture(redirect(routes.HomeController.index().url()));
--- server/app/filters/NonUserRoutes.java
+++ server/app/filters/NonUserRoutes.java
@@ -11,7 +11,7 @@ import play.mvc.Http.RequestHeader;
 final class NonUserRoutes {
   public static ImmutableList<String> prefixes =
       ImmutableList.of(
-          "/api/", "/assets/", "/dev/", "/favicon", "/apple-touch-icon", "/playIndex", "/metrics");
+          "/api/", "/assets/", "/dev/", "/favicon", "/apple-touch-icon", "/playIndex", "/metrics", "/logoutAllSessions");
 
   public static boolean anyMatch(RequestHeader requestHeader) {
     String path = requestHeader.path();
