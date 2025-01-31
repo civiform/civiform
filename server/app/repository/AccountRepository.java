363917d7547e828b1856138dfde206ca04581e02
dkatzz <86739416+dkatzz@users.noreply.github.com>
2025-01-31 21:27:24 +0000

    fmt

--- server/app/repository/AccountRepository.java
+++ server/app/repository/AccountRepository.java
@@ -27,7 +27,6 @@ import models.AccountModel;
 import models.ApplicantModel;
 import models.SessionLifecycle;
 import models.TrustedIntermediaryGroupModel;
-import modules.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import services.CiviFormError;
