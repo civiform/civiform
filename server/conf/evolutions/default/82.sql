6a5dd8583075e6a99b063fb137abb5b33c4b9ad3
azhangggggg <149536150+azhangggggg@users.noreply.github.com>
2025-02-19 20:31:48 +0000

    making the column not null

--- server/conf/evolutions/default/82.sql
+++ server/conf/evolutions/default/82.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 
-ALTER TABLE applications ADD COLUMN IF NOT EXISTS eligibility_determination VARCHAR DEFAULT 'NOT_COMPUTED';
+ALTER TABLE applications ADD COLUMN IF NOT EXISTS eligibility_determination VARCHAR DEFAULT 'NOT_COMPUTED' NOT NULL;
 
 # --- !Downs
 ALTER TABLE applications DROP COLUMN IF EXISTS eligibility_determination;
