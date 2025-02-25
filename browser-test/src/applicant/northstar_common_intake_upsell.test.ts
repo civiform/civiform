564038c7198b19610256f61e619c234711b90ce6
Drew Obbard <78387000+watermandrewc@users.noreply.github.com>
2025-02-25 17:32:45 +0000

    use usa-sr-only component instead of paragraph element

--- browser-test/src/applicant/northstar_common_intake_upsell.test.ts
+++ browser-test/src/applicant/northstar_common_intake_upsell.test.ts
@@ -251,11 +251,7 @@ test.describe(
       await test.step('Setup: submit application', async () => {
         await tiDashboard.clickOnViewApplications()
         // Validate accessibility label
-        await expect(
-          page.getByLabel(
-            'For your information: You are applying for last, first. Are you trying to apply for a different client?',
-          ),
-        ).toBeVisible()
+        await expect(page.getByLabel('For your information:')).toBeVisible()
         await applicantQuestions.clickApplyProgramButton(programName)
         await applicantQuestions.submitFromReviewPage(
           /* northStarEnabled= */ true,
