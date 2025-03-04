37a1d38ad3fa5ac24ad55fcf683cb5152a2c7039
Amit Sharma <amishar@outlook.com>
2025-03-04 15:23:58 +0000

    use fixed clock in tests

--- server/test/auth/oidc/applicant/Auth0ProviderTest.java
+++ server/test/auth/oidc/applicant/Auth0ProviderTest.java
@@ -11,6 +11,8 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import java.net.URI;
 import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
 import junitparams.JUnitParamsRunner;
 import org.junit.Before;
 import org.junit.Test;
@@ -70,12 +72,14 @@ public class Auth0ProviderTest extends ResetPostgres {
   @Test
   public void testLogout() throws Exception {
     String afterLogoutUri = "https://civiform.dev";
+    Clock clock = Clock.fixed(Instant.ofEpochSecond(10), ZoneOffset.UTC);
+
     var logoutAction =
         auth0Provider
             .get()
             .getLogoutAction(
                 new CallContext(webContext, mockSessionStore),
-                new CiviFormProfileData(1L, Clock.systemUTC()),
+                new CiviFormProfileData(1L, clock),
                 afterLogoutUri);
     assertThat(logoutAction).containsInstanceOf(FoundAction.class);
     var logoutUri = new URI(((FoundAction) logoutAction.get()).getLocation());
