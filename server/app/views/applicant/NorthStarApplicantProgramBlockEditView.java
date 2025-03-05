99b2b7acba93665ba0dd1cba8f58d82d171055f1
tallulah <tallulah.kay@exygy.com>
2025-03-05 10:08:52 -0800

    add back to admin button on admin applicant preview view

--- server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
+++ server/app/views/applicant/NorthStarApplicantProgramBlockEditView.java
@@ -77,6 +77,7 @@ public final class NorthStarApplicantProgramBlockEditView extends NorthStarBaseV
             applicationParams.blockList().size(),
             applicationParams.messages());
     context.setVariable("pageTitle", pageTitle);
+    context.setVariable("goBackToAdminUrl", getGoBackToAdminUrl(applicationParams));
 
     // Progress bar
     ProgressBar progressBar =
@@ -258,6 +259,10 @@ public final class NorthStarApplicantProgramBlockEditView extends NorthStarBaseV
                   }
                   return paramsBuilder.build();
                 }));
+  }
+
+  private String getGoBackToAdminUrl(ApplicationBaseViewParams params) {
+    return controllers.admin.routes.AdminProgramPreviewController.back(params.programId()).url();
   }
 
   // One field at most should be autofocused on the page. If there are errors, it
