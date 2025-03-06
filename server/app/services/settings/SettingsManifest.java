2fa1ed5ee10c90c6f9b66e8c031169bf4adfb1ce
Drew Obbard <78387000+watermandrewc@users.noreply.github.com>
2025-03-06 00:36:24 +0000

    add admin-writeable settings for primary and primary-dark colors

--- server/app/services/settings/SettingsManifest.java
+++ server/app/services/settings/SettingsManifest.java
@@ -70,6 +70,22 @@ public final class SettingsManifest extends AbstractSettingsManifest {
     return getString("FAVICON_URL");
   }
 
+  /**
+   * The hex code value of the color to use as the primary branding color of the website. Not ready
+   * for production use.
+   */
+  public Optional<String> getColorPrimary(RequestHeader request) {
+    return getString("COLOR_PRIMARY", request);
+  }
+
+  /**
+   * The hex code value of the color to use as the primary-dark branding color of the website. Not
+   * ready for production use.
+   */
+  public Optional<String> getColorPrimaryDark(RequestHeader request) {
+    return getString("COLOR_PRIMARY_DARK", request);
+  }
+
   /** What identity provider to use for applicants. */
   public Optional<String> getCiviformApplicantIdp() {
     return getString("CIVIFORM_APPLICANT_IDP");
@@ -1116,7 +1132,23 @@ public final class SettingsManifest extends AbstractSettingsManifest {
                               + " image, in GIF, PNG, or ICO format.",
                           /* isRequired= */ false,
                           SettingType.STRING,
-                          SettingMode.ADMIN_READABLE))))
+                          SettingMode.ADMIN_READABLE),
+                      SettingDescription.create(
+                          "COLOR_PRIMARY",
+                          "The hex code value of the color to use as the primary branding color of"
+                              + " the website. Not ready for production use.",
+                          /* isRequired= */ false,
+                          SettingType.STRING,
+                          SettingMode.ADMIN_WRITEABLE,
+                          Pattern.compile("^#(?:[0-9a-fA-F]{3}){1,2}$")),
+                      SettingDescription.create(
+                          "COLOR_PRIMARY_DARK",
+                          "The hex code value of the color to use as the primary-dark branding"
+                              + " color of the website. Not ready for production use.",
+                          /* isRequired= */ false,
+                          SettingType.STRING,
+                          SettingMode.ADMIN_WRITEABLE,
+                          Pattern.compile("^#(?:[0-9a-fA-F]{3}){1,2}$")))))
           .put(
               "External Services",
               SettingsSection.create(
