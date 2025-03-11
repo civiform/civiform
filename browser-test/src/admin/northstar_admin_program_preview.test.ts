601c00680b140e03dd5e6af07d8b3b8c466ad003
tallulah <tallulah.kay@exygy.com>
2025-03-11 11:27:11 -0700

    update test to have steps

--- browser-test/src/admin/northstar_admin_program_preview.test.ts
+++ browser-test/src/admin/northstar_admin_program_preview.test.ts
@@ -6,46 +6,55 @@ import {
   waitForPageJsLoad,
 } from '../support'
 
-test.describe('admin program preview', {tag: ['@northstar']}, () => {
+test.describe('admin preview as applicant', {tag: ['@northstar']}, () => {
   test.beforeEach(async ({page}) => {
+    await loginAsAdmin(page)
     await enableFeatureFlag(page, 'north_star_applicant_ui')
   })
 
-  test('preview program and use back button', async ({
+  test('preview as applicant and use to admin back button', async ({
     page,
     adminPrograms,
     adminQuestions,
     applicantQuestions,
   }) => {
-    await loginAsAdmin(page)
-
-    await adminQuestions.addEmailQuestion({questionName: 'email-q'})
     const programName = 'test program'
-    await adminPrograms.addProgram(programName)
-    await adminPrograms.editProgramBlockUsingSpec(programName, {
-      description: 'description',
-      questions: [{name: 'email-q'}],
+    await test.step("create test program", async () => {
+      await adminQuestions.addEmailQuestion({questionName: 'email-q'})
+      await adminPrograms.addProgram(programName)
+      await adminPrograms.editProgramBlockUsingSpec(programName, {
+        description: 'description',
+        questions: [{name: 'email-q'}],
+      })
     })
-    await adminPrograms.gotoEditDraftProgramPage(programName)
 
-    await validateScreenshot(page, 'northstar-admin-edit-draft-program-page')
-    await page.click('button:has-text("Preview as applicant")')
-    await waitForPageJsLoad(page)
+    await test.step("preview as applicant and check that back to admin button is visible", async () => {
+      await adminPrograms.gotoEditDraftProgramPage(programName)
+      await page.click('button:has-text("Preview as applicant")')
+      await waitForPageJsLoad(page)
+  
+      await validateScreenshot(
+        page,
+        'northstar-admin-program-preview-application-review-page',
+      )
+      await page.isVisible('a:has-text("Back to admin view")')
+    })
 
-    await validateScreenshot(
-      page,
-      'northstar-admin-program-preview-application-review-page',
-    )
-    await page.isVisible('a:has-text("Back to admin view")')
+    await test.step("navigate in applicant preview", async () => {
+      await applicantQuestions.clickContinue()
+    
+      await page.isVisible('a:has-text("Back to admin view")')
 
-    await applicantQuestions.clickContinue()
+      await validateScreenshot(
+        page,
+        'northstar-admin-program-preview-application-block-edit-page',
+      )
+    }) 
 
-    await validateScreenshot(
-      page,
-      'northstar-admin-program-preview-application-block-edit-page',
-    )
-    await page.click('a:has-text("Back to admin view")')
-
-    await adminPrograms.expectProgramBlockEditPage(programName)
+    await test.step("navigate back to admin view", async () => {
+      await page.click('a:has-text("Back to admin view")')
+  
+      await adminPrograms.expectProgramBlockEditPage(programName)  
+    })
   })
 })
