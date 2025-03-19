5e6e75d538f56209895ff6266df518ea38014749
Emilia Paz <emipazojeda@gmail.com>
2025-03-19 03:50:04 +0000

    [External Program Cards] Add program type

--- server/app/services/program/ProgramType.java
+++ server/app/services/program/ProgramType.java
@@ -10,6 +10,7 @@ import io.ebean.annotation.DbEnumValue;
 public enum ProgramType {
   DEFAULT("default"),
   COMMON_INTAKE_FORM("common_intake_form");
+  EXTERNAL_PROGRAM("external_program");
 
   private final String dbValue;
 
