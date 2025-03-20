93c20cdaf931bec1d0598b08fc1a246e10ee394a
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2025-03-20 15:28:04 -0700

    testing

--- browser-test/playwright.config.ts
+++ browser-test/playwright.config.ts
@@ -1,6 +1,40 @@
 import {defineConfig} from '@playwright/test'
 import {BASE_URL} from './src/support/config'
 
+import fs from 'fs'
+import path from 'path'
+import crypto from 'crypto'
+
+/**
+ * Recursively finds all 'test.ts' files in './src', sorts them by MD5 hash.
+ */
+/* eslint-disable */
+function getTestFilesSortedByHash(dir: string = './src'): string[] {
+  const results: string[] = []
+
+  function search(directory: string) {
+    for (const file of fs.readdirSync(directory)) {
+      const fullPath = path.join(directory, file)
+      fs.statSync(fullPath).isDirectory()
+        ? search(fullPath)
+        : file.endsWith('test.ts') && results.push(fullPath)
+    }
+  }
+
+  search(dir)
+
+  return results
+    .map((file) => ({
+      file,
+      hash: crypto
+        .createHash('md5')
+        .update(fs.readFileSync(file))
+        .digest('hex'),
+    }))
+    .sort((a, b) => a.hash.localeCompare(b.hash))
+    .map((entry) => entry.file)
+}
+
 // For details see: https://playwright.dev/docs/api/class-testconfig
 
 export default defineConfig({
@@ -41,5 +75,10 @@ export default defineConfig({
     ['html', {open: 'never', outputFolder: 'tmp/html-output'}],
     ['json', {outputFile: 'tmp/json-output/results.json'}],
     ['./src/reporters/file_placement_reporter.ts'],
+  ],
+  projects: [
+    {
+      testMatch: getTestFilesSortedByHash(),
+    },
   ],
 })
