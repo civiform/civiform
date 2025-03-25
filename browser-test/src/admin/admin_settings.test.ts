0348a49f3989310ff6d6f233f53e55d86ba604e2
rocky <rachelefine@gmail.com>
2025-03-25 15:04:40 -0400

    Fix test for staging run take 2 (#10137)

--- browser-test/src/admin/admin_settings.test.ts
+++ browser-test/src/admin/admin_settings.test.ts
@@ -2,10 +2,12 @@ import {test} from '../support/civiform_fixtures'
 import {disableFeatureFlag, loginAsAdmin, validateScreenshot} from '../support'
 
 test.describe('Managing system-wide settings', () => {
-  test('Displays the settings page', async ({page, adminSettings}) => {
+  test.beforeEach(async ({page}) => {
     await loginAsAdmin(page)
     await disableFeatureFlag(page, 'allow_civiform_admin_access_programs')
+  })
 
+  test('Displays the settings page', async ({page, adminSettings}) => {
     await test.step('Go to admin settings page and take screenshot', async () => {
       await page.setViewportSize({
         width: 1280,
@@ -46,9 +48,7 @@ test.describe('Managing system-wide settings', () => {
     })
   })
 
-  test('Updates settings on save', async ({page, adminSettings}) => {
-    await loginAsAdmin(page)
-
+  test('Updates settings on save', async ({adminSettings}) => {
     await adminSettings.gotoAdminSettings()
 
     await test.step('button check', async () => {
