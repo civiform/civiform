e65b4939513315ffc533d5eec537b890ad79f219
tallulahkay <tallulah.kay@gmail.com>
2025-04-09 09:22:49 -0700

    update playwright test with best practices

--- browser-test/src/applicant/questions/northstar_address.test.ts
+++ browser-test/src/applicant/questions/northstar_address.test.ts
@@ -92,7 +92,7 @@ test.describe('address applicant flow', {tag: ['@northstar']}, () => {
       })
     })
 
-    test('with invalid address does not submit', async ({
+    test.only('with invalid address does not submit', async ({
       page,
       applicantQuestions,
     }) => {
@@ -112,11 +112,11 @@ test.describe('address applicant flow', {tag: ['@northstar']}, () => {
       })
 
       await test.step('Confirm aria-invalid applies only to the invalid field', async () => {
-        const addressStreet1 = page.locator('.cf-address-street-1 input')
-        const addressStreet2 = page.locator('.cf-address-street-2 input')
-        const addressCity = page.locator('.cf-address-city input')
-        const addressState = page.locator('.cf-address-state select')
-        const addressZip = page.locator('.cf-address-zip input')
+        const addressStreet1 = page.getByRole('textbox', { name: 'Address'})
+        const addressStreet2 = page.getByRole('textbox', { name: 'Apartment, suite, etc. (' })
+        const addressCity = page.getByRole('textbox', { name: 'City' })
+        const addressState = page.getByLabel('State *')
+        const addressZip = page.getByRole('textbox', { name: 'ZIP Code' })
         await expect(addressStreet1).toHaveAttribute('aria-invalid', 'false')
         await expect(addressStreet2).toHaveAttribute('aria-invalid', 'false')
         await expect(addressCity).toHaveAttribute('aria-invalid', 'false')
