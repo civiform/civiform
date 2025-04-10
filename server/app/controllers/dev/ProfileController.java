08bf88c97e7240dfb4090a4481c713f8deb80c0a
Drew Obbard <78387000+watermandrewc@users.noreply.github.com>
2025-04-10 21:00:43 +0000

    allow resources from same origin; fix profile debugging page warning

--- server/app/controllers/dev/ProfileController.java
+++ server/app/controllers/dev/ProfileController.java
@@ -1,6 +1,7 @@
 package controllers.dev;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+// import static j2html.TagCreator.pre;
 
 import auth.CiviFormProfile;
 import auth.ProfileUtils;
@@ -27,6 +28,6 @@ public final class ProfileController extends Controller {
         maybeProfile
             .map((p) -> JsonPrettifier.asPrettyJsonString(p.getProfileData()))
             .orElse("No profile present");
-    return ok(profileContent).as(Http.MimeTypes.TEXT);
+    return ok(String.format("<pre>%s</pre>", profileContent)).as(Http.MimeTypes.HTML);
   }
 }
