71cba7258f2d7595c597c72662faeb2b781ad475
Shane McDaniel <shane.mcdaniel@exygy.com>
2025-04-15 13:37:42 -0400

    Update TransactionManager doc to reflect it does not work on Async code

--- server/app/repository/TransactionManager.java
+++ server/app/repository/TransactionManager.java
@@ -16,6 +16,10 @@ import org.slf4j.LoggerFactory;
  * A class for managing workflows that span multiple repositories, but should still be wrapped in a
  * transaction to prevent race conditions and data integrity errors.
  *
+ * <p>Warning: This class does not work across Async calls, they will not see
+ * the wrapping transaction due to transactions being stored thread-local.
+ * <a href="https://github.com/civiform/civiform/wiki/Concurrency-and-Transactions#wrapping-logic-in-transactions">Details here</a>
+ *
  * <p>See https://www.postgresql.org/docs/current/transaction-iso.html#XACT-SERIALIZABLE for details
  * on transactions.
  */
@@ -24,8 +28,6 @@ public final class TransactionManager {
 
   /**
    * Run the supplied code in a {@link TxIsolation#SERIALIZABLE} transaction.
-   *
-   * <p>Warning: This has not been tested with a supplier that does work asynchronously.
    *
    * <p>Notes:
    *
