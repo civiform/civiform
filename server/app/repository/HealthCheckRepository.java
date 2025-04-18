2202b8383e4c3bfd1e7d8bcc7fdfa1e77a710639
swatkat1 <swathi.murthi@seattle.gov>
2025-04-17 17:44:03 -0700

    supressed warnings and added javadocs

--- server/app/repository/HealthCheckRepository.java
+++ server/app/repository/HealthCheckRepository.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/** Implements a simple query needed to check if DB is reachable */
 public final class HealthCheckRepository {
   private final Database database;
   private final Logger log = LoggerFactory.getLogger(HealthCheckRepository.class);
@@ -17,6 +18,11 @@ public final class HealthCheckRepository {
     this.database = DB.getDefault();
   }
 
+  /**
+   * Does a simple query to check if the DB is reachable. In case of any exception, error is logged
+   * and an empty object is returned.
+   */
+  @SuppressWarnings("unchecked")
   public Optional<SqlRow> checkDBHealth() {
     Optional<SqlRow> result = Optional.empty();
     try {
