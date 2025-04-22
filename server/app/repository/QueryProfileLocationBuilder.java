3006a1cf819d8c3707325aa90923b2d876b89d0a
Nick Burgan <nick.burgan@exygy.com>
2025-04-21 20:38:45 -0700

    Cache ProfileLocation per-repository
    Previously, we were creating new ProfileLocation objects for every single database call we did where we used the .setProfileLocation(queryProfileLocationBuilder.create()) function. This caused the heap to fill up with these objects and they were never garbage collected. By creating a cache of these objects, we only create unique ones once each time the same repository function is called.

--- server/app/repository/QueryProfileLocationBuilder.java
+++ server/app/repository/QueryProfileLocationBuilder.java
@@ -1,10 +1,13 @@
 package repository;
 
 import io.ebean.ProfileLocation;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /** Utility class for creating ProfileLocation objects. */
 public final class QueryProfileLocationBuilder {
   private final String fileName;
+  private final ConcurrentMap<String, ProfileLocation> cache = new ConcurrentHashMap<>();
 
   /**
    * Initializes the QueryUtils with a file name.
@@ -22,6 +25,6 @@ public final class QueryProfileLocationBuilder {
    * @return ProfileLocation created using the provided file name and function name.
    */
   ProfileLocation create(String functionName) {
-    return ProfileLocation.create(fileName + "." + functionName);
+    return cache.computeIfAbsent(functionName, fn -> ProfileLocation.create(fileName + "." + fn));
   }
 }
