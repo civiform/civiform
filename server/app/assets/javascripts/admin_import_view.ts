4777189e72bb3a0d8ac77e9829c1490e1845341c
Ezra (Avery) Einhorn <30905413+ezraeinhorn624@users.noreply.github.com>
2025-05-08 14:19:53 +0000

    Add toplevel "use existing" and client scripting

--- server/app/assets/javascripts/admin_import_view.ts
+++ server/app/assets/javascripts/admin_import_view.ts
@@ -1,15 +1,75 @@
 import {attachRedirectToPageListeners} from './main'
 import {HtmxRequest} from './htmx_request'
 
+/**
+ * Dynamic behavior for AdminImportView.
+ *
+ * Some HTMX swap listeners ensure that the program import form (and its contents,
+ * such as the stringified program JSON) work correctly.
+ *
+ * Listeners are also attached to the duplicate-handling radio groups, to support
+ * dynamic interaction between the top-level radio group and each individual
+ * question's radio group.
+ *
+ * For example, if a certain selection is made at the top-level, all individual radio
+ * groups will be set to that selection. Converseley, if an individual question
+ * selection is then made, the top-level radio group will reflect that the user is
+ * choosing how to handle each question individually.
+ */
 class AdminImportView {
   constructor() {
     this.addRedirectListeners()
     this.addStringifyJsonListener()
   }
 
+  /**
+   * Adds listeners to all duplicate-handling radio groups to support dynamic
+   * interactions between the top-level radio and each question's individual question.
+   */
+  addDuplicateHandlingListeners() {
+    const toplevelRadio = document
+      .getElementsByName('toplevelDuplicateQuestionHandling')
+      .item(0) as HTMLFieldSetElement
+
+    const duplicateQuestionRadios = document.getElementsByName(
+      'duplicateQuestionHandling-',
+    )
+
+    toplevelRadio.addEventListener('change', (event) => {
+      this.setAllQuestionRadiosToSpecifiedValue(
+        duplicateQuestionRadios,
+        (<HTMLInputElement>event.target)?.value,
+      )
+    })
+
+    duplicateQuestionRadios.forEach((radio) => {
+      radio.addEventListener('change', () =>
+        this.setRadioToValue(toplevelRadio, 'DECIDE_FOR_EACH'),
+      )
+    })
+  }
+
+  setAllQuestionRadiosToSpecifiedValue(
+    questionRadios: NodeList,
+    value: string,
+  ) {
+    Array.from(questionRadios).forEach((radio) => {
+      this.setRadioToValue(<HTMLFieldSetElement>radio, value)
+    })
+  }
+
+  setRadioToValue(radio: HTMLFieldSetElement, value: string) {
+    Array.from(radio.getElementsByTagName('input')).forEach((radioInput) => {
+      if (radioInput.value === value) {
+        radioInput.checked = true
+      }
+    })
+  }
+
   addRedirectListeners() {
     document.addEventListener('htmx:afterSwap', () => {
       attachRedirectToPageListeners()
+      this.addDuplicateHandlingListeners()
     })
   }
 
