396dc77a79821018ea1b6f15093edb40b07bd00f
dkatzz <86739416+dkatzz@users.noreply.github.com>
2025-05-22 18:51:04 -0400

    Update modal.ts

--- server/app/assets/javascripts/modal.ts
+++ server/app/assets/javascripts/modal.ts
@@ -73,7 +73,7 @@ export class ModalController {
   constructor() {
     const modalContainer = document.querySelector('#modal-container')
     if (modalContainer == null) {
-      console.error('Modal Container display not found')
+      console.info('Legacy modal Container not initialized.')
       return
     }
 
