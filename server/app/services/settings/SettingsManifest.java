f3818793ab8185bf8ad1ce7cc338a51a3ea47e74
Emilia Paz <emipazojeda@gmail.com>
2025-06-02 00:39:09 +0000

    Add PROGRAM_SLUG_URLS_ENABLED feature

--- server/app/services/settings/SettingsManifest.java
+++ server/app/services/settings/SettingsManifest.java
@@ -1097,6 +1097,11 @@ public final class SettingsManifest extends AbstractSettingsManifest {
     return getBool("IMPORT_DUPLICATE_HANDLING_OPTIONS_ENABLED", request);
   }
 
+  /** (NOT FOR PRODUCTION USE) Use program slugs instead of program IDs in URLs. */
+  public boolean getProgramSlugUrlsEnabled(RequestHeader request) {
+    return getBool("PROGRAM_SLUG_URLS_ENABLED", request);
+  }
+
   private static final ImmutableMap<String, SettingsSection> GENERATED_SECTIONS =
       ImmutableMap.<String, SettingsSection>builder()
           .put(
@@ -2332,6 +2337,13 @@ public final class SettingsManifest extends AbstractSettingsManifest {
                           "(NOT FOR PRODUCTION USE) Enable options for handling duplicate questions"
                               + " when importing/migrating programs: create a duplicate, use the"
                               + " existing question, or overwrite the existing question.",
+                          /* isRequired= */ false,
+                          SettingType.BOOLEAN,
+                          SettingMode.ADMIN_WRITEABLE),
+                      SettingDescription.create(
+                          "PROGRAM_SLUG_URLS_ENABLED",
+                          "(NOT FOR PRODUCTION USE) Use program slugs instead of program IDs in"
+                              + " URLs.",
                           /* isRequired= */ false,
                           SettingType.BOOLEAN,
                           SettingMode.ADMIN_WRITEABLE))))
