c1942b053aab1466b772d69b5c369338d8562c48
Drew Obbard <78387000+watermandrewc@users.noreply.github.com>
2025-06-16 16:39:22 +0000

    fix failing tests

--- server/test/services/export/AbstractExporterTest.java
+++ server/test/services/export/AbstractExporterTest.java
@@ -158,7 +158,8 @@ public abstract class AbstractExporterTest extends ResetPostgres {
         QuestionAnswerer.answerPhoneQuestion(applicantDataOne, answerPath, "US", "(615) 757-1010");
         break;
       case YES_NO:
-        // Do nothing for now. TODO(#10800): handle once yes/no question is implemented.
+        QuestionAnswerer.answerSingleSelectQuestion(applicantDataOne, answerPath, 1L);
+        break;
       case STATIC:
         // Do nothing.
         break;
