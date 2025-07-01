ae0c2a9fd01d440c2b72daa1019850063bee8c72
Drew Obbard <78387000+watermandrewc@users.noreply.github.com>
2025-07-01 15:29:08 -0700

    [Yes/No] Implement applicant view (#10870)
    ### Description
    
    Add YesNoQuestionFragment to handle yes/no questions. Each option will
    show translated text; the message is chosen based on the admin ID for
    yes/no question options, which are hard-coded and the same for each
    option across yes/no questions. Each option is also conditionally
    displayed based on whether the admin selected for the option to be
    displayed to the applicant.
    
    Created a reusable subfragment to share rendering between
    RadioButtonQuestionFragment and YesNoQuestionFragment.
    
    ### Checklist
    
    #### General
    
    Read the full guidelines for PRs
    [here](https://github.com/civiform/civiform/wiki/Technical-contribution-guide#creating-a-pull-request)
    
    - [x] Added the correct label (see
    [docs](https://github.com/civiform/civiform/wiki/Technical-contribution-guide#adding-appropriate-labels)
    for more info): < feature | enhancement | bug | under-development |
    dependencies | infrastructure | ignore-for-release | database >
    - [ ] Assigned to a specific person, `civiform/developers`, or a [more
    specific round-robin
    list](https://github.com/civiform/civiform/wiki/Technical-contribution-guide#adding-reviewers)
    - [ ] Added an additional reviewer from `civiform/eng-admin` as FYI (if
    the primary reviewer is not an admin and this PR includes functionality
    changes)
    - [x] Removed the release notes section if the title is sufficient for
    the release notes description, or put more details in that section.
    - [x] Created unit and/or browser tests which fail without the change
    (if possible)
    - [x] Performed manual testing (Chrome and Firefox if it includes
    front-end changes)
    - [ ] Extended the README / documentation, if necessary. For user-facing
    features, consider updating [the user
    docs](https://github.com/civiform/docs). For "under-the-hood" changes or
    things more relevant to developers, consider updating [the dev
    wiki](https://github.com/civiform/civiform/wiki).
    - [ ] Ensured PII wasn't added to any new logs, unless it was guarded by
    `isDevOrStaging`
    - [ ] Noted in the PR description which, if any, code in this PR was
    generated by AI.
    
    #### User visible changes
    
    - [x] Followed steps to
    [internationalize](https://github.com/civiform/civiform/wiki/Internationalization-%28i18n%29#application-strings)
    any new strings
    - [x] Added context strings to new
    [messages](https://github.com/civiform/civiform/blob/main/server/conf/i18n/messages)
    - [x] Didn't use a message in applicant facing code that isn't
    translated yet (unless behind a flag)
    - [x] Wrote browser tests using the
    [validateAccessibility](https://sourcegraph.com/github.com/civiform/civiform/-/blob/browser-test/src/support/index.ts?L437:14&subtree=true)
    method
    - [ ] Made equivalent changes in Thymeleaf for applicant-facing features
    and changes (or created an issue in the [Northstar
    epic](https://github.com/orgs/civiform/projects/1/views/94) to track
    that it's missing from Thymeleaf)
    - [x] Tested on mobile view. See [mobile device
    mode](https://developer.chrome.com/docs/devtools/device-mode/)
    - [x] Manually tested at 200% size
    - [ ] Manually evaluated tab order
    - [x] Manually tested with a screen reader if the feature is
    applicant-facing. See [screen reader
    testing](https://github.com/civiform/civiform/wiki/Testing#screen-reader-testing)
    
    ### Instructions for manual testing
    
    If instructions are needed for manual testing by reviewers, include them
    here.
    
    ### Issue(s) this completes
    
    Fixes #<issue_number>; Fixes #<issue_number>...

--- server/app/services/applicant/AnswerData.java
+++ server/app/services/applicant/AnswerData.java
@@ -172,6 +172,7 @@ public abstract class AnswerData {
       case ADDRESS:
         return applicantQuestion().createAddressQuestion();
       case DROPDOWN:
+      case YES_NO:
       case RADIO_BUTTON:
         return applicantQuestion().createSingleSelectQuestion();
       case FILEUPLOAD:
