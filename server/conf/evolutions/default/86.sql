cacd4178b5de30aea10ef77de4e07af6cff25c3f
tallulahkay <tallulah.kay@gmail.com>
2025-07-21 17:02:43 -0700

    remove evolution

--- server/conf/evolutions/default/86.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-create table if not exists map_data (
-  endpoint TEXT NOT NULL,
-  geojson JSONB NOT NULL,
-  last_updated TIMESTAMP NOT NULL
-);
-
-# --- !Downs
-drop table if exists map_data;
