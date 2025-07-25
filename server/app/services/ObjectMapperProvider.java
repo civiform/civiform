75ddd272c92cbc54beefbdd2a7839e8a57dce655
Tiffany Sonoda <tiffanywlim@gmail.com>
2025-07-25 19:00:48 +0000

    [Date Validation] Install JavaTimeModule in default ObjectMapper

--- server/app/services/ObjectMapperProvider.java
+++ server/app/services/ObjectMapperProvider.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.inject.Provider;
 import play.libs.Json;
 
@@ -24,7 +25,8 @@ public class ObjectMapperProvider implements Provider<ObjectMapper> {
             // This adds support for Optional
             .registerModule(new Jdk8Module())
             // This adds support for ImmutableList, ImmutableSet, etc.
-            .registerModule(new GuavaModule());
+            .registerModule(new GuavaModule())
+            .registerModule(new JavaTimeModule());
 
     // Needs to set to Json helper
     Json.setObjectMapper(mapper);
