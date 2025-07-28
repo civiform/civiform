62e81453149e0966560a575a007dd118f66f79d8
tallulahkay <tallulah.kay@gmail.com>
2025-07-28 11:56:55 -0700

    update row naming

--- server/app/models/GeoJsonDataModel.java
+++ server/app/models/GeoJsonDataModel.java
@@ -3,7 +3,6 @@ package models;
 import io.ebean.Model;
 import io.ebean.annotation.DbJsonB;
 import io.ebean.annotation.WhenCreated;
-import io.ebean.annotation.WhenModified;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import java.time.Instant;
@@ -20,7 +19,11 @@ public class GeoJsonDataModel extends Model {
 
   @Constraints.Required @Setter private String endpoint;
 
-  @WhenModified private Instant updateTime;
+  /**
+   * The timestamp this data was last confirmed to be up to date via the API. If the data has
+   * changed, a new row is created.
+   */
+  @Constraints.Required @Setter private Instant confirmTime;
 
   @WhenCreated private Instant createTime;
 }
