b400da9c7426dc0be0aea53f2f333621dd31e03b
tallulahkay <tallulah.kay@gmail.com>
2025-07-28 13:20:53 -0700

    update comment

--- server/app/assets/javascripts/config.d.ts
+++ server/app/assets/javascripts/config.d.ts
@@ -2,7 +2,7 @@ interface Window {
   app: {
     data: {
       maps: {
-        [id: string]: object // Maps map IDs to GeoJSON strings
+        [id: string]: object // Maps map IDs to GeoJSON objects
       }
     }
   }
