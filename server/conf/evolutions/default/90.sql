b8af91b292c27657a5eade0d118305c64e597ab1
tallulahkay <tallulah.kay@gmail.com>
2025-08-07 10:20:34 -0700

    fix evolutions

--- server/conf/evolutions/default/90.sql
+++ server/conf/evolutions/default/90.sql
@@ -1,11 +1,7 @@
---Add question settings for map questions
-
 # --- !Ups
-alter table if exists questions add question_settings jsonb;
 alter table if exists geo_json_data
 add column if not exists id bigserial primary key not null;
 
 # --- !Downs
-alter table if exists questions drop column question_settings;
 alter table if exists geo_json_data
 drop column if exists id;
