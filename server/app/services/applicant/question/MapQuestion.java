5c71f9160c2d1b75004df4e49d72c059a5957575
tallulahkay <tallulah.kay@gmail.com>
2025-08-06 17:20:06 -0700

    display filters for map question

--- server/app/services/applicant/question/MapQuestion.java
+++ server/app/services/applicant/question/MapQuestion.java
@@ -3,8 +3,11 @@ package services.applicant.question;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import java.util.Locale;
 import services.Path;
 import services.applicant.ValidationErrorMessage;
+import services.question.LocalizedQuestionSetting;
+import services.question.types.MapQuestionDefinition;
 
 // TODO(#11003): Build out map question.
 public final class MapQuestion extends AbstractQuestion {
@@ -29,5 +32,41 @@ public final class MapQuestion extends AbstractQuestion {
   public ImmutableList<Path> getAllPaths() {
     // @TODO(#11003): Return a real value for the map question
     return ImmutableList.of();
+  }
+
+  public MapQuestionDefinition getQuestionDefinition() {
+    return (MapQuestionDefinition) applicantQuestion.getQuestionDefinition();
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getFilters() {
+    return getFilters(applicantQuestion.getApplicant().getApplicantData().preferredLocale());
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getFilters(Locale locale) {
+    return getQuestionDefinition().getFiltersForLocaleOrDefault(locale);
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getNameSetting() {
+    return getFilters(applicantQuestion.getApplicant().getApplicantData().preferredLocale());
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getNameSetting(Locale locale) {
+    return getQuestionDefinition().getFiltersForLocaleOrDefault(locale);
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getAddressSetting() {
+    return getFilters(applicantQuestion.getApplicant().getApplicantData().preferredLocale());
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getAddressSetting(Locale locale) {
+    return getQuestionDefinition().getFiltersForLocaleOrDefault(locale);
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getLocationDetailsUrlSetting() {
+    return getFilters(applicantQuestion.getApplicant().getApplicantData().preferredLocale());
+  }
+
+  public ImmutableList<LocalizedQuestionSetting> getLocationDetailsUrlSetting(Locale locale) {
+    return getQuestionDefinition().getFiltersForLocaleOrDefault(locale);
   }
 }
