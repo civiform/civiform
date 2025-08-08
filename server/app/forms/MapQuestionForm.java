eabcdce423e13914997c6269083caf488ff1d581
tallulahkay <tallulah.kay@gmail.com>
2025-08-07 17:08:28 -0700

    remove unneeded change

--- server/app/forms/MapQuestionForm.java
+++ server/app/forms/MapQuestionForm.java
@@ -1,59 +1,44 @@
 package forms;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.OptionalInt;
-import java.util.stream.Collectors;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import lombok.Getter;
-import lombok.Setter;
-import services.question.QuestionSetting;
 import services.question.types.MapQuestionDefinition;
 import services.question.types.MapQuestionDefinition.MapValidationPredicates;
 import services.question.types.QuestionDefinitionBuilder;
 import services.question.types.QuestionType;
 
-/** Form for {@link QuestionType#MAP} question configuration and settings. */
-@Getter
+// TODO(#11001): Add settings for filters
 public class MapQuestionForm extends QuestionForm {
 
-  @Setter private String geoJsonEndpoint;
+  private String geoJsonEndpoint;
   private OptionalInt maxLocationSelections;
-  @Setter private QuestionSetting locationName;
-  @Setter private QuestionSetting locationAddress;
-  @Setter private QuestionSetting locationDetailsUrl;
-  @Setter private List<QuestionSetting> filters;
-
-  public static final String LOCATION_NAME_DISPLAY = "Name";
-  public static final String LOCATION_ADDRESS_DISPLAY = "Address";
-  public static final String LOCATION_DETAILS_URL_DISPLAY = "URL";
-  private static final List<String> DEFAULT_MAP_QUESTION_KEYS = Arrays.asList(LOCATION_NAME_DISPLAY, LOCATION_ADDRESS_DISPLAY, LOCATION_DETAILS_URL_DISPLAY);
-
 
   public MapQuestionForm() {
     super();
     this.geoJsonEndpoint = "";
     this.maxLocationSelections = OptionalInt.empty();
-    setFormWithDefaultQuestionSettings();
   }
 
   public MapQuestionForm(MapQuestionDefinition qd) {
     super(qd);
     this.geoJsonEndpoint = qd.getMapValidationPredicates().geoJsonEndpoint();
     this.maxLocationSelections = qd.getMapValidationPredicates().maxLocationSelections();
-
-    if (qd.getQuestionSettings().isPresent()) {
-      setFormWithQuestionSettings(qd.getQuestionSettings().get());
-    } else {
-      setFormWithDefaultQuestionSettings();
-    }
   }
 
   @Override
   public QuestionType getQuestionType() {
     return QuestionType.MAP;
+  }
+
+  public String getGeoJsonEndpoint() {
+    return geoJsonEndpoint;
+  }
+
+  public void setGeoJsonEndpoint(String geoJsonEndpoint) {
+    this.geoJsonEndpoint = geoJsonEndpoint;
+  }
+
+  public OptionalInt getMaxLocationSelections() {
+    return maxLocationSelections;
   }
 
   /**
@@ -81,53 +66,6 @@ public class MapQuestionForm extends QuestionForm {
       predicateBuilder.setMaxLocationSelections(getMaxLocationSelections());
     }
 
-    return super.getBuilder()
-        .setValidationPredicates(predicateBuilder.build())
-        .setQuestionSettings(buildQuestionSettings());
+    return super.getBuilder().setValidationPredicates(predicateBuilder.build());
   }
-
-  private void setFormWithDefaultQuestionSettings() {
-    this.locationName = QuestionSettingUtils.emptySetting();
-    this.locationAddress = QuestionSettingUtils.emptySetting();
-    this.locationDetailsUrl = QuestionSettingUtils.emptySetting();
-    this.filters = emptyFilters();
-  }
-
-  public static List<QuestionSetting> emptyFilters() {
-    return ImmutableList.of(
-        QuestionSettingUtils.emptySetting(),
-        QuestionSettingUtils.emptySetting(),
-        QuestionSettingUtils.emptySetting());
-  }
-
-  /** Populates specific map question form fields from persistent {@link QuestionSetting} objects. */
-  private void setFormWithQuestionSettings(ImmutableSet<QuestionSetting> settings) {
-    this.locationName = QuestionSettingUtils.findByDisplayName(settings, LOCATION_NAME_DISPLAY);
-    this.locationAddress = QuestionSettingUtils.findByDisplayName(settings, LOCATION_ADDRESS_DISPLAY);
-    this.locationDetailsUrl = QuestionSettingUtils.findByDisplayName(settings, LOCATION_DETAILS_URL_DISPLAY);
-
-    this.filters =
-        settings.stream()
-            .filter(
-                setting ->
-                    !DEFAULT_MAP_QUESTION_KEYS.contains(setting.localizedSettingDisplayName().getDefault()))
-            .collect(Collectors.toList());
-  }
-
-  /** Returns the form {@link QuestionSetting} objects for database storage. */
-  private ImmutableSet<QuestionSetting> buildQuestionSettings() {
-    ImmutableSet.Builder<QuestionSetting> builder = ImmutableSet.builder();
-
-    builder.add(getLocationName());
-    builder.add(getLocationAddress());
-    builder.add(getLocationDetailsUrl());
-
-    for (QuestionSetting filter : getFilters()) {
-      if (QuestionSettingUtils.isValidSetting(filter)) {
-        builder.add(filter);
-      }
-    }
-    return builder.build();
-  }
-
 }
