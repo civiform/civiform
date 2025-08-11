c49520206d91ac98cdc46f543bc4095c01dc2a89
Tallulah Kay <43218114+tallulahkay@users.noreply.github.com>
2025-08-11 08:01:48 -0700

    [Maps] Create question settings (#11200)
    ### Description
    
    Create an agnostic `question_settings` db column on the `questions`
    table. This will be used for the map question configurable admin
    settings, but is agnostic so it can be used for any question setting
    whose display needs to be named and translated by admins.
    [TDD](https://docs.google.com/document/d/1qFnKE90CRm_H9Iwa1Sx4FpSqT4fp3XCaOcNDS-AgNso/edit?tab=t.ezni65nrvlip#heading=h.lghclbane53b)
    explaining the use of this field for map questions.
    
    Totally open to alternative naming suggestions! `question_metadata` was
    thrown around previously
    
    ## Release notes
    
    Add question settings that allow configuring unique admin inputted
    settings per question.
    
    ### Checklist
    
    #### General
    
    Read the full guidelines for PRs
    [here](https://github.com/civiform/civiform/wiki/Technical-contribution-guide#creating-a-pull-request)
    
    - [x] Added the correct label (see
    [docs](https://github.com/civiform/civiform/wiki/Technical-contribution-guide#adding-appropriate-labels)
    for more info): < feature | enhancement | bug | under-development |
    dependencies | infrastructure | ignore-for-release | database >
    - [x] Assigned to a specific person, `civiform/developers`, or a [more
    specific round-robin
    list](https://github.com/civiform/civiform/wiki/Technical-contribution-guide#adding-reviewers)
    - [x] Added an additional reviewer from `civiform/eng-admin` as FYI (if
    the primary reviewer is not an admin and this PR includes functionality
    changes)
    - [ ] Removed the release notes section if the title is sufficient for
    the release notes description, or put more details in that section.
    - [ ] Created unit and/or browser tests which fail without the change
    (if possible)
    - [ ] Performed manual testing (Chrome and Firefox if it includes
    front-end changes)
    - [ ] Extended the README / documentation, if necessary. For user-facing
    features, consider updating [the user
    docs](https://github.com/civiform/docs). For "under-the-hood" changes or
    things more relevant to developers, consider updating [the dev
    wiki](https://github.com/civiform/civiform/wiki).
    - [ ] Ensured PII wasn't added to any new logs, unless it was guarded by
    `isDevOrStaging`
    - [x] Noted in the PR description which, if any, code in this PR was
    generated by AI.
    
    #### Database evolutions
    
    Read the guidelines
    [here](https://github.com/civiform/civiform/wiki/Database#writing-database-evolutions)
    
    - [x] Assigned two reviewers
    - [x] Guarded against already existing resources using `IF NOT EXISTS`
    and `IF EXISTS`
    - [x] Downs created to undo changes in Ups
    - [x] Every comment in script should begin with -- and not # --- unless
    it denotes Ups or Downs. See
    [here](https://www.playframework.com/documentation/2.9.x/Evolutions) for
    details.
    - [x] Tested both the Downs and the Ups scripts manually (When testing,
    include all comments from the evolution in your test script to ensure
    any syntax errors in the comments are caught.)
    - [x] Data migrations aren't being done (please use a [Durable
    Job](https://github.com/civiform/civiform/wiki/Database#durable-jobs-for-data-updates)
    if doing a data migration)
    - [ ] Update the model documentation in our
    [wiki](https://github.com/civiform/civiform/wiki/Database) if necessary
    
    ### Issue(s) this completes
    
    Fixes #11160

---
+++ server/app/services/question/LocalizedQuestionSetting.java
@@ -0,0 +1,34 @@
+package services.question;
+
+import com.google.auto.value.AutoValue;
+import java.util.Locale;
+import views.components.TextFormatter;
+
+/**
+ * Represents a question setting in a {@link services.question.types.QuestionDefinition} for
+ * question types that support Question Settings, localized to a specific locale.
+ */
+@AutoValue
+public abstract class LocalizedQuestionSetting {
+
+  /** Create a LocalizedQuestionSetting. */
+  public static LocalizedQuestionSetting create(
+      String settingKey, String settingDisplayName, Locale locale) {
+    return new AutoValue_LocalizedQuestionSetting(settingKey, settingDisplayName, locale);
+  }
+
+  /** The key used for the setting. */
+  public abstract String settingKey();
+
+  /** The text strings to display to the user. */
+  public abstract String settingDisplayName();
+
+  /** Sanitized HTML for the option that processes Markdown. */
+  public String formattedSettingDisplayName(String ariaLabelForNewTabs) {
+    return TextFormatter.formatTextToSanitizedHTML(
+        settingDisplayName(), false, false, ariaLabelForNewTabs);
+  }
+
+  /** The locale this option is localized to. */
+  public abstract Locale locale();
+}
