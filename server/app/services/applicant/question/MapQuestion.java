146f3c58766cebec18ba5bc1b56c9b40ef045a8b
tallulahkay <tallulah.kay@gmail.com>
2025-08-12 12:04:10 -0700

    format

--- server/app/services/applicant/question/MapQuestion.java
+++ server/app/services/applicant/question/MapQuestion.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import java.util.Locale;
-import java.util.Optional;
 import services.Path;
 import services.applicant.ValidationErrorMessage;
 import services.question.LocalizedQuestionSetting;
