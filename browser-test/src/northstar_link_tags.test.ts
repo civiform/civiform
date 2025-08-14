a975ec7f40b5fc04cb1775162e87a7a45fa7d528
Mark Buckner <buckner.a.mark@gmail.com>
2025-08-14 16:20:25 -0600

    remove favicon url except for application.conf; refactor applicant favicon logic; add northstar favicon

---
+++ browser-test/src/northstar_link_tags.test.ts
@@ -0,0 +1,17 @@
+import {test, expect} from './support/civiform_fixtures'
+import {enableFeatureFlag} from './support'
+
+test.describe('navigating to a deep link', {tag: ['@northstar']}, () => {
+  test.beforeEach(async ({page}) => {
+    await enableFeatureFlag(page, 'north_star_applicant_ui')
+  })
+
+  test('has favicon link', async ({page, applicantQuestions}) => {
+    await applicantQuestions.gotoApplicantHomePage()
+
+    const linkTagLocator = page.locator('link[rel="icon"]')
+
+    await expect(linkTagLocator).toHaveAttribute('href')
+    expect(await linkTagLocator.getAttribute('href')).not.toBeNull()
+  })
+})
