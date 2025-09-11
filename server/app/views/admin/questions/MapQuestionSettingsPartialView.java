6ca37863ed4fe9670d95f97287d6033cd2937f01
tallulahkay <tallulah.kay@gmail.com>
2025-09-11 14:51:20 -0700

    remove unneeded changes

--- server/app/views/admin/questions/MapQuestionSettingsPartialView.java
+++ server/app/views/admin/questions/MapQuestionSettingsPartialView.java
@@ -3,10 +3,8 @@ package views.admin.questions;
 import javax.inject.Inject;
 import modules.ThymeleafModule;
 import org.thymeleaf.TemplateEngine;
-import play.mvc.Http;
 import services.settings.SettingsManifest;
 import views.admin.BaseView;
-import views.components.Icons;
 
 public final class MapQuestionSettingsPartialView
     extends BaseView<MapQuestionSettingsPartialViewModel> {
@@ -21,11 +19,5 @@ public final class MapQuestionSettingsPartialView
   @Override
   protected String pageTemplate() {
     return "admin/questions/MapQuestionSettingsPartial.html";
-  }
-
-  @Override
-  protected void customizeContext(
-      Http.Request request, ThymeleafModule.PlayThymeleafContext context) {
-    context.setVariable("deleteIcon", Icons.DELETE);
   }
 }
