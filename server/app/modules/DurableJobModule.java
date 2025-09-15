42a3188c58712c8984a4933dd7c47e6f6a8bc792
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2025-09-15 16:17:36 -0700

    Revert "Removing One time job - UpdateLastActivityTime " (#11492)

--- server/app/modules/DurableJobModule.java
+++ server/app/modules/DurableJobModule.java
@@ -25,6 +25,7 @@ import durablejobs.jobs.OldJobCleanupJob;
 import durablejobs.jobs.ReportingDashboardMonthlyRefreshJob;
 import durablejobs.jobs.UnusedAccountCleanupJob;
 import durablejobs.jobs.UnusedProgramImagesCleanupJob;
+import durablejobs.jobs.UpdateLastActivityTimeForAccounts;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Random;
@@ -198,6 +199,11 @@ public final class DurableJobModule extends AbstractModule {
                 persistedDurableJob,
                 mapperProvider.get(),
                 categoryTranslationFileParser));
+
+    durableJobRegistry.registerStartupJob(
+        DurableJobName.UPDATE_LAST_ACTIVITY_TIME_FOR_ACCOUNTS,
+        JobType.RUN_ONCE,
+        UpdateLastActivityTimeForAccounts::new);
 
     return durableJobRegistry;
   }
