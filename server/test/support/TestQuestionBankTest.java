41467b8e0557a2a2b896daa7ce930a7b1e75e338
Osa Julian <osao@google.com>
2025-09-16 02:43:50 +0000

    test: Add tests for new `TestQuestionBank` helper methods
    - Add tests for static `createQuestionDefinition` methods
    - Add tests for instance `createCustomQuestion` methods
    - Ensure proper coverage of new helper methods

--- server/test/support/TestQuestionBankTest.java
+++ server/test/support/TestQuestionBankTest.java
@@ -2,12 +2,17 @@ package support;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Optional;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import models.QuestionModel;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import services.question.QuestionOption;
+import services.question.types.MultiOptionQuestionDefinition;
+import services.question.types.QuestionDefinition;
 import services.question.types.QuestionType;
 
 @RunWith(JUnitParamsRunner.class)
@@ -44,5 +49,122 @@ public class TestQuestionBankTest {
     }
 
     assertThat(testQuestionBank.getSampleQuestionsForAllTypes().get(questionType)).isNotNull();
+  }
+
+  @Test
+  public void createQuestionDefinition_createsQuestionWithSpecificId() {
+    QuestionDefinition question =
+        TestQuestionBank.createQuestionDefinition(
+            "test-question", 123L, QuestionType.TEXT, Optional.empty());
+
+    assertThat(question.getId()).isEqualTo(123L);
+    assertThat(question.getName()).isEqualTo("test-question");
+    assertThat(question.getQuestionType()).isEqualTo(QuestionType.TEXT);
+    assertThat(question.getEnumeratorId()).isEmpty();
+  }
+
+  @Test
+  public void createQuestionDefinition_withEnumeratorId_setsEnumeratorId() {
+    QuestionDefinition question =
+        TestQuestionBank.createQuestionDefinition(
+            "repeated-question", 456L, QuestionType.TEXT, Optional.of(789L));
+
+    assertThat(question.getId()).isEqualTo(456L);
+    assertThat(question.getName()).isEqualTo("repeated-question");
+    assertThat(question.getEnumeratorId()).hasValue(789L);
+  }
+
+  @Test
+  public void createYesNoQuestionDefinition_createsYesNoWithCustomOptions() {
+    QuestionDefinition question =
+        TestQuestionBank.createYesNoQuestionDefinition(
+            "yes-no-question", 100L, ImmutableList.of("yes", "no", "maybe"));
+
+    assertThat(question.getId()).isEqualTo(100L);
+    assertThat(question.getName()).isEqualTo("yes-no-question");
+    assertThat(question.getQuestionType()).isEqualTo(QuestionType.YES_NO);
+
+    MultiOptionQuestionDefinition multiOption = (MultiOptionQuestionDefinition) question;
+    ImmutableList<QuestionOption> options = multiOption.getOptions();
+    assertThat(options).hasSize(3);
+    assertThat(options.stream().map(QuestionOption::adminName))
+        .containsExactly("yes", "no", "maybe");
+  }
+
+  @Test
+  public void createDropdownQuestionDefinition_createsDropdownWithCustomOptions() {
+    QuestionDefinition question =
+        TestQuestionBank.createDropdownQuestionDefinition(
+            "dropdown-question", 200L, ImmutableList.of("option1", "option2", "option3"));
+
+    assertThat(question.getId()).isEqualTo(200L);
+    assertThat(question.getName()).isEqualTo("dropdown-question");
+    assertThat(question.getQuestionType()).isEqualTo(QuestionType.DROPDOWN);
+
+    MultiOptionQuestionDefinition multiOption = (MultiOptionQuestionDefinition) question;
+    ImmutableList<QuestionOption> options = multiOption.getOptions();
+    assertThat(options).hasSize(3);
+    assertThat(options.stream().map(QuestionOption::adminName))
+        .containsExactly("option1", "option2", "option3");
+  }
+
+  @Test
+  public void createCustomYesNoQuestion_createsQuestionModel() {
+    QuestionModel question =
+        testQuestionBank.createCustomYesNoQuestion("custom-yes-no", ImmutableList.of("yes", "no"));
+
+    assertThat(question).isNotNull();
+    assertThat(question.getQuestionDefinition().getName()).isEqualTo("custom-yes-no");
+    assertThat(question.getQuestionDefinition().getQuestionType()).isEqualTo(QuestionType.YES_NO);
+  }
+
+  @Test
+  public void createCustomDropdownQuestion_createsQuestionModel() {
+    QuestionModel question =
+        testQuestionBank.createCustomDropdownQuestion(
+            "custom-dropdown", ImmutableList.of("a", "b", "c"));
+
+    assertThat(question).isNotNull();
+    assertThat(question.getQuestionDefinition().getName()).isEqualTo("custom-dropdown");
+    assertThat(question.getQuestionDefinition().getQuestionType()).isEqualTo(QuestionType.DROPDOWN);
+  }
+
+  @Test
+  public void createCustomTextQuestion_createsTextQuestionModel() {
+    QuestionModel question = testQuestionBank.createCustomTextQuestion("custom-text");
+
+    assertThat(question).isNotNull();
+    assertThat(question.getQuestionDefinition().getName()).isEqualTo("custom-text");
+    assertThat(question.getQuestionDefinition().getQuestionType()).isEqualTo(QuestionType.TEXT);
+    assertThat(question.getQuestionDefinition().getEnumeratorId()).isEmpty();
+  }
+
+  @Test
+  public void createCustomTextQuestion_withEnumerator_setsEnumeratorId() {
+    QuestionModel question =
+        testQuestionBank.createCustomTextQuestion("custom-repeated-text", Optional.of(999L));
+
+    assertThat(question).isNotNull();
+    assertThat(question.getQuestionDefinition().getName()).isEqualTo("custom-repeated-text");
+    assertThat(question.getQuestionDefinition().getEnumeratorId()).hasValue(999L);
+  }
+
+  @Test
+  public void createCustomAddressQuestion_createsAddressQuestionModel() {
+    QuestionModel question = testQuestionBank.createCustomAddressQuestion("custom-address");
+
+    assertThat(question).isNotNull();
+    assertThat(question.getQuestionDefinition().getName()).isEqualTo("custom-address");
+    assertThat(question.getQuestionDefinition().getQuestionType()).isEqualTo(QuestionType.ADDRESS);
+  }
+
+  @Test
+  public void createCustomEnumeratorQuestion_createsEnumeratorQuestionModel() {
+    QuestionModel question = testQuestionBank.createCustomEnumeratorQuestion("custom-enumerator");
+
+    assertThat(question).isNotNull();
+    assertThat(question.getQuestionDefinition().getName()).isEqualTo("custom-enumerator");
+    assertThat(question.getQuestionDefinition().getQuestionType())
+        .isEqualTo(QuestionType.ENUMERATOR);
   }
 }
