0d23b783603e06a3cac7511b911e4fc1d615ebbd
Tiffany Sonoda <tiffanywlim@gmail.com>
2025-10-06 18:37:13 -0400

    [Expanded logic] Edit condition card (#11561)

---
+++ server/app/views/admin/programs/predicates/EditConditionPartialViewModel.java
@@ -0,0 +1,5 @@
+package views.admin.programs.predicates;
+
+/** Model for rendering the EditConditionPartial.html */
+public record EditConditionPartialViewModel(long conditionId)
+    implements EditPredicateBaseViewModel {}
