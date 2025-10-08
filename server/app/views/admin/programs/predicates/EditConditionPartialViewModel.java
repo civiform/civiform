9c78a20e4dff68a8deb006e7202a6dd4058bba10
Tiffany Sonoda <tiffanywlim@gmail.com>
2025-10-08 16:16:39 -0400

    [Expanded logic] Populate available questions (#11588)

--- server/app/views/admin/programs/predicates/EditConditionPartialViewModel.java
+++ server/app/views/admin/programs/predicates/EditConditionPartialViewModel.java
@@ -1,5 +1,21 @@
 package views.admin.programs.predicates;
 
+import com.google.common.collect.ImmutableList;
+import controllers.admin.routes;
+import services.program.predicate.PredicateUseCase;
+import services.question.types.QuestionDefinition;
+
 /** Model for rendering the EditConditionPartial.html */
-public record EditConditionPartialViewModel(long conditionId)
-    implements EditPredicateBaseViewModel {}
+public record EditConditionPartialViewModel(
+    long programId,
+    long blockId,
+    PredicateUseCase predicateUseCase,
+    long conditionId,
+    ImmutableList<QuestionDefinition> questions)
+    implements EditPredicateBaseViewModel {
+  public String hxEditConditionEndpoint() {
+    return routes.AdminProgramBlockPredicatesController.hxEditCondition(
+            programId, blockId, predicateUseCase.name())
+        .url();
+  }
+}
