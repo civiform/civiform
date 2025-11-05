e46959598f36353bacd72e6e5e6afcdc4fd38e29
swatkat1 <swathi.murthi@seattle.gov>
2025-11-05 10:07:15 -0800

    updating sql comment

--- server/conf/evolutions/default/93.sql
+++ server/conf/evolutions/default/93.sql
@@ -1,4 +1,4 @@
-# --- Allow programs to specify their display mode
+# --- Specificies if the program is available to logged in applicants only
 # --- !Ups
 ALTER TABLE IF EXISTS programs
 ADD COLUMN IF NOT EXISTS login_only boolean DEFAULT FALSE NOT NULL;
