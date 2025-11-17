a575e984d38da6636e0669fd6efbb43bb30712db
tallulahkay <tallulah.kay@gmail.com>
2025-11-17 13:42:24 -0800

    make map question ff admin writeable

--- server/app/services/settings/SettingsManifest.java
+++ server/app/services/settings/SettingsManifest.java
@@ -1103,8 +1103,8 @@ public final class SettingsManifest extends AbstractSettingsManifest {
    * (NOT FOR PRODUCTION USE) Enable allowing CiviForm admins to add a map question to their
    * programs.
    */
-  public boolean getMapQuestionEnabled() {
-    return getBool("MAP_QUESTION_ENABLED");
+  public boolean getMapQuestionEnabled(RequestHeader request) {
+    return getBool("MAP_QUESTION_ENABLED", request);
   }
 
   /**
@@ -2385,7 +2385,7 @@ public final class SettingsManifest extends AbstractSettingsManifest {
                               + " question to their programs.",
                           /* isRequired= */ false,
                           SettingType.BOOLEAN,
-                          SettingMode.ADMIN_READABLE),
+                          SettingMode.ADMIN_WRITEABLE),
                       SettingDescription.create(
                           "SETTINGS_CACHE_ENABLED",
                           "(NOT FOR PRODUCTION USE) Enables reading settings from the cache instead"
