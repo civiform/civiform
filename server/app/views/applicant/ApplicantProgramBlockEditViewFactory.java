bc5bbb7309b45e7dc4802843b6d4342749ed7e77
Tallulah Kay <43218114+tallulahkay@users.noreply.github.com>
2025-12-01 10:08:37 -0800

    [North Star] Deprecate legacy ApplicantProgramBlockEditView (#11993)

--- server/app/views/applicant/ApplicantProgramBlockEditViewFactory.java
+++
@@ -1,8 +0,0 @@
-package views.applicant;
-
-import views.questiontypes.ApplicantQuestionRendererFactory;
-
-public interface ApplicantProgramBlockEditViewFactory {
-
-  ApplicantProgramBlockEditView create(ApplicantQuestionRendererFactory rendererFactory);
-}
