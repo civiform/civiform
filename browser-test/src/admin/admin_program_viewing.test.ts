47e2542733c36d3134b4f16e635711cc6870c6f3
swathi <swathi.murthi@seattle.gov>
2026-01-14 15:41:22 -0800

    PR feedback

--- browser-test/src/admin/admin_program_viewing.test.ts
+++ browser-test/src/admin/admin_program_viewing.test.ts
@@ -5,6 +5,8 @@ import {
   logout,
   loginAsTestUser,
   validateScreenshot,
+  testUserDisplayName,
+  loginAsProgramAdmin,
 } from '../support'
 import {
   ProgramCategories,
@@ -269,45 +271,78 @@ test.describe('admin program view page', () => {
       ProgramHeaderButton.DOWNLOAD_PDF_PREVIEW,
     )
     await adminPrograms.expectBlockPanelHidden()
+    await logout(page)
   })
-})
 
-test.describe('admin program view long paragraph answer', () => {
-  const programName = 'longTextProgram'
-  const longParagraph =
-    'Fur seals and sea lions make up the family Otariidae. Along with the Phocidae and Odobenidae, ottariids are pinnipeds descending from a common ancestor most closely related to modern bears (as hinted by the subfamily Arctocephalinae, meaning "bear-headed"). The name pinniped refers to mammals with front and rear flippers. Otariids arose about 15-17 million years ago in the Miocene, and were originally land mammals that rapidly diversified and adapted to a marine environment, giving rise to the semiaquatic marine mammals that thrive today. Fur seals and sea lions are closely related and commonly known together as the "eared seals". Until recently, fur seals were all grouped under a single subfamily of Pinnipedia, called the Arctocephalinae, to contrast them with Otariinae – the sea lions – based on the most prominent common feature, namely the coat of dense underfur intermixed with guard hairs. Recent genetic evidence, however, suggests Callorhinus is more closely related to some sea lion species, and the fur seal/sea lion subfamily distinction has been eliminated from many taxonomies. Nonetheless, all fur seals have certain features in common: the fur, generally smaller sizes, farther and longer foraging trips, smaller and more abundant prey items, and greater sexual dimorphism. For these reasons, the distinction remains useful. Fur seals comprise two genera: Callorhinus, and Arctocephalus. Callorhinus is represented by just one species in the Northern Hemisphere, the northern fur seal (Callorhinus ursinus), and Arctocephalus is represented by eight species in the Southern Hemisphere. The southern fur seals comprising the genus Arctocephalus include Antarctic fur seals, Galapagos fur seals, Juan Fernandez fur seals, New Zealand fur seals, brown fur seals, South American fur seals, and subantarctic fur seals.'
+  test('admin views application with long text answer', async ({
+    page,
+    adminPrograms,
+    adminQuestions,
+    applicantQuestions,
+  }) => {
+    const ProgramWithLongText = 'longTextProgram'
+    const longParagraph =
+      'Fur seals and sea lions make up the family Otariidae. Along with the Phocidae and Odobenidae, ottariids are pinnipeds descending from a common ancestor most closely related to modern bears (as hinted by the subfamily Arctocephalinae, meaning "bear-headed"). The name pinniped refers to mammals with front and rear flippers. Otariids arose about 15-17 million years ago in the Miocene, and were originally land mammals that rapidly diversified and adapted to a marine environment, giving rise to the semiaquatic marine mammals that thrive today. Fur seals and sea lions are closely related and commonly known together as the "eared seals". Until recently, fur seals were all grouped under a single subfamily of Pinnipedia, called the Arctocephalinae, to contrast them with Otariinae – the sea lions – based on the most prominent common feature, namely the coat of dense underfur intermixed with guard hairs. Recent genetic evidence, however, suggests Callorhinus is more closely related to some sea lion species, and the fur seal/sea lion subfamily distinction has been eliminated from many taxonomies. Nonetheless, all fur seals have certain features in common: the fur, generally smaller sizes, farther and longer foraging trips, smaller and more abundant prey items, and greater sexual dimorphism. For these reasons, the distinction remains useful. Fur seals comprise two genera: Callorhinus, and Arctocephalus. Callorhinus is represented by just one species in the Northern Hemisphere, the northern fur seal (Callorhinus ursinus), and Arctocephalus is represented by eight species in the Southern Hemisphere. The southern fur seals comprising the genus Arctocephalus include Antarctic fur seals, Galapagos fur seals, Juan Fernandez fur seals, New Zealand fur seals, brown fur seals, South American fur seals, and subantarctic fur seals.'
 
-  test.beforeEach(
-    async ({page, adminPrograms, adminQuestions, applicantQuestions}) => {
-      await test.step('create a new program', async () => {
-        await loginAsAdmin(page)
-        await adminPrograms.addProgram(programName)
+    await test.step('create a new program', async () => {
+      await loginAsAdmin(page)
+      await adminPrograms.addProgram(ProgramWithLongText)
 
-        await adminQuestions.addTextQuestion({
-          questionName: 'long-text',
-          questionText: 'long text question',
-        })
-        await adminPrograms.editProgramBlockUsingSpec(programName, {
-          description: 'First block',
-          questions: [{name: 'long-text', isOptional: false}],
-        })
-        await adminPrograms.publishAllDrafts()
-        await logout(page)
+      await adminQuestions.addTextQuestion({
+        questionName: 'long-text',
+        questionText: 'long text question',
+      })
+      await adminQuestions.addAddressQuestion({questionName: 'address-1'})
+      await adminQuestions.addDateQuestion({questionName: 'date-2'})
+      await adminQuestions.addEmailQuestion({questionName: 'email-3'})
+
+      await adminPrograms.editProgramBlockUsingSpec(ProgramWithLongText, {
+        name: 'First Block',
+        description: 'First block',
+        questions: [{name: 'long-text', isOptional: false}],
       })
 
-      await test.step('Applicant applies and answers with long text', async () => {
-        await loginAsTestUser(page)
-        await page.goto(`/programs/${programName}`)
-        await applicantQuestions.answerTextQuestion(longParagraph)
-        await applicantQuestions.clickSubmitApplication()
-        await logout(page)
+      await adminPrograms.editProgramBlockUsingSpec(ProgramWithLongText, {
+        name: 'Second Block',
+        description: 'Second block',
+        questions: [
+          {name: 'address-1', isOptional: false},
+          {name: 'date-2', isOptional: false},
+          {name: 'email-3', isOptional: false},
+        ],
       })
+      await adminPrograms.publishAllDrafts()
+      await logout(page)
+    })
 
-      await test.step('Admin views applications and verifies answer', async () => {
-        await loginAsAdmin(page)
-        await adminPrograms.viewApplications(programName)
-        await expect(page.getByText(longParagraph)).toBeVisible()
-      })
-    },
-  )
+    await test.step('Applicant applies and answers with long text', async () => {
+      await loginAsTestUser(page)
+      await applicantQuestions.applyProgram(ProgramWithLongText)
+      await applicantQuestions.answerTextQuestion(longParagraph)
+      await applicantQuestions.answerAddressQuestion(
+        '1234 St',
+        'Unit B',
+        'Sim',
+        'WA',
+        '54321',
+      )
+      await applicantQuestions.answerMemorableDateQuestion(
+        '2025',
+        '05 - May',
+        '2',
+      )
+      await applicantQuestions.answerEmailQuestion('test@example.com')
+      await applicantQuestions.clickContinue()
+      await applicantQuestions.submitFromReviewPage()
+      await logout(page)
+    })
+
+    await test.step('Admin views applications and verifies answer', async () => {
+      await loginAsProgramAdmin(page)
+      await adminPrograms.viewApplications(ProgramWithLongText)
+      await adminPrograms.viewApplicationForApplicant(testUserDisplayName())
+
+      await validateScreenshot(page, 'admin-view-application-long-text-answer')
+    })
+  })
 })
