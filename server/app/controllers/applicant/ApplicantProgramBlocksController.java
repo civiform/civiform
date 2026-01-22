7c28dca0cf93d8f1120fc4b452e2cd8ff0ef6268
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-01-22 16:13:18 -0500

    Small cleanups

--- server/app/controllers/applicant/ApplicantProgramBlocksController.java
+++ server/app/controllers/applicant/ApplicantProgramBlocksController.java
@@ -1129,7 +1129,7 @@ public final class ApplicantProgramBlocksController extends CiviFormController {
                           }
                         }
 
-                        return ensureFileRecordExists(key.get(), originalFileName)
+                        return ensureFileRecordExists(key.get(), originalFileName, applicantId)
                             .thenComposeAsync(
                                 (StoredFileModel unused) ->
                                     applicantService.stageAndUpdateIfValid(
@@ -1733,7 +1733,7 @@ public final class ApplicantProgramBlocksController extends CiviFormController {
 
   /** Gets StoredFileModel for {@code key}, creating one if it is not present. */
   private CompletionStage<StoredFileModel> ensureFileRecordExists(
-      String key, Optional<String> originalFileName) {
+      String key, Optional<String> originalFileName, long applicantId) {
     return storedFileRepository
         .lookupFile(key)
         .thenComposeAsync(
