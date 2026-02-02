419aea9871c0166682491026ed47d0fbdaa766c1
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-02-02 09:56:06 -0500

    add and update tests

--- server/test/services/cloud/ApplicantFileNameFormatterTest.java
+++ server/test/services/cloud/ApplicantFileNameFormatterTest.java
@@ -6,32 +6,42 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.Test;
 
 public class ApplicantFileNameFormatterTest {
+  private static final String FILE_NAME =
+      ApplicantFileNameFormatter.formatFileUploadQuestionFilename(
+          /* applicantId= */ 1L, /* programId= */ 2L, /* blockId= */ "3-4");
 
   @Test
   public void formatFileUploadQuestionFilename() {
-    assertThat(ApplicantFileNameFormatter.formatFileUploadQuestionFilename(1L, 2L, "3-4"))
-        .isEqualTo("applicant-1/program-2/block-3-4/${filename}");
+    assertThat(FILE_NAME).isEqualTo("applicant-1/program-2/block-3-4/${filename}");
   }
 
   @Test
   public void isApplicantOwnedFileKey_applicantIdMatches_isTrue() {
+    assertThat(ApplicantFileNameFormatter.isApplicantOwnedFileKey(FILE_NAME, /* applicantId= */ 1L))
+        .isTrue();
+  }
+
+  @Test
+  public void isApplicantOwnedFileKey_applicantIdInFileNameOnly_isFalse() {
+    // Must match at the start.
     assertThat(
             ApplicantFileNameFormatter.isApplicantOwnedFileKey(
-                "applicant-1/program-2/block-3-4/${filename}", 1L))
-        .isEqualTo(true);
+                "applicant-9/program-2/block-3-4/applicant-1", /* applicantId= */ 1L))
+        .isFalse();
   }
 
   @Test
   public void isApplicantOwnedFileKey_applicantIdDoesNotMatch_isFalse() {
-    assertThat(
-            ApplicantFileNameFormatter.isApplicantOwnedFileKey(
-                "applicant-1/program-2/block-3-4/${filename}", 2L))
-        .isEqualTo(false);
+    assertThat(ApplicantFileNameFormatter.isApplicantOwnedFileKey(FILE_NAME, /* applicantId= */ 2L))
+        .isFalse();
   }
 
   @Test
   public void isApplicantOwnedFileKey_badFileKey() {
-    assertThatThrownBy(() -> ApplicantFileNameFormatter.isApplicantOwnedFileKey("", 1L))
+    assertThatThrownBy(
+            () ->
+                ApplicantFileNameFormatter.isApplicantOwnedFileKey(
+                    /* fileKey= */ "", /* applicantId= */ 1L))
         .isInstanceOf(IllegalArgumentException.class);
   }
 
@@ -40,7 +50,7 @@ public class ApplicantFileNameFormatterTest {
     assertThatThrownBy(
             () ->
                 ApplicantFileNameFormatter.isApplicantOwnedFileKey(
-                    "applicant-1/program-2/block-3-4/${filename}", 0))
+                    "applicant-1/program-2/block-3-4/${filename}", /* applicantId= */ 0))
         .isInstanceOf(IllegalArgumentException.class);
   }
 }
