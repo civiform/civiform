c16ea419a03efaa1e3ff9f438c248b8745425ac1
shane-exygy <shane.mcdaniel@exygy.com>
2026-02-19 10:25:13 -0500

    Update server/app/auth/oidc/CiviformOidcProfileCreator.java
    Co-authored-by: rocky <rachelefine@gmail.com>

--- server/app/auth/oidc/CiviformOidcProfileCreator.java
+++ server/app/auth/oidc/CiviformOidcProfileCreator.java
@@ -230,7 +230,7 @@ public abstract class CiviformOidcProfileCreator extends OidcProfileCreator {
   /**
    * If the CiviForm user is a TI or Admin, and not a standard user, log and return as such.
    *
-   * @return if the guest should not be used based on the logged-in users type being not a standard
+   * @return whether or not the guest should be used based on the logged-in users type
    *     applicant user type.
    */
   @VisibleForTesting
