d88d447ee0c1a273612bfda5e4a4839e2e5ccdff
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-03-10 19:13:05 -0400

    add index

--- server/conf/evolutions/default/94.sql
+++ server/conf/evolutions/default/94.sql
@@ -4,6 +4,11 @@
 ALTER TABLE IF EXISTS applications
 ADD COLUMN IF NOT EXISTS original_applicant_id bigint;
 
+CREATE INDEX IF NOT EXISTS index_applications_original_applicant_id
+ON applications (original_applicant_id);
+
 # --- !Downs
+DROP INDEX IF EXISTS index_applications_original_applicant_id;
+
 ALTER TABLE IF EXISTS applications
 DROP COLUMN IF EXISTS original_applicant_id;
