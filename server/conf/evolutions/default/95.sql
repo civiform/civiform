c35b19eb170a3f2b66f96758fcda579a428513a1
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-04-09 15:09:27 -0400

    update name

--- server/conf/evolutions/default/95.sql
+++ server/conf/evolutions/default/95.sql
@@ -1,6 +1,7 @@
 # --- Add unique index on files.name with varchar_pattern_ops to support prefix queries
+# --- https://www.postgresql.org/docs/8.4/indexes-opclass.html
 # --- !Ups
-CREATE UNIQUE INDEX IF NOT EXISTS index_file_name_pattern ON files (name varchar_pattern_ops);
+CREATE UNIQUE INDEX IF NOT EXISTS index_file_name_prefix_pattern ON files (name varchar_pattern_ops);
 
 # --- !Downs
-DROP INDEX IF EXISTS index_file_name_pattern;
+DROP INDEX IF EXISTS index_file_name_prefix_pattern;
