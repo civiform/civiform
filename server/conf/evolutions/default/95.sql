6c4cfa0f1ce0b63cfc8906e72b7d1b1a188ff744
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-04-14 17:17:27 -0400

    update evolution comment style

--- server/conf/evolutions/default/95.sql
+++ server/conf/evolutions/default/95.sql
@@ -1,5 +1,6 @@
-# --- Add unique index on files.name with varchar_pattern_ops to support prefix queries
-# --- https://www.postgresql.org/docs/8.4/indexes-opclass.html
+-- Add unique index on files.name with varchar_pattern_ops to support prefix queries
+-- https://www.postgresql.org/docs/8.4/indexes-opclass.html
+
 # --- !Ups
 CREATE UNIQUE INDEX IF NOT EXISTS index_file_name_prefix_pattern ON files (name varchar_pattern_ops);
 
