b05a4b165d2d4dcb7150b7e2a11fefbe0f3db9f9
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-04-15 15:13:33 -0400

    drop previous index in favor of new

--- server/conf/evolutions/default/95.sql
+++ server/conf/evolutions/default/95.sql
@@ -2,6 +2,9 @@
 -- https://www.postgresql.org/docs/8.4/indexes-opclass.html
 # --- !Ups
 CREATE UNIQUE INDEX IF NOT EXISTS index_file_name_prefix_pattern ON files (name varchar_pattern_ops);
+DROP INDEX IF EXISTS index_file_names
 
 # --- !Downs
+-- From evolution 39, added IF NOT EXISTS.
+CREATE UNIQUE INDEX IF NOT EXISTS index_file_names ON files (name);
 DROP INDEX IF EXISTS index_file_name_prefix_pattern;
