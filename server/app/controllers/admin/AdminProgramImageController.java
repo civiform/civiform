9797a1cf184692ba70d27cf6e72d6ca4909c6206
swathi <swathi.murthi@seattle.gov>
2026-05-18 12:21:33 -0700

    temp

--- server/app/controllers/admin/AdminProgramImageController.java
+++ server/app/controllers/admin/AdminProgramImageController.java
@@ -9,8 +9,10 @@ import controllers.FlashKey;
 import forms.admin.ProgramImageDescriptionForm;
 import javax.inject.Inject;
 import org.pac4j.play.java.Secure;
+import parsers.StreamingMultipartUploadResult;
 import play.data.Form;
 import play.data.FormFactory;
+import play.mvc.BodyParser;
 import play.mvc.Http;
 import play.mvc.Result;
 import repository.VersionRepository;
@@ -24,6 +26,10 @@ import views.admin.programs.ProgramEditStatus;
 import views.admin.programs.ProgramImagePageView;
 import views.admin.programs.ProgramImagePageViewModel;
 import views.admin.programs.ProgramImageView;
+import parsers.admin.ProgramImageStreamingMultipartBodyParser;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 /** Controller for displaying and modifying the image (and alt text) associated with a program. */
 public final class AdminProgramImageController extends CiviFormController {
@@ -37,15 +43,15 @@ public final class AdminProgramImageController extends CiviFormController {
 
   @Inject
   public AdminProgramImageController(
-      PublicStorageClient publicStorageClient,
-      ProgramService programService,
-      ProgramImageView programImageView,
-      ProgramImagePageView programImagePageView,
-      SettingsManifest settingsManifest,
-      RequestChecker requestChecker,
-      FormFactory formFactory,
-      ProfileUtils profileUtils,
-      VersionRepository versionRepository) {
+    PublicStorageClient publicStorageClient,
+    ProgramService programService,
+    ProgramImageView programImageView,
+    ProgramImagePageView programImagePageView,
+    SettingsManifest settingsManifest,
+    RequestChecker requestChecker,
+    FormFactory formFactory,
+    ProfileUtils profileUtils,
+    VersionRepository versionRepository) {
     super(profileUtils, versionRepository);
     this.publicStorageClient = checkNotNull(publicStorageClient);
     this.programService = checkNotNull(programService);
@@ -63,32 +69,32 @@ public final class AdminProgramImageController extends CiviFormController {
    */
   @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
   public Result index(Http.Request request, long programId, String editStatus)
-      throws ProgramNotFoundException {
+    throws ProgramNotFoundException {
     requestChecker.throwIfProgramNotDraft(programId);
     if (settingsManifest.getFileUploadQuestionImprovementsEnabled(request)) {
       return ok(programImagePageView.render(request, new ProgramImagePageViewModel()))
-          .as(Http.MimeTypes.HTML);
+        .as(Http.MimeTypes.HTML);
     }
     return ok(
-        programImageView.render(
-            request, programService.getFullProgramDefinition(programId), editStatus));
+      programImageView.render(
+        request, programService.getFullProgramDefinition(programId), editStatus));
   }
 
   @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
   public Result updateDescription(Http.Request request, long programId, String editStatus) {
     requestChecker.throwIfProgramNotDraft(programId);
     Form<ProgramImageDescriptionForm> form =
-        formFactory
-            .form(ProgramImageDescriptionForm.class)
-            .bindFromRequest(
-                request, ProgramImageDescriptionForm.FIELD_NAMES.toArray(new String[0]));
+      formFactory
+        .form(ProgramImageDescriptionForm.class)
+        .bindFromRequest(
+          request, ProgramImageDescriptionForm.FIELD_NAMES.toArray(new String[0]));
     String newDescription = form.get().getSummaryImageDescription();
 
     String toastType;
     String toastMessage;
     try {
       programService.setSummaryImageDescription(
-          programId, LocalizedStrings.DEFAULT_LOCALE, newDescription);
+        programId, LocalizedStrings.DEFAULT_LOCALE, newDescription);
       toastType = "success";
       if (newDescription.isBlank()) {
         toastMessage = "Image description removed";
@@ -108,27 +114,27 @@ public final class AdminProgramImageController extends CiviFormController {
 
   @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
   public Result updateFileKey(Http.Request request, long programId, String editStatus)
-      throws ProgramNotFoundException {
+    throws ProgramNotFoundException {
     requestChecker.throwIfProgramNotDraft(programId);
 
     // We need to check that the bucket exists even if we don't use it so that we know the request
     // was formatted correctly.
     @SuppressWarnings("unused")
     String bucket =
-        request
-            .queryString("bucket")
-            .orElseThrow(() -> new IllegalArgumentException("Request must contain bucket name"));
+      request
+        .queryString("bucket")
+        .orElseThrow(() -> new IllegalArgumentException("Request must contain bucket name"));
     if (!bucket.equals(publicStorageClient.getBucketName())) {
       throw new IllegalArgumentException(
-          String.format(
-              "The bucket name in the request [%s] doesn't match the public bucket name [%s]",
-              bucket, publicStorageClient.getBucketName()));
+        String.format(
+          "The bucket name in the request [%s] doesn't match the public bucket name [%s]",
+          bucket, publicStorageClient.getBucketName()));
     }
 
     String key =
-        request
-            .queryString("key")
-            .orElseThrow(() -> new IllegalArgumentException("Request must contain file key name"));
+      request
+        .queryString("key")
+        .orElseThrow(() -> new IllegalArgumentException("Request must contain file key name"));
     // Note: If Azure support is needed, see ApplicantProgramBlocksController#updateFile for
     // some additional Azure-specific logic that should also be added here.
 
@@ -143,10 +149,48 @@ public final class AdminProgramImageController extends CiviFormController {
 
   @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
   public Result deleteFileKey(Http.Request request, long programId, String editStatus)
-      throws ProgramNotFoundException {
+    throws ProgramNotFoundException {
     requestChecker.throwIfProgramNotDraft(programId);
     programService.deleteSummaryImageFileKey(programId);
     final String indexUrl = routes.AdminProgramImageController.index(programId, editStatus).url();
     return redirect(indexUrl).flashing(FlashKey.SUCCESS, "Image removed");
   }
+
+  @Secure(authorizers = Authorizers.Labels.CIVIFORM_ADMIN)
+  @BodyParser.Of(ProgramImageStreamingMultipartBodyParser.class)
+  public CompletionStage<Result> hxUploadProgramImage(Http.Request request, long programId)
+    throws ProgramNotFoundException {
+    requestChecker.throwIfProgramNotDraft(programId);
+
+    Http.MultipartFormData<String> body = request.body().asMultipartFormData();
+    if (body == null) {
+      return (CompletionStage<Result>) badRequest();
+    }
+
+    Http.MultipartFormData.FilePart<String> filePart = body.getFile("file");
+    if (filePart == null) {
+      return CompletableFuture.completedFuture(badRequest());
+    }
+
+    String originalFileName = filePart.getFilename();
+    String fileKey = filePart.getRef();
+    StreamingMultipartUploadResult uploadResult = ;
+    if (uploadResult.getStatus() === FAILURE) {
+      // return failed htmx partial
+    }
+
+    String fileKey = uploadResult.getStoredFileName();
+
+    try {
+      programService.setSummaryImageFileKey(programId, fileKey); // Save to db
+// differs from applicant file upload in that the file key is stored on the program, not the StoredFileModel record, so we don't have an originalFileName to display
+      ProgramDefinition program = programService.getFullProgramDefinition(programId);
+      // return successful htmx partial
+    } catch (ProgramNotFoundException e) {
+      // handle error
+    }
+  }
+
+
+
 }
--- server/app/parsers/admin/ProgramImageStreamingMultipartBodyParser.java
+++ server/app/parsers/admin/ProgramImageStreamingMultipartBodyParser.java
@@ -1,9 +1,9 @@
 package parsers.admin;
 
-import auth.ProfileFactory;
-import auth.ProfileUtils;
+import com.google.common.collect.ImmutableList;
 import org.apache.pekko.stream.Materializer;
 import org.apache.pekko.util.ByteString;
+import parsers.FileTypeSpecifier;
 import parsers.FileTypeValidation;
 import parsers.StreamingMultipartBodyParser;
 import parsers.cloud.MultipartUploadSinks;
@@ -17,7 +17,6 @@ import services.cloud.PublicFileNameFormatter;
 import services.cloud.BucketType;
 
 import javax.inject.Inject;
-import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,21 +29,21 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * <p>Streams an admin file upload to the public s3 bucket. The cloud-storage file key
  * is generated server-side using {@link PublicFileNameFormatter}.
  */
-public final class ProgramImageStreamingBodyParser extends StreamingMultipartBodyParser {
+public final class ProgramImageStreamingMultipartBodyParser extends StreamingMultipartBodyParser {
 
   public static final long MAX_FILE_SIZE = 1024 * 1024; // 1MB
 
   private static final Set<String> ALLOWED_MIME_TYPES = Set.of("image/jpeg", "image/png");
 
   // Matches "program-summary-image/program-";{programId}/* in the request path.
-  private static final Pattern PROGRAM_BLOCK_PATH_PATTERN =
+  private static final Pattern PROGRAM_IMAGE_PATH_PATTERN =
     Pattern.compile("/programs-summary-image/program-(\\d+)/(/|$)");
 
   private long programId;
 
 
   @Inject
-  public ProgramImageStreamingBodyParser(
+  public ProgramImageStreamingMultipartBodyParser(
     Materializer materializer,
     DefaultHttpErrorHandler errorHandler,
     MultipartUploadSinks streamingMultipartUploadSinks,
@@ -54,16 +53,21 @@ public final class ProgramImageStreamingBodyParser extends StreamingMultipartBod
       errorHandler,
       streamingMultipartUploadSinks,
       fileTypeValidation,
-      MAX_FILE_SIZE,ALLOWED_MIME_TYPES);
+      MAX_FILE_SIZE);
+  }
+
+  @Override
+  protected ImmutableList<FileTypeSpecifier> getAllowedFileTypeSpecifiers() {
+    return FileTypeSpecifier.parseCommaSeparated(ALLOWED_MIME_TYPES.toString());
   }
 
   @Override
   public Accumulator<ByteString, F.Either<Result, Http.MultipartFormData<String>>> apply(
     Http.RequestHeader request) {
-    Matcher matcher = PROGRAM_BLOCK_PATH_PATTERN.matcher(request.path());
+    Matcher matcher = PROGRAM_IMAGE_PATH_PATTERN.matcher(request.path());
     if (!matcher.find()) {
       throw new IllegalStateException(
-        "Request path does not contain program or block ids: " + request.path());
+        "Request path does not contain program image: " + request.path());
     }
     this.programId = Long.parseLong(matcher.group(1));
     return super.apply(request);
