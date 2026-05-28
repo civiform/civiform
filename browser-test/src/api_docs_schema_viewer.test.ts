b83fdf53686608e19b9365b3a227c7089832627f
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-05-28 14:16:33 -0700

    Migrate openapi schema viewer

--- browser-test/src/api_docs_schema_viewer.test.ts
+++ browser-test/src/api_docs_schema_viewer.test.ts
@@ -1,12 +1,14 @@
 import {test, expect} from './support/civiform_fixtures'
-import {loginAsAdmin, waitForPageJsLoad} from './support'
+import {enableFeatureFlag, loginAsAdmin} from './support'
 import {ProgramVisibility} from './support/admin_programs'
+import {ApiSchemaViewerPage} from './page/admin/docs/api_schema_viewer_page'
 
-test.describe('Viewing API docs', () => {
+test.describe('Viewing API schema', () => {
   const program1 = 'comprehensive-sample-program'
   const program2 = 'minimal-sample-program'
 
-  test.beforeEach(async ({seeding}) => {
+  test.beforeEach(async ({page, seeding}) => {
+    await enableFeatureFlag(page, 'ADMIN_UI_MIGRATION_SC_ENABLED')
     await seeding.seedProgramsAndCategories()
   })
 
@@ -15,44 +17,47 @@ test.describe('Viewing API docs', () => {
   })
 
   test('Views OpenApi Schema', async ({page, adminPrograms}) => {
+    const schemaViewerPage = new ApiSchemaViewerPage(page)
+
     await test.step('Login as admin and publish drafts', async () => {
       await page.goto('/')
       await loginAsAdmin(page)
       await adminPrograms.publishAllDrafts()
     })
 
-    await test.step(`Going to slugless route defaults ${program1}`, async () => {
-      await page.goto('/docs/api/schemas')
-      await waitForPageJsLoad(page)
-      await expect(page.getByRole('heading', {name: program1})).toBeAttached()
+    await test.step(`Going to slugless route defaults to ${program1}`, async () => {
+      await schemaViewerPage.goto()
+      await expect(
+        schemaViewerPage.getSwaggerProgramHeading(program1),
+      ).toBeAttached()
     })
 
     await test.step(`Change program to ${program2}`, async () => {
-      await page.getByRole('combobox', {name: 'Program'}).selectOption(program2)
-      await waitForPageJsLoad(page)
-      await expect(page.getByRole('heading', {name: program2})).toBeAttached()
+      await schemaViewerPage.selectProgram(program2)
+      await expect(
+        schemaViewerPage.getSwaggerProgramHeading(program2),
+      ).toBeAttached()
     })
 
     await test.step(`Change status to draft for ${program2} shows error message`, async () => {
-      await page.getByRole('combobox', {name: 'Status'}).selectOption('draft')
-      await waitForPageJsLoad(page)
-      await expect(page.getByTestId('ui-error')).toContainText(
+      await schemaViewerPage.selectStatus('draft')
+      await expect(schemaViewerPage.getErrorMessage()).toContainText(
         'A program with this status could not be found',
       )
     })
 
-    await test.step(`Change status to active for ${program2}`, async () => {
-      await page.getByRole('combobox', {name: 'Status'}).selectOption('active')
-      await waitForPageJsLoad(page)
-      await expect(page.getByRole('heading', {name: program2})).toBeAttached()
+    await test.step(`Change status back to active for ${program2}`, async () => {
+      await schemaViewerPage.selectStatus('active')
+      await expect(
+        schemaViewerPage.getSwaggerProgramHeading(program2),
+      ).toBeAttached()
     })
 
-    await test.step(`Change openapi version to swagger-v2`, async () => {
-      await page
-        .getByRole('combobox', {name: 'OpenApi Version'})
-        .selectOption('swagger-v2')
-      await waitForPageJsLoad(page)
-      await expect(page.getByRole('heading', {name: program2})).toBeAttached()
+    await test.step('Change openapi version to swagger-v2', async () => {
+      await schemaViewerPage.selectOpenApiVersion('swagger-v2')
+      await expect(
+        schemaViewerPage.getSwaggerProgramHeading(program2),
+      ).toBeAttached()
     })
 
     await test.step('Add external program does not show in program options', async () => {
@@ -62,12 +67,11 @@ test.describe('Viewing API docs', () => {
         'https://usa.gov',
         ProgramVisibility.PUBLIC,
       )
-      await page.goto('/docs/api/schemas')
-      await waitForPageJsLoad(page)
+      await schemaViewerPage.goto()
 
-      await expect(
-        page.getByRole('combobox', {name: 'Program'}),
-      ).not.toContainText('external-program-name')
+      await expect(schemaViewerPage.getProgramSelect()).not.toContainText(
+        'external-program-name',
+      )
     })
   })
 })
