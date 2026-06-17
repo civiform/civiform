2fa027517af3a07743ceb5b40c3a63a6ad513fa8
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-06-17 15:12:43 -0400

    test timing flake fix

--- browser-test/src/support/admin_questions.ts
+++ browser-test/src/support/admin_questions.ts
@@ -144,10 +144,9 @@ export class AdminQuestions {
   }
 
   async expectAdminQuestionsPageWithSuccessToast(successText: string) {
-    const toastContainer = await this.page.innerHTML('#toast-container')
-
-    expect(toastContainer).toContain('bg-cf-toast-success')
-    expect(toastContainer).toContain(successText)
+    await expect(
+      this.page.locator('#toast-container .bg-cf-toast-success'),
+    ).toContainText(successText)
     await this.expectAdminQuestionsPage()
   }
 
