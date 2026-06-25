6b29b72dcd3ba7d2eccf0604c394e5a1078c2e17
Lesley Katzen <lkatzen@google.com>
2026-06-25 17:07:25 +0000

    Scope duplicate submission screenshot to dialog element

--- browser-test/src/applicant/applicant_prevent_duplicate_submission.test.ts
+++ browser-test/src/applicant/applicant_prevent_duplicate_submission.test.ts
@@ -62,7 +62,10 @@ test.describe('Prevent Duplicate Submission', () => {
         page.getByText('There are no changes to save for the ' + programName),
       ).toBeVisible()
 
-      await validateScreenshot(page, 'prevent-duplicate-submission')
+      await validateScreenshot(
+        page.getByRole('dialog'),
+        'prevent-duplicate-submission',
+      )
       await validateAccessibility(page)
     })
 
