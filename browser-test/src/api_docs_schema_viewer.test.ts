ce4e6f873754fcaab5cde711e87da86fc443972e
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-07-28 07:52:18 -0700

    Renaming admin ui migration feature flags (#13690)

--- browser-test/src/api_docs_schema_viewer.test.ts
+++ browser-test/src/api_docs_schema_viewer.test.ts
@@ -8,7 +8,7 @@ test.describe('Viewing API schema', () => {
   const program2 = 'minimal-sample-program'
 
   test.beforeEach(async ({page, seeding}) => {
-    await enableFeatureFlag(page, 'ADMIN_UI_MIGRATION_SC_ENABLED')
+    await enableFeatureFlag(page, 'ADMIN_UI_MIGRATION_UX_REDESIGN_SC_ENABLED')
     await seeding.seedProgramsAndCategories()
   })
 
