d193572246573f544568983b4a0ae2864655648c
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-07-30 09:37:04 -0400

    fmt

--- server/app/controllers/dev/DevToolsController.java
+++ server/app/controllers/dev/DevToolsController.java
@@ -244,9 +244,9 @@ public class DevToolsController extends Controller {
 
   /**
    * Log blank lines to create visual spacing the log file.
-
-   * <p>Normally a developer could just press enter as desired, but in our setup
-   * that terminates the local server.
+   *
+   * <p>Normally a developer could just press enter as desired, but in our setup that terminates the
+   * local server.
    */
   public Result logBlankLines() {
     // Log enough so that it'll be visibly discoverable when scrolling through logs.
