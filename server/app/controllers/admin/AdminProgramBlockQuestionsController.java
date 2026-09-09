d28bfade84ecf9461a0f7f700a18ea0ccd51ff5e
Shane McDaniel <shane.mcdaniel@exygy.com>
2026-09-09 10:07:31 -0400

    remove comment

--- server/app/controllers/admin/AdminProgramBlockQuestionsController.java
+++ server/app/controllers/admin/AdminProgramBlockQuestionsController.java
@@ -160,8 +160,6 @@ public class AdminProgramBlockQuestionsController extends Controller {
   /** HTMX POST endpoint for creating a new enumerator question and adding it to a screen. */
   @Secure(authorizers = Labels.CIVIFORM_ADMIN)
   public Result hxCreateEnumerator(Request request, long programId, long blockId) {
-    // stack 1 DO NOT SUBMIT
-
     Messages messages = messagesApi.preferred(request);
     requestChecker.throwIfProgramNotDraft(programId);
 
