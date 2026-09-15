bd6adb24f37ace9c5bd8d9521aa23b469b266d93
rocky <rachelefine@gmail.com>
2026-09-15 11:25:13 -0400

    Fix admin data access (#13980)

--- server/app/auth/oidc/admin/GenericOidcProfileCreator.java
+++ server/app/auth/oidc/admin/GenericOidcProfileCreator.java
@@ -69,16 +69,14 @@ public class GenericOidcProfileCreator extends CiviformOidcProfileCreator {
 
   @Override
   protected void adaptForRole(CiviFormProfile profile, ImmutableSet<Role> roles) {
-    if (roles.contains(Role.ROLE_CIVIFORM_ADMIN)) {
-      profile
-          .getAccount()
-          .thenAccept(
-              account -> {
-                account.setGlobalAdmin(true);
-                account.save();
-              })
-          .join();
-    }
+    profile
+        .getAccount()
+        .thenAccept(
+            account -> {
+              account.setGlobalAdmin(roles.contains(Role.ROLE_CIVIFORM_ADMIN));
+              account.save();
+            })
+        .join();
   }
 
   @Override
