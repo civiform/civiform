9c3c19a1effa016eb01282a35db2b18c9a877824
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2026-09-22 10:15:11 -0700

    Visual indicator when Admin Note contains content (#13913)

--- server/app/views/admin/programs/ProgramApplicationView.java
+++ server/app/views/admin/programs/ProgramApplicationView.java
@@ -359,7 +359,11 @@ public final class ProgramApplicationView extends BaseHtmlView {
   private Modal renderUpdateNoteConfirmationModal(
       long programId, ApplicationModel application, Optional<String> noteMaybe, Request request) {
     ButtonTag triggerButton =
-        makeSvgTextButton("Edit note", Icons.EDIT).withClasses(ButtonStyles.CLEAR_WITH_ICON);
+        makeSvgTextButton("Add note", Icons.EDIT).withClasses(ButtonStyles.CLEAR_WITH_ICON);
+    if (noteMaybe.isPresent() && !noteMaybe.get().isEmpty()) {
+      triggerButton =
+          makeSvgTextButton("Edit note", Icons.EDIT).withClasses(ButtonStyles.CLEAR_WITH_ICON);
+    }
     String formId = Modal.randomModalId();
     // No form action or content is rendered since admin_application_view.ts extracts the values
     // and calls postMessage rather than attempting a submission. The main frame is responsible for
