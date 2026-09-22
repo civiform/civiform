1a30723bfd39f9b09409730698ac27226865beec
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-09-22 13:25:18 -0700

    Scoring CSV Exports

--- server/app/controllers/admin/AdminApplicationController.java
+++ server/app/controllers/admin/AdminApplicationController.java
@@ -222,7 +222,11 @@ public final class AdminApplicationController extends CiviFormController {
       ProgramDefinition program = programService.getFullProgramDefinition(programId);
       checkProgramAdminAuthorization(request, program.adminName()).join();
       String filename = String.format("%s-%s.csv", program.adminName(), nowProvider.get());
-      String csv = exporterService.getProgramAllVersionsCsv(programId, filters);
+      String csv =
+          exporterService.getProgramAllVersionsCsv(
+              programId,
+              filters,
+              /* scoringEnabled= */ settingsManifest.getAnswerOptionScoringEnabled(request));
       return ok(csv)
           .as(Http.MimeTypes.BINARY)
           .withHeader(
